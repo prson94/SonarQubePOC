361a5fa8d5f575159edbc800b9ee5c639f653223
Michael Pappas <mpappas@infogix.com>
2015-11-04 16:01:08 +0000

    Adding missing jobs to solution, per Ken's findings.

---
+++ Jobs/d360.jobs.queue.ProcessObjectCache/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Azure.WebJobs;
+using d360.core;
+using System.Diagnostics;
+using Dapper;
+
+namespace d360.jobs.queue.ProcessObjectCache
+{
+    class Program: FunctionsBase
+    {
+        static void Main()
+        {
+            var host = new JobHost(new JobHostConfiguration(constants.WEBJOBS_STORAGE_CONNECTION));
+
+            var mex = new List<Exception>();
+
+            try
+            {
+                var companies = GetActiveCompanyIDs();
+                var domainPrefixes = GetCompanyDomainPrefixes();
+
+                companies.AsParallel().WithDegreeOfParallelism(4).ForAll(companyID =>
+                {
+                    var companyConnection = GetCompanyConnection(companyID);
+                    companyConnection.Open();
+
+                    #region HACK FOR INTERSECTS
+                    try
+                    {
+                        companyConnection.Execute(@"insert into [queue].[ObjectCache] ([Object], ObjectID)
+	select [Object], ObjectID from cache.ObjectDetails where Name = 'Name cannot be resolved'");
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine(ex.GetFullExceptionData());
+                    }
+                    #endregion
+
+                    var queueItems = companyConnection.Query<dynamic>(@"select * from [queue].ObjectCache where MachineAssigned is null and NumberOfRetries < 3").ToList();
+                    
+                    Trace.TraceInformation("Found {0} queue items for company {1}.  Starting to process them.", queueItems.Count, companyID);
+
+                    queueItems.ForEach(q =>
+                    {
+                        companyConnection.Execute("update [queue].ObjectCache set MachineAssigned = @m where ID = @queueID", new { m = Environment.MachineName, queueID = q.ID });
+                    });
+
+                    queueItems.ForEach(q =>
+                    {
+                        try
+                        {
+                            companyConnection.Execute("exec cache.SynchronizeObjectDetails @t, @id", new { t = q.Object, id = q.ObjectID }, null, 180);    // 3 minute timeout.
+                            companyConnection.Execute("delete [queue].ObjectCache where ID = @queueID", new { queueID = q.ID }, null, 500);
+                        }
+                        catch (Exception ex)
+                        {
+                            mex.Add(ex);
+                            companyConnection.Execute(@"update [queue].ObjectCache set MachineAssigned = null, HasError = 1, NumberOfRetries = NumberOfRetries + 1, ErrorMessage = @error where ID = @queueID", new { queueID = q.ID, error = ex.GetFullExceptionData() }, null, 500);
+                        }
+                    });                    
+
+                    companyConnection.Close();
+                    companyConnection.Dispose();
+                });
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message + ((ex.InnerException != null) ? "  " + ex.InnerException.Message : "");
+                Trace.TraceError(msg);
+            }
+
+            if (mex.Count > 0) throw new AggregateException("One or more exceptions occurred", mex);
+        }
+    }
+}
---
+++ Jobs/d360.jobs.queue.ProcessRecache/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Azure.WebJobs;
+using d360.core;
+using Dapper;
+using d360.core.entities;
+using System.Data.SqlClient;
+using d360.extensions.caching;
+
+namespace d360.jobs.queue.ProcessRecache
+{
+    public class cd
+    {
+        public int CompanyID { get; set; }
+        public string UrlPrefix { get; set; }
+    }
+
+    public class usercompany
+    {
+        public int CompanyID { get; set; }
+        public bool IsAdministrator { get; set; }
+    }
+    public class user
+    {
+        public user()
+        {
+            Companies = new List<usercompany>();
+        }
+
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string APIPublicKey { get; set; }
+        public string APIPrivateKey { get; set; }
+        public string APIReadOnlyAccessToken { get; set; }
+        public List<usercompany> Companies { get; set; }
+    }
+
+    public class CacheStatus
+    {
+        public string Name { get; set; }
+        public bool ShouldReCache { get; set; }
+    }
+    class Program: FunctionsBase
+    {
+        static void Main()
+        {
+            var host = new JobHost(new JobHostConfiguration(constants.WEBJOBS_STORAGE_CONNECTION));
+
+            var mex = new List<Exception>();
+
+            try
+            {
+                var ctx = new SqlConnection(constants.COMMUNITY_DATABASE_CONNECTION);
+                ctx.Open();
+
+                var caches = ctx.Query<CacheStatus>("select * from CacheStatus where ShouldReCache = 1").ToList();
+                var cache = new RedisCachingProvider();
+                caches.ForEach(c =>
+                {
+                    switch (c.Name)
+                    {
+                        case "Users":
+                            var users = ctx.Query<user>("select ID, Username, Password, APIPublicKey, APIPrivateKey, APIReadOnlyAccessToken from Resource").ToList();
+                            var usercompanies = ctx.Query<CompanyResource>("select * from CompanyResource").ToList();
+
+                            users.ForEach(u =>
+                            {
+                                u.Companies.AddRange(
+                                    usercompanies
+                                    .Where(i => i.ResourceID == u.ID).Select(i => new usercompany
+                                    {
+                                        CompanyID = i.CompanyID,
+                                        IsAdministrator = i.IsAdministrator
+                                    })
+                                );
+                            });
+                            usercompanies = null;
+
+                            cache.SetItem(c.Name, users, true, 15);
+                            break;
+                        case "CompanyPrefixes":
+                            var dict = ctx.Query<cd>("select CompanyID, UrlPrefix from CompanyDomainSetting").ToDictionary(k => k.UrlPrefix, v => v.CompanyID);
+                            cache.SetItem(c.Name, dict, true, 15);
+                            break;
+                    }
+
+                    ctx.Execute("update CacheStatus set ShouldReCache = 0 where Name = @n", new { n = c.Name });
+                });
+
+                ctx.Close();
+                ctx.Dispose();
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message + ((ex.InnerException != null) ? "  " + ex.InnerException.Message : "");
+                Console.WriteLine(msg);
+            }
+
+            if (mex.Count > 0) throw new AggregateException("One or more exceptions occurred", mex);
+        }
+    }
+}
