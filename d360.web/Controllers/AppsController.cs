1221939d19ae628525fdda6832fe57589ec8f329
Kenneth McNamee <kmcnamee@infogix.com>
2015-11-05 22:32:54 +0000

    changed new search to be default search page, removed angular search page, renamed old search to searchold - so it lives another day

--- d360.web/Controllers/AppsController.cs
+++ d360.web/Controllers/AppsController.cs
@@ -24,23 +24,15 @@ namespace d360.web.Controllers
 
         #endregion
 
+        public ActionResult SearchOld(string phrase)
+        {
+            ViewData.Add("VersionNumber", typeof(HomeController).Assembly.GetName().Version);
+            ViewData.Add("ResourceID", Company.CurrentResourceID);
+            ViewBag.Phrase = phrase;
+            return View();
+        }
+        
         public ActionResult Search(string phrase)
-        {
-            ViewData.Add("VersionNumber", typeof(HomeController).Assembly.GetName().Version);
-            ViewData.Add("ResourceID", Company.CurrentResourceID);
-            ViewBag.Phrase = phrase;
-            return View();
-        }
-
-        public ActionResult SearchBeta(string phrase)
-        {
-            ViewData.Add("VersionNumber", typeof(HomeController).Assembly.GetName().Version);
-            ViewData.Add("ResourceID", Company.CurrentResourceID);
-            ViewBag.Phrase = phrase;
-            return View();
-        }
-
-        public ActionResult Searchg(string phrase)
         {
             ViewData.Add("VersionNumber", typeof(HomeController).Assembly.GetName().Version);
             ViewData.Add("ResourceID", Company.CurrentResourceID);
