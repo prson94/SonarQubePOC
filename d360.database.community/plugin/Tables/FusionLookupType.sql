98ac3dad3b788c6d049010de80811cb8ece02b13
Kenneth McNamee <kmcnamee@infogix.com>
2015-12-04 21:33:31 +0000

    Added tables to store vendor mappings / lookups.  Added job to push out eagle startags to various client dbs.

---
+++ d360.database.community/plugin/Tables/FusionLookupType.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [plugin].[FusionLookupType]
+(
+	[ID] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	[Name] nvarchar(500) NOT NULL,
+	[Description] nvarchar(2000) NULL,
+	[Provider] [nvarchar](500) NULL,
+	CONSTRAINT [UC_FusionLookupTypes] UNIQUE([Name]) 
+)
