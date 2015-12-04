dc4f388039768c7f60a468b65789c8ea77aa8e30
Kenneth McNamee <kmcnamee@infogix.com>
2015-12-04 02:31:18 +0000

    eagle mc fusion

---
+++ Jobs/d360.jobs.AnalyzeCloudFusionData/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Azure.WebJobs;
+using System;
+using System.Linq;
+
+namespace d360.jobs.AnalyzeCloudFusionData
+{
+    class Program : FunctionsBase
+    {
+        static void Main()
+        {
+            var host = new JobHost(new JobHostConfiguration(d360.core.constants.WEBJOBS_STORAGE_CONNECTION));
+
+            try
+            {
+                var companies = GetActiveCompanyIDs();//.Where(i => i == 4).ToList();
+#if DEBUG
+                companies = GetActiveCompanyIDs().Where(i => i == 4).ToList();
+#endif
+                var domainPrefixes = GetCompanyDomainPrefixes();
+
+                companies.AsParallel().WithDegreeOfParallelism(3).ForAll(companyID =>
+                {
+                    // run EagleMC cloud fusion analysis
+                    EagleMCCloudFusionAnalyzer.Analyze(companyID);                    
+                });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("AN EXCEPTION OCCURED WHILE RUNNING D360.JOBS.ANALYZECLOUDFUSIONDATA FOR DETAILS:" + ex.Message);
+            }
+        }
+        
+                        
+    }
+}
