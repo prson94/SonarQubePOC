0bab622a3082042f35803dc13738f1ade52d7396
Kenneth McNamee <kmcnamee@infogix.com>
2015-12-16 22:29:37 +0000

    missing file

--- d360.database.company/fusion/Tables/StagingRelationMapping.sql
+++ d360.database.company/fusion/Tables/StagingRelationMapping.sql
@@ -1,4 +1,5 @@
 ﻿CREATE TABLE [fusion].[StagingRelationMapping](	
+	[ID] BIGINT IDENTITY(1,1) NOT NULL,
 	[ExecutionID] [int] NOT NULL,
 	[StartID] [nvarchar](500) NOT NULL,
 	[EndID] [nvarchar](500) NOT NULL
