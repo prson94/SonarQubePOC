afe665ccf140ac3192d84f4ac9af1649c4ea7bcb
Kenneth McNamee <kmcnamee@infogix.com>
2016-01-20 19:04:32 +0000

    capitalization issue lower case d vs upper case D

--- d360.database.company/dbo/Tables/FieldTypeFusionLookupDefinition.sql
+++ d360.database.company/dbo/Tables/FieldTypeFusionLookupDefinition.sql
@@ -1,11 +1,11 @@
 ﻿CREATE TABLE [dbo].[FieldTypeFusionLookupDefinition] (
-    [Id]                          INT          IDENTITY (1, 1) NOT NULL,
+    [ID]                          INT          IDENTITY (1, 1) NOT NULL,
     [FieldTypeID]                 INT          NOT NULL,
     [SourceFusionAttributeTypeID] INT          NOT NULL,
     [TargetFusionAttributeTypeID] INT          NOT NULL,
     [Display]                     VARCHAR (25) NOT NULL,
 	[IsParentChild]				  BIT		   NOT NULL DEFAULT(0),
-    PRIMARY KEY CLUSTERED ([Id] ASC),
+    PRIMARY KEY CLUSTERED ([ID] ASC),
     CONSTRAINT [FK_FieldTypeFusionLookupDefinition_FieldType] FOREIGN KEY ([FieldTypeID]) REFERENCES [dbo].[FieldType] ([ID]) ON DELETE CASCADE
 );
 
