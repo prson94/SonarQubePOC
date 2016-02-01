45641c050f63a12656e857ef3ca470be3060713d
Kenneth McNamee <kmcnamee@infogix.com>
2016-02-01 19:13:49 +0000

    Enhancement - Allow me to define templates that copy a diagram from one item to many.

---
+++ d360.database.company/utility/Stored Procedures/AddRelationDiagramRelations.sql
@@ -0,0 +1,80 @@
+﻿CREATE procedure [utility].[AddRelationDiagramRelations]
+	@diagramRelations [utility].[DiagramRelationshipTable] readonly,
+	@NumberOfIntersectsAdded int OUTPUT,
+	@NumberOfObjectsUpdated int OUTPUT
+as
+begin
+	set nocount on;
+
+	If EXISTS (SELECT 1 FROM @diagramRelations)		
+			begin
+				Declare @IDList Table(IntersectID int,RelID Int);
+				Declare @SourceIntersectNodeList Table(IntersectNodeID int,Item Int);
+				Declare @TargetIntersectNodeList Table(IntersectNodeID int,Item Int);
+				Declare @IntersectMapTemp Table(SubjectIntersectNode int, ObjectIntersectNode int, PredicateID int, [Type] int);
+				Declare @Intersects IDTable;
+
+				select @NumberOfIntersectsAdded = @NumberOfIntersectsAdded + (select count(1) from @diagramRelations);
+
+				--insert intersect records and save there id's
+				-- trick is to use merge to keep the sequence id and staging row ids
+				-- http://stackoverflow.com/questions/15614261/using-output-clause-to-insert-value-not-in-inserted
+				MERGE
+							INTO    [Intersect] d
+							USING   (
+										select rel.IntersectTypeID as isectid, 2 as class, rel.ItemID as srID from @diagramRelations rel						
+									) s
+							ON      (1 = 0)
+							WHEN NOT MATCHED THEN
+							INSERT  (IntersectTypeID, Classification, Description)
+							VALUES  (isectid, class, NULL)
+							OUTPUT  INSERTED.ID, s.srID into @IDList;
+							
+				--insert start records into intersect node track the id that it gets 
+				MERGE
+							INTO    IntersectNode d
+							USING   (
+										select sr.SourceIntersectTypeNodeID as IsectTypeNodeID, il.IntersectID as IsectID, sr.SourceObject as objType,sr.SourceObjectID as objeID, sr.ItemID as itemID from @diagramRelations sr inner join @IDList il on (sr.ItemID = il.RelID)											
+									) s
+							ON      (1 = 0)
+							WHEN NOT MATCHED THEN
+							INSERT  (IntersectTypeNodeID, IntersectID, ObjectType, ObjectID)
+							VALUES  (IsectTypeNodeID, IsectID, objType, objeID)
+							OUTPUT  INSERTED.ID, s.itemID into @SourceIntersectNodeList;
+					
+				MERGE
+							INTO    IntersectNode d
+							USING   (
+										select sr.TargetIntersectTypeNodeID as IsectTypeNodeID, il.IntersectID as IsectID, sr.TargetObject as objType,sr.TargetObjectID as objeID, sr.ItemID as itemID from @diagramRelations sr inner join @IDList il on (sr.ItemID = il.RelID)											
+									) s
+							ON      (1 = 0)
+							WHEN NOT MATCHED THEN
+							INSERT  (IntersectTypeNodeID, IntersectID, ObjectType, ObjectID)
+							VALUES  (IsectTypeNodeID, IsectID, objType, objeID)
+							OUTPUT  INSERTED.ID, s.itemID into @TargetIntersectNodeList;
+					
+				--add record for each to intersectmap table
+				insert into intersectmap
+					select 
+						sList.IntersectNodeID as SubjectIntersectNode,
+						tList.IntersectNodeID as ObjectIntersectNode,
+						itemList.[predicateid] as PredicateID,
+						itemList.[type] as [Type]
+					from
+						@diagramRelations itemList
+						inner join @SourceIntersectNodeList sList on (itemList.ItemID = sList.Item)
+						inner join @TargetIntersectNodeList tList on (itemList.ItemID = tList.Item)
+					where 
+						itemList.needsMapRecord = 1
+					
+				select @NumberOfObjectsUpdated = @NumberOfObjectsUpdated + 1;
+					
+				insert into @Intersects select idl.intersectid from @IDList idl;
+
+				if exists (select 1 from @Intersects)
+				begin
+					EXEC cache.SynchronizeRelationships @Intersects		
+				end
+			end	-- end if intersects are needed
+
+end
---
+++ d360.database.company/utility/User Defined Types/DiagramRelationshipTable.sql
@@ -0,0 +1,15 @@
+﻿CREATE TYPE [utility].[DiagramRelationshipTable] AS TABLE(
+	ItemID int identity,
+	IntersectTypeID int, 
+	IntersectID int, 
+	ID int, 
+	SourceObject varchar(50),
+	SourceObjectID int, 
+	SourceIntersectTypeNodeID int, 
+	[TargetObject] varchar(50), 
+	TargetObjectID int, 
+	TargetIntersectTypeNodeID int,
+	[type] int, 
+	predicateid int, 
+	needsMapRecord int	
+)
