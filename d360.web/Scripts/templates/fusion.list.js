2586089f84bd054d038ab481b22b4f13d990b4c7
Kenneth McNamee <kmcnamee@infogix.com>
2016-02-09 19:17:50 +0000

    bug fix to KPI for fusion showing NaN% when fusion has not run in the past 7 days.

--- d360.web/Scripts/templates/fusion.list.js
+++ d360.web/Scripts/templates/fusion.list.js
@@ -43,11 +43,11 @@
                     );
                     console.log(data);
                     if ($(controlID).find('.AgentKpi').length) {                        
-                        var score = (((data.AgentExecutions - data.AgentErrors)/data.AgentExecutions) * 100).toFixed(0);                        
+                        var score = (data.AgentExecutions > 0) ? (((data.AgentExecutions - data.AgentErrors)/data.AgentExecutions) * 100).toFixed(0) : 100;                        
                         drawKpi($(controlID).find('.AgentKpi'), 'Agent % Success', score, 100 - score, true);
                     }
-                    if ($(controlID).find('.FusionKpi').length) {
-                        var score = (((data.FusionExecutions - data.FusionErrors) / data.FusionExecutions) * 100).toFixed(0);                        
+                    if ($(controlID).find('.FusionKpi').length) {                        
+                        var score = (data.FusionErrors > 0) ? (((data.FusionExecutions - data.FusionErrors) / data.FusionExecutions) * 100).toFixed(0) : 100;                        
                         drawKpi($(controlID).find('.FusionKpi'), 'Processing % Success', score, 100 - score, true);
                     }
                 }
