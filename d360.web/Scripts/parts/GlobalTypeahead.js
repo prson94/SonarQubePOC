2dbc4e0ba21f04269efbb8a35e090f4b01093198
Kenneth McNamee <kmcnamee@infogix.com>
2016-05-13 18:36:17 +0000

    enabled suggest on global search.

---
+++ d360.web/Scripts/parts/GlobalTypeahead.js
@@ -0,0 +1,32 @@
+﻿function GlobalTypeahead() {
+    var $vartypeahead = $("#SearchString");
+    var engine = new Bloodhound({
+        name: 'typeaheads',
+        remote: {
+            "url": '/search/typeahead?q=',
+            prepare: function (query, settings) {
+                settings.url += encodeURIComponent(query) + '&t=' + encodeURIComponent(CompanySettings.DefaultSearchTypes);
+                return settings;
+            }
+        },
+        datumTokenizer: function (d) { return d; },
+        queryTokenizer: function (d) { return d; }
+    });
+    engine.initialize();
+
+    $vartypeahead.typeahead({
+        "minLength": 2,
+        "highlight": true
+    },
+    {
+        "source": engine.ttAdapter(),
+        display: 'Name',
+        limit: 10,
+        templates: {
+            suggestion: Handlebars.compile('<div><span class="type">{{Type}}:</span> {{{DisplayName}}}</div>'),            
+         //   header: "<div class='header'>Select an item from the dropdown to go directly to it, or to see more search results type in the text you want to search by.</div>"
+        }
+    }).on('typeahead:selected typeahead:autocompleted', function (e, datum) {
+        document.location.assign(datum.Url);
+    });
+}
