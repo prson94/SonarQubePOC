6e40ff8a088dc0070a67ceed03f959c0246283d5
Kenneth McNamee <kmcnamee@infogix.com>
2016-05-19 20:16:48 +0000

    bug - fusion ownership screen doesnt display type.  Missing isnull check in underlying view.

--- d360.database.company/fusion/Views/AttributeOwner.sql
+++ d360.database.company/fusion/Views/AttributeOwner.sql
@@ -1,17 +1,19 @@
-﻿CREATE view [fusion].[AttributeOwner]
+﻿create view [fusion].[AttributeOwner]
 as
 	select	R.FusionID,
 			R.ID,
 			R.ObjectID,
 			R.ObjectType,
-			OBJ.Name + ' (' + OBJ.ObjectTypeName + ')' as ObjectName,
+			OBJ.Name + ISNULL( ' (' + OBJ.ObjectTypeName + ')','') as ObjectName,
 			R.ParentObjectID,
 			R.ParentObjectType,
-			PAR.Name + ' (' + PAR.ObjectTypeName + ')' as ParentName,
+			PAR.Name + ISNULL( ' (' + PAR.ObjectTypeName + ')','')  as ParentName,
 			R.RelationshipOwnerObjectID,
 			R.RelationshipOwnerObjectType,
-			REL.Name + ' (' + REL.ObjectTypeName + ')' as RelationshipOwnerName
+			REL.Name +  ISNULL(' (' + REL.ObjectTypeName + ')','') as RelationshipOwnerName
 	from	FusionAttributeOwnerRule R
 			left join cache.ObjectDetails OBJ on OBJ.[Object] = R.ObjectType and OBJ.ObjectID = R.ObjectID--outer apply utility.ObjectDetail(R.ObjectType, R.ObjectID) OBJ
 			left join cache.ObjectDetails PAR on PAR.[Object] = R.ParentObjectType and PAR.ObjectID = R.ParentObjectID--outer apply utility.ObjectDetail(R.ParentObjectType, R.ParentObjectID) PAR
 			left join cache.ObjectDetails REL on REL.[Object] = R.RelationshipOwnerObjectType and REL.ObjectID = R.RelationshipOwnerObjectID--outer apply utility.ObjectDetail(R.RelationshipOwnerObjectType, R.RelationshipOwnerObjectID) REL
+
+GO
