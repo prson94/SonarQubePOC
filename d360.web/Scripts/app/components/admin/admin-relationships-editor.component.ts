f07a07c9dcc3a688c672958bb4bd28ace731e251
Kenneth McNamee <kmcnamee@infogix.com>
2016-07-14 17:49:37 +0000

    relationship editor bug fixes. removed friendly name stuff.

--- d360.web/Scripts/app/components/admin/admin-relationships-editor.component.ts
+++ d360.web/Scripts/app/components/admin/admin-relationships-editor.component.ts
@@ -5,6 +5,7 @@ import { NgForm, REACTIVE_FORM_DIRECTIVES } from '@angular/forms';
 import {Button, Editor, InputText, Dropdown, SelectItem, MultiSelect} from 'primeng/primeng';
 import { RelationshipsService} from '../../services/index';
 import { RelationshipDetail} from '../../models/relationship.model';
+import { DropdownOption } from '../../models/dropdown.model';
 
 import _ from 'lodash';
 
@@ -18,28 +19,30 @@ import _ from 'lodash';
                 <div class="row" *ngIf="!isLoading && !isLoadingItem">
                     <div class="form-instructions">When creating a relationship, Side 1 should always be the higher-level item in the relationship, while Side 2 is the lower-level, or atomic, item in the relationship.  For example, when defining a relationships between Application and Business Term you would set Application as Side 1 and Business Term as Side 2.  This will impact how sourcing and synonym inheritance works, as Side 2 is what you are sourcing as well as where synonyms defined on the relationship will also appear.</div>            
                     <form (ngSubmit)="onSubmit()" #relationshipEditorForm="ngForm">                        
-                        <div class="col l6 s12">
+                        <div class="col l12 s12">
                             <div class="FieldName">Relationship Side 1</div>
-                            <div><p-dropdown required name="Side1" [options]="side1Options" [disabled]="editedRelationship.LimitedChangesOnly" (onChange)="side1Changed($event);" [(ngModel)]="editedRelationship.Side1" [style]="{width:'100%'}"  #side1="ngModel"></p-dropdown></div>
+                            <div>                                 
+                                <select (change)="side1Changed($event.target);" [disabled]="editedRelationship.LimitedChangesOnly"  required [(ngModel)]="editedRelationship.Side1" name="Side1" #side1="ngModel" style="width:100%;">
+                                  <option></option>
+                                  <option *ngFor="let p of side1Options" [value]="p.value">{{p.title}}</option>
+                                </select>
+                            </div>
                             <div [hidden]="side1.valid || side1.pristine">Relationship Side 1 is required</div>
                         </div>                        
-                        <div *ngIf="isLoadingSide2" class="col l6 s12">
+                        <div *ngIf="isLoadingSide2" class="col l12 s12">
                             <div style="padding:10px;text-align:center;"><i class="fa fa-spinner fa-spin fa-2x"></i></div>
                         </div>
-                        <div class="col l6 s12" *ngIf="!isLoadingSide2">
+                        <div class="col l12 s12" *ngIf="!isLoadingSide2">
                             <div class="FieldName">Relationship Side 2</div>
-                            <div><p-dropdown required name="Side2" [options]="side2Options" [disabled]="editedRelationship.LimitedChangesOnly" [(ngModel)]="editedRelationship.Side2" [style]="{width:'100%'}"  #side2="ngModel"></p-dropdown></div>
+                            <div>                                
+                                <select required [disabled]="editedRelationship.LimitedChangesOnly"  [(ngModel)]="editedRelationship.Side2" name="Side2" #side2="ngModel" style="width:100%;">
+                                  <option></option>
+                                  <option *ngFor="let p of side2Options" [value]="p.value">{{p.title}}</option>
+                                </select>
+                            </div>
                             <div [hidden]="side2.valid || side2.pristine">Relationship Side 2 is required</div>
-                        </div>
-                        <div class="col l6 s12">
-                            <div class="FieldName">Friendly Name</div>
-                            <div><input style="width: 100%;" name="side1displaytext" [type]="'string'" [(ngModel)]="editedRelationship.Side1DisplayText"></div>                                                        
-                        </div>  
-                        <div class="col l6 s12">
-                            <div class="FieldName">Friendly Name</div>
-                            <div><input style="width: 100%;" name="side2displaytext" [type]="'string'" [(ngModel)]="editedRelationship.Side2DisplayText"></div>                                                        
-                        </div>  
-                        <div class="col l6 s12">                                                        
+                        </div>                        
+                        <div class="col l12 s12">                                                        
                             <div class="FieldName">Predicates</div>
                             <div><p-multiSelect name="predicates" [options]="predicates" [(ngModel)]="editedRelationship.Predicates" [style]="{width:'100%'}"></p-multiSelect></div>
                         </div>
@@ -62,8 +65,8 @@ export class AdminRelationshipsEditor {
     action: string = "Edit";
     error: any;
     editedRelationship: RelationshipDetail;
-    side1Options: SelectItem[] = [];
-    side2Options: SelectItem[] = [];
+    side1Options: DropdownOption[] = [];
+    side2Options: DropdownOption[] = [];
     predicates: SelectItem[] = [];
     isLoading: boolean = false;
     isLoadingSide2: boolean = false;
@@ -99,6 +102,7 @@ export class AdminRelationshipsEditor {
     }
 
     private side1Changed(event) {
+        console.log(event);
         if (!event.value) return;
         let info = event.value.split('|');
         if (info.length < 2) return;
@@ -116,11 +120,8 @@ export class AdminRelationshipsEditor {
 
     private loadSide1Options() {
         this.isLoading = true;
-        this.relationshipsService.getSide1Options().then(result => {
-            this.side1Options = [];
-            for (let item of result) {
-                this.side1Options.push({ label: item.title, value: item.value });
-            }
+        this.relationshipsService.getSide1Options().then(result => {            
+            this.side1Options = result;
             this.isLoading = false;
         });
     }
@@ -128,10 +129,7 @@ export class AdminRelationshipsEditor {
     private loadSide2Options(id: number, type: string, side2Id?: number, side2Type?: string) {
         this.isLoadingSide2 = true;
         this.relationshipsService.getSide2Options(id, type, side2Id, side2Type).then(result => {
-            this.side2Options = [];
-            for (let item of result) {                
-                this.side2Options.push({ label: item.title, value: item.value });
-            }
+            this.side2Options = result;            
             this.isLoadingSide2 = false;
         });
     }
