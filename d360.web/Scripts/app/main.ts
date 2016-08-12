59f628658c0b093facd1e2084f6a6ecdc560d3ff
Kenneth McNamee <kmcnamee@infogix.com>
2016-08-12 22:09:05 +0000

    ... mising change

--- d360.web/Scripts/app/main.ts
+++ d360.web/Scripts/app/main.ts
@@ -2,7 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule }              from './app.module';
 
-if (window.location.href.indexOf('.local/') < 0) {
+if (window.location.href.indexOf('.local') < 0) {
     enableProdMode();
 }
 else {
