78aa3ba633d8923d18147f53a181f2b01aa66c09
Kenneth McNamee <kmcnamee@infogix.com>
2016-08-12 22:07:23 +0000

    changed angular main to run in production mode if not running on a developers box by default.

--- d360.web/Scripts/app/main.ts
+++ d360.web/Scripts/app/main.ts
@@ -2,7 +2,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule }              from './app.module';
 
-
-//enableProdMode();
+if (window.location.href.indexOf('.local/') < 0) {
+    enableProdMode();
+}
+else {
+    console.log("Running in d3s developer mode...");
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
