1f3af0a2da46de1032ad7178e394210df305e969
Michael Donato <mdonato@infogix.com>
2016-08-15 15:08:02 +0000

    -added missing relations.model.ts

---
+++ d360.web/Scripts/app/models/relations.model.ts
@@ -0,0 +1,30 @@
+﻿export class HierarchyModel {
+    ID: number;
+    Subject: string;
+    Object: string;
+    SubjectID: number;
+    ObjectID: number;
+    ObjectType: string;
+    ObjectTypeID: number;
+    ParentID: string;
+    Name: string;
+    Path: string;
+    Url: string;
+    ObjectTypeName: string;
+    Level: number;
+    PredicateID: number;
+    PredicatePhrase: string;
+    Type: PredicateType;
+    GroupNumber: number;
+    UID: string; 
+}
+
+export enum PredicateType {
+    Lineage = 1,
+    SourceToTarget = 2,
+    TypeHierarchy = 3,
+    GroupHierarchy = 4,
+    ParentChildHierarchy = 5,
+    Synonym = 6,
+    Simple = 7
+}
