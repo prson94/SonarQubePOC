1f659aee30958d7e1494c47e0249af8510e7b20f
Kenneth McNamee <kmcnamee@infogix.com>
2016-10-10 22:05:57 +0000

    bug fix - fusion attributes click event not working correctly unless you clicked in really specific spot.

--- d360.web/Scripts/app/components/fusion/fusion-attribute-summary.component.ts
+++ d360.web/Scripts/app/components/fusion/fusion-attribute-summary.component.ts
@@ -19,7 +19,7 @@ import { GridDefinition, GridColumn, GridField, GridFilterColumn, GridFilterExpr
                         <p-dataTable [lazy]="true" [totalRecords]="results?.total" scrollable="true" scrollWidth="100%" [value]="results?.results" selectionMode="single" [rows]="rowsPerPage" [paginator]="true" [pageLinks]="4" [selection]="fusionAttribute" (selectionChange)="fusionAttribute=$event;fusionAttributeChange.emit(fusionAttribute);" (onLazyLoad)="loadFusionAttributesLazy($event)" [rowsPerPageOptions]="[5,10,20]" [responsive]="true" [stacked]="stacked">                                                                       
                             <p-column *ngFor="let column of columns" [field]="column.datafield" [header]="column.text" [sortable]="column.sortable"  [style]="{'width':'250px'}">
                                 <template let-col let-item="rowData" pTemplate type="body">
-                                    <div [innerHtml]="item[column.datafield]" class="truncate"></div>
+                                    <span [innerHtml]="item[column.datafield]" class="truncate" style="display:inline-block;width:245px"></span>
                                 </template>
                             </p-column>                            
                         </p-dataTable>                   
@@ -71,7 +71,7 @@ export class FusionAttributeSummaryComponent extends BaseComponent implements On
             this.getFieldsDefinition();
         }
     }
-
+    
     getFieldsDefinition() {
         this.isLoading = true;
         this.gridDefinitionService.getGridDefinition(this.fusionAttributeTypeId, 'FusionAttributeType', this.fusionId, 'FusionID')
