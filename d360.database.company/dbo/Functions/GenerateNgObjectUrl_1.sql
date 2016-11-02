2b81c9e12db9059b9765e041c941d33969dc58ba
Kenneth McNamee <kmcnamee@infogix.com>
2016-11-02 18:58:56 +0000

    From Pat  - Models (TaxonomyType) should bring me to the taxonomy type page not the first taxonomy in the taxonomy type.
    Workitems:
    #1414 Mega Menu: Info Model bring me into first level of model

--- d360.database.company/dbo/Functions/GenerateNgObjectUrl_1.sql
+++ d360.database.company/dbo/Functions/GenerateNgObjectUrl_1.sql
@@ -7,33 +7,31 @@
 RETURNS varchar(500)
 AS
 BEGIN
+	DECLARE @Prefix varchar(5) = ''--'a/'
 	DECLARE @Url varchar(500)
-	SET @Url = 'a'
+	SET @Url = @Prefix
 
 	SET @Url = CASE @Type
-	WHEN 'Artifact' THEN 'a/artifact/' +  + CAST(@TypeID as varchar(15)) + '/' + CAST(@ObjectID as varchar(15))
-	WHEN 'ArtifactType' THEN 'a/artifact/' + CAST(@TypeID as varchar(15))
-	WHEN 'Domain' THEN 'a/domain/' +  + CAST(@TypeID as varchar(15)) + '/' +  + CAST(@ObjectID as varchar(15))
-	WHEN 'DomainType' THEN 'a/domain/' + CAST(@TypeID as varchar(15))
-	WHEN 'FusionAttribute' THEN 'a/fusion/fusionattribute/' + CAST(@TypeID as varchar(15)) + '/' + CAST(@ObjectID as varchar(15))
-	WHEN 'Fusion' THEN 'a/fusion/' + CAST(@TypeID as varchar(15)) + '/' + + CAST(@ObjectID as varchar(15))
-	WHEN 'FusionType' THEN 'a/fusion/' + CAST(@TypeID as varchar(15))
-	WHEN 'Group' THEN 'a/groups/' + CAST(@ObjectID as varchar(15))	
-	WHEN 'Lookup' THEN 'a/lookups/administration/' + CAST(@TypeID as varchar(15)) + '/' + + CAST(@ObjectID as varchar(15))
-	WHEN 'LookupType' THEN 'a/lookups/administration/' + CAST(@TypeID as varchar(15))
-	WHEN 'Policy' THEN 'a/policy/' + CAST(@TypeID as varchar(15)) + '/' + CAST(@ObjectID as varchar(15))
-	WHEN 'PolicyType' THEN 'a/policy/' + CAST(@TypeID as varchar(15))
-	WHEN 'Resource' THEN 'a/resource/' + CAST(@ObjectID as varchar(15))
-	WHEN 'ResourceType' THEN 'a/resource/list/' + CAST(@TypeID as varchar(15))
-	WHEN 'Rule' THEN 'a/rule/' + CAST(@ObjectID as varchar(15))
-	WHEN 'Taxonomy' THEN 'a/model/' + CAST(@TypeID as varchar(15)) + '/' + CAST(@ObjectID as varchar(15))
-	WHEN 'TaxonomyType' THEN 'a/model/' + CAST(@TypeID as varchar(15))
+		WHEN 'Artifact' THEN 'artifact/' +  + CAST(@TypeID as varchar(15)) + '/' + CAST(@ObjectID as varchar(15))
+		WHEN 'ArtifactType' THEN 'artifact/' + CAST(@TypeID as varchar(15))
+		WHEN 'Domain' THEN 'domain/' +  + CAST(@TypeID as varchar(15)) + '/' +  + CAST(@ObjectID as varchar(15))
+		WHEN 'DomainType' THEN 'domain/' + CAST(@TypeID as varchar(15))
+		WHEN 'FusionAttribute' THEN 'fusion/fusionattribute/' + CAST(@TypeID as varchar(15)) + '/' + CAST(@ObjectID as varchar(15))
+		WHEN 'Fusion' THEN 'fusion/' + CAST(@TypeID as varchar(15)) + '/' + + CAST(@ObjectID as varchar(15))
+		WHEN 'FusionType' THEN 'fusion/' + CAST(@TypeID as varchar(15))
+		WHEN 'Group' THEN 'groups/' + CAST(@ObjectID as varchar(15))	
+		WHEN 'Lookup' THEN 'lookups/administration/' + CAST(@TypeID as varchar(15)) + '/' + + CAST(@ObjectID as varchar(15))
+		WHEN 'LookupType' THEN 'lookups/administration/' + CAST(@TypeID as varchar(15))
+		WHEN 'Policy' THEN 'policy/' + CAST(@TypeID as varchar(15)) + '/' + CAST(@ObjectID as varchar(15))
+		WHEN 'PolicyType' THEN 'policy/' + CAST(@TypeID as varchar(15))
+		WHEN 'Resource' THEN 'resource/' + CAST(@ObjectID as varchar(15))
+		WHEN 'ResourceType' THEN 'resource/list/' + CAST(@TypeID as varchar(15))
+		WHEN 'Rule' THEN 'rule/' + CAST(@ObjectID as varchar(15))
+		WHEN 'Taxonomy' THEN 'model/' + CAST(@TypeID as varchar(15)) + '/id/' + CAST(@ObjectID as varchar(15))
+		WHEN 'TaxonomyType' THEN 'model/' + CAST(@TypeID as varchar(15)) + '/structure'
 	END
+
+	SET @Url = @Prefix + @Url
 
 	RETURN @Url
 END
-
-
-GO
-
-
