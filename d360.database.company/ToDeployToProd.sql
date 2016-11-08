76bff3fde97c4e4a9d816ba883902a2b956a15c8
Kenneth McNamee <kmcnamee@infogix.com>
2016-11-08 21:50:19 +0000

    prevent duplicate reference item type names
    Workitems:
    #1477 Duplicate Code Names for Reference Codes

--- d360.database.company/ToDeployToProd.sql
+++ d360.database.company/ToDeployToProd.sql
@@ -457,3 +457,7 @@ Pull the following object updates:
 	[utility].[GetHierarchyAssignedResponsibilityList]
 	[utility].[ObjectDetail]
 */
+
+
+-- add a constraint on name to reference item type to prevent duplicate names
+ALTER TABLE [dbo].[ReferenceItemType] ADD CONSTRAINT CONST_Reference_Item_Type_Name UNIQUE (Name); 
