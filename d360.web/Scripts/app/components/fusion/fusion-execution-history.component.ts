36605243190303e4c2ef10455699ee3136a327e1
Kenneth McNamee <kmcnamee@infogix.com>
2016-11-21 17:18:16 +0000

    enabled refresh on fusion agent / execution history so you dont have to reload the page while waiting for fusion jobs to run.

--- d360.web/Scripts/app/components/fusion/fusion-execution-history.component.ts
+++ d360.web/Scripts/app/components/fusion/fusion-execution-history.component.ts
@@ -8,7 +8,7 @@ import * as _ from 'lodash';
     selector: 'd3s-fusion-execution-history',
     template: `                 
                 <div class="tile tile-detail" *ngIf="!showExecutionErrors && !showExecutionResults">
-                    <header>Execution History<span *ngIf="fusion"> - {{fusion.Name}}</span><d3s-tile-actions [hasAdd]="false" [hasFilterMode]="true" [(filterMode)]="showSimpleFilter"></d3s-tile-actions></header>
+                    <header>Execution History<span *ngIf="fusion"> - {{fusion.Name}}</span><d3s-tile-actions [hasAdd]="false" [hasFilterMode]="true" [(filterMode)]="showSimpleFilter" [hasRefresh]="true" (refreshClick)="load();"></d3s-tile-actions></header>
                     <d3s-loading [isLoading]="isLoading"></d3s-loading>
                     <span *ngIf="!isLoading">
                         <input [hidden]="!showSimpleFilter" #gb type="text" pInputText size="100" placeholder="Search..." class="grid-simple-filter">                                              
