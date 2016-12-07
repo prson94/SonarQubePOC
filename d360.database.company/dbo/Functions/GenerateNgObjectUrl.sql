e10f6c977f951d4723b66ebb0ec7564cebd7f51b
Kenneth McNamee <kmcnamee@infogix.com>
2016-12-07 14:25:02 +0000

    added missing changes to generatengobjecturl db function that were in demo dev, added back removed fix for policy type object urls.

--- d360.database.company/dbo/Functions/GenerateNgObjectUrl.sql
+++ d360.database.company/dbo/Functions/GenerateNgObjectUrl.sql
@@ -12,24 +12,25 @@ BEGIN
 	SET @Url = @Prefix
 
 	SET @Url = CASE @Type
-		WHEN 'Artifact' THEN 'artifact/' +  + CAST(@TypeID as varchar(15)) + '/' + CAST(@ObjectID as varchar(15))
-		WHEN 'ArtifactType' THEN 'artifact/' + CAST(@TypeID as varchar(15))
-		WHEN 'Domain' THEN 'domain/' +  + CAST(@TypeID as varchar(15)) + '/' +  + CAST(@ObjectID as varchar(15))
-		WHEN 'DomainType' THEN 'domain/' + CAST(@TypeID as varchar(15))
-		WHEN 'FusionAttribute' THEN 'fusion/fusionattribute/' + CAST(@TypeID as varchar(15)) + '/' + CAST(@ObjectID as varchar(15))		
-		WHEN 'Fusion' THEN 'fusion/' + CAST(@TypeID as varchar(15)) + '/' + + CAST(@ObjectID as varchar(15))
-		WHEN 'FusionType' THEN 'fusion/' + CAST(@TypeID as varchar(15))
-		WHEN 'Group' THEN 'groups/' + CAST(@ObjectID as varchar(15))	
-		WHEN 'Lookup' THEN 'lookups/administration/' + CAST(@TypeID as varchar(15)) + '/' + + CAST(@ObjectID as varchar(15))
-		WHEN 'LookupType' THEN 'lookups/administration/' + CAST(@TypeID as varchar(15))
-		WHEN 'Policy' THEN 'policy/' + CAST(@TypeID as varchar(15)) + '/id/' + CAST(@ObjectID as varchar(15))
-		WHEN 'PolicyType' THEN 'policy/' + CAST(@TypeID as varchar(15)) + '/structure'		
-		WHEN 'Resource' THEN 'resource/' + CAST(@ObjectID as varchar(15))
-		WHEN 'ResourceType' THEN 'resource/list/' + CAST(@TypeID as varchar(15))
-		WHEN 'Rule' THEN 'quality/rule/' + CAST(@ObjectID as varchar(15))
-		WHEN 'Taxonomy' THEN 'model/' + CAST(@TypeID as varchar(15)) + '/id/' + CAST(@ObjectID as varchar(15))
-		WHEN 'TaxonomyType' THEN 'model/' + CAST(@ObjectID as varchar(15)) + '/structure'				
-		WHEN 'ReferenceItemType' THEN 'reference/' + CAST(@ObjectID as varchar(15))
+		WHEN 'Artifact' THEN 'artifact/' +  + CAST(@TypeID as varchar) + '/' + CAST(@ObjectID as varchar)
+		WHEN 'ArtifactType' THEN 'artifact/' + CAST(@TypeID as varchar)
+		WHEN 'Domain' THEN 'domain/' +  + CAST(@TypeID as varchar) + '/' +  + CAST(@ObjectID as varchar)
+		WHEN 'DomainType' THEN 'domain/' + CAST(@TypeID as varchar)
+		WHEN 'ReferenceItem' THEN 'reference/' +  + CAST(@TypeID as varchar)-- + '/' +  + CAST(@ObjectID as varchar)
+		WHEN 'ReferenceItemType' THEN 'reference/' + CAST(@TypeID as varchar)
+		WHEN 'FusionAttribute' THEN 'fusion/fusionattribute/' + CAST(@TypeID as varchar) + '/' + CAST(@ObjectID as varchar)		
+		WHEN 'Fusion' THEN 'fusion/' + CAST(@TypeID as varchar) + '/' + + CAST(@ObjectID as varchar)
+		WHEN 'FusionType' THEN 'fusion/' + CAST(@TypeID as varchar)
+		WHEN 'Group' THEN 'groups/' + CAST(@ObjectID as varchar)	
+		WHEN 'Lookup' THEN 'admin/lookups/' + CAST(@TypeID as varchar) + '/' + + CAST(@ObjectID as varchar)
+		WHEN 'LookupType' THEN 'admin/lookups/' + CAST(@TypeID as varchar)
+		WHEN 'Policy' THEN 'policy/' + CAST(@TypeID as varchar(15)) + '/id/' + CAST(@ObjectID as varchar)
+		WHEN 'PolicyType' THEN 'policy/' + CAST(@TypeID as varchar) + '/structure'		
+		WHEN 'Resource' THEN 'resource/' + CAST(@ObjectID as varchar)
+		WHEN 'ResourceType' THEN 'resource/list/' + CAST(@TypeID as varchar)
+		WHEN 'Rule' THEN 'quality/rule/' + CAST(@ObjectID as varchar)
+		WHEN 'Taxonomy' THEN 'model/' + CAST(@TypeID as varchar) + '/id/' + CAST(@ObjectID as varchar)
+		WHEN 'TaxonomyType' THEN 'model/' + CAST(@ObjectID as varchar) + '/structure'		
 	END
 
 	SET @Url = @Prefix + @Url
