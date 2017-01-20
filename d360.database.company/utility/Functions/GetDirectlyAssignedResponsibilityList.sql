3ed625ec9cf73efe8d11f28e50d999ee4ec0f5c0
Kenneth McNamee <kmcnamee@infogix.com>
2017-01-20 22:54:03 +0000

    assigning ownership in the policy type screens doesnt work.
    Workitems:
    #1859 Allocating Ownership for Policy is broken

--- d360.database.company/utility/Functions/GetDirectlyAssignedResponsibilityList.sql
+++ d360.database.company/utility/Functions/GetDirectlyAssignedResponsibilityList.sql
@@ -193,5 +193,25 @@ BEGIN
 									or (@ObjectID is null)
 								);
 		end
+		if @Object = 'PolicyType'
+		begin
+			insert into @tbl
+				select	'Policy Type Direct' as [Source],
+						R.Visible,
+						R.ID,
+						R.ResponsibilityTypeID,
+						@Object as AssigningItemType,
+						T.ID as AssigningItemID,
+						@Object as ObjectType,
+						T.ID as ObjectID,
+						utility.GetResponsibilityContextHash(R.ID),
+						@Priority as [Priority]
+				from	PolicyType T 
+						inner join Responsibility R on R.ObjectType = @Object and R.ObjectID = T.ID
+							and (
+								(T.ID = @ObjectID and @ObjectID is not null) or (@ObjectID is null)
+								)
+		end
 	RETURN 
 END
+GO
