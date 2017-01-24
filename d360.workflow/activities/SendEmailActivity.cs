b719ebe8b508eb1e5dea7f5743f8e853fa4abffb
Michael Donato <mdonato@infogix.com>
2017-01-24 16:18:30 +0000

    -fixed a bug where some email templates were using the reserved encoding tag 'url' as their tag name
    Workitems:
    #1611 Issue Notification: Link in the email does not work

--- d360.workflow/activities/SendEmailActivity.cs
+++ d360.workflow/activities/SendEmailActivity.cs
@@ -37,61 +37,28 @@ namespace d360.workflow
             string templateID = context.GetValue(this.TemplateID);
             var templateTags = context.GetValue(this.TemplateTags);
 
-            //context.WorkflowInstanceId
-            
-            // Create the email object first, then add the properties.
-            var message = new MandrillMessage();// SendGridMessage();
+            var message = new MandrillMessage();
 
             message.AddTo(toEmail, toName);
             message.FromEmail = "no-reply@data3sixty.com";
             message.FromName = "Data3Sixty Workflow";
 
-            // Add the message properties.
-            //message.From = new MailAddress("no-reply@data3sixty.com", "Data3Sixty Workflow");
-
-            // Add multiple addresses to the To field.
-            //List<String> recipients = new List<String>
-            //{
-            //    string.Format(@"{0} <{1}>", toName, toEmail)
-            //};
-
-            //message.AddTo(recipients);
-
             message.Subject = subject;
 
-            //message.EnableFooter("text", "html");
 
-            message.TrackOpens = false;//message.DisableOpenTracking();
-            //message.DisableUnsubscribe();
-            message.TrackClicks = false; //message.DisableClickTracking();
+            message.TrackOpens = false;
+            message.TrackClicks = false;
 
-            //if (!string.IsNullOrEmpty(templateID))
-            //{
-            //    //message.EnableTemplateEngine(templateID);
-            var tags = new Dictionary<string, object>();
             if (templateTags != null)
             {
                 foreach (var k in templateTags.Keys)
                 {
                     message.AddRcptMergeVars(toEmail, k, templateTags[k]);
-                    //message.AddSubstitution(k, new List<string>() { templateTags[k] });
                 }
             }
-            //}
-            //else 
-            //{
-           
-            //}
-
-            //Add the HTML and Text bodies
-            //message.Html = body;
-            //message.Text = "Hello World plain text!"; 
 
             var api = new MandrillApi(constants.MANDRILL_API_KEY);
             api.Messages.SendTemplate(message, templateID);
-            //var credential = new NetworkCredential(constants.SMTP_USERNAME, constants.SMTP_PASSWORD);
-            //var transport = new Web(credential);
-            //transport.Deliver(message);
         }
     }
 }
