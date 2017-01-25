f1e5187ce14e47cae90f6524f79ad59decdc9fc1
Michael Donato <mdonato@infogix.com>
2017-01-25 15:44:26 +0000

    -added new unit test project to solution

---
+++ d360.test.web/DummySecurityContextProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using d360.extensions;
+
+namespace d360.test.web
+{
+    public class DummySecurityContextProvider: ISecurityContextProvider
+    {
+        public string CompanyPrefix { get; set; }
+        public int CompanyID { get; set; }
+        public int ResourceID { get; set; }
+        public bool IsAdministrator { get; set; }
+
+    }
+}
---
+++ d360.test.web/Tests/Controllers/ApiController/ArtifactTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using d360.web.Controllers;
+using d360.model;
+using d360.extensions;
+using d360.extensions.caching;
+using d360.extensions.queue;
+
+namespace d360.test.web
+{
+    [TestClass]
+    public class ArtifactTests
+    {
+
+        [TestMethod, Description("")]
+        public void GetArtifact()
+        {
+            
+            ICachingProvider cache = new DummyCachingProvider();
+            IQueueSource queue = new DummyQueueSource();
+            ISecurityContextProvider context = new DummySecurityContextProvider();
+            context.CompanyID = 4;
+            context.CompanyPrefix = "demo.dev";
+            context.IsAdministrator = true;
+            context.ResourceID = 3243;
+            var community = new CommunityContext(cache, queue, context);
+            var company = new CompanyContext(community, cache, queue, context, true);
+
+            var api = new D3SApiController(community, company, context);
+
+            var testId = 4651;
+            var model = api.GetArtifact(testId);
+
+            Assert.IsNotNull(model);
+            Assert.AreEqual(model["ID"].ToString(), testId.ToString());
+        }
+    }
+}
