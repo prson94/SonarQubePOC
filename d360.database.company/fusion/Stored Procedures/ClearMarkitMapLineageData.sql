ae2610e3885ba23a5ec10f877cb4691e0a7a17df
Kenneth McNamee <kmcnamee@infogix.com>
2017-03-13 14:04:06 +0000

    added proc to encapsalate logic to clear markit auto lineage

---
+++ d360.database.company/fusion/Stored Procedures/ClearMarkitMapLineageData.sql
@@ -0,0 +1,8 @@
+﻿create procedure [fusion].[ClearMarkitMapLineageData]
+as
+begin
+	delete from mapitem where [owner] = 'MARKIT LINEAGE';
+	delete from mapruleitem where [owner] = 'MARKIT LINEAGE';
+	delete from mapruleitemmapitem where [owner] = 'MARKIT LINEAGE';
+	delete from [intersect] where [owner] = 'MARKIT LINEAGE';
+end
