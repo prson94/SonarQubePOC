18afed0eed35063e93506a806cb8f88ffaedaea1
Kenneth McNamee <kmcnamee@infogix.com>
2017-04-06 18:54:06 +0000

    added form response type enum

---
+++ d360.core/enums/Workflow/FormResponseType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d360.core.enums.Workflow
+{
+    public enum FormResponseType
+    {
+        FirstResponse = 0,
+        All = 1,
+        Majority = 2
+    }
+}
