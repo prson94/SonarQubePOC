52b50457030fc39c0ce5c4723f702f1572cc0aac
Kenneth McNamee <kmcnamee@infogix.com>
2017-04-28 19:57:24 +0000

    bug fix - fusion query attribute formatted value doesnt work correctly when value is > 4000 characters.

--- d360.database.company/ToDeployToClientDev.sql
+++ d360.database.company/ToDeployToClientDev.sql
@@ -128,3 +128,112 @@ go
 
 update FieldType set IsEditable = 0 where Object = 'FusionAttributeType'
 GO
+
+----------------------------------------------------------------------------------------------------------------------------------------
+-- 4/28 bug with getformattedfieldfusionqueryattributevalue / wrapper limited to 4000 characters then giving sql error
+----------------------------------------------------------------------------------------------------------------------------------------
+
+alter table [dbo].[FusionQueryAttribute] DROP COLUMN DisplayValue;
+go;
+
+ALTER FUNCTION [utility].[GetFormattedFieldFusionQueryAttributeValueWrapper]
+(
+	@FusionQueryAttributeID int,
+	@FusionQueryAttributeTypeID int	
+)
+RETURNS nvarchar(max)
+AS
+BEGIN
+	return utility.GetFormattedFieldFusionQueryAttributeValue(@FusionQueryAttributeID, @FusionQueryAttributeTypeID)
+END
+
+go
+
+
+ALTER FUNCTION [utility].[GetFormattedFieldFusionQueryAttributeValue]
+(
+	@FusionQueryAttributeID int,
+	@FusionQueryAttributeTypeID int	
+)
+RETURNS nvarchar(max)
+AS
+BEGIN
+	declare @formattedValue nvarchar(max)
+	declare @tokens table(ID int identity(1,1), Token nvarchar(100), Field nvarchar(100))
+	declare @fieldValues table(Field nvarchar(100), Value nvarchar(max))
+	declare @displayFormat nvarchar(250)
+
+	set @displayFormat = (select displayformat from FusionQueryAttributeType where ID = @FusionQueryAttributeTypeID);
+
+	set @formattedValue = @displayFormat
+
+	while patindex('%{%',@formattedValue) > 0
+	begin
+		declare @txt nvarchar(100) = SUBSTRING(@formattedValue, patindex('%{%',@formattedValue), PATINDEX('%}%', @formattedValue))
+		insert into @tokens Values (@txt, REPLACE(REPLACE(@txt,'{',''),'}',''))
+		set @formattedValue = replace(@formattedValue, @txt, '')
+	end
+
+	insert into @fieldValues
+		SELECT	Name,
+				FormattedValue
+		FROM	FieldWithRelation 
+		WHERE	ObjectType = 'FusionQueryAttribute' 
+				and ObjectID = @FusionQueryAttributeID
+
+				
+	insert into @fieldValues
+		SELECT 'ID',
+				ID
+		FROM	FusionQueryAttribute
+		WHERE	ID = @FusionQueryAttributeID
+
+	declare @current int,
+			@max int
+
+	set @current = 1
+	select @max = Max(ID) from @tokens
+	set @formattedValue = @displayFormat
+
+	while(@current <= @max)
+	begin
+		declare @currentToken nvarchar(100) = null,
+				@currentField nvarchar(100) = null,
+				@currentValue nvarchar(4000) = null,
+				@lkpType nvarchar(250) = null, 
+				@lkpID int = null, 
+				@lkpFormat nvarchar(250) = null
+
+		select	@currentField = Field, 
+				@currentToken = Token 
+		from	@tokens
+		where	ID = @current
+
+		select	@currentValue = Value
+		from	@fieldValues 
+		where	Field = @currentField
+
+		if @currentValue is not null
+		begin
+			SET @formattedValue = REPLACE(@formattedValue, @currentToken, @currentValue)
+		end
+		else
+		begin
+			SET @formattedValue = REPLACE(@formattedValue, @currentToken, '')
+		end
+
+		SET @current = @current + 1
+	end
+
+	return ''--@formattedValue
+END
+
+go;
+
+
+alter table [dbo].[FusionQueryAttribute] add [DisplayValue]  AS ([utility].[GetFormattedFieldFusionQueryAttributeValueWrapper]([ID],[FusionQueryAttributeTypeID]));
+go;
+
+----------------------------------------------------------------------------------------------------------------------------------------
+-- end of bug fix: 4/28 bug with getformattedfieldfusionqueryattributevalue / wrapper limited to 4000 characters then giving sql error
+----------------------------------------------------------------------------------------------------------------------------------------
