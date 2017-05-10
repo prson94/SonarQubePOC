15495d949ea09a49a7de63a034eb8331f2ea3e64
Kenneth McNamee <kmcnamee@infogix.com>
2017-05-10 17:27:49 +0000

    missing index causing workflow visualization to draw slowly
    Workitems:
    #2185 Monitor takes a long time to show the visual for the workflow

--- d360.database.company/workflow/Tables/Item.sql
+++ d360.database.company/workflow/Tables/Item.sql
@@ -9,7 +9,14 @@
     [CompletedBy] INT      NOT NULL,
     [CompletedOn] DATETIME NOT NULL,
     [IsTest]      BIT      CONSTRAINT [DF_WorkflowItem_IsTest] DEFAULT ((0)) NOT NULL,
+	[Object]	  VARCHAR(50) NOT NULL,
+	[Objectid]	  INT      NOT NULL,
+	[NumberOfEvents]	  INT      NOT NULL,
     CONSTRAINT [PK_WorkflowItem] PRIMARY KEY CLUSTERED ([ID] ASC),
     CONSTRAINT [FK_WorkflowItem_WorkflowVersion] FOREIGN KEY ([VersionID]) REFERENCES [workflow].[Version] ([ID]) ON DELETE CASCADE
 );
 GO
+
+
+create nonclustered index IX_WorkflowItem_VersionObjectObjectID on [workflow].[item] (VersionID, [Object], ObjectID)
+go
