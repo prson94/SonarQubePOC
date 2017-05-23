5d952fdbf1100214c16b58538758aebbce384b54
Kenneth McNamee <kmcnamee@infogix.com>
2017-05-23 18:58:24 +0000

    URLs for groups in ownership lookup are broken
    Workitems:
    #2239 Hyperlink for a group role in Ownership lookup is broken

--- d360.database.company/dbo/Functions/GenerateNgObjectUrl.sql
+++ d360.database.company/dbo/Functions/GenerateNgObjectUrl.sql
@@ -1,4 +1,4 @@
-﻿CREATE FUNCTION [dbo].[GenerateNgObjectUrl] 
+﻿ CREATE FUNCTION [dbo].[GenerateNgObjectUrl] 
 (
 	@Type varchar(50),
 	@TypeID int,
@@ -21,14 +21,15 @@ BEGIN
 		WHEN 'FusionAttribute' THEN 'fusion/fusionattribute/' + CAST(@TypeID as varchar) + '/' + CAST(@ObjectID as varchar)		
 		WHEN 'Fusion' THEN 'fusion/' + CAST(@TypeID as varchar) + '/' + + CAST(@ObjectID as varchar)
 		WHEN 'FusionType' THEN 'fusion/' + CAST(@TypeID as varchar)
-		WHEN 'Group' THEN 'groups/' + CAST(@ObjectID as varchar)	
+		WHEN 'Group' THEN 'group/' + CAST(@ObjectID as varchar)	
 		WHEN 'Lookup' THEN 'admin/lookups/' + CAST(@TypeID as varchar) + '/' + + CAST(@ObjectID as varchar)
 		WHEN 'LookupType' THEN 'admin/lookups/' + CAST(@TypeID as varchar)
 		WHEN 'Policy' THEN 'policy/' + CAST(@TypeID as varchar(15)) + '/id/' + CAST(@ObjectID as varchar)
 		WHEN 'PolicyType' THEN 'policy/' + CAST(@TypeID as varchar) + '/structure'		
 		WHEN 'Resource' THEN 'resource/' + CAST(@ObjectID as varchar)
 		WHEN 'ResourceType' THEN 'resource/list/' + CAST(@TypeID as varchar)
-		WHEN 'Rule' THEN 'quality/rule/' + CAST(@ObjectID as varchar)
+		WHEN 'Rule' THEN 'quality/rule/' + CAST(@TypeID as varchar) + '/' + CAST(@ObjectID as varchar)
+		WHEN 'RuleType' THEN 'quality/rule/' + CAST(@TypeID as varchar)	
 		WHEN 'Taxonomy' THEN 'model/' + CAST(@TypeID as varchar) + '/id/' + CAST(@ObjectID as varchar)
 		WHEN 'TaxonomyType' THEN 'model/' + CAST(@ObjectID as varchar) + '/structure'		
 	END
