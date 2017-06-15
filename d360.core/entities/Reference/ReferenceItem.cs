c3b44f1b2488e2afeaa0213d36fe3311c46b232b
Kenneth McNamee <kmcnamee@infogix.com>
2017-06-15 22:12:35 +0000

    enabled workflow tracking for reference items, shut off cdn on nightly so i can restructure folders.

--- d360.core/entities/Reference/ReferenceItem.cs
+++ d360.core/entities/Reference/ReferenceItem.cs
@@ -2,11 +2,12 @@
 using System;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Runtime.Serialization;
+using d360.core.queue;
 
 namespace d360.core.entities
 {
     [DataContract(Namespace = NAMESPACE)]
-    public class ReferenceItem : BaseIntObject, IIntObject, ISearchable, ICreatedMetadata, IUpdatedMetadata
+    public class ReferenceItem : BaseIntObject, IIntObject, ISearchable, ICreatedMetadata, IUpdatedMetadata, IEventTrackedEntity
     {
         [DataMember]
         public string Code { get; set; }
@@ -26,5 +27,16 @@ namespace d360.core.entities
 
         [IgnoreDataMember]
         public virtual ReferenceItemType ReferenceItemType { get; set; }
+
+        public EventObjectInfo GetEventObjectInfo()
+        {
+            return new EventObjectInfo
+            {
+                Object = SystemObjects.ReferenceItem,
+                ObjectID = ID,
+                ObjectType = SystemObjects.ReferenceItemType,
+                ObjectTypeID = ReferenceItemTypeID
+            };
+        }
     }
 }
