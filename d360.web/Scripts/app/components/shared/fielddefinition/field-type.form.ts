60c10b5e080e2a97e15d87603fcf0fef1195cdc2
Kenneth McNamee <kmcnamee@infogix.com>
2017-06-21 21:49:31 +0000

    field names with a dash '-' in the name automaticly put the dash in the api name which causes errors and isnt supported.
    Workitems:
    #2280 API Names with '-' in them cause error in the ui.  Please block them.

--- d360.web/Scripts/app/components/shared/fielddefinition/field-type.form.ts
+++ d360.web/Scripts/app/components/shared/fielddefinition/field-type.form.ts
@@ -804,7 +804,7 @@ export class FieldTypeForm extends BaseComponent implements OnInit, OnChanges {
     }
 
     private updateApiName(event) {
-        this.model.FieldType.Name = event.target.value.replace(/[^a-zA-Z0-9-_]/g, '');
+        this.model.FieldType.Name = event.target.value.replace(/[^a-zA-Z0-9_]/g, '');
     }
 
     private addFusion() {
