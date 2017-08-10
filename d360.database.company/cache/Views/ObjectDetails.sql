2054c755e5e2d90320d845fd24088939c4fde2fe
Kenneth McNamee <kmcnamee@infogix.com>
2017-08-10 21:31:46 +0000

    undefined appearing when reference list items appear in impact diagram
    Workitems:
    #2231 Impact Analysis : Need to resolve "Underfined" for Reference List Items

--- d360.database.company/cache/Views/ObjectDetails.sql
+++ d360.database.company/cache/Views/ObjectDetails.sql
@@ -1,9 +1,9 @@
-﻿CREATE view [cache].[ObjectDetails]
+﻿create view [cache].[ObjectDetails]
 as
 	select	D.[Object],
 			D.[ObjectID],
-			coalesce(O1.Name, O2.Name, O4.Name, O5.Name, O6.Name, O7.Name, O8.Name, O9.Name, O10.Name, O11.Name, O12.Name, O13.Name, case when O14.ResourceID is not null then O14.FirstName + ' ' + O14.LastName else null end, O15.Name, O16.Name, O17.Name, O18.Name, O21.Name, O22.Name, O23.Name, O24.Name, O25.DisplayValue, O26.Name, O27.Name, O28.Name, O29.Name, null) as Name,
-			coalesce(O1.TextPath, O2.TextPath, O4.TextPath, O5.Name, O6.Name, O7.Name, O8.Name, O9.Name, O10.Name, O11.Name, O12.Name, O13.TextPath, case when O14.ResourceID is not null then O14.FirstName + ' ' + O14.LastName else null end, O15.Name, O16.Name, O17.TextPath, O18.Name, O21.Name, O22.Name, O23.Name, O24.Name, O25.DisplayValue, O26.Name, O27.Name, O28.Name, O29.Name, '') as TextPath,
+			coalesce(O1.Name, O2.Name, O5.Name, O6.Name, O7.Name, O8.Name, O9.Name, O10.Name, O11.Name, O12.Name, O13.Name, case when O14.ResourceID is not null then O14.FirstName + ' ' + O14.LastName else null end, O15.Name, O16.Name, O17.Name, O18.Name, O21.Name, O22.Name, O23.Name, O24.Name, O25.DisplayValue, O26.Name, O27.Name, O28.Name, O29.Name, null) as Name, --O4.Name, 
+			coalesce(O1.TextPath, O2.TextPath, O5.Name, O6.Name, O7.Name, O8.Name, O9.Name, O10.Name, O11.Name, O12.Name, O13.TextPath, case when O14.ResourceID is not null then O14.FirstName + ' ' + O14.LastName else null end, O15.Name, O16.Name, O17.TextPath, O18.Name, O21.Name, O22.Name, O23.Name, O24.Name, O25.DisplayValue, O26.Name, O27.Name, O28.Name, O29.Name, '') as TextPath, --O4.TextPath, 
 			coalesce(O1.Description, O2.Description, O6.Description, O7.Description, O8.Description, O9.Description, O10.Description, O12.Description, O13.Description, O26.Description, NULL) as Description,
 			case D.[Object]
 				when 'Lookup' then dbo.GenerateNgObjectUrl('Lookup', O20.LookupTypeID, O20.ID)
@@ -15,19 +15,18 @@ as
 			case 
 				when P1.ID is not null then 'Artifact'
 				when P2.ID is not null then 'Taxonomy'
-				when P4.ID is not null then 'FusionAttribute'
-				when P4.ID is not null then 'FusionAttribute'
+				--when P4.ID is not null then 'FusionAttribute'
 				when P7.ID is not null then 'ArtifactType'
 				when P10.ID is not null then 'AttributeType'
 				when P13.ID is not null then 'PolicyType'
 				when P17.ID is not null then 'FusionAttributeType'
 				else NULL
 			end as Parent,
-			coalesce(O1.ParentID, O2.ParentID, O4.ParentID, O7.ParentID, O10.ParentID, O13.ParentID, O17.ParentID, NULL) as ParentID,
-			coalesce(P1.Name, P2.Name, P4.Name, P7.Name, P10.Name, P13.Name, P17.Name, NULL) as ParentName,
+			coalesce(O1.ParentID, O2.ParentID, O7.ParentID, O10.ParentID, O13.ParentID, O17.ParentID, NULL) as ParentID, --O4.ParentID, 
+			coalesce(P1.Name, P2.Name, P7.Name, P10.Name, P13.Name, P17.Name, NULL) as ParentName,	--P4.Name, 
 			D.[ObjectType],
 			D.ObjectTypeID,
-			coalesce(OT1.Name, OT2.Name, OT4.TextPath, OT5.Name, OT12.Name, OT13.Name, OT14.Name, OT15.Name, OT20.Name, OT24.Name, NULL) as ObjectTypeName,
+			coalesce(OT1.Name, OT2.Name, OT5.Name, OT12.Name, OT13.Name, OT14.Name, OT15.Name, OT20.Name, OT24.Name, OT25.Name, NULL) as ObjectTypeName, --OT4.TextPath, 
 			coalesce(S.IconBackColor, '#000') as IconBackColor,
 			coalesce(S.IconForeColor, '#fff') as IconForeColor,
 			coalesce(S.IconText, 'leaf') as IconText,
@@ -47,9 +46,9 @@ as
 			left join TaxonomyType OT2 with(nolock) on D.[Object] = 'Taxonomy' and OT2.ID = O2.TaxonomyTypeID
 			left join Taxonomy P2 with(nolock) on D.[Object] = 'Taxonomy' and P2.ID = O2.ParentID
 
-			left join FusionAttribute O4 with(nolock) on D.[Object] = 'FusionAttribute' and O4.ID = D.ObjectID
-			left join FusionAttributeType OT4 with(nolock) on D.[Object] = 'FusionAttribute' and OT4.ID = O4.FusionAttributeTypeID
-			left join FusionAttribute P4 with(nolock) on D.[Object] = 'FusionAttribute' and P4.ID = O4.ParentID
+			--left join FusionAttribute O4 with(nolock) on D.[Object] = 'FusionAttribute' and O4.ID = D.ObjectID
+			--left join FusionAttributeType OT4 with(nolock) on D.[Object] = 'FusionAttribute' and OT4.ID = O4.FusionAttributeTypeID
+			--left join FusionAttribute P4 with(nolock) on D.[Object] = 'FusionAttribute' and P4.ID = O4.ParentID
 
 			left join Fusion O5 with(nolock) on D.[Object] = 'Fusion' and O5.ID = D.ObjectID
 			left join FusionType OT5 with(nolock) on D.[Object] = 'Fusion' and OT5.ID = O5.FusionTypeID
@@ -69,15 +68,7 @@ as
 			left join IntersectType O11 with(nolock) on D.[Object] = 'IntersectType' and O11.ID = D.ObjectID
 
 			left join [Rule] O12 with(nolock) on D.[Object] = 'Rule' and O12.ID = D.ObjectID
-			left join	(
-						select 1 as ID, 'Informational Rule' as Name
-						union
-						select 2 as ID, 'Quality Check Rule' as Name
-						union
-						select 3 as ID, 'Metric Rule' as Name
-						union
-						select 4 as ID, 'Profile Rule' as Name
-						) OT12 on D.[Object] = 'Rule' and OT12.ID = O12.RuleType
+			left join RuleType OT12 with(nolock) on D.[Object] = 'Rule' and OT12.ID = O12.RuleTypeID
 
 			left join [Policy] O13 with(nolock) on D.[Object] = 'Policy' and O13.ID = D.ObjectID
 			left join PolicyType OT13 with(nolock) on D.[Object] = 'Policy' and OT13.ID = O13.PolicyTypeID
@@ -98,15 +89,7 @@ as
 			left join FusionAttributeType O17 with(nolock) on D.[Object] = 'FusionAttributeType' and O17.ID = D.ObjectID
 			left join FusionAttributeType P17 with(nolock) on D.[Object] = 'FusionAttributeType' and P17.ID = O17.ParentID
 
-			left join	(
-						select 1 as ID, 'Informational Rule' as Name
-						union
-						select 2 as ID, 'Quality Check Rule' as Name
-						union
-						select 3 as ID, 'Metric Rule' as Name
-						union
-						select 4 as ID, 'Profile Rule' as Name
-						) O18 on D.[Object] = 'RuleType' and O18.ID = D.ObjectID
+			left join RuleType O18 on D.[Object] = 'RuleType' and O18.ID = D.ObjectID
 
 			left join [Lookup] O20 with(nolock) on D.[Object] = 'Lookup' and O20.ID = D.ObjectID
 			left join LookupType OT20 with(nolock) on D.[Object] = 'Lookup' and OT20.ID = O20.LookupTypeID
@@ -142,6 +125,5 @@ as
 			) O29 on D.[Object] = 'ReferenceItemType' and O29.ID = D.ObjectID
 			
 			left join ObjectStyle S with(nolock) on S.ObjectType = D.ObjectType and S.ObjectID = D.[ObjectTypeID]
-
 
 GO
