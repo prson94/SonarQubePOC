49aa2e4c36b112568876ed3587131ebe1f814313
Kenneth McNamee <kmcnamee@infogix.com>
2017-08-28 16:28:32 +0000

    Worflow proc needs to look at workflow instance not workflow overal for responsibilities.

--- d360.database.company/utility/Stored Procedures/GetOwnersForWorkflowV2.sql
+++ d360.database.company/utility/Stored Procedures/GetOwnersForWorkflowV2.sql
@@ -1,6 +1,7 @@
-﻿create procedure [utility].[GetOwnersForWorkflowV2]
+﻿CREATE PROCEDURE [utility].[GetOwnersForWorkflowV2]
 	@workflowID int,
-	@workflowStepID int = 0
+	@workflowStepID int = 0,
+	@workflowItemID int = 0
 as
 begin
 	declare @objectId int,			
@@ -8,36 +9,66 @@ begin
 			@responsibilityTypeID int;
 
 	declare @tbl table (ResourceID int, FirstName nvarchar(250), LastName nvarchar(250), Email nvarchar(500), Username nvarchar(500), DateLastLoggedIn datetime null, ResourceTypeID int, Status nvarchar(25))
-
-	select @objectType = object, @objectId = objectid from [workflow].[eventregistration] where typeid = @workflowID;
 	
 	--get the responsibility for this step from the settings of the step
 	select @responsibilityTypeID = settings.value('(/settings/ResponsibilityTypeID)[1]', 'int') from [workflow].[VersionStep] where id = @workflowStepID
 	
-		--1. Check for vocabulary owners
-	insert into @tbl
-		select	R.ResourceID, R.FirstName, R.LastName, R.Email, R.Email, R.DateLastLoggedIn, 1 as ResourceTypeID, R.Status 
-		from	ResponsibilityDetail RD 					
-					inner join reporting.Global_Resource R 
-						on RD.ObjectType = @objectType
-						and RD.ObjectID = @objectId
-						and RD.ResponsibilityTypeID = @responsibilityTypeID
-						and	(
-								--(RD.ResponsibleObjectType = 'Group' and R.ResourceID = RD.PrimaryOwnerResourceID) or 
-								(RD.ResponsibleObjectType = 'Resource' and R.ResourceID = RD.ResponsibleObjectID)
-							)
-						and R.Email not like '%?subject=%' and R.Status = 'Active'
-		union all		
-		select	R.ResourceID, R.FirstName, R.LastName, R.Email, R.Email, R.DateLastLoggedIn, 1 as ResourceTypeID, R.Status 
-		from	ResponsibilityDetail RD 					
-					inner join resourcegroup Rg on (RD.ResponsibleObjectID = Rg.GroupID 
-						and RD.ObjectType = @objectType
-						and RD.ObjectID = @objectId
-						and RD.ResponsibilityTypeID = @responsibilityTypeID
-						and	RD.ResponsibleObjectType = 'Group')
-					inner join reporting.Global_Resource R 
-						on (Rg.ResourceID = R.ResourceID and R.Email not like '%?subject=%' and R.Status = 'Active');
+	-- check object
+	begin
+			select @objectType = object, @objectId = objectid from [workflow].[item] where id = @workflowItemID;
 
+			insert into @tbl
+				select	R.ResourceID, R.FirstName, R.LastName, R.Email, R.Email, R.DateLastLoggedIn, 1 as ResourceTypeID, R.Status 
+				from	ResponsibilityDetail RD 					
+							inner join reporting.Global_Resource R 
+								on RD.ObjectType = @objectType
+								and RD.ObjectID = @objectId
+								and RD.ResponsibilityTypeID = @responsibilityTypeID
+								and	(
+										--(RD.ResponsibleObjectType = 'Group' and R.ResourceID = RD.PrimaryOwnerResourceID) or 
+										(RD.ResponsibleObjectType = 'Resource' and R.ResourceID = RD.ResponsibleObjectID)
+									)
+								and R.Email not like '%?subject=%' and R.Status = 'Active'
+				union all		
+				select	R.ResourceID, R.FirstName, R.LastName, R.Email, R.Email, R.DateLastLoggedIn, 1 as ResourceTypeID, R.Status 
+				from	ResponsibilityDetail RD 					
+							inner join resourcegroup Rg on (RD.ResponsibleObjectID = Rg.GroupID 
+								and RD.ObjectType = @objectType
+								and RD.ObjectID = @objectId
+								and RD.ResponsibilityTypeID = @responsibilityTypeID
+								and	RD.ResponsibleObjectType = 'Group')
+							inner join reporting.Global_Resource R 
+								on (Rg.ResourceID = R.ResourceID and R.Email not like '%?subject=%' and R.Status = 'Active');
+		end
+
+	--2. Check for type owners
+	if not exists (select 1 from @tbl)
+		begin
+			select @objectType = object, @objectId = objectid from [workflow].[eventregistration] where typeid = @workflowID;
+
+			insert into @tbl
+				select	R.ResourceID, R.FirstName, R.LastName, R.Email, R.Email, R.DateLastLoggedIn, 1 as ResourceTypeID, R.Status 
+				from	ResponsibilityDetail RD 					
+							inner join reporting.Global_Resource R 
+								on RD.ObjectType = @objectType
+								and RD.ObjectID = @objectId
+								and RD.ResponsibilityTypeID = @responsibilityTypeID
+								and	(
+										--(RD.ResponsibleObjectType = 'Group' and R.ResourceID = RD.PrimaryOwnerResourceID) or 
+										(RD.ResponsibleObjectType = 'Resource' and R.ResourceID = RD.ResponsibleObjectID)
+									)
+								and R.Email not like '%?subject=%' and R.Status = 'Active'
+				union all		
+				select	R.ResourceID, R.FirstName, R.LastName, R.Email, R.Email, R.DateLastLoggedIn, 1 as ResourceTypeID, R.Status 
+				from	ResponsibilityDetail RD 					
+							inner join resourcegroup Rg on (RD.ResponsibleObjectID = Rg.GroupID 
+								and RD.ObjectType = @objectType
+								and RD.ObjectID = @objectId
+								and RD.ResponsibilityTypeID = @responsibilityTypeID
+								and	RD.ResponsibleObjectType = 'Group')
+							inner join reporting.Global_Resource R 
+								on (Rg.ResourceID = R.ResourceID and R.Email not like '%?subject=%' and R.Status = 'Active');
+		end
 	
 	
 	-- if noone found email admins
