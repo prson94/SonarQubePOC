e2130fb7125c622ae2e8b93d91ad9a6f9304bdf1
Kenneth McNamee <kmcnamee@infogix.com>
2017-10-05 15:46:32 +0000

    workflows on issues now support responsibilities on the issue it was raised on.  Also got rid of old workflow proc renamed new one to get rid of v2 crap.

--- d360.database.company/utility/Stored Procedures/GetOwnersForWorkflow.sql
+++ d360.database.company/utility/Stored Procedures/GetOwnersForWorkflow.sql
@@ -1,158 +1,91 @@
-﻿CREATE procedure [utility].[GetOwnersForWorkflow]
---declare 
-	@workflowID uniqueidentifier
---set @workflowID = '387A8094-565E-45AF-B049-01329EEF2209' --=> wt 1
---set @workflowID = '0C573C9B-D237-4468-8822-7D515750675B'--'CEE2AF0D-DAB8-432B-AF08-00E52B808C52' --=> wt 2
---set @workflowID = 'FD3C4A3D-C9BB-477A-B5CD-BC99C62AF53F' --=> wt 3
+﻿CREATE PROCEDURE [utility].[GetOwnersForWorkflow]
+	@workflowID int,
+	@workflowStepID int = 0,
+	@workflowItemID int = 0
 as
 begin
-	declare @workflowType int,
-			@fields xml
-	declare @tbl table (ID int, FirstName nvarchar(250), LastName nvarchar(250), Email nvarchar(500), Username nvarchar(500), DateLastLoggedIn datetime null, ResourceTypeID int, Status nvarchar(25))
+	declare @objectId int,			
+			@objectType varchar(50),
+			@responsibilityTypeID int,
+			@issueId int;
 
-	select	@workflowType = WorkflowType,
-			@fields = Data
-	from	Workflow
-	where	ID = @workflowID
-
-	if @workflowType = 1
+	declare @tbl table (ResourceID int, FirstName nvarchar(250), LastName nvarchar(250), Email nvarchar(500), Username nvarchar(500), DateLastLoggedIn datetime null, ResourceTypeID int, Status nvarchar(25))
+	
+	--get the responsibility for this step from the settings of the step
+	select @responsibilityTypeID = settings.value('(/settings/ResponsibilityTypeID)[1]', 'int') from [workflow].[VersionStep] where id = @workflowStepID
+		
+	-- check object	
 	begin
-		--1. Check for vocabulary owners
-		insert into @tbl
-			select	R.ResourceID, R.FirstName, R.LastName, R.Email, R.Email, R.DateLastLoggedIn, 1 as ResourceTypeID, R.Status 
-			from	ResponsibilityDetail RD 
-					inner join WorkflowTypeRelation WTR on WTR.Parent = 'TaxonomyType' and WTR.ParentID = @fields.value('(/fields/TaxonomyTypeID)[1]', 'int') and WTR.WorkflowType = @workflowType and WTR.ResponsibilityTypeID = RD.ResponsibilityTypeID
-					inner join reporting.Global_Resource R 
-						on RD.ObjectType = 'TaxonomyType' 
-						and RD.ObjectID = @fields.value('(/fields/TaxonomyTypeID)[1]', 'int')
-						and	(
-								(RD.ResponsibleObjectType = 'Group' and R.ResourceID = RD.PrimaryOwnerResourceID) or 
-								(RD.ResponsibleObjectType = 'Resource' and R.ResourceID = RD.ResponsibleObjectID)
-							)
-						and R.Email not like '%?subject=%' and R.Status = 'Active'
+			select @objectType = object, @objectId = objectid from [workflow].[item] where id = @workflowItemID;
+			
+			if @objectType = 'Issue'
+			begin				
+				select @issueId = id, @objectType = [object], @objectId = [objectid] from Issue where id = @objectId
+			end
 
-		if not exists(select * from @tbl)
-		begin
 			insert into @tbl
 				select	R.ResourceID, R.FirstName, R.LastName, R.Email, R.Email, R.DateLastLoggedIn, 1 as ResourceTypeID, R.Status 
-				from	ResponsibilityDetail RD 
-						inner join WorkflowTypeRelation WTR on WTR.[Object] = 'ArtifactType' and WTR.ObjectID = @fields.value('(/fields/ArtifactTypeID)[1]', 'int') and WTR.Parent is null and WTR.WorkflowType = @workflowType and WTR.ResponsibilityTypeID = RD.ResponsibilityTypeID
-						inner join reporting.Global_Resource R 
-							on RD.ObjectType = 'ArtifactType' 
-							and RD.ObjectID = @fields.value('(/fields/ArtifactTypeID)[1]', 'int')
-							and (
-									(RD.ResponsibleObjectType = 'Group' and R.ResourceID = RD.PrimaryOwnerResourceID) or 
-									(RD.ResponsibleObjectType = 'Resource' and R.ResourceID = RD.ResponsibleObjectID)
-								)
-							and R.Email not like '%?subject=%' and R.Status = 'Active'
-		end
-	end
-
-	if @workflowType = 5
-	begin
-		--1. Check for vocabulary owners
-		insert into @tbl
-			select	R.ResourceID, R.FirstName, R.LastName, R.Email, R.Email, R.DateLastLoggedIn, 1 as ResourceTypeID, R.Status 
-			from	ResponsibilityDetail RD 
-					inner join WorkflowTypeRelation WTR on WTR.Parent = 'TaxonomyType' and WTR.ParentID = @fields.value('(/fields/TaxonomyTypeID)[1]', 'int') and WTR.WorkflowType = @workflowType and WTR.ResponsibilityTypeID = RD.ResponsibilityTypeID
-					inner join reporting.Global_Resource R 
-						on RD.ObjectType = 'TaxonomyType' 
-						and RD.ObjectID = @fields.value('(/fields/TaxonomyTypeID)[1]', 'int')
-						and	(
-								(RD.ResponsibleObjectType = 'Group' and R.ResourceID = RD.PrimaryOwnerResourceID) or 
-								(RD.ResponsibleObjectType = 'Resource' and R.ResourceID = RD.ResponsibleObjectID)
-							)
-						and R.Email not like '%?subject=%' and R.Status = 'Active'
-
-		if not exists(select * from @tbl)
-		begin
-			insert into @tbl
-				select	R.ResourceID, R.FirstName, R.LastName, R.Email, R.Email, R.DateLastLoggedIn, 1 as ResourceTypeID, R.Status 
-				from	ResponsibilityDetail RD 
-						inner join WorkflowTypeRelation WTR on WTR.[Object] = 'ArtifactType' and WTR.ObjectID = @fields.value('(/fields/ArtifactTypeID)[1]', 'int') and WTR.Parent is null and WTR.WorkflowType = @workflowType and WTR.ResponsibilityTypeID = RD.ResponsibilityTypeID
-						inner join reporting.Global_Resource R 
-							on RD.ObjectType = 'ArtifactType' 
-							and RD.ObjectID = @fields.value('(/fields/ArtifactTypeID)[1]', 'int')
-							and (
-									(RD.ResponsibleObjectType = 'Group' and R.ResourceID = RD.PrimaryOwnerResourceID) or 
-									(RD.ResponsibleObjectType = 'Resource' and R.ResourceID = RD.ResponsibleObjectID)
-								)
-							and R.Email not like '%?subject=%' and R.Status = 'Active'
-		end
-	end
-
-	if @workflowType = 2
-	begin
-		insert into @tbl
-			select	R.ResourceID, R.FirstName, R.LastName, R.Email, R.Email, R.DateLastLoggedIn, 1 as ResourceTypeID, R.Status 
-			from	ResponsibilityDetail RD 
-					inner join Artifact A on RD.ObjectType = 'Artifact' and RD.ObjectID = A.ID and A.ID = @fields.value('(/fields/ArtifactID)[1]', 'int')
-					inner join WorkflowTypeRelation WTR		on WTR.[Object] = 'ArtifactType' and WTR.ObjectID = A.ArtifactTypeID 
-															and WTR.Parent = 'TaxonomyType' and WTR.ParentID = A.TaxonomyTypeID
-															and WTR.WorkflowType = @workflowType 
-															and WTR.ResponsibilityTypeID = RD.ResponsibilityTypeID
-					inner join reporting.Global_Resource R 
-						on	(
-								(RD.ResponsibleObjectType = 'Group' and R.ResourceID = RD.PrimaryOwnerResourceID) or 
-								(RD.ResponsibleObjectType = 'Resource' and R.ResourceID = RD.ResponsibleObjectID)
-							)
-						and R.Email not like '%?subject=%' and R.Status = 'Active' 
-
-		if not exists(select * from @tbl)
-		begin
-			insert into @tbl
-				select	R.ResourceID, R.FirstName, R.LastName, R.Email, R.Email, R.DateLastLoggedIn, 1 as ResourceTypeID, R.Status 
-				from	ResponsibilityDetail RD 
-						inner join Artifact A on RD.ObjectType = 'Artifact' and RD.ObjectID = A.ID and A.ID = @fields.value('(/fields/ArtifactID)[1]', 'int')
-						inner join WorkflowTypeRelation WTR		on WTR.[Object] = 'ArtifactType' and WTR.ObjectID = A.ArtifactTypeID 
-																and WTR.WorkflowType = @workflowType 
-																and WTR.ResponsibilityTypeID = RD.ResponsibilityTypeID
-						inner join reporting.Global_Resource R 
-							on	(
-									(RD.ResponsibleObjectType = 'Group' and R.ResourceID = RD.PrimaryOwnerResourceID) or 
-									(RD.ResponsibleObjectType = 'Resource' and R.ResourceID = RD.ResponsibleObjectID)
-								)
-							and R.Email not like '%?subject=%' and R.Status = 'Active' 
-		end
-	end
-
-	if @workflowType = 3
-	begin
-
-		insert into @tbl
-			select	distinct
-						R.ResourceID, R.FirstName, R.LastName, R.Email, R.Email, R.DateLastLoggedIn, 1 as ResourceTypeID, R.Status 
-				from	Comment C
-						inner join CommentRelation CR on CR.CommentID = C.ID and C.ID = @fields.value('(fields/CommentID)[1]', 'int') and CR.ObjectType not in ('Resource', 'Group')
-						inner join ResponsibilityDetail RD on RD.ObjectType = CR.ObjectType and RD.ObjectID = CR.ObjectID 
-						inner join WorkflowTypeRelation WTR		on WTR.[Object] = RD.ObjectType +'Type' and WTR.ObjectID = RD.ObjectTypeID 
-																and WTR.WorkflowType = @workflowType 
-																and WTR.ResponsibilityTypeID = RD.ResponsibilityTypeID
-																and WTR.[Enabled] = 1
-						inner join reporting.Global_Resource R 
-							on	(
-									(RD.ResponsibleObjectType = 'Group' and R.ResourceID = RD.PrimaryOwnerResourceID) or 
-									(RD.ResponsibleObjectType = 'Resource' and R.ResourceID = RD.ResponsibleObjectID)
-								) 
+				from	ResponsibilityDetail RD 					
+							inner join reporting.Global_Resource R 
+								on RD.ObjectType = @objectType
+								and RD.ObjectID = @objectId
+								and RD.ResponsibilityTypeID = @responsibilityTypeID
+								and	(
+										--(RD.ResponsibleObjectType = 'Group' and R.ResourceID = RD.PrimaryOwnerResourceID) or 
+										(RD.ResponsibleObjectType = 'Resource' and R.ResourceID = RD.ResponsibleObjectID)
+									)
 								and R.Email not like '%?subject=%' and R.Status = 'Active'
-
-		if not exists (select 1 from @tbl)
-		begin
-			insert into @tbl
-				select	distinct
-						R.ResourceID, R.FirstName, R.LastName, R.Email, R.Email, R.DateLastLoggedIn, 1 as ResourceTypeID, R.Status 
-				from	Comment C
-						inner join CommentRelation CR on CR.CommentID = C.ID and C.ID = @fields.value('(fields/CommentID)[1]', 'int') and CR.ObjectType not in ('Resource', 'Group')
-						inner join ResponsibilityDetail RD on RD.ObjectType = CR.ObjectType and RD.ObjectID = CR.ObjectID 
-						inner join reporting.Global_Resource R 
-							on	(
-									(RD.ResponsibleObjectType = 'Group' and R.ResourceID = RD.PrimaryOwnerResourceID) or 
-									(RD.ResponsibleObjectType = 'Resource' and R.ResourceID = RD.ResponsibleObjectID)
-								) 
-								and R.Email not like '%?subject=%' and R.Status = 'Active'
+				union all		
+				select	R.ResourceID, R.FirstName, R.LastName, R.Email, R.Email, R.DateLastLoggedIn, 1 as ResourceTypeID, R.Status 
+				from	ResponsibilityDetail RD 					
+							inner join resourcegroup Rg on (RD.ResponsibleObjectID = Rg.GroupID 
+								and RD.ObjectType = @objectType
+								and RD.ObjectID = @objectId
+								and RD.ResponsibilityTypeID = @responsibilityTypeID
+								and	RD.ResponsibleObjectType = 'Group')
+							inner join reporting.Global_Resource R 
+								on (Rg.ResourceID = R.ResourceID and R.Email not like '%?subject=%' and R.Status = 'Active');
 		end
 
-		if not exists (select 1 from @tbl)
+	--2. Check for type owners
+	if not exists (select 1 from @tbl)
+		begin
+			if @issueId > 0
+			begin
+				select @objectType = [object], @objectId = [objectid] from Issue where id = @objectId
+			end
+			else
+			begin
+				select @objectType = object, @objectId = objectid from [workflow].[eventregistration] where typeid = @workflowID;
+			end
+
+			insert into @tbl
+				select	R.ResourceID, R.FirstName, R.LastName, R.Email, R.Email, R.DateLastLoggedIn, 1 as ResourceTypeID, R.Status 
+				from	ResponsibilityDetail RD 					
+							inner join reporting.Global_Resource R 
+								on RD.ObjectType = @objectType
+								and RD.ObjectID = @objectId
+								and RD.ResponsibilityTypeID = @responsibilityTypeID
+								and	(
+										--(RD.ResponsibleObjectType = 'Group' and R.ResourceID = RD.PrimaryOwnerResourceID) or 
+										(RD.ResponsibleObjectType = 'Resource' and R.ResourceID = RD.ResponsibleObjectID)
+									)
+								and R.Email not like '%?subject=%' and R.Status = 'Active'
+				union all		
+				select	R.ResourceID, R.FirstName, R.LastName, R.Email, R.Email, R.DateLastLoggedIn, 1 as ResourceTypeID, R.Status 
+				from	ResponsibilityDetail RD 					
+							inner join resourcegroup Rg on (RD.ResponsibleObjectID = Rg.GroupID 
+								and RD.ObjectType = @objectType
+								and RD.ObjectID = @objectId
+								and RD.ResponsibilityTypeID = @responsibilityTypeID
+								and	RD.ResponsibleObjectType = 'Group')
+							inner join reporting.Global_Resource R 
+								on (Rg.ResourceID = R.ResourceID and R.Email not like '%?subject=%' and R.Status = 'Active');
+		end
+	
+	
+	-- if noone found email admins
+	if not exists (select 1 from @tbl)
 		begin
 			insert into @tbl
 				select 
@@ -160,50 +93,7 @@ begin
 				from 
 					reporting.Global_Resource R where isadministrator = 1 and status = 'Active'
 		end
-	end
-
-	if @workflowType = 4
-	begin
-		insert into @tbl
-				select	R.ResourceID, R.FirstName, R.LastName, R.Email, R.Email, R.DateLastLoggedIn, 1 as ResourceTypeID, R.Status 
-				from	ResponsibilityDetail RD 
-						inner join Artifact A on RD.ObjectType = 'Artifact' and RD.ObjectID = A.ID and A.ID = @fields.value('(/fields/ArtifactID)[1]', 'int')
-						inner join WorkflowTypeRelation WTR		on WTR.[Object] = 'ArtifactType' and WTR.ObjectID = A.ArtifactTypeID 
-																and WTR.WorkflowType = @workflowType 
-																and WTR.ResponsibilityTypeID = RD.ResponsibilityTypeID
-																and WTR.[Enabled] = 1
-						inner join reporting.Global_Resource R 
-							on	(
-									(RD.ResponsibleObjectType = 'Group' and R.ResourceID = RD.PrimaryOwnerResourceID) or 
-									(RD.ResponsibleObjectType = 'Resource' and R.ResourceID = RD.ResponsibleObjectID)
-								)
-							and R.Email not like '%?subject=%' and R.Status = 'Active' 
-
-		if not exists (select 1 from @tbl)
-		begin
-			insert into @tbl
-				select	distinct
-						R.ResourceID, R.FirstName, R.LastName, R.Email, R.Email, R.DateLastLoggedIn, 1 as ResourceTypeID, R.Status 
-				from	Comment C
-						inner join CommentRelation CR on CR.CommentID = C.ID and C.ID = @fields.value('(fields/CommentID)[1]', 'int') and CR.ObjectType not in ('Resource', 'Group')
-						inner join ResponsibilityDetail RD on RD.ObjectType = CR.ObjectType and RD.ObjectID = CR.ObjectID 
-						inner join reporting.Global_Resource R 
-							on	(
-									(RD.ResponsibleObjectType = 'Group' and R.ResourceID = RD.PrimaryOwnerResourceID) or 
-									(RD.ResponsibleObjectType = 'Resource' and R.ResourceID = RD.ResponsibleObjectID)
-								) 
-								and R.Email not like '%?subject=%' and R.Status = 'Active'
-		end
-
-		if not exists (select 1 from @tbl)
-		begin
-			insert into @tbl
-				select 
-					R.ResourceID, R.FirstName, R.LastName, R.Email, R.Email, R.DateLastLoggedIn, 1 as ResourceTypeID, R.Status 
-				from 
-					reporting.Global_Resource R where isadministrator = 1 and status = 'Active'
-		end
-	end
+	
 
 	select * from @tbl
 end
--- d360.model/CompanyContext_Workflow.cs
+++ d360.model/CompanyContext_Workflow.cs
@@ -837,7 +837,7 @@ namespace d360.model
             }
             else if (settings.RecipientType == EmailTaskRecipientType.Responsibility || settings.RecipientType == EmailTaskRecipientType.None)
             {
-                users = Query<core.entities.GlobalReportingResource>("[utility].[GetOwnersForWorkflowV2] @id, @stepId, @itemId", new { id = item.Step.Version.TypeID, @stepId = item.Step.ID, @itemId = item.ItemID }).ToList();
+                users = Query<core.entities.GlobalReportingResource>("[utility].[GetOwnersForWorkflow] @id, @stepId, @itemId", new { id = item.Step.Version.TypeID, @stepId = item.Step.ID, @itemId = item.ItemID }).ToList();
             }
             else if(settings.RecipientType == EmailTaskRecipientType.SpecificUser)
             {
