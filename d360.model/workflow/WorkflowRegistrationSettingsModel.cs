213abf966b8539e450b0cf4ab154b34ab8e2ed77
Kenneth McNamee <kmcnamee@infogix.com>
2017-10-05 15:18:03 +0000

    removed subject area logic from workflow backend

--- d360.model/workflow/WorkflowRegistrationSettingsModel.cs
+++ d360.model/workflow/WorkflowRegistrationSettingsModel.cs
@@ -6,39 +6,27 @@ namespace d360.model.workflow
     public class WorkflowRegistrationSettingsModel
     {
         public bool? Visible { get; set; }
-        public int TaxonomyTypeID { get; set; }
-
+        
         public static WorkflowRegistrationSettingsModel parseXml(XElement xml)
         {
-            bool? vis = null;
-            int taxonomyTypeId = 0;
+            bool? vis = null;            
 
             if (xml == null) throw new Exception("INVALID XML SPECIFIED");
 
             if (xml.HasElements) {
-                var visString = "";
-                var taxonomyTypeIdString = "";
+                var visString = "";                
 
                 if(xml.Element("Visible")!=null)
                     visString = xml.Element("Visible").Value;
                 
                 if (visString == "1" || (visString ??"").ToUpper() == "TRUE") vis = true;
                 else vis = false;
-
-                if (xml.Element("TaxonomyTypeID") != null)
-                    taxonomyTypeIdString = xml.Element("TaxonomyTypeID").Value;
-
-                if (!string.IsNullOrEmpty(taxonomyTypeIdString))
-                {
-                    int.TryParse(taxonomyTypeIdString, out taxonomyTypeId);                    
-                }
-
             }
 
             return new WorkflowRegistrationSettingsModel
             {
-                Visible = vis,
-                TaxonomyTypeID = taxonomyTypeId
+                Visible = vis
+                
             };
         }
     }
