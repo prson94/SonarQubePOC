9a0a723add593de19ae5ea8a16c67058bcccee2d
Kenneth McNamee <kmcnamee@infogix.com>
2017-10-19 19:42:29 +0000

    some minor component updates

--- d360.web/webpack.prod.config.js
+++ d360.web/webpack.prod.config.js
@@ -26,7 +26,26 @@ var webpackConfig = {
            path.join(__dirname, '/scripts/app/') // location of your src
         ),
         new webpack.optimize.UglifyJsPlugin({
-            compress: { warnings: false },comments:false
+            beautify: false,
+            output: {
+                comments: false
+            },
+            mangle: {
+                screw_ie8: true
+            },
+            compress: {
+                screw_ie8: true,
+                warnings: false,
+                conditionals: true,
+                unused: true,
+                comparisons: true,
+                sequences: true,
+                dead_code: true,
+                evaluate: true,
+                if_return: true,
+                join_vars: true,
+                negate_iife: false
+            }
         }),
       new webpack.optimize.CommonsChunkPlugin({ name: ['main', 'vendor', 'polyfills'], minChunks: Infinity }),
       new webpack.DefinePlugin({
