a71dd21daf2bd330bf54b98c5bf56e97098a0ed8
Kenneth McNamee <kmcnamee@infogix.com>
2017-11-05 21:07:25 +0000

    renamed getintersecttypename to be consistent with mikes inline table functions

--- d360.web/Controllers/FormController.cs
+++ d360.web/Controllers/FormController.cs
@@ -8588,7 +8588,7 @@ select 'ReferenceItemType|' + cast(ID as varchar(10)) as value, 'Reference Item:
                 case "R":   // Relation
                 case "U":   // Unrelation
                     #region
-                    sql = @"select 'IntersectType|' + cast(itd.ID as varchar(10)) as value, IName.Name as title from intersecttypedetail itd cross apply utility.GetIntersectTypeNames(itd.ID) IName	 where itd.IsSystem = 0 order by IName.Name";
+                    sql = @"select 'IntersectType|' + cast(itd.ID as varchar(10)) as value, IName.Name as title from intersecttypedetail itd cross apply dbo.GetIntersectTypeNames(itd.ID) IName	 where itd.IsSystem = 0 order by IName.Name";
                     break;
                     #endregion
                 case "BL":   // Lineage
