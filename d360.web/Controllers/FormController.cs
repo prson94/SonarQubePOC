f344feed0b0873bf06d52622566ab8d4a5dc02c4
Karamjeet Kaur <kkaur@infogix.com>
2017-12-04 12:46:06 +0000

    We have removed extra check on Object Type.
    Workitems:
    #2605 Custom field on user not populating (If i use the name ID)

--- d360.web/Controllers/FormController.cs
+++ d360.web/Controllers/FormController.cs
@@ -3565,8 +3565,7 @@ namespace d360.web.Controllers
                     throw new ConflictException("Error Occurred!", $"{Resources.FieldInfo.ApiName_Name} can only have uppercase letters, lowercase letters, numbers, dash, or underscore. It must also begin with a letter.");
                 }
 
-                if (!string.IsNullOrEmpty(model.FieldType.Object) && (model.FieldType.Object).Equals("ResourceType")
-                    && !string.IsNullOrEmpty(model.FieldType.Name) && (model.FieldType.Name).ToUpper().Equals("ID"))
+                if (!string.IsNullOrEmpty(model.FieldType.Name) && (model.FieldType.Name).ToUpper().Equals("ID"))
                 {
                     throw new ConflictException("Error Occurred!", "You can not add field with API Name [ID].");
                 }
