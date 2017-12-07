47c9be6390cde78757e176dff7fe31a08045a65b
Kenneth McNamee <kmcnamee@infogix.com>
2017-12-07 13:52:18 +0000

    fix to regression caused by lm tom fix to comments blank in workflows raised on actions.  LMTOM has no issue description which is the default value.

--- d360.web/Controllers/FormController.cs
+++ d360.web/Controllers/FormController.cs
@@ -8118,7 +8118,7 @@ namespace d360.web.Controllers
                 comment.OwnerObjectType = SystemObjects.Resource.ToString();
                 comment.OwnerObjectID = Company.CurrentResourceID;
                 comment.CommentTypeID = CommentType.Issue;
-                comment.Body = $"New {issueType.Name} Raised, criticality is {criticality.ToString()}.";
+                comment.Body = desc ?? $"New {issueType.Name} Raised, criticality is {criticality.ToString()}.";
                 
 
                 //add relation to current artifact
