e22e6187c7bbf12716ad5bc063c7527b8b238f08
Kenneth McNamee <kmcnamee@infogix.com>
2018-01-25 22:39:39 +0000

    (GOV-3083/IGX-4367) Clicking on ownership in model structure page returns an error.

--- d360.web/Scripts/app/components/model/model-item-structure.component.ts
+++ d360.web/Scripts/app/components/model/model-item-structure.component.ts
@@ -123,7 +123,7 @@ export class ModelItemStructureComponent extends BaseComponent implements OnInit
             this.modelId = +params['modelId'];
 
             this.setObjectInfo('TaxonomyType', this.modelId);
-            this.setCommonRightSideBar(true, true);
+            this.setCommonRightSideBar(true);
 
             this.getFieldsDefinition();
             
@@ -139,8 +139,7 @@ export class ModelItemStructureComponent extends BaseComponent implements OnInit
                         this.model = result;
 
                         this.headerBreadcrumbService.clearBreadcrumbs();
-                        this.headerBreadcrumbService.showBreadcrumb(new Breadcrumb('Models', `${SiteUrlHelpers.SITE_URL_MODEL_ROOT}/${SiteUrlHelpers.SITE_URL_MODEL_CLASSIFICATION}`));
-                        this.headerBreadcrumbService.showBreadcrumb(new Breadcrumb(this.model.ClassificationName, `${SiteUrlHelpers.SITE_URL_MODEL_ROOT}/${SiteUrlHelpers.SITE_URL_MODEL_CLASSIFICATION}/${this.model.ClassificationName}`));
+                        this.headerBreadcrumbService.showBreadcrumb(new Breadcrumb('Models', `${SiteUrlHelpers.SITE_URL_MODEL_ROOT}/${SiteUrlHelpers.SITE_URL_MODEL_CLASSIFICATION}`));                        
                         this.headerBreadcrumbService.showBreadcrumb(new Breadcrumb(this.model.Name, SiteUrlHelpers.getObjectUrl('TAXONOMYTYPE', this.model.ID)));
 
                         this.loadModelHierarchy(this.modelId);
