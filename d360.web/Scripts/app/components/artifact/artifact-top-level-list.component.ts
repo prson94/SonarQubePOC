051f31092c3f2c55e497581142eccac537fb6785
Jim Hoell <jhoell@infogix.com>
2018-03-05 15:00:54 +0000

    GOV-3301: Artifact - Glossary structure page is no longer a tree after Parent ID column changes

--- d360.web/Scripts/app/components/artifact/artifact-top-level-list.component.ts
+++ d360.web/Scripts/app/components/artifact/artifact-top-level-list.component.ts
@@ -11,104 +11,67 @@ import { SiteUrlHelpers } from '../../static/site-url-helpers';
 
 @Component({
     selector: 'd3s-artifact-top-level-list',
-    template: `                 
+    template: `           
+
                 <div class="row">
                     <div class="col s12">
                         <d3s-loading [isLoading]="isLoading"></d3s-loading>
                     <div class="tile tile-detail" *ngIf="!isLoading">                            
                         <header>Glossary</header>                              
                         <input type="text" [(ngModel)]="searchValue" placeholder="Search" style="width: 100%;margin-bottom:10px;">  
-                        <p-treeTable [value]="treeNodeArray | treeSearch: searchValue : 'Name'" selectionMode="single" [(selection)]="selected" styleClass="breadcrumbTree" [style]="{'line-height':'25px'}">
-                            <p-column field="Name" header="Name">
-                                <ng-template let-item="rowData" pTemplate type="body">
-                                    <a (click)="showItem(item)">{{item.data.Name}}</a>
-                                </ng-template>
-                            </p-column>                                                   
-                            <p-column field="Description" header="Description">
-                                <ng-template let-item="rowData" pTemplate type="body">
-                                    <span [innerHtml]="item.data.Description"></span>
+                        <p-treeTable [value]="ArtifactTypes | treeSearch: searchValue:'Name'" selectionMode="single" [(selection)]="selectedRow" [style]="{ 'width': '100%' }">
+                            <p-column field="Name" header="Name" [style]="{ 'width': '85%' }"></p-column>
+                            <p-column [style]="{ 'width': '15%','overflow':'visible' }" header="Select Item">
+                                <ng-template let-col let-item="rowData" pTemplate="body">
+                                    <d3s-preview-tooltip objectType="ArtifactType" [objectId]="item.data.ID" icon="info" (click)="navigate(item.data)"></d3s-preview-tooltip>
                                 </ng-template>
                             </p-column>
-                            <p-column field="Total" header="Total" [style]="{width:'100px'}"></p-column>
-                            <p-column  [style]="{width:'40px',overflow:'visible'}">
-                                    <ng-template let-item="rowData" pTemplate type="body">
-                                        <div class="RowTools">                                                                            
-                                            <d3s-preview-tooltip objectType="ArtifactType" [objectId]="item.data.ID" (click)="showItem(item)" icon="info"></d3s-preview-tooltip>
-                                        </div>
-                                    </ng-template>
-                            </p-column>       
-                        </p-treeTable>                                   
+                        </p-treeTable>
                     </div>
                 </div>
                 `,
     providers: [ArtifactTypeService],
 })
 
-export class ArtifactTopLevelListComponent extends ArtifactBaseComponent implements OnInit {    
+export class ArtifactTopLevelListComponent extends ArtifactBaseComponent implements OnInit {   
+    searchFilter: string = "";
+    objectType: string = "ArtifactType";
+    adminType: string = "Artifacts";
+    selectedRow: TreeNode;
+    ArtifactTypes: TreeNode[];
+
     private searchValue: string;
     private treeNodeArray: TreeNode[] = [];
 
     constructor(private route: ActivatedRoute,
         private router: Router,
-        private artifactTypeService: ArtifactTypeService,        
+        private artifactsService: ArtifactTypeService,        
         headerBreadcrumbService: HeaderBreadcrumbService,
         private titleService: Title) {
         super(headerBreadcrumbService);
-
-
     }
 
     ngOnInit() {
         this.setBrowserTitle(this.titleService, 'Glossary');
-
         this.headerBreadcrumbService.clearBreadcrumbs();
         this.headerBreadcrumbService.clearCurrentObjectInfo();
         this.headerBreadcrumbService.showBreadcrumb(new Breadcrumb('Glossary'));
-
         this.load();
     }
 
 
     private load() {
         this.isLoading = true;
-        this.artifactTypeService.getTopLevelSummary()
-            .then(res => {
-                this.treeNodeArray = this.buildTreeNodeArray(res);
+        this.artifactsService.getArtifactTypeTree()
+            .then(data => {
+                this.ArtifactTypes = data;
+                this.selectedRow = this.ArtifactTypes[0];
                 this.isLoading = false;
-            });
+            }); 
     }
 
-
-    private buildTreeNodeArray(artifactTypeSummary: ArtifactTypeSummary[], Parent?: number): TreeNode[] {
-        //find the root items then 
-
-        let rootNodes = artifactTypeSummary.filter(x => (Parent != undefined ? x.ParentID == Parent : !x.ParentID));
-
-        if (rootNodes.length == 0) return null;
-
-        let res: TreeNode[] = [];
-
-        for (let root of rootNodes) {
-            res.push({
-                label: root.Name,
-                data: {
-                    ID: root.ID,
-                    Name: root.Name,
-                    Description: root.Description,
-                    Total: root.Total
-                },
-                children: (this.buildTreeNodeArray(artifactTypeSummary, root.ID)) //recursively find its children
-            });
-        }
-
-        return res;
+    navigate(item: any) {
+        this.router.navigateByUrl(SiteUrlHelpers.getObjectUrl('ArtifactType', item.ID));
     }
 
-    private showItem(item) {
-        if (!item.data || !item.data.ID) {
-            console.log("ERROR : MISSING ID ON THE SELECTED ROW");
-            return;
-        }        
-        this.router.navigateByUrl(SiteUrlHelpers.getObjectUrl('ArtifactType', item.data.ID));
-    }
 };
