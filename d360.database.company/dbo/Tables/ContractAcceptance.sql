e2e241d96e65d9dbe0069a9245f332e766f21f45
Michael Donato <mdonato@infogix.com>
2018-03-06 20:19:23 +0000

    -wrong table script in last checkin

--- d360.database.company/dbo/Tables/ContractAcceptance.sql
+++ d360.database.company/dbo/Tables/ContractAcceptance.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE [dbo].[ContractAcceptanceHistory](
+﻿CREATE TABLE [dbo].[ContractAcceptance](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	[ResourceID] [int] NOT NULL,
 	[Accepted] [bit] NOT NULL,
@@ -12,16 +12,18 @@ PRIMARY KEY CLUSTERED
 )
 GO
 
-ALTER TABLE [dbo].[ContractAcceptanceHistory]  WITH CHECK ADD  CONSTRAINT [FK_ContractAcceptanceHistory_Contract] FOREIGN KEY([ContractID])
+ALTER TABLE [dbo].[ContractAcceptance]  WITH CHECK ADD  CONSTRAINT [FK_ContractAcceptance_Contract] FOREIGN KEY([ContractID])
 REFERENCES [dbo].[Contract] ([ID])
 GO
 
-ALTER TABLE [dbo].[ContractAcceptanceHistory] CHECK CONSTRAINT [FK_ContractAcceptanceHistory_Contract]
+ALTER TABLE [dbo].[ContractAcceptance] CHECK CONSTRAINT [FK_ContractAcceptance_Contract]
 GO
 
-ALTER TABLE [dbo].[ContractAcceptanceHistory]  WITH CHECK ADD  CONSTRAINT [FK_ContractAcceptanceHistory_Organization] FOREIGN KEY([OrganizationID])
+ALTER TABLE [dbo].[ContractAcceptance]  WITH CHECK ADD  CONSTRAINT [FK_ContractAcceptance_Organization] FOREIGN KEY([OrganizationID])
 REFERENCES [dbo].[Organization] ([ID])
 GO
 
-ALTER TABLE [dbo].[ContractAcceptanceHistory] CHECK CONSTRAINT [FK_ContractAcceptanceHistory_Organization]
+ALTER TABLE [dbo].[ContractAcceptance] CHECK CONSTRAINT [FK_ContractAcceptance_Organization]
 GO
+
+
