735ee63130d46659dfca287f26a3aa19272d9658
Jim Hoell <jhoell@infogix.com>
2018-03-14 21:01:18 +0000

    GOV-3367: Fusion ownership title is incorrect

--- d360.web/Scripts/app/components/sidebar/ownership/ownership.component.ts
+++ d360.web/Scripts/app/components/sidebar/ownership/ownership.component.ts
@@ -2,6 +2,7 @@
 import { Router, ActivatedRoute } from '@angular/router';
 import { BaseComponent } from '../../shared/base.component';
 import { ObjectDetailService } from '../../../services/object-detail.service';
+import { FusionService } from '../../../services/fusion.service';
 
 @Component({
     selector: 'd3s-ownership',
@@ -9,31 +10,41 @@ import { ObjectDetailService } from '../../../services/object-detail.service';
                 <div class="row">
                     <div class="col s12">
                         <div class="tile tile-detail">   
-                            <d3s-people-responsibilities-tile [assetID]="assetID" [title]="'Ownership of ' + objectName"></d3s-people-responsibilities-tile>
+                            <d3s-people-responsibilities-tile [assetID]="assetID" [title]="'Ownership of ' + [objectName]"></d3s-people-responsibilities-tile>
                         </div>
                     </div>
                 </div>
         `,
-    providers: [ObjectDetailService]
+    providers: [
+        ObjectDetailService,
+        FusionService
+    ]
 })
 
 export class OwnershipComponent extends BaseComponent implements OnInit, OnDestroy {
+
     private sub: any;
     
     constructor(
         private route: ActivatedRoute,
         private router: Router,
-        private objectDetailService: ObjectDetailService
+        private objectDetailService: ObjectDetailService,
+        private fusionStuff: FusionService
     ) {
         super();
     }
 
     ngOnInit() {
-        this.sub = this.route.params.subscribe(params => {            
-            this.assetID = +params['assetID'];            
-                this.objectDetailService.getAsset(this.assetID).then(res => {
-                    this.objectName = res.DisplayValue;
-            });            
+        this.sub = this.route.params.subscribe(params => {
+            this.assetID = +params['assetID'];
+            this.objectDetailService.getAsset(this.assetID).then(res => {
+                if (res.Type == "FusionType") {
+                    this.fusionStuff.getFusionConfigurationsByType(res.TypeID).then(fus => {
+                        this.objectName = fus[0].Name;
+                    })
+                }
+                else this.objectName = res.DisplayValue;
+            });
         });
     }
 
