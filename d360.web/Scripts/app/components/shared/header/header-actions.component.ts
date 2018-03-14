4280e2977ab99848c36ca9599eed116318a02653
Sebastian Abraham <sabraham@infogix.com>
2018-03-14 22:15:31 +0000

    Govern GOV-3371 User profile has take action button that lets you take an action on a user

--- d360.web/Scripts/app/components/shared/header/header-actions.component.ts
+++ d360.web/Scripts/app/components/shared/header/header-actions.component.ts
@@ -35,7 +35,7 @@ export class HeaderActionsComponent {
     private uri = "";
     public notTopArtifact: boolean = true;
     public testuri: string[] = [];
-    public hasRaiseIssueButton: boolean = true;
+    public hasRaiseIssueButton: boolean = false;
     public showShoppingCart: boolean = false;
 
     private routerSub;
@@ -69,11 +69,13 @@ export class HeaderActionsComponent {
                 else {
                     this.notTopArtifact = true;
                 }
-                //dont show raise issue button on raise issue screen or any admin screens or top level artifact screen           
+               
+                //dont show raise issue button on raise issue screen or any admin screens or user profile           
                 this.isAdminUrl = (this.uri || '').toUpperCase().startsWith(SiteUrlHelpers.SITE_URL_ADMIN_ROOT.toUpperCase());
-                this.hasRaiseIssueButton = (!e.urlAfterRedirects.toLowerCase().endsWith('workflow/raiseissue') && !this.isAdminUrl && CompanySettings.DisableIssueManagement != 'true');
-                //this.hasRaiseIssueButton = (!e.urlAfterRedirects.toLowerCase().endsWith('workflow/raiseissue') && !this.isAdminUrl && this.notTopArtifact && CompanySettings.DisableIssueManagement != 'true');
-
+                let isResourceUrl = (this.uri || '').toUpperCase().startsWith(SiteUrlHelpers.SITE_URL_RESOURCE_ROOT.toUpperCase());
+                this.hasRaiseIssueButton = ((!e.urlAfterRedirects.toLowerCase().endsWith('workflow/raiseissue') && !this.isAdminUrl && !isResourceUrl && (CompanySettings.DisableIssueManagement==='false') ) == true);
+                
+                               
                 this.calculateControlWidth();
             }
         });
