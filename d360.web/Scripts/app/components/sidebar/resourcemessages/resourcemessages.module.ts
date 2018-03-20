2044f78842ac44a062f443a5770f6946e8a5a1cf
Jim Hoell <jhoell@infogix.com>
2018-03-20 22:34:17 +0000

    temp work on GOV enhancement

---
+++ d360.web/Scripts/app/components/sidebar/resourcemessages/resourcemessages.module.ts
@@ -0,0 +1,48 @@
+﻿import { NgModule } from '@angular/core';
+import { CommonModule, DeprecatedI18NPipesModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpModule, XHRBackend } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { AuthenticationConnectionBackend } from '../../../authentication-connection-backend';
+
+import {
+    ButtonModule,
+} from 'primeng/primeng';
+
+import { CoreModule } from '../../shared/core.module';
+import { TilesModule } from '../../shared/tiles/tiles.module';
+
+import { ResourceMessagesRoutingModule } from './resourcemessages.routes';
+
+import { ResourceMessagesComponent } from './resourcemessages.component';
+import { ResourceModule } from '../../resource/resource.module';
+
+
+@NgModule({
+    imports: [CommonModule,
+        DeprecatedI18NPipesModule,
+        FormsModule,
+        HttpModule,
+        RouterModule,
+
+        //routing 
+        ResourceMessagesRoutingModule,
+
+        //prime
+        ButtonModule,
+
+        //d3s        
+        CoreModule,
+        TilesModule,
+        ResourceModule
+
+    ],
+    declarations: [
+        ResourceMessagesComponent
+    ],
+    providers: [
+        { provide: XHRBackend, useClass: AuthenticationConnectionBackend },
+    ]
+})
+export class ResourceMessagesModule { }
---
+++ d360.web/Scripts/app/components/sidebar/resourcemessages/resourcemessages.routes.ts
@@ -0,0 +1,13 @@
+﻿import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ResourceMessagesComponent } from './resourcemessages.component';
+
+const routes: Routes = [
+    { path: ':resourceID', component: ResourceMessagesComponent},
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
+})
+export class ResourceMessagesRoutingModule { }
