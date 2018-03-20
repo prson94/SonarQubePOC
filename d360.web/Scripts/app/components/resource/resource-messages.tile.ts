a6e6b80c1af80e9386ee4f689a778cc32774785a
Jim Hoell <jhoell@infogix.com>
2018-03-20 18:47:05 +0000

    temp work on GOV enhancement

---
+++ d360.web/Scripts/app/components/resource/resource-messages.tile.ts
@@ -0,0 +1,90 @@
+﻿import { Component, Input, OnInit, OnChanges } from '@angular/core';
+import { ResourcesService } from '../../services/resources.service';
+import { Resource, CountObject } from '../../models/resource.model';
+import { BaseComponent } from '../shared/base.component';
+
+declare var CurrentResourceID;
+
+@Component({
+    selector: 'd3s-resource-following-tile',
+    template: `
+                <header *ngIf="isMe">
+                    Items You Follow
+                    <d3s-tile-actions hasExport="true" (exportClick)="export()" hasFilterMode="true" [(filterMode)]="showFilter"></d3s-tile-actions>    
+                </header>
+                <header *ngIf="!isMe">
+                    Items {{resource?.FirstName}} Follows
+                    <d3s-tile-actions hasExport="true" (exportClick)="export()" hasFilter="true" [(filterMode)]="showFilter"></d3s-tile-actions>      
+                </header>
+                <d3s-loading [isLoading]="isLoading"></d3s-loading>      
+                <div *ngIf="!isLoading" class="row">
+                    <div class="col l3 s12 relationship-container">
+                        <div class="row relationship" *ngFor="let r of items; let i = index" [ngClass]="{'active' : isSelected(r)}" (click)="select(r)">
+                            <div class="col s10 name" [title]="r.Type | technicalNameToDisplayValue">{{r.TypeName}}</div>
+                            <div class="col s2 count center" [ngClass]="{'empty-count': r.Count == 0, 'count': r.Count != 0}">{{r.Count}}</div>
+                        </div>                        
+                    </div>
+                    <div class="col l9 s12">       
+                        <d3s-resource-following-grid-tile *ngIf="selected != null" [simpleFilter]="showFilter" [resourceId]="resourceId" [objectType]="selected.Type" [objectId]="selected.TypeID"></d3s-resource-following-grid-tile>
+                    </div>                    
+                </div>
+`
+    ,
+    providers: [ResourcesService]
+})
+
+export class ResourceFollowingTile extends BaseComponent implements OnChanges {
+    @Input() resourceId: any = 0;
+    @Input() resource: Resource = null;
+    private items: CountObject[] = new Array<CountObject>();
+    private selected: CountObject;
+
+    showFilter = true;
+    isLoading = false;
+    isMe = false;
+
+    constructor(private resourcesService: ResourcesService) {
+        super();
+    }
+
+    ngOnChanges() {
+        this.load();
+    }
+
+    isSelected(item: any) {
+        return (item == this.selected);
+    }
+
+    select(item: any) {
+        this.selected = item;
+    }
+
+    load() {
+        this.isLoading = true;
+
+        if (this.resource != null)
+            this.resourceId = this.resource.ID;
+
+        this.isMe = (this.resourceId == CurrentResourceID);
+
+        this.resourcesService.getFollowingBreakdownByResource(this.resourceId)
+            .then(r => {
+                this.items = r;
+                if (this.items && this.items.length > 0)
+                    this.select(this.items[0]);
+
+                if (this.resource == null)
+                    this.resourcesService.getResource(this.resourceId)
+                        .then(res => {
+                            this.resource = res;
+                            this.isLoading = false;
+                        });
+                else
+                    this.isLoading = false;
+            });
+    }
+
+    export() {
+        this.resourcesService.exportFollowingByResourceByType(this.resourceId, this.selected.Type, this.selected.TypeID);
+    }
+}
