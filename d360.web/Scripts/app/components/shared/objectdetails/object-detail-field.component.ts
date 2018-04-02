8fb269d773b7e589bc7c5f04def84cfb545e5d0c
Kenneth McNamee <kmcnamee@infogix.com>
2018-04-02 19:02:26 +0000

    gov-3509 owner / parent column no longer showing on artifact detail page

--- d360.web/Scripts/app/components/shared/objectdetails/object-detail-field.component.ts
+++ d360.web/Scripts/app/components/shared/objectdetails/object-detail-field.component.ts
@@ -24,16 +24,21 @@ import { Router } from '@angular/router';
                     <i *ngIf="(field.Value || '').toUpperCase() == 'TRUE'" class="fa fa-check enabled" title="True"></i>
                     <i *ngIf="(field.Value || '').toUpperCase() == 'FALSE'" class="fa fa-times disabled" title="False"></i>                
                 </div>
-                <div *ngIf="field.Type == DetailFieldType.Field && field.Name != 'Email' && field.DataType != 'date' && field.DataType != 'text' && field.DataType != 'bool'" class="FieldDisplayContent" [innerHtml]="field.Value | safeHtml" [ngStyle]="{'font-weight':(field.Name == 'Name' ? 'bold':'')}"></div>
-                <div *ngIf="field.Type == DetailFieldType.Tooltip" class="FieldDisplayContent">                    
-                    <d3s-lookup-tooltip *ngIf="field.TooltipUrl;else noLinkTooltip" [objectType]="field.TooltipType" [objectId]="field.TooltipID">
-                        <a (click)="navigate(field.TooltipUrl)" [innerHtml]="field.Value"></a>                        
-                    </d3s-lookup-tooltip>
-                    <ng-template #noLinkTooltip>
-                        <d3s-lookup-tooltip  [objectType]="field.TooltipType" [objectId]="field.TooltipID">
-                            <span [innerHtml]="field.Value"></span>
+                <div *ngIf="field.Type == DetailFieldType.Field && field.Name != 'Email' && field.DataType != 'date' && field.DataType != 'text' && field.DataType != 'bool'" class="FieldDisplayContent" [innerHtml]="field.Value | safeHtml" [ngStyle]="{'font-weight':(field.Name == 'Name' ? 'bold':'')}"></div>            
+                <div *ngIf="field.Type == DetailFieldType.Tooltip" class="FieldDisplayContent">                                        
+                    <d3s-preview-tooltip *ngIf="field.TooltipContext == 'Preview';else listTooltip"  [objectType]="field.TooltipType" [objectId]="field.TooltipID">
+                        <a (click)="navigate(field.TooltipUrl)">{{field.Value}}</a>                        
+                    </d3s-preview-tooltip>
+                    <ng-template #listTooltip>
+                        <d3s-lookup-tooltip *ngIf="field.TooltipUrl;else noLinkTooltip" [objectType]="field.TooltipType" [objectId]="field.TooltipID">
+                            <a (click)="navigate(field.TooltipUrl)" [innerHtml]="field.Value"></a>                        
                         </d3s-lookup-tooltip>
-                    </ng-template>                    
+                        <ng-template #noLinkTooltip>
+                            <d3s-lookup-tooltip  [objectType]="field.TooltipType" [objectId]="field.TooltipID">
+                                <span [innerHtml]="field.Value"></span>
+                            </d3s-lookup-tooltip>
+                        </ng-template>                    
+                    </ng-template>
                 </div>
                 <div *ngIf="field.Type == DetailFieldType.Lookup">
                     <d3s-dynamic-lookup-grid *ngIf="field.Data && field.Data.Values && field.Data.Values.length > 0" [data]="field.Data" [hideHeader]="field.HideHeader" [hideFooter]="field.HideFooter" [hideFilter]="field.HideFilter"></d3s-dynamic-lookup-grid>
