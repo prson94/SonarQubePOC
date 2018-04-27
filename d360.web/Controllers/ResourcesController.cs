d5c34c49467d4af6cd2f9f28d2285a0693e94c8d
Sebastian Abraham <sabraham@infogix.com>
2018-04-27 14:28:19 +0000

    Govern GOV-3680 Hover over model type change asset type to model type

--- d360.web/Controllers/ResourcesController.cs
+++ d360.web/Controllers/ResourcesController.cs
@@ -648,6 +648,7 @@ order by A.ID, FT.SortOrder", new { id, attribute });
                 List<FieldTooltipValueModel> res = new List<FieldTooltipValueModel>();
                 string desc = "";
                 string dispName = "";
+                string typeName = "";
 
                 if (accessCount <= 0)
                 {
@@ -676,12 +677,13 @@ order by A.ID, FT.SortOrder", new { id, attribute });
                     desc = Company.Query<string>(descSql, new { ty = objectType, obj = objectID }).FirstOrDefault();
 
                      dispName = det != null ? det.Name : "";
+                     typeName = det != null ? det.Type : "";
 
                     if (objectType == "TaxonomyType")
                     {
                         var desSql = @"Select Description from AssetType where objectid =@id and object= @ty";
                         desc = Company.Query<string>(desSql, new { ty = objectType, id = objectID }).FirstOrDefault();
-                       
+                        typeName = "Model Type";
                     }
 
                         if (objectType == "Fusion")
@@ -694,7 +696,7 @@ order by A.ID, FT.SortOrder", new { id, attribute });
 
                 }
 
-                return Json(new { ShowTooltip = show, AssetID = (det != null ? det.AssetID : -1), DisplayName = dispName, TypeName = (det != null ? det.TypeName : ""), Url = (det != null ? $"/{det.Url}" : ""), FieldValues = res, Description = desc }, JsonRequestBehavior.AllowGet);
+                return Json(new { ShowTooltip = show, AssetID = (det != null ? det.AssetID : -1), DisplayName = dispName, TypeName = typeName, Url = (det != null ? $"/{det.Url}" : ""), FieldValues = res, Description = desc }, JsonRequestBehavior.AllowGet);
             }
             catch (Exception ex)
             {
