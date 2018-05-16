f7d859d2fa6077b0b1ef30c53049bfccac972baa
Michael Donato <mdonato@infogix.com>
2018-05-16 14:16:56 +0000

    -added OBJECT_TYPE token to workflow editor (GOV-3812)

--- d360.web/Scripts/app/components/shared/diagram/workflow/workflow-template-tool.component.ts
+++ d360.web/Scripts/app/components/shared/diagram/workflow/workflow-template-tool.component.ts
@@ -28,6 +28,7 @@ export class WorkflowTemplateToolComponent implements OnInit, AfterViewChecked {
 
     private defaultFields = [
         { value: '[OBJECT_NAME]', label: 'Object Name' },
+        { value: '[OBJECT_TYPE]', label: 'Object Type' },
         { value: '[SCORE]', label: 'Object Score' },
         { value: '[WORKFLOW_INITIATOR]', label: 'Workflow Initiator Name' },
         { value: '[ACTION_DETAILS]', label: 'Action Details' },
