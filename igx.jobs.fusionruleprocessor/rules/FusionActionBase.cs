aa7ed96f8f3b7b26cc5177e62a443163029343a3
Kenneth McNamee <kmcnamee@infogix.com>
2018-05-25 19:35:56 +0000

    GOV-3897 - fusion rule promotion step timeout was too low.

--- igx.jobs.fusionruleprocessor/rules/FusionActionBase.cs
+++ igx.jobs.fusionruleprocessor/rules/FusionActionBase.cs
@@ -15,7 +15,7 @@ namespace igx.jobs.fusionruleprocessor
             Stats = new FusionRuleStepStatistics();
         }
 
-        public static int EXECUTION_TIMEOUT = 300;
+        public static int EXECUTION_TIMEOUT = 600;
         public FusionRuleStepModel Step { get; set; }
         public int CompanyId { get; set; }
         public d360.core.entities.FusionRule Rule { get; set; }
