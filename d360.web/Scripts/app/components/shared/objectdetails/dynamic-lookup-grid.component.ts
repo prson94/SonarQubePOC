cb5ca136d3ddcb30f4b4e62d8ffbbe76d1e7388f
Sebastian Abraham <sabraham@infogix.com>
2018-06-05 18:26:39 +0000

    Govern GOV-3972 Artifact Details page: Ownership grid shows null in context colmn in IE

--- d360.web/Scripts/app/components/shared/objectdetails/dynamic-lookup-grid.component.ts
+++ d360.web/Scripts/app/components/shared/objectdetails/dynamic-lookup-grid.component.ts
@@ -37,8 +37,8 @@ import { BaseComponent } from '../base.component';
                                                     <a (click)="navigate(item[column.urlfield])" [innerText]="item[column.datafield]"></a>
                                                 </d3s-preview-tooltip>
                                             </span>
-                                            <span *ngSwitchCase="'html'" style="display:block; word-wrap:break-word !important" [innerHtml]="item[column.datafield] || ''"></span>
-                                            <span *ngSwitchDefault style="display:block; word-wrap:break-word !important" [innerText]="item[column.datafield] || ''"></span>
+                                            <span *ngSwitchCase="'html'" style="display:block; word-wrap:break-word !important" [innerHtml]="item[column.datafield]"></span>
+                                            <span *ngSwitchDefault style="display:block; word-wrap:break-word !important" [innerText]="item[column.datafield]"></span>
                                         </div>
                              </ng-template>
                         </p-column>                                                                                         
@@ -74,7 +74,7 @@ export class DynamicLookupGridComponent extends BaseComponent implements OnInit 
                     v[c.datafield] = this.formatAsNumber(v[c.datafield]);
                 });
             }
-            if (c.type == 'string' || c.type == 'preview' || c.type == 'lookup') {
+            if (c.type == 'string' || c.type == 'preview' || c.type == 'lookup' || c.type=='html') {
                 this.data.Values.forEach(v => {
                     if (v[c.datafield] === null) {
                         v[c.datafield] = ''; //prevent IE from displaying 'null'
