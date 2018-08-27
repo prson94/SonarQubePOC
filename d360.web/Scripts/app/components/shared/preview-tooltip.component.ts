95c056e0ab2673f79f2d4b4e909a38f0e5bb018b
Kenneth McNamee <kmcnamee@infogix.com>
2018-08-27 21:30:09 +0000

    GOV-5299 - relationship tooltip looks ugly

--- d360.web/Scripts/app/components/shared/preview-tooltip.component.ts
+++ d360.web/Scripts/app/components/shared/preview-tooltip.component.ts
@@ -13,7 +13,7 @@ import { TooltipSingletonService } from '../../services/tooltip-singleton.servic
                     <div *ngIf="innerHtmlContent != null" [innerHtml]="innerHtmlContent"></div>
                     <div class="tooltip-child tooltip-panel" #tip>
                         <h3 style="positon: relative"><a *ngIf="data && data.Url" [routerLink]="data?.Url">{{data?.DisplayName}}</a> 
-                                <small *ngIf="data && !data.Url && data.DisplayName" style="background-color: #fff; float:left;font-size:80%;">{{data?.DisplayName}}</small>
+                                <span *ngIf="data && !data.Url && data.DisplayName" style="background-color: #fff;">{{data?.DisplayName}}</span>
                                 <small *ngIf="data && data.TypeName" style="background-color: #fff; float:right;font-size:65%;">{{data.TypeName}}</small></h3>
                         <div>&nbsp;</div>
                         <div *ngFor="let field of data?.Levels; let ii=index">
