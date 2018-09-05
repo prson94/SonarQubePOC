b355c68477cbc3523614612669e7d1c5cc5e16ed
Michael Pappas <mpappas@infogix.com>
2018-09-05 22:52:52 +0000

    GOV-5373 - fix for trailing .0 on IGC values.

--- igx.jobs.igc/Program.cs
+++ igx.jobs.igc/Program.cs
@@ -453,7 +453,7 @@ from		(
                                         ExecutionID = executionAssetType.ExecutionID,
                                         SynchedAssetTypeID = executionAssetType.SynchedAssetTypeID,
                                         SourceID = obj["_id"].Value<string>(),
-                                        RawObject = obj.ToString(Formatting.None)
+                                        RawObject = JsonConvert.SerializeObject(obj, Formatting.None, new DecimalJsonConverter())//obj.ToString(Formatting.None)
                                     };
                                     list.Add(executionAsset);
                                 }
@@ -951,5 +951,62 @@ from		(
         }
 
         #endregion
+    }
+
+    /// <summary>
+    /// GOV-5373: Removes trailing .0 on any inferred numbers.
+    /// </summary>
+    internal class DecimalJsonConverter : JsonConverter
+    {
+        public DecimalJsonConverter()
+        {
+        }
+
+        public override bool CanRead
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException("Unnecessary because CanRead is false. The type will skip the converter.");
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(decimal) || objectType == typeof(float) || objectType == typeof(double));
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (DecimalJsonConverter.IsWholeValue(value))
+            {
+                writer.WriteRawValue(JsonConvert.ToString(Convert.ToInt64(value)));
+            }
+            else
+            {
+                writer.WriteRawValue(JsonConvert.ToString(value));
+            }
+        }
+
+        private static bool IsWholeValue(object value)
+        {
+            if (value is decimal)
+            {
+                decimal decimalValue = (decimal)value;
+                int precision = (Decimal.GetBits(decimalValue)[3] >> 16) & 0x000000FF;
+                return precision == 0;
+            }
+            else if (value is float || value is double)
+            {
+                double doubleValue = (double)value;
+                return doubleValue == Math.Truncate(doubleValue);
+            }
+
+            return false;
+        }
     }
 }
