346a237af9f0d1e921ecce8c5501edec76e9a1ec
Michael Pappas <mpappas@infogix.com>
2018-10-16 13:52:19 -0400

    GOV-5740

---
+++ d360.database.company/Releases/2018.10.12/Drops.sql
@@ -0,0 +1,37 @@
+﻿DROP TABLE [dbo].[AssetDataQualityProperty]
+GO;
+
+DROP TABLE [dbo].[AssetDataQualityImplementationResultQualifier]
+GO;
+
+DROP TABLE [dbo].[AssetDataQualityImplementationResultFusion]
+GO;
+
+DROP TABLE [dbo].[AssetDataQualityImplementationResult]
+GO;
+
+DROP TABLE [dbo].[AssetDataQualityDimension]
+GO;
+
+DROP TABLE [dbo].[AssetDataQualityImplementationQualifierType]
+GO;
+
+DROP TABLE [dbo].[AssetDataQualityImplementation]
+GO;
+
+DROP TABLE [dbo].[AssetDisplayFieldTypes]
+GO;
+
+DROP TABLE [dbo].[AssetDisplayFormatFieldTypes]
+GO;
+
+DROP TABLE [dbo].[AssetScheduleItem]
+GO;
+
+DROP TABLE [dbo].[AssetSchedule]
+GO;
+
+DROP TABLE [dbo].[AssetTypeQuery]
+GO;
+
+--ALTER procedure [utility].[ClearDatabase]
