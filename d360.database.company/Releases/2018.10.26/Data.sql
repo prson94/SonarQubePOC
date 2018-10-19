8f3cf3edf054e54e64bd114078ba570ed13a8ce2
Michael Pappas <mpappas@infogix.com>
2018-10-19 16:24:41 -0400

    GOV-5730 - fixed the procedure that renders the history chart to NOT go beyond today's date.

---
+++ d360.database.company/Releases/2018.10.26/Data.sql
@@ -0,0 +1,35 @@
+﻿if not exists (select 1 from [predicate] where name = 'Asset Owned For' and [Type] = 7 and IsSystem = 1)
+begin
+	insert into [predicate] values('Asset Owned For','Asset Owned By',7,1,0)
+end
+GO;
+
+if not exists (select 1 from [predicate] where name = 'Validates' and [Type] = 7 and IsSystem = 1)
+begin
+    insert into [predicate] values('Validates','Is Validated By',7,1,0)
+end
+GO;
+
+
+update ruleresultqualifiertype set name = name +cast(id as varchar(20)) where id in (
+SELECT max(id)
+FROM ruleresultqualifiertype
+GROUP BY ruleimplementationid, name
+HAVING COUNT(1) > 1)
+
+ALTER TABLE ruleresultqualifiertype ADD CONSTRAINT DF_RuleResultQualifierType_RuleImplementationID_Name UNIQUE(RuleImplementationID, Name)
+
+-- BEGIN GOV-5718 DUPLICATED COMMENTS DUE TO DUPLICATED USERS IN ASSET TABLE
+-- delete duplicated resources in asset table
+with R as (
+select *, row_number() over(partition by [object], objectid order by (select null)) as rn
+from asset where [object] = 'Resource'
+)
+delete R
+where rn > 1;
+go
+
+-- add constraint
+ALTER TABLE Asset ADD CONSTRAINT UC_Asset_Object_ObjectID UNIQUE ([Object],[ObjectID]);
+go
+-- END GOV-5718 DUPLICATED COMMENTS DUE TO DUPLICATED USERS IN ASSET TABLE
---
+++ d360.database.company/Releases/2018.10.26/Drops.sql
@@ -0,0 +1,41 @@
+﻿DROP TABLE [dbo].[AssetDataQualityProperty]
+GO;
+
+DROP TABLE [dbo].[AssetDataQualityImplementationResultQualifier]
+GO;
+
+DROP TABLE [dbo].[AssetDataQualityImplementationResultFusion]
+GO;
+
+DROP TABLE [dbo].[AssetDataQualityImplementationResult]
+GO;
+
+DROP TABLE [dbo].[AssetDataQualityDimension]
+GO;
+
+DROP TABLE [dbo].[AssetDataQualityImplementationQualifierType]
+GO;
+
+DROP TABLE [dbo].[AssetDataQualityImplementation]
+GO;
+
+DROP TABLE [dbo].[AssetDisplayFieldTypes]
+GO;
+
+DROP TABLE [dbo].[AssetDisplayFormatFieldTypes]
+GO;
+
+DROP TABLE [dbo].[AssetScheduleItem]
+GO;
+
+DROP TABLE [dbo].[AssetSchedule]
+GO;
+
+DROP TABLE [dbo].[AssetTypeQuery]
+GO;
+
+--ALTER procedure [utility].[ClearDatabase]
+
+
+--run scan with RedGate to capture the rest.
+--Also ensure that temporal storage on Asset is removed across all DBs.
