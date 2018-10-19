0d64f160e2e14365b2814d75dbaf46e71f7c04df
Michael Pappas <mpappas@infogix.com>
2018-10-18 21:33:44 -0400

    GOV-5765

--- d360.web/App_Start/WebApiConfig.cs
+++ d360.web/App_Start/WebApiConfig.cs
@@ -58,7 +58,11 @@ namespace d360.web
             var versionInfoBuilder = new Action<VersionInfoBuilder>(info => {
                 foreach (var group in apiExplorer.ApiDescriptions)
                 {
-                    info.Version(group.Name, $"Govern REST API v{group.ApiVersion}");
+                    info
+                    .Version(group.Name, $"Govern REST API v{group.ApiVersion}")
+                    .Description(@"
+Below you will find a list of various REST services to access information or to modify content within your Govern environment. 
+When modifying content on assets, please be aware that you will need to use the API Names on field definitions when updating or referencing content.");
                 }
             });
 
@@ -75,50 +79,50 @@ namespace d360.web
                     // If you want the output Swagger docs to be indented properly, enable the "PrettyPrint" option.
                     c.PrettyPrint();
 
-                // If your API has multiple versions, use "MultipleApiVersions" instead of "SingleApiVersion".
-                // In this case, you must provide a lambda that tells Swashbuckle which actions should be
-                // included in the docs for a given API version. Like "SingleApiVersion", each call to "Version"
-                // returns an "Info" builder so you can provide additional metadata per API version.
-                c.MultipleApiVersions(versionSupportResolver, versionInfoBuilder);
+                    // If your API has multiple versions, use "MultipleApiVersions" instead of "SingleApiVersion".
+                    // In this case, you must provide a lambda that tells Swashbuckle which actions should be
+                    // included in the docs for a given API version. Like "SingleApiVersion", each call to "Version"
+                    // returns an "Info" builder so you can provide additional metadata per API version.
+                    c.MultipleApiVersions(versionSupportResolver, versionInfoBuilder);
 
-                //c.MultipleApiVersions(
-                // (apiDesc, version) =>
-                // {
-                //     var path = apiDesc.RelativePath.Split('/');
-                //     var pathVersion = path[1];
+                    //c.MultipleApiVersions(
+                    // (apiDesc, version) =>
+                    // {
+                    //     var path = apiDesc.RelativePath.Split('/');
+                    //     var pathVersion = path[1];
 
-                //     return CultureInfo.InvariantCulture.CompareInfo.IndexOf(pathVersion, version, CompareOptions.IgnoreCase) >= 0;
-                // },
-                // vc =>
-                // {
-                //     vc.Version("2.0", "Data3Sixty Govern API 2.0");
-                //     vc.Version("1.0", "Data3Sixty Govern API 1.0");
-                // });
+                    //     return CultureInfo.InvariantCulture.CompareInfo.IndexOf(pathVersion, version, CompareOptions.IgnoreCase) >= 0;
+                    // },
+                    // vc =>
+                    // {
+                    //     vc.Version("2.0", "Data3Sixty Govern API 2.0");
+                    //     vc.Version("1.0", "Data3Sixty Govern API 1.0");
+                    // });
 
-                 // You can use "BasicAuth", "ApiKey" or "OAuth2" options to describe security schemes for the API.
-                 // See https://github.com/swagger-api/swagger-spec/blob/master/versions/2.0.md for more details.
-                 // NOTE: These only define the schemes and need to be coupled with a corresponding "security" property
-                 // at the document or operation level to indicate which schemes are required for an operation. To do this,
-                 // you'll need to implement a custom IDocumentFilter and/or IOperationFilter to set these properties
-                 // according to your specific authorization implementation
-                 // NOTE: You must also configure 'EnableApiKeySupport' below in the SwaggerUI section
-                 c.ApiKey("ApiKey")
-                     .Description("API Key Authentication (i.e.   KEY;SECRET)")
-                     .Name("Authorization")
-                     .In("header");
+                     // You can use "BasicAuth", "ApiKey" or "OAuth2" options to describe security schemes for the API.
+                     // See https://github.com/swagger-api/swagger-spec/blob/master/versions/2.0.md for more details.
+                     // NOTE: These only define the schemes and need to be coupled with a corresponding "security" property
+                     // at the document or operation level to indicate which schemes are required for an operation. To do this,
+                     // you'll need to implement a custom IDocumentFilter and/or IOperationFilter to set these properties
+                     // according to your specific authorization implementation
+                     // NOTE: You must also configure 'EnableApiKeySupport' below in the SwaggerUI section
+                     c.ApiKey("ApiKey")
+                         .Description("API Key Authentication (i.e.   KEY;SECRET)")
+                         .Name("Authorization")
+                         .In("header");
 
-                 // If you annotate Controllers and API Types with
-                 // Xml comments (http://msdn.microsoft.com/en-us/library/b2s063f7(v=vs.110).aspx), you can incorporate
-                 // those comments into the generated docs and UI. You can enable this by providing the path to one or
-                 // more Xml comment files.
-                 c.IncludeXmlComments($@"{System.AppDomain.CurrentDomain.BaseDirectory}\App_Data\Documentation.XML");
+                     // If you annotate Controllers and API Types with
+                     // Xml comments (http://msdn.microsoft.com/en-us/library/b2s063f7(v=vs.110).aspx), you can incorporate
+                     // those comments into the generated docs and UI. You can enable this by providing the path to one or
+                     // more Xml comment files.
+                     c.IncludeXmlComments($@"{System.AppDomain.CurrentDomain.BaseDirectory}\App_Data\Documentation.XML");
 
-                 // In accordance with the built in JsonSerializer, Swashbuckle will, by default, describe enums as integers.
-                 // You can change the serializer behavior by configuring the StringToEnumConverter globally or for a given
-                 // enum type. Swashbuckle will honor this change out-of-the-box. However, if you use a different
-                 // approach to serialize enums as strings, you can also force Swashbuckle to describe them as strings.
-                 c.DescribeAllEnumsAsStrings();
-             })
+                     // In accordance with the built in JsonSerializer, Swashbuckle will, by default, describe enums as integers.
+                     // You can change the serializer behavior by configuring the StringToEnumConverter globally or for a given
+                     // enum type. Swashbuckle will honor this change out-of-the-box. However, if you use a different
+                     // approach to serialize enums as strings, you can also force Swashbuckle to describe them as strings.
+                     c.DescribeAllEnumsAsStrings();
+                 })
                 .EnableSwaggerUi(c => {
 
                     // Use the "DocumentTitle" option to change the Document title.
@@ -126,6 +130,9 @@ namespace d360.web
                     c.DocumentTitle("Data3Sixty Govern API Console");
 
                     c.EnableDiscoveryUrlSelector();
+
+                    c.CustomAsset("index", typeof(WebApiConfig).Assembly, "d360.web.Content.Swagger.index.html");
+                    c.InjectStylesheet(typeof(WebApiConfig).Assembly, "d360.web.Content.Swagger.swagger.css");
 
                     // Use the "InjectStylesheet" option to enrich the UI with one or more additional CSS stylesheets.
                     // The file must be included in your project as an "Embedded Resource", and then the resource's
