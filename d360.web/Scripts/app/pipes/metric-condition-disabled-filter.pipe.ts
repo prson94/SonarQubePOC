77d09b3b991ac54bf487f319e4df3a8e9e547e9f
Michael Pappas <mpappas@infogix.com>
2018-12-04 16:10:09 -0500

    Adding missing pipe file TS

---
+++ d360.web/Scripts/app/pipes/metric-condition-disabled-filter.pipe.ts
@@ -0,0 +1,9 @@
+﻿import { Pipe, PipeTransform, Injectable } from '@angular/core';
+import { MetricFieldTypeViewModel } from '../models/metrics.model';
+
+@Pipe({ name: 'metricConditionDisabledFilter' })
+export class MetricConditionDisabledFilterPipe implements PipeTransform {
+    transform(items: MetricFieldTypeViewModel[]): any {
+        return items.filter(item => item.Disabled === false);
+    }
+}
