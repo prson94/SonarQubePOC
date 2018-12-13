6ea94fdc9ac847390094c14fa7501ba03d8ea014
Kenneth McNamee <kmcnamee@infogix.com>
2018-12-13 09:07:52 -0500

    new markit webjob

---
+++ d360.model/CompanyContext_MarkitLineage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d360.model
+{
+    partial class CompanyContext : BaseContext
+    {
+        
+        public async Task GenerateMarkitBusinessLineage()
+        {
+            // read the markit table
+        }
+    }
+}
