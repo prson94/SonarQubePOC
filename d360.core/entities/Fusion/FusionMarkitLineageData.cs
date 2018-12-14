56cfe107de6c40b8b1ecd15686bec06edcde751d
Kenneth McNamee <kmcnamee@infogix.com>
2018-12-14 12:53:10 -0500

    initial

---
+++ d360.core/entities/Fusion/FusionMarkitLineageData.cs
@@ -0,0 +1,30 @@
+﻿using d360.core.entities.Contracts;
+using System;
+
+namespace d360.core.entities
+{
+    public class FusionMarkitLineageData : BaseIntObject, IIntObject
+    {
+        public int? MapRuleItemID { get; set; }
+        public int? ParentID { get; set; }
+        public int? UltimateParentID { get; set; }
+        public int? Level { get; set; }
+        public int? SourceFusionAttributeID { get; set; }
+        public int? SourceFusionAttributeTypeID { get; set; }
+        public string SourceObject { get; set; }
+        public string SourceParentObject { get; set; }
+        public int? SourceParentObjectFusionAttributeID { get; set; }
+        public int? SourceParentObjectFusionAttributeTypeID { get; set; }
+        public int? TargetFusionAttributeID { get; set; }
+        public int? TargetFusionAttributeTypeID { get; set; }
+        public string TargetObject { get; set; }
+        public string TargetParentObject { get; set; }
+        public int? TargetParentObjectFusionAttributeID { get; set; }
+        public int? TargetParentObjectFusionAttributeTypeID { get; set; }
+        public string Source { get; set; }
+        public int? SourceID { get; set; }
+        public string Target { get; set; }
+        public int? TargetID { get; set; }
+        public DateTime UpdatedOn { get; set; }
+    }
+}
--- d360.model/CompanyContext_MarkitLineage.cs
+++ d360.model/CompanyContext_MarkitLineage.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.ApplicationInsights;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 
 namespace d360.model
 {
@@ -11,9 +13,74 @@ namespace d360.model
         
         public async Task GenerateMarkitBusinessLineage()
         {
-            // read the markit table
+            TelemetryClient client = new TelemetryClient();
 
-            // 
+            //check if anything has changed since last run
+            if (!(await MarkitRequiresRun()))
+            {
+                return;
+            }
+
+            client.TrackEvent($"Markit data requires processing on company {CurrentCompanyID}");
+
+            var maps = await LoadMarkitMapRawData();
+
+            client.TrackEvent($"Loaded {maps.Count()} Markit Map Records");
+            
+            // parse through the maps 
+
+
+            // save the results
+
+        }
+
+        private async Task<bool> MarkitRequiresRun()
+        {
+            // is htere any data? and what is the date its for            
+            var dataDate = await (Database.Connection.QueryFirstOrDefaultAsync<DateTime>("select max(updatedon) from [fusion].MarkitLineageData"));
+
+            if (dataDate == DateTime.MinValue)
+                return false;
+
+            // compare max last run to date in                
+            var lastRun = await Database.Connection.QueryFirstOrDefaultAsync<DateTime>("select max(completedOn) from [fusion].MarkitLineageHistory");
+
+            // has data but never run
+            if (lastRun == DateTime.MinValue)
+                return true;
+
+            if (dataDate > lastRun) return true;
+
+            return false;
+        }
+
+        private async Task<IEnumerable<core.entities.FusionMarkitLineageData>> LoadMarkitMapRawData()
+        {
+            var sql = @"SELECT [ID]
+                          ,[MapRuleItemID]
+                          ,[ParentID]
+                          ,[UltimateParentID]
+                          ,[Level]
+                          ,[SourceFusionAttributeID]
+                          ,[SourceFusionAttributeTypeID]
+                          ,[SourceObject]
+                          ,[SourceParentObject]
+                          ,[SourceParentObjectFusionAttributeID]
+                          ,[SourceParentObjectFusionAttributeTypeID]
+                          ,[TargetFusionAttributeID]
+                          ,[TargetFusionAttributeTypeID]
+                          ,[TargetObject]
+                          ,[TargetParentObject]
+                          ,[TargetParentObjectFusionAttributeID]
+                          ,[TargetParentObjectFusionAttributeTypeID]
+                          ,[Source]
+                          ,[SourceID]
+                          ,[Target]
+                          ,[TargetID]
+                          ,[UpdatedOn]
+                      FROM [fusion].[MarkitLineageData]";
+
+            return await QueryAsync<core.entities.FusionMarkitLineageData>(sql);
         }
     }
 }
