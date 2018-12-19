15b57cad0033be900977ec6e7d195a313c6275fa
Michael Pappas <mpappas@infogix.com>
2018-12-19 17:44:18 -0500

    removed junk in reporting layer

--- igx.jobs.reportlayer/Program.cs
+++ igx.jobs.reportlayer/Program.cs
@@ -131,7 +131,6 @@ namespace igx.jobs.reportlayer
                     {
                         using (var companyConnection = CompanyConnectionUtils.GetCompanyConnection(c.CompanyID, c.Server, c.Username, c.Password))
                         {
-                            CoreFunction.AITrackEvent(functionName, "Pappas - test", new Dictionary<string, string> { { "Environment", "junk" }, { "ReportLayer", "awesome" } }, c.CompanyID, null);
                             companyConnection.OpenWithRetry(RetryPolicy.DefaultProgressive);
 
                             var selectSql = "";
