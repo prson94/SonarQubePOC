f5403aec503c1025d323afb18c086d4450c3d466
seban200 <sabraham@infogix.com>
2018-12-21 09:40:44 -0500

    GOV-6261 Move Client side error endpoint to a v2 controller out of generic 'ApiController'

---
+++ d360.web/Controllers/V2/ErrorsController.cs
@@ -0,0 +1,57 @@
+﻿using d360.core.exceptions;
+using d360.model;
+using d360.web.Models;
+using Microsoft.Web.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace d360.web.Controllers.V2
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [
+    ApiVersion("2.0"),
+    RoutePrefix("api/v{version:apiVersion}/errors"), Authorize
+    ]
+    public class ErrorsController : BaseApiController
+    {
+        #region DI
+
+        public ErrorsController(CommunityContext community, CompanyContext company)
+            : base(community, company)
+        {
+
+        }
+
+        #endregion
+
+        #region LogClientError
+
+        /// <summary>
+        /// This logs the client error on server side
+        /// </summary>
+        /// <param name="model">Error model</param>
+        /// <returns>Returns Http Status code 200 if logged successfully; else return http status code 500 with error message</returns>
+        [HttpPost, Route("log/clienterror")]
+        public HttpResponseMessage SaveClientError(ClientErrorModel model)
+        {
+            try
+            {
+                IDictionary<string, string> properties = new Dictionary<string, string>();
+                properties.Add("name", model.Name);
+                properties.Add("stacktrace", model.Stack);
+                this.SendException(new ClientSideException(model.Message), properties);
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+        #endregion
+    }
+}
