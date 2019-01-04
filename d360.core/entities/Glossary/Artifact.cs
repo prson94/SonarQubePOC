77147a863d8ebda65f4efa02124d0cc3edf17d9a
Brian Howard <bhoward@infogix.com>
2019-01-04 16:20:28 +0000

    Removing the children collection that was causing an error

--- d360.core/entities/Glossary/Artifact.cs
+++ d360.core/entities/Glossary/Artifact.cs
@@ -21,9 +21,6 @@ namespace d360.core.entities
         [IgnoreDataMember]
         public virtual ArtifactType ArtifactType { get; set; }
 
-        [ IgnoreDataMember]
-        public virtual ICollection<Artifact> Children { get; set; }
-
         [IgnoreDataMember]
         public virtual ICollection<Fusion> OwnedFusions { get; set; }
 
