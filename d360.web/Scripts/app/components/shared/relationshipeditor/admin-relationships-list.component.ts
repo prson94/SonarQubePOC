c2e4a5604ac7f73666e4005d4b66dd8872331a38
Brian Howard <bhoward@infogix.com>
2019-01-11 12:44:15 +0000

    Adding the UID popup to the relationships page

--- d360.web/Scripts/app/components/shared/relationshipeditor/admin-relationships-list.component.ts
+++ d360.web/Scripts/app/components/shared/relationshipeditor/admin-relationships-list.component.ts
@@ -37,12 +37,14 @@ import { BaseComponent } from '../../shared/base.component';
                                     <th style="width: 40px"></th>
                                     <th style="width: 40px"></th>
                                     <th style="width: 40px"></th>
+                                    <th style="width: 40px"></th>
                                 </tr>
                                 <tr [hidden]="showSimpleFilter">
                                     <th><d3s-column-filter [field]="'ID'" [datatype]="'text'"></d3s-column-filter></th>
                                     <th><d3s-column-filter [field]="'SubjectTypeName'" [datatype]="'text'"></d3s-column-filter></th>
                                     <th><d3s-column-filter [field]="'PredicateName'" [datatype]="'text'"></d3s-column-filter></th>
                                     <th><d3s-column-filter [field]="'ObjectTypeName'" [datatype]="'text'"></d3s-column-filter></th>
+                                    <th></th>
                                     <th></th>
                                     <th></th>
                                     <th></th>
@@ -59,6 +61,9 @@ import { BaseComponent } from '../../shared/base.component';
                                     </td>
                                     <td>
                                         <span>{{item?.ObjectTypeName}}<span style="color: #999;font-size:75%;"> ({{displayTypeName(item?.ObjectClass.Name)}})</span></span>
+                                    </td>
+                                    <td class="RowTools">
+                                        <d3s-preview-tooltip objectType="IntersectType" [objectId]="item.ID" icon="info"></d3s-preview-tooltip>
                                     </td>
                                     <td>
                                         <div class="RowTools">
