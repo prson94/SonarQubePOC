e152cb264bb45507d52d21f408c1b98663a71d12
Kenneth McNamee <kmcnamee@infogix.com>
2019-03-05 07:51:59 -0500

    initial work

---
+++ d360.web/Controllers/V2/ResponsibilitiesController.cs
@@ -0,0 +1,108 @@
+﻿using d360.core.entities;
+using d360.model;
+using d360.web.Filters;
+using d360.web.Models;
+using Microsoft.Web.Http;
+using Swashbuckle.Swagger.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace d360.web.Controllers.V2
+{
+    /// <summary>
+    /// This service houses all endpoints handling glossary-related data such as artifacts and models.
+    /// </summary>
+    [
+        ApiVersion("2.0"),
+        RoutePrefix("api/v{version:apiVersion}/responsibilities"),
+        Authorize
+    ]
+    public class ResponsibilitiesController : BaseApiController
+    {
+        public ResponsibilitiesController(CommunityContext community, CompanyContext company)
+            : base(community, company)
+        {            
+        }
+
+        /// <summary>
+        /// Retrieves a list of all responsibility types.
+        /// </summary>
+        /// <returns>Returns a list of responsibility types.</returns>
+        [
+            HttpGet,
+            Route("types"),
+            SwaggerConsumes("application/json", "application/xml"), SwaggerProduces("application/json", "application/xml"),
+            SwaggerResponse(HttpStatusCode.OK, "A list of responsibility types.", typeof(List<ResponsibilityTypeViewModel>)),
+            SwaggerResponse(HttpStatusCode.InternalServerError, "An unknown error occured while processing this request.", typeof(ErrorResponse))
+        ]
+        public async Task<HttpResponseMessage> GetResponsibilityTypesAsync()
+        {
+            var prefix = "Responsibilities.GetResponsibilityTypesAsync => ";
+            var errorMessage = "";
+
+            if (!Company.CurrentResourceIsAdmin)
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.Forbidden, "Access Denied"));
+
+            try
+            {
+                var responsibilityTypes = await Company.QueryAsync<ResponsibilityTypeViewModel>(@"
+                            select [Name], [Description], [uid], [UpdatedOn], [UpdatedBy] from [dbo].[responsibilitytype] order by [Name] asc
+                            ");
+
+                return Request.CreateResponse(HttpStatusCode.OK, responsibilityTypes);
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message + (ex.InnerException != null ? ex.InnerException.Message : "");
+                SendException(ex, new Dictionary<string, string>() {
+                    { "Endpoint Method", prefix }
+                });
+
+                return ReturnApiError(HttpStatusCode.InternalServerError, errorMessage);
+            }
+        }
+
+        /// <summary>
+        /// Retrieves a list of all responsibility types.  The allocations for those types and any rules defined for those types.
+        /// </summary>
+        /// <returns>Returns a list of responsibility types.</returns>
+        [
+            HttpGet,
+            Route("typedetails"),
+            SwaggerConsumes("application/json", "application/xml"), SwaggerProduces("application/json", "application/xml"),
+            SwaggerResponse(HttpStatusCode.OK, "A list of responsibility types.", typeof(List<ResponsibilityTypeViewModel>)),
+            SwaggerResponse(HttpStatusCode.InternalServerError, "An unknown error occured while processing this request.", typeof(ErrorResponse))
+        ]
+        public async Task<HttpResponseMessage> GetResponsibilityTypeDetailsAsync()
+        {
+            var prefix = "Responsibilities.GetResponsibilityTypesAsync => ";
+            var errorMessage = "";
+
+            if (!Company.CurrentResourceIsAdmin)
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.Forbidden, "Access Denied"));
+
+            try
+            {
+                var responsibilityTypes = await Company.QueryAsync<ResponsibilityTypeViewModel>(@"
+                            select [Name], [Description], [uid], [UpdatedOn], [UpdatedBy] from [dbo].[responsibilitytype] order by [Name] asc
+                            ");
+
+                return Request.CreateResponse(HttpStatusCode.OK, responsibilityTypes);
+            }
+            catch (Exception ex)
+            {
+                errorMessage = ex.Message + (ex.InnerException != null ? ex.InnerException.Message : "");
+                SendException(ex, new Dictionary<string, string>() {
+                    { "Endpoint Method", prefix }
+                });
+
+                return ReturnApiError(HttpStatusCode.InternalServerError, errorMessage);
+            }
+        }
+    }
+}
