ee492c3534b451549816dbb68a86a395d716ad56
Vlad <vkyry>
2019-03-06 20:48:31 +0200

    CR-fix

--- d360.web/Controllers/V2/SocialController.cs
+++ d360.web/Controllers/V2/SocialController.cs
@@ -11,8 +11,8 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.Http;
 using System.Web.Http.Description;
-using System.Web.Mvc;
 
 namespace d360.web.Controllers.V2
 {
@@ -24,12 +24,14 @@ namespace d360.web.Controllers.V2
     ]
     public class SocialController : BaseApiController
     {
-        public SocialController(CommunityContext community, CompanyContext company) : base(community, company)
+        public SocialController(CommunityContext community, CompanyContext company)
+            : base(community, company)
         {
         }
 
         [
             HttpGet,
+            MapToApiVersion("2.0"),
             Route("FollowingBreakdownByResource"),
             SwaggerConsumes("application/json", "application/xml"), SwaggerProduces("application/json", "application/xml"),
             SwaggerResponse(HttpStatusCode.OK, "A list of FollowingBreakdown.", typeof(List<dynamic>)),
@@ -39,75 +41,77 @@ namespace d360.web.Controllers.V2
         public async Task<HttpResponseMessage> FollowingBreakdownByResource(int id)
         {
             var query = await Company.QueryAsync<dynamic>(@"select  		                
-		                T.[Type], 
-		                T.TypeName,
-		                T.TypeID, 		
-		                T.[Count],
-		                coalesce(S.IconBackColor, '#000') as IconBackColor,
-                        coalesce(S.IconForeColor, '#fff') as IconForeColor,
-                        coalesce(S.IconText, substring(T.TypeName, 1, 2)) as IconText
-                from (
-                select 
-	                [Type], 
-	                TypeName, 
-	                TypeID, 
-	                count(1) as [Count]
-                from 
-	                FollowDetail
-                where 
-	                ResourceID = @r
-	                and ObjectType not in ('ArtifactType', 'PolicyType', 'ReferenceItemType', 'ResourceType', 'TaxonomyType')
-                group by Type, TypeName, TypeID) T
-                left join ObjectStyle S on  T.[Type] = S.ObjectType and T.TypeID = S.ObjectID order by TypeName", new { r = id });
-            
+        		                T.[Type], 
+        		                T.TypeName,
+        		                T.TypeID, 		
+        		                T.[Count],
+        		                coalesce(S.IconBackColor, '#000') as IconBackColor,
+                                coalesce(S.IconForeColor, '#fff') as IconForeColor,
+                                coalesce(S.IconText, substring(T.TypeName, 1, 2)) as IconText
+                        from (
+                        select 
+        	                [Type], 
+        	                TypeName, 
+        	                TypeID, 
+        	                count(1) as [Count]
+                        from 
+        	                FollowDetail
+                        where 
+        	                ResourceID = @r
+        	                and ObjectType not in ('ArtifactType', 'PolicyType', 'ReferenceItemType', 'ResourceType', 'TaxonomyType')
+                        group by Type, TypeName, TypeID) T
+                        left join ObjectStyle S on  T.[Type] = S.ObjectType and T.TypeID = S.ObjectID order by TypeName", new { r = id });
+
             return Request.CreateResponse(HttpStatusCode.OK, query);
         }
 
         [
             HttpGet,
+            MapToApiVersion("2.0"),
             Route("ResponsibilityBreakdownByResource"),
             SwaggerConsumes("application/json", "application/xml"), SwaggerProduces("application/json", "application/xml"),
             SwaggerResponse(HttpStatusCode.OK, "A list of ResponsibilityBreakdown.", typeof(List<dynamic>)),
             SwaggerResponse(HttpStatusCode.InternalServerError, "An unknown error occured while processing this request.", typeof(ErrorResponse)),
-            NonNullableParameters]
+        ]
         public async Task<HttpResponseMessage> ResponsibilityBreakdownByResource(int id, int? responsibilityTypeID)
         {
 
             var sql = $@"select  
-		                    {QueryConstants.HighLevelTypeCaseStatement} + T.Name as TypeName,
-			                			                 R.Type,
-			                 R.TypeID,
-			                 R.[Count] * R.AssetCount as [Count]
-		                from AssetType T
-		                inner join (
-			                select 
-						                C.[Type],
-						                C.TypeID,
-						                count(1) as [Count],
-										A.Count as AssetCount
-			                from ResponsibilityDetail C
-							cross apply (
-								select 
-										case when C.ApplyToType = 1 and C.AssetID = 0 then 
-											(select count(*) from Asset where AssetTypeID = C.AssetTypeID) 
-										else 
-											1
-								end as [Count]
-							) A
-			                where		C.IsVisible = 1 
-						                 {(responsibilityTypeID.HasValue ? "and C.ResponsibilityTypeID = @rt" : "")}
-						                and C.ResourceID = @r
-			                group by C.[Type], C.TypeID, A.Count
-		                ) R on R.[Type] = T.Object and R.TypeID = T.ObjectID
-						";
+        		                    {QueryConstants.HighLevelTypeCaseStatement} + T.Name as TypeName,
+        			                			                 R.Type,
+        			                 R.TypeID,
+        			                 R.[Count] * R.AssetCount as [Count]
+        		                from AssetType T
+        		                inner join (
+        			                select 
+        						                C.[Type],
+        						                C.TypeID,
+        						                count(1) as [Count],
+        										A.Count as AssetCount
+        			                from ResponsibilityDetail C
+        							cross apply (
+        								select 
+        										case when C.ApplyToType = 1 and C.AssetID = 0 then 
+        											(select count(*) from Asset where AssetTypeID = C.AssetTypeID) 
+        										else 
+        											1
+        								end as [Count]
+        							) A
+        			                where		C.IsVisible = 1 
+        						                 {(responsibilityTypeID.HasValue ? "and C.ResponsibilityTypeID = @rt" : "")}
+        						                and C.ResourceID = @r
+        			                group by C.[Type], C.TypeID, A.Count
+        		                ) R on R.[Type] = T.Object and R.TypeID = T.ObjectID
+        						";
 
             var query = await Company.QueryAsync<dynamic>(sql, new { r = id, rt = responsibilityTypeID });
-            
+
             return Request.CreateResponse(HttpStatusCode.OK, query);
         }
 
         [
             HttpGet,
+            MapToApiVersion("2.0"),
             Route("ResponsibilityBreakdownByGroup"),
             SwaggerConsumes("application/json", "application/xml"), SwaggerProduces("application/json", "application/xml"),
             SwaggerResponse(HttpStatusCode.OK, "A list of ResponsibilityBreakdown.", typeof(List<dynamic>)),
@@ -116,19 +120,19 @@ namespace d360.web.Controllers.V2
         public async Task<HttpResponseMessage> ResponsibilityBreakdownByGroup(int id)
         {
             var sql = $@"            
-select		RD.Type,
-			RD.TypeID,
-			{QueryConstants.HighLevelTypeCaseStatement} + T.Name as TypeName,
-			count(1) as [Count]
-from		ResponsibilityDetail RD 
-			inner join AssetType T on T.ID = RD.AssetTypeID and RD.SecurityAsset = 'G' and RD.SecurityAssetID = @id and RD.IsVisible = 1
-group by    RD.Type, 
-            RD.TypeID, 
-            { QueryConstants.HighLevelTypeCaseStatement} + T.Name 
-order by    { QueryConstants.HighLevelTypeCaseStatement} + T.Name";
+        select		RD.Type,
+        			RD.TypeID,
+        			{QueryConstants.HighLevelTypeCaseStatement} + T.Name as TypeName,
+        			count(1) as [Count]
+        from		ResponsibilityDetail RD 
+        			inner join AssetType T on T.ID = RD.AssetTypeID and RD.SecurityAsset = 'G' and RD.SecurityAssetID = @id and RD.IsVisible = 1
+        group by    RD.Type, 
+                    RD.TypeID, 
+                    { QueryConstants.HighLevelTypeCaseStatement} + T.Name 
+        order by    { QueryConstants.HighLevelTypeCaseStatement} + T.Name";
 
             var query = await Company.QueryAsync<dynamic>(sql, new { id });
-            
+
             return Request.CreateResponse(HttpStatusCode.OK, query);
         }
     }
