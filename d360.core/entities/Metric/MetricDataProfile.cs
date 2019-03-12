1a092f2091c80c446ebffc2b4097b07e94d8358c
Michael Donato <mdonato@infogix.com>
2019-03-12 14:02:21 -0400

    GOV-6602 - initial commit

---
+++ d360.core/entities/Metric/MetricDataProfile.cs
@@ -0,0 +1,37 @@
+﻿using d360.core.enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace d360.core.entities.Metric
+{
+    [DataContract(Namespace = NAMESPACE), Table("DataProfile", Schema = "metrics")]
+    public class MetricDataProfile : BaseCreatedAndUpdatedObject
+    {
+        [DataMember, Key, Column(Order = 1)]
+        public Guid Uid { get; set; }
+
+        public Guid AssetUid { get; set; }
+
+        public int RowCount { get; set; }
+        public decimal Uniqueness { get; set; }
+        public int UniqueCount { get; set; }
+        public decimal Completeness { get; set; }
+        public int NullCount { get; set; }
+        public int BlankCount { get; set; }
+        public string DataType { get; set; }
+        public string MinimumValue { get; set; }
+        public string MaximumValue { get; set; }
+        public int? Precision { get; set; }
+        public int? Scale { get; set; }
+        public decimal? Average { get; set; }
+        public decimal? Median { get; set; }
+        public decimal? StandardDeviation { get; set; }
+        public string Top10Values { get; set; }
+
+        public DateTime EffectiveDate { get; set; }
+    }
+}
+
---
+++ d360.web/Controllers/V2/ProfilesController.cs
@@ -0,0 +1,42 @@
+﻿using d360.extensions;
+using d360.model;
+using Microsoft.Web.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Dapper;
+using System.Data.SqlClient;
+using System.Data;
+using System.Data.Entity;
+using Swashbuckle.Swagger.Annotations;
+
+namespace d360.web.Controllers.V2
+{
+    [
+        ApiVersion("2.0"),
+        RoutePrefix("api/v{version:apiVersion}/profiles"),
+        Authorize
+    ]
+
+    public class ProfilesController : BaseApiController
+    {
+        #region DI
+
+        public ProfilesController
+(CommunityContext community, CompanyContext company)
+            : base(community, company)
+        {
+
+        }
+
+        #endregion
+
+        private static string DefaultImplementationName = "default";
+    }
+
+
+}
