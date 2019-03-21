a2252beb31d5ca57e8f8084a4cb6724902cf00a5
Thomas Klok <tklok@infogix.com>
2019-03-21 12:39:22 -0400

    GOV-6806 Fixed DOM element typos in template

--- d360.web/Scripts/app/components/workflowmonitor/workflowmonitor-action-details.component.ts
+++ d360.web/Scripts/app/components/workflowmonitor/workflowmonitor-action-details.component.ts
@@ -19,17 +19,17 @@ import { ToolTipService } from '../../services/tooltip.service';
                     <span class="FieldName">
                         {{field.Name}}:&nbsp;
                     </span>
-                    <ngTemplate [ngIf]="field.Values && field.Values.length > 0" [ngIfElse]="singlevalue">
+                    <ng-template [ngIf]="field.Values && field.Values.length > 0" [ngIfElse]="singlevalue">
                         <span>
                             <span *ngFor="let singleitem of field.Values" style="margin-left:2em;text-indent:-1em;display:block;">
                                 <span [innerHtml]="singleitem"></span>
                             </span>
                         </span>
-                    </ngTemplate>
-                    <ngTemplate #singlevalue>
+                    </ng-template>
+                    <ng-template #singlevalue>
                         <span [innerHtml]="field.Value">
-                    </ngTemplate>
-                    </span>
+                        </span>
+                    </ng-template>
                 </div>
             </ng-container>                        
         </div>
