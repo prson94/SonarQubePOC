410d16fb35ac9c4816779965655218371c9101d8
Anton Lunenko <alunenko@infogix.com>
2019-03-25 12:40:33 +0200

    GOV-6771: finish with follower service

--- d360.web/Scripts/app/services/follower.service.ts
+++ d360.web/Scripts/app/services/follower.service.ts
@@ -1,35 +1,61 @@
-﻿import { Injectable } from '@angular/core';
-import { Headers, Http } from '@angular/http';
-import { MessagesService } from './messages.service';
-import { BaseService } from './base.service';
-import { FollowDetail, FollowInfo } from '../models/follower.model';
+﻿import {Observable} from "rxjs";
+import {catchError, map} from "rxjs/operators";
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from '@angular/core';
 
+import {FollowDetail, FollowInfo} from '../models/follower.model';
+
+import {MessagesService} from './messages.service';
+import {BaseObservableService} from "./baseObservable.service";
 
 @Injectable()
-export class FollowerService extends BaseService {
+export class FollowerService extends BaseObservableService {
 
-    constructor(private http: Http, messagesService: MessagesService) { super(messagesService); }
+    constructor(
+        private http: HttpClient,
+        messagesService: MessagesService
+    ) {
+        super(messagesService);
+    }
 
-    getFollowers(type: string, id: number): Promise<FollowDetail[]> {
-        return this.http.get(`api/${type}/${id}/followers`)
-            .toPromise()
-            .then(response => <FollowDetail[]>response.json())
-            .catch(err => this.handleError(err));
+    getFollowers(
+        type: string,
+        id: number
+    ): Observable<FollowDetail[]> {
+        return this
+            .http
+            .get(`api/${type}/${id}/followers`)
+            .pipe(
+                map(response => <FollowDetail[]>response),
+                catchError(err => this.handleError(err))
+            );
 
     }
 
-    getFollowInfo(type: string, id: number): Promise<FollowInfo> {
-        return this.http.get(`api/followinfo/${type}/${id}`)
-            .toPromise()
-            .then(response => <FollowInfo>response.json())
-            .catch(err => this.handleError(err));
+    getFollowInfo(
+        type: string,
+        id: number
+    ): Observable<FollowInfo> {
+        return this
+            .http
+            .get(`api/followinfo/${type}/${id}`)
+            .pipe(
+                map(response => <FollowInfo>response),
+                catchError(err => this.handleError(err))
+            );
     }
 
-    updateFollowStatus(type: string, id: number, includeChildren: boolean = false): Promise<any> {
-        return this.http.post('resources/UpdateFollowStatus', { type: type, id: id, includeChildren: includeChildren })
-            .toPromise()
-            .then(response => <any>response.json())
-            .catch(err => this.handleError(err));
+    updateFollowStatus(
+        type: string,
+        id: number,
+        includeChildren: boolean = false
+    ): Observable<any> {
+        return this
+            .http
+            .post('resources/UpdateFollowStatus', {type: type, id: id, includeChildren: includeChildren})
+            .pipe(
+                map(response => <any>response),
+                catchError(err => this.handleError(err))
+            );
     }
-
 }
