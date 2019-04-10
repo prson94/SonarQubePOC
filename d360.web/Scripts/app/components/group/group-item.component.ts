9fd59074441382f6440d0b55b4f496f87675d689
Anton Lunenko <alunenko@infogix.com>
2019-04-10 16:33:39 +0300

    GOV-6953: make a group service as observable

--- d360.web/Scripts/app/components/group/group-item.component.ts
+++ d360.web/Scripts/app/components/group/group-item.component.ts
@@ -1,36 +1,38 @@
-﻿import { Input, Component, EventEmitter, Output, OnInit, OnDestroy } from '@angular/core';
-import { Router, ActivatedRoute }       from '@angular/router';
-import { BaseComponent } from '../shared/base.component';
-import { Title } from '@angular/platform-browser';
-import { HeaderBreadcrumbService } from '../../services/header-breadcrumb.service';
-import { GroupService } from '../../services/group.service';
-import { Breadcrumb } from '../../models/breadcrumb.model';
-import { GroupEditorModel } from '../../models/group.model';
-import { SiteUrlHelpers } from '../../static/site-url-helpers';
+﻿import {Input, Component, EventEmitter, Output, OnInit, OnDestroy} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
+import {BaseComponent} from '../shared/base.component';
+import {Title} from '@angular/platform-browser';
+import {HeaderBreadcrumbService} from '../../services/header-breadcrumb.service';
+import {GroupService} from '../../services/group.service';
+import {Breadcrumb} from '../../models/breadcrumb.model';
+import {GroupEditorModel} from '../../models/group.model';
+import {SiteUrlHelpers} from '../../static/site-url-helpers';
 
+/* FIXME: Extract templates and styles to their own files
+*  https://angular.io/guide/styleguide#style-05-04 */
 @Component({
     selector: 'd3s-group-item',
     providers: [GroupService],
-    template: ` 
-                <div class="row">
+    template: `
+        <div class="row">
+            <div class="col s12">
+                <d3s-loading [isLoading]="isLoading"></d3s-loading>
+                <div class="row" *ngIf="!isLoading">
                     <div class="col s12">
-                        <d3s-loading [isLoading]="isLoading"></d3s-loading>
-                        <div class="row" *ngIf="!isLoading">                        
-                            <div class="col s12">
-                                <div class="tile tile-detail">
-                                    <d3s-group-responsibility [group]="model?.group"></d3s-group-responsibility>
-                                </div>
-                            </div>
-                            <div class="col s12">
-                                <div class="tile tile-detail">
-                                    <d3s-group-members [groupId]="groupId" [groupName]="model?.group?.Name"></d3s-group-members>
-                                </div>
-                            </div>                            
-                        </div>                            
-                        
+                        <div class="tile tile-detail">
+                            <d3s-group-responsibility [group]="model?.group"></d3s-group-responsibility>
+                        </div>
+                    </div>
+                    <div class="col s12">
+                        <div class="tile tile-detail">
+                            <d3s-group-members [groupId]="groupId" [groupName]="model?.group?.Name"></d3s-group-members>
+                        </div>
                     </div>
                 </div>
-                `
+
+            </div>
+        </div>
+    `
 })
 
 export class GroupItemComponent extends BaseComponent implements OnInit {
@@ -40,9 +42,9 @@ export class GroupItemComponent extends BaseComponent implements OnInit {
     private groupId: number;
 
     constructor(private route: ActivatedRoute,
-        private router: Router,
-        private groupService: GroupService,
-        protected titleService: Title, protected headerBreadcrumbService: HeaderBreadcrumbService) {
+                private router: Router,
+                private groupService: GroupService,
+                protected titleService: Title, protected headerBreadcrumbService: HeaderBreadcrumbService) {
         super();
     }
 
@@ -53,8 +55,8 @@ export class GroupItemComponent extends BaseComponent implements OnInit {
             this.logAction('open', 'Group', this.groupId);
             this.isLoading = true;
 
-            this.groupService.getGroup(this.groupId)
-                .then(group => {
+            this.groupService.getGroup(this.groupId).subscribe(
+                group => {
                     this.model = group;
                     this.headerBreadcrumbService.clearBreadcrumbs();
 
@@ -64,18 +66,17 @@ export class GroupItemComponent extends BaseComponent implements OnInit {
                     this.setBrowserTitle(this.titleService, this.model.group.Name);
 
                     this.isLoading = false;
-                });
-            });
-        }
+                }
+            );
+        });
+    }
 
     ngOnDestroy() {
         this.sub.unsubscribe();
         this.clearSidebar();
     }
 
-
     private load() {
 
     }
-
-};
+}
