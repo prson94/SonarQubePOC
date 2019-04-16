ad918027c5fee3b5e7fd7483b4034b621e0e5973
Anton Lunenko <alunenko@infogix.com>
2019-04-16 16:45:10 +0300

    GOV-6772: make a lookup service as observable

--- d360.web/Scripts/app/components/admin/lookups/admin-lookups.component.ts
+++ d360.web/Scripts/app/components/admin/lookups/admin-lookups.component.ts
@@ -1,27 +1,47 @@
-﻿import { Component, OnInit, OnDestroy} from '@angular/core';
-import { Router, ActivatedRoute }       from '@angular/router';
-import { HeaderBreadcrumbService } from '../../../services/header-breadcrumb.service';
-import { RightSidebarService } from '../../../services/right-sidebar.service';
-import { LookupService } from '../../../services/lookup.service';
-import { MessagesService } from '../../../services/messages.service';
-import { AdminBaseComponent } from '../admin-base.component';
-import { Lookup } from '../../../models/lookup.model';
-import { Title } from '@angular/platform-browser';
+﻿import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
+import {Title} from '@angular/platform-browser';
 
+import {Lookup} from '../../../models/lookup.model';
 
+import {HeaderBreadcrumbService} from '../../../services/header-breadcrumb.service';
+import {RightSidebarService} from '../../../services/right-sidebar.service';
+import {LookupService} from '../../../services/lookup.service';
+import {MessagesService} from '../../../services/messages.service';
+
+import {AdminBaseComponent} from '../admin-base.component';
+
+/* FIXME: Extract templates and styles to their own files
+*  https://angular.io/guide/styleguide#style-05-04 */
 @Component({
     selector: 'd3s-admin-lookups-component',
     providers: [LookupService],
-    template: ` <div class="row">
-                    <div class="col l4 s12">                    
-                        <div class="tile tile-detail">
-                            <header *ngIf="!showEditor && !showDelete">Lookup Types
-                                <d3s-tile-actions [hasAdd]="true" (addClick)="add()"></d3s-tile-actions>                            
-                            </header>   
-                            <d3s-loading [isLoading]="isLoading"></d3s-loading>
-                            <span *ngIf="!showEditor && !showDelete && !isLoading">       
-                                <input type="text" [hidden]="!showSimpleFilter" pInputText size="100" (input)="dt.filterGlobal($event.target.value, 'contains')" placeholder="Search..." class="grid-simple-filter">
-                                <p-table #dt [value]="lookups" selectionMode="single" [metaKeySelection]="true" [globalFilterFields]="['ID','Name']" [pageLinks]="3" [paginator]="true" [rows]="20" [(selection)]="selectedLookup">
+    template: `
+        <div class="row">
+            <div class="col l4 s12">
+                <div class="tile tile-detail">
+                    <header *ngIf="!showEditor && !showDelete">Lookup Types
+                        <d3s-tile-actions [hasAdd]="true"
+                                          (addClick)="add()"></d3s-tile-actions>
+                    </header>
+                    <d3s-loading [isLoading]="isLoading"></d3s-loading>
+                    <span *ngIf="!showEditor && !showDelete && !isLoading">       
+                                <input type="text"
+                                       [hidden]="!showSimpleFilter"
+                                       pInputText
+                                       size="100"
+                                       (input)="dt.filterGlobal($event.target.value, 'contains')"
+                                       placeholder="Search..."
+                                       class="grid-simple-filter">
+                                <p-table #dt
+                                         [value]="lookups"
+                                         selectionMode="single"
+                                         [metaKeySelection]="true"
+                                         [globalFilterFields]="['ID','Name']"
+                                         [pageLinks]="3"
+                                         [paginator]="true"
+                                         [rows]="20"
+                                         [(selection)]="selectedLookup">
                                     <ng-template pTemplate="header">
                                         <tr>
                                             <th [pSortableColumn]="'ID'">
@@ -36,55 +56,74 @@ import { Title } from '@angular/platform-browser';
                                             <th style="width: 40px"></th>
                                         </tr>
                                     </ng-template>
-                                    <ng-template pTemplate="body" let-item>
-                                        <tr (dblclick)="selectedLookup=item;showEditor=true;" [pSelectableRow]="item">
+                                    <ng-template pTemplate="body"
+                                                 let-item>
+                                        <tr (dblclick)="selectedLookup=item;showEditor=true;"
+                                            [pSelectableRow]="item">
                                             <td>{{item.ID}}</td>
                                             <td>{{item.Name}}</td>
                                             <td>
                                                 <div class="RowTools">
-                                                    <a style="cursor:pointer;" (click)="selectedLookup=item;showEditor=true"><i class="fa fa-pencil"></i></a>
+                                                    <a style="cursor:pointer;"
+                                                       (click)="selectedLookup=item;showEditor=true"><i class="fa fa-pencil"></i></a>
                                                 </div>
                                             </td>
                                             <td>
                                                 <div class="RowTools">
-                                                    <a style="cursor:pointer;" (click)="selectedLookup=item;showDelete=true"><i class="fa fa-trash-o"></i></a>
+                                                    <a style="cursor:pointer;"
+                                                       (click)="selectedLookup=item;showDelete=true"><i class="fa fa-trash-o"></i></a>
                                                 </div>
                                             </td>
                                         </tr>
                                     </ng-template>
-                                    <ng-template *ngIf="dt.totalRecords" pTemplate="summary">
-                                        <d3s-grid-paging-info [first]="dt.first" [rows]="dt.rows" [totalRecords]="dt.totalRecords"></d3s-grid-paging-info>
+                                    <ng-template *ngIf="dt.totalRecords"
+                                                 pTemplate="summary">
+                                        <d3s-grid-paging-info [first]="dt.first"
+                                                              [rows]="dt.rows"
+                                                              [totalRecords]="dt.totalRecords"></d3s-grid-paging-info>
                                     </ng-template>
                                 </p-table>
-                            </span> 
-                            <d3s-delete-form *ngIf="showDelete"
-                                [callback]="theDeleteCallback"
-                                [itemId]="selectedLookup?.ID"
-                                [method]="'callback'"
-                                [prompt]="'Are you sure you want to delete the lookup [' + [selectedLookup?.Name] + ']?'"                                         
-                                (onCancel)="showDelete=false;"
-                            ></d3s-delete-form>  
-                            <d3s-admin-lookup-type-editor *ngIf="showEditor" [lookup]="selectedLookup" (saveClick)="saveLookup($event)" (closeClick)="closeEditor()"></d3s-admin-lookup-type-editor>                       
+                            </span>
+                    <d3s-delete-form *ngIf="showDelete"
+                                     [callback]="theDeleteCallback"
+                                     [itemId]="selectedLookup?.ID"
+                                     [method]="'callback'"
+                                     [prompt]="'Are you sure you want to delete the lookup [' + [selectedLookup?.Name] + ']?'"
+                                     (onCancel)="showDelete=false;"
+                    ></d3s-delete-form>
+                    <d3s-admin-lookup-type-editor *ngIf="showEditor"
+                                                  [lookup]="selectedLookup"
+                                                  (saveClick)="saveLookup($event)"
+                                                  (closeClick)="closeEditor()"></d3s-admin-lookup-type-editor>
+                </div>
+            </div>
+            <div class="col l8 s12"
+                 *ngIf="!showDelete && !showEditor && selectedLookup">
+                <div class="row">
+                    <div class="col s12">
+                        <div class="tile tile-detail">
+                            <d3s-field-definition-tile [objectType]="'LookupType'"
+                                                       [objectID]="selectedLookup?.ID"></d3s-field-definition-tile>
                         </div>
-                    </div>                    
-                    <div class="col l8 s12" *ngIf="!showDelete && !showEditor && selectedLookup">
-                        <div class="row">
-                            <div class="col s12">
-                                <div class="tile tile-detail">                                              
-                                    <d3s-field-definition-tile [objectType]="'LookupType'" [objectID]="selectedLookup?.ID" ></d3s-field-definition-tile>
-                                </div>
-                            </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col s12">
+                        <div class="tile tile-detail">
+                            <d3s-dynamic-grid [title]="'Items'"
+                                              [itemName]="'Lookup'"
+                                              [objectType]="'LookupType'"
+                                              [objectID]="selectedLookup?.ID"
+                                              [createUri]="'form/dynamicedit/create/lookup/'"
+                                              [editUri]="'form/dynamicedit/edit/lookup/'"
+                                              [dataUri]="lookupUri()"
+                                              [deleteUri]="'form/DeleteLookupByIdRaw?id='"></d3s-dynamic-grid>
                         </div>
-                        <div class="row">
-                            <div class="col s12">
-                                <div class="tile tile-detail">           
-                                    <d3s-dynamic-grid [title]="'Items'" [itemName]="'Lookup'" [objectType]="'LookupType'" [objectID]="selectedLookup?.ID" [createUri]="'form/dynamicedit/create/lookup/'" [editUri]="'form/dynamicedit/edit/lookup/'" [dataUri]="lookupUri()" [deleteUri]="'form/DeleteLookupByIdRaw?id='"></d3s-dynamic-grid>                                                                       
-                                </div>
-                            </div>
-                        </div>
-                    <div>
-                </div>  
-                `
+                    </div>
+                </div>
+                <div>
+                </div>
+    `
 })
 
 export class AdminLookupsComponent extends AdminBaseComponent implements OnInit, OnDestroy {
@@ -96,17 +135,21 @@ export class AdminLookupsComponent extends AdminBaseComponent implements OnInit,
     showDelete: boolean = false;
     theDeleteCallback: Function;
 
-    constructor(private route: ActivatedRoute,
+    constructor(
+        private route: ActivatedRoute,
         private router: Router,
         rightSidebarService: RightSidebarService,
         private lookupService: LookupService,
         protected messagesService: MessagesService,
         headerBreadcrumbService: HeaderBreadcrumbService,
-        titleService: Title) {
-        super(headerBreadcrumbService, titleService, rightSidebarService);                
+        titleService: Title
+    ) {
+        super(headerBreadcrumbService, titleService, rightSidebarService);
+
         this.areaName = "Lookup Types";
         this.setCommonItems();
         this.setCommonRightSideBar(true);
+
         if (this.auditSidebar) {
             this.auditSidebar.hasDynamicUrl = true;
             this.auditSidebar.dynamicUrlCallback = (() => {
@@ -117,15 +160,30 @@ export class AdminLookupsComponent extends AdminBaseComponent implements OnInit,
 
     ngOnInit() {
         this.theDeleteCallback = this.deleteLookup.bind(this);
-        this.getLookups().then(result => {
-            this.sub = this.route.params.subscribe(params => {
-                this.selectedLookupTypeId = +params['lookupTypeId']; // (+) converts string 'id' to a number
-                let preselected = this.lookups.find(i => i.ID === this.selectedLookupTypeId);
-                if (preselected) {
-                    this.selectedLookup = preselected;
+
+        this.getLookups().subscribe(
+            result => {
+                this.lookups = result;
+
+                if (this.lookups.length > 0) {
+                    this.selectedLookup = this.lookups[0];
                 }
-            });
-        });
+
+                this.sub = this.route.params.subscribe(
+                    params => {
+                        this.selectedLookupTypeId = +params['lookupTypeId']; // (+) converts string 'id' to a number
+
+                        let preselected = this.lookups.find(i => i.ID === this.selectedLookupTypeId);
+
+                        if (preselected) {
+                            this.selectedLookup = preselected;
+                        }
+                    }
+                );
+
+                this.isLoading = false;
+            }
+        );
     }
 
     ngOnDestroy() {
@@ -134,19 +192,15 @@ export class AdminLookupsComponent extends AdminBaseComponent implements OnInit,
 
     getLookups() {
         this.isLoading = true;
-        return this.lookupService.getLookups()
-            .then(result => {
-                this.lookups = result;
-                this.isLoading = false;
-                if (this.lookups.length > 0) this.selectedLookup = this.lookups[0];
-            });            
+
+        return this.lookupService.getLookups();
     }
 
     deleteLookup(id: number) {
         this.isLoading = true;
-        this.lookupService.deleteLookupType(id)
-            .then(result => {
-                this.isLoading = false;
+
+        this.lookupService.deleteLookupType(id).subscribe(
+            result => {
                 this.showMessageForResult(this.messagesService, result);
                 this.showDelete = false;
 
@@ -154,14 +208,19 @@ export class AdminLookupsComponent extends AdminBaseComponent implements OnInit,
                     this.selectedLookup = this.lookups.length > 0 ? this.lookups[0] : null;
                     this.lookups = this.lookups.filter(x => x.ID != id);
                 }
-            });
+
+                this.isLoading = false;
+            }
+        );
     }
 
     lookupUri() {
-        if (this.selectedLookup == null) return "";
+        if (this.selectedLookup == null) {
+            return "";
+        }
 
         return `resources/lookups/${this.selectedLookup.ID}/items.json`;
-    }    
+    }
 
     add() {
         this.showEditor = true;
@@ -170,6 +229,7 @@ export class AdminLookupsComponent extends AdminBaseComponent implements OnInit,
 
     closeEditor() {
         this.showEditor = false;
+
         if (this.selectedLookup == null) {
             this.selectedLookup = this.lookups.length > 0 ? this.lookups[0] : null;
         }
@@ -177,10 +237,10 @@ export class AdminLookupsComponent extends AdminBaseComponent implements OnInit,
 
     saveLookup(event) {
         this.isLoading = true;
-        this.lookupService.saveLookup(event.lookup)
-            .then(result => {
-                this.isLoading = false;
+        this.lookupService.saveLookup(event.lookup).subscribe(
+            result => {
                 this.showMessageForResult(this.messagesService, result);
+
                 if (result.type == "error") {
                     return;
                 }
@@ -188,18 +248,19 @@ export class AdminLookupsComponent extends AdminBaseComponent implements OnInit,
                 if (event.lookup.ID == undefined) {
                     event.lookup.ID = Number(result.id);
                     this.lookups[this.lookups.length] = event.lookup;
-                }
-                else {
+                } else {
                     let index = this.lookups.findIndex(x => x.ID == event.lookup.ID);
 
                     if (index >= 0 && index < this.lookups.length) {
                         this.lookups[index] = event.lookup;
                     }
                 }
+
                 this.selectedLookup = event.lookup;
+
                 this.showEditor = false;
-            });
-        
+                this.isLoading = false;
+            }
+        );
     }
-    
 }
