567fc962063c3070fe6d2a3c32343cea35704810
Kenneth McNamee <kmcnamee@infogix.com>
2019-04-19 15:33:12 -0400

    GOV-6884 - move lineage endpoints out of v1 api controller move v1 relationship controller to obsolete / hide.  Update any affected endpoints

---
+++ d360.web/Controllers/Services/Obsolete/RelationshipsController.cs
@@ -0,0 +1,29 @@
+﻿using d360.core.entities;
+using d360.model;
+using d360.core;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Web.Http;
+
+namespace d360.web.Controllers.Services
+{
+    [ApiVersion("1.0"), ApiExplorerSettings(IgnoreApi = true), RoutePrefix("services/deprecated/relationships"), Name("Relationships"), Authorize]
+    public class RelationshipsController : BaseApiController
+    {
+        public RelationshipsController(ICommunityContext community, ICompanyContext company)
+            : base(community, company)
+        {
+        }
+        
+        /// <summary>
+        /// Allows for OData filtering on relationships types.
+        /// </summary>
+        /// <returns>A list of relationships types present in the system.</returns>
+        [Route(""), HttpGet, ApiExplorerSettings(IgnoreApi = true)]
+        public IQueryable<IntersectType> GetIntersectTypes()
+        {
+            return Company.Table<IntersectType>();
+        }
+    }
+}
