726c4aa87ecfe283012182ca1a6cff3f868faf90
Michael Pappas <mpappas@infogix.com>
2019-04-19 06:33:54 -0400

    GOV-6882 - changed workflow service call to point to relationships v2 API

--- d360.web/Controllers/V2/RelationshipsController.cs
+++ d360.web/Controllers/V2/RelationshipsController.cs
@@ -486,6 +486,12 @@ namespace d360.web.Controllers.V2
             }
         }
 
+        [Route("types/{id:int}"), HttpGet, ApiExplorerSettings(IgnoreApi = true)]
+        public IQueryable<IntersectType> GetIntersectType(int id)
+        {
+            return Company.Filter<IntersectType>(i => i.ID == id);
+        }
+
         #region Bulk Relationships
 
         /// <summary>
@@ -711,8 +717,6 @@ namespace d360.web.Controllers.V2
         }
 
         #endregion
-
-
 
     }
 
