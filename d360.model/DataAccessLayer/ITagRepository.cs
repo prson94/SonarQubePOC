97e970d677c3578f8be8459e2d8b1c0226ddc309
Kenneth McNamee <kmcnamee@infogix.com>
2019-05-24 16:14:15 -0400

    GOV-7255

---
+++ d360.model/DataAccessLayer/ITagRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using d360.core.entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace d360.model.DataAccessLayer
+{
+    public interface ITagRepository
+    {
+        Task<IEnumerable<Tag>> GetTags(IEnumerable<KeyValuePair<string, string>> queryParams);
+    }
+}
---
+++ d360.model/DataAccessLayer/repositories/TagRepository.cs
@@ -0,0 +1,46 @@
+﻿using d360.core.entities;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d360.model.DataAccessLayer.repositories
+{
+    public class TagRepository : ITagRepository
+    {
+        ICompanyContext companyContext;
+        public TagRepository(ICompanyContext context)
+        {
+            this.companyContext = context;
+        }
+
+        public async Task<IEnumerable<Tag>> GetTags(IEnumerable<KeyValuePair<string, string>> queryParams)
+        {
+            var dbArgs = new DynamicParameters();
+            var sql = "select uid, DataSource, Type, ExternalID, FieldHash from [dbo].[AssetCrossReference]";
+            List<string> queryFilters = new List<string>();
+
+
+            if (queryParams.ToList().Any(q => q.Key.ToLower() == "_assetuid"))
+            {
+                Guid uid = new Guid();
+
+                var tagUidString = queryParams.ToList().FirstOrDefault(q => q.Key.ToLower() == "uid").Value;
+                if (Guid.TryParse(tagUidString, out uid))
+                {
+                    dbArgs.Add("@uid", uid);
+                    queryFilters.Add($"[UID] = @uid");
+                }
+            }
+
+            if (queryFilters.Count > 0)
+            {
+                sql += " where " + string.Join(" and ", queryFilters);
+            }
+            return (await companyContext.QueryAsync<Tag>(sql, dbArgs));            
+        }
+
+    }
+}
