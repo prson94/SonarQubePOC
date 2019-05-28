8b85ea6fa9215bbac59ac49f1cb01c9a3fa2d491
Ken McNamee <kmcnamee@gmail.com>
2019-05-28 18:34:40 -0400

    Update TagsController.cs

--- d360.web/Controllers/V2/TagsController.cs
+++ d360.web/Controllers/V2/TagsController.cs
@@ -24,7 +24,7 @@ namespace d360.web.Controllers.V2
         ApiVersion("2.0"),
         RoutePrefix("api/v{version:apiVersion}/tags"),
         Authorize,
-        ApiExplorerSettings(IgnoreApi = false)
+        ApiExplorerSettings(IgnoreApi = true)
     ]
     public class TagsController : BaseV2ApiController
     {        
