af0a2ade02af4c286cf145628717875a1e80a0aa
Tomislav Đurić <tduric@infogix.com>
2019-05-29 16:02:02 +0100

    - created IssueController - created GET Issue Types endpoint - added issue repository - moved Issue and IssueType models to separate folder

---
+++ d360.core/entities/Issue/Issue.cs
@@ -0,0 +1,59 @@
+﻿using System.Runtime.Serialization;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+using d360.core.entities.Contracts;
+using d360.core.enums;
+using d360.core.queue;
+
+namespace d360.core.entities
+{
+    [DataContract(Namespace = NAMESPACE)]
+    public class Issue : BaseIntObject, IIntObject, IFieldsObject, IUpdatedMetadata, IEventTrackedEntity
+    {
+        public int IssueTypeID { get; set; }
+
+        [DataMember]
+        public int? CreatedBy { get; set; }
+
+        [DataMember]
+        public DateTime? CreatedOn { get; set; }
+
+        public DateTime? UpdatedOn { get; set; }
+
+        public int? UpdatedBy { get; set; }
+
+        [DataMember, Column(TypeName = "varchar"), StringLength(50)]
+        public string Object { get; set; }
+
+        [DataMember]
+        public int ObjectID { get; set; }
+
+        [DataMember, Column(TypeName = "varchar"), StringLength(25)]
+        public string ObjectType { get; set; }
+
+        [DataMember]
+        public int ObjectTypeID { get; set; }
+
+        public virtual IssueType IssueType { get; set; }
+
+        [DataMember]
+        public int? CommentID { get; set; }
+
+        public EventObjectInfo GetEventObjectInfo()
+        {
+            return new EventObjectInfo
+            {
+                Object = SystemObjects.Issue,
+                ObjectID = ID,
+                ObjectType = SystemObjects.IssueType,
+                ObjectTypeID = IssueTypeID
+            };
+        }
+
+        public FieldsObjectModel GetFieldsObjectInfo()
+        {
+            return new FieldsObjectModel { Type = SystemObjects.IssueType, Object = SystemObjects.Issue, TypeID = IssueTypeID };
+        }
+    }
+}
---
+++ d360.web/Controllers/V2/IssueController.cs
@@ -0,0 +1,54 @@
+﻿using d360.core.entities;
+using d360.model;
+using d360.model.DataAccessLayer;
+using d360.web.Filters;
+using Microsoft.Web.Http;
+using Swashbuckle.Swagger.Annotations;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace d360.web.Controllers.V2
+{
+    /// <summary>
+    /// This service houses all endpoints handling glossary-related data such as artifacts and models.
+    /// </summary>
+    [
+        ApiVersion("2.0"),
+        RoutePrefix("api/v{version:apiVersion}/issues"),
+        Authorize
+    ]
+    public class IssueController : BaseV2ApiController
+    {
+        IIssueRepository issueRepository;
+
+        public IssueController(ICommunityContext community, ICompanyContext company, IIssueRepository repository)
+            : base(community, company)
+        {
+            this.issueRepository = repository;
+        }
+
+        /// <summary>
+        /// Returns all issue types that are defined in Govern.  
+        /// 
+        /// </summary>
+        /// <returns>A list of issue types</returns>
+        [
+            HttpGet, MapToApiVersion("2.0"), Route("types"),
+            SwaggerConsumes("application/json"), SwaggerProduces("application/json"),
+            SwaggerResponse(HttpStatusCode.OK, "A full list of issue types.", typeof(List<IssueTypeApiModel>)),
+            SwaggerResponse(HttpStatusCode.Forbidden, "Access Denied")
+        ]
+        public async Task<HttpResponseMessage> Get()
+        {
+            if (!Company.CurrentResourceIsAdmin)
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.Forbidden, "Access Denied"));
+
+            var issueTypes = await issueRepository.GetIssueTypes();
+
+            return Request.CreateResponse(issueTypes);
+        }
+    }
+}
