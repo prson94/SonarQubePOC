fa2deedcce8fa52829e38dded82418abd864809d
seban200 <sabraham@infogix.com>
2019-05-31 14:19:34 -0400

    GOV-7247 GET workflow types

---
+++ d360.model/validators/IWorkflowApiModelValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d360.model.validators
+{
+    public interface IWorkflowApiModelValidator
+    {
+       bool ValidateWorkflowGetTypeModel(IEnumerable<KeyValuePair<string, string>> queryParams);
+    }
+}
