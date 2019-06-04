aea1ebe5bb4e632b901ae5188061f0d4b17761a6
seban200 <sabraham@infogix.com>
2019-06-04 09:27:11 -0400

    GOV-7248 GET workflow versions

--- d360.core/entities/Workflow/Models.cs
+++ d360.core/entities/Workflow/Models.cs
@@ -366,17 +366,7 @@ namespace d360.core.entities.Workflow
 
     }
 
-    //public class GetWorkflowVersionApiViewModel
-    //{
-    //    [DataMember]
-    //    public int pageSize { get; set; } = 25000;
-    //    [DataMember]
-    //    public int pageNum { get; set; } = 1;
-    //    [DataMember]
-    //    public int total { get; set; } = 0;
-    //    [DataMember]
-    //    public IEnumerable<WorkflowVersionApiViewModel1> items { get; set; }
-    //}
+
 
     public class WorkflowVersionsApiViewModel
     {
