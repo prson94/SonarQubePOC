3100929aa68045c78259648a8aa0ec9320c0bd1c
Kenneth McNamee <kmcnamee@infogix.com>
2019-06-05 11:15:19 -0400

    GOV-7122

--- d360.web/Scripts/app/services/qualifier.service.ts
+++ d360.web/Scripts/app/services/qualifier.service.ts
@@ -1,54 +1,62 @@
 ﻿import { Injectable } from '@angular/core';
-import { Headers, Http, ResponseContentType, Response } from '@angular/http';
-import { MessagesService } from './messages.service';
-import { BaseService } from './base.service';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { catchError, map } from "rxjs/operators";
+
+import { MessagesObservableService } from './messages-observable.service';
+import { BaseObservableService } from "./baseObservable.service";
 import { QualifierType, ResolutionObjectType } from '../models/qualifier.model';
 
 @Injectable()
-export class QualifierService extends BaseService {
-    constructor(private http: Http, messagesService: MessagesService) { super(messagesService); }
+export class QualifierService extends BaseObservableService {
+    constructor(private http: HttpClient, messagesService: MessagesObservableService) { super(messagesService); }
 
 
-    getQualifierTypes(implementationId: number): Promise<QualifierType[]> {
-        return this.http.get(`api/ruleimplementations/${implementationId}/qualifiers`)
-            .toPromise()
-            .then(response => <QualifierType[]>response.json())
-            .catch(err => this.handleError(err));
+    getQualifierTypes(implementationId: number): Observable<QualifierType[]> {
+        return this
+            .http
+            .get(`api/ruleimplementations/${implementationId}/qualifiers`)            
+            .pipe(
+                map(response => <QualifierType[]>response),
+                catchError(err => this.handleError(err))
+            );
     }
 
-    putMoveQualifierType(id: number, moveUp: boolean = false): Promise<any> {
-        return this.http.put(`form/MoveRuleQualifierType`, { id, moveUp })
-            .toPromise()
-            .then(response => response.json())
-            .catch(err => this.handleError(err));
+    putMoveQualifierType(id: number, moveUp: boolean = false): Observable<any> {
+        return this
+            .http
+            .put(`form/MoveRuleQualifierType`, { id, moveUp })
+            .pipe(
+                map(response => <any>response),
+                catchError(err => this.handleError(err))
+            );
     }
 
-    getQualifierResolutionObjects(): Promise<ResolutionObjectType[]> {
+    getQualifierResolutionObjects(): Observable<ResolutionObjectType[]> {
         return this.http.get(`api/qualifier/resolutiontypes`)
             .toPromise()
             .then(response => <ResolutionObjectType[]>response.json())
             .catch(err => this.handleError(err));
     }
 
-    getQualifierResolutionFields(id: number, type: string): Promise<any> {
+    getQualifierResolutionFields(id: number, type: string): Observable<any> {
         return this.http.get(`fields/${type}/${id}.json`)
             .toPromise()
             .then(response => response.json())
             .catch(err => this.handleError(err));
     }
 
-    postAddRuleQualifierType(model: QualifierType): Promise<any> {
+    postAddRuleQualifierType(model: QualifierType): Observable<any> {
         return this.http.post('form/AddRuleQualifierType', model)
             .toPromise()
             .then(response => response.json())
             .catch(err => this.handleError(err));
     }
 
-    putEditRuleQualifierType(model: QualifierType): Promise<any> {
+    putEditRuleQualifierType(model: QualifierType): Observable<any> {
         return this.http.put('form/EditRuleQualifierType', model)
             .toPromise()
             .then(response => response.json())
             .catch(err => this.handleError(err));
     }
-
 }
