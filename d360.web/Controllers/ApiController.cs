3ddfb15c4e22d9d266d07f68cde2cbd9529dbd7d
seban200 <sabraham@infogix.com>
2019-06-19 15:50:00 -0400

    GOV-7362 Prevent an asset from being related to itself when Subject = Object

--- d360.web/Controllers/ApiController.cs
+++ d360.web/Controllers/ApiController.cs
@@ -4445,7 +4445,7 @@ order by C.DisplayValue";
             var result = Company.GetRelationshipFieldItems(fieldTypeID, @object, objectID, offset, rows, query, false);
             return Request.CreateResponse(HttpStatusCode.OK, new
             {
-                items = ((List<dynamic>)result["Items"]).Select(s => new System.Web.Mvc.SelectListItem { Text = s.Text, Value = s.Value.ToString(), Selected = s.Selected == 1 ? true : false }).ToList(),
+                items = ((List<dynamic>)result["Items"]).Where(x=>x.Value != objectID).Select(s => new System.Web.Mvc.SelectListItem { Text = s.Text, Value = s.Value.ToString(), Selected = s.Selected == 1 ? true : false }).ToList(),
                 count = (int)result["Count"]
             });
         }
