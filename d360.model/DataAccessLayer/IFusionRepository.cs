91d14d0c51e0901e8e5c821c7d736cb465e5e4f2
Tomislav Đurić <tduric@infogix.com>
2019-06-21 11:24:10 +0100

    - saving changes

---
+++ d360.model/DataAccessLayer/IFusionRepository.cs
@@ -0,0 +1,15 @@
+﻿
+using d360.core.entities;
+using d360.core.queue;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace d360.model.DataAccessLayer
+{
+    public interface IFusionRepository
+    {
+        Asset GetFusionByUID(Guid guid);
+        Task<ApiExecutionInfo> BulkDeleteFusionConfiguration(Guid assetUid, bool Cascade, ApiExecution execution);
+    }
+}
