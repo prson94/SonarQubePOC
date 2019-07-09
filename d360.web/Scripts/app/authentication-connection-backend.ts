89a44680d850b5241a629e5813f683ffed3c5258
Tomislav Đurić <tduric@infogix.com>
2019-07-09 12:49:56 +0100

    Revert "-removed AuthenticationConnectionBackend"
    This reverts commit 94187a9c143fcbd6834f39f4ba52ba3cc36d7784.

---
+++ d360.web/Scripts/app/authentication-connection-backend.ts
@@ -0,0 +1,27 @@
+import {throwError as observableThrowError,  Observable } from 'rxjs';
+
+import {catchError} from 'rxjs/operators';
+import {XHRBackend, Request, XHRConnection, Response, RequestMethod} from '@angular/http';
+
+export class AuthenticationConnectionBackend extends XHRBackend {
+    /* https://blog.angularindepth.com/insiders-guide-into-interceptors-and-httpclient-mechanics-in-angular-103fbdb397bf */
+    createConnection(request: Request): XHRConnection {
+        let xhrConnection = super.createConnection(request);
+
+        if (xhrConnection.request.method == RequestMethod.Post) {
+            xhrConnection.request.headers.append('RequestVerificationToken', (<HTMLInputElement>document.getElementById('antiForgeryToken')).value);
+            xhrConnection.request.headers.append('X-Requested-With', 'XMLHttpRequest');
+        }
+
+        xhrConnection.response = xhrConnection.response.pipe(catchError((error: Response) => {
+            if ((error.status === 401 || error.status === 403) && (window.location.href.match(/\?/g) || []).length < 2) {
+                console.log('The authentication session expires or the user is not authorized. Forcing refresh of the current page.');
+                window.location.href = '/slo';
+            }
+
+            return observableThrowError(error);
+        }));
+        
+        return xhrConnection;
+    }
+}
