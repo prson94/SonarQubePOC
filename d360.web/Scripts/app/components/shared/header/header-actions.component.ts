d77d767e990d1e743b7d2dd38ca371353abcfdc2
Brian Howard <bhoward@infogix.com>
2019-08-07 10:41:34 +0100

    GOV-7791 - fixing sidebar for navigating from admin only

--- d360.web/Scripts/app/components/shared/header/header-actions.component.ts
+++ d360.web/Scripts/app/components/shared/header/header-actions.component.ts
@@ -18,9 +18,9 @@ declare var CompanySettings;
                         <li class="header-action-li spacer" *ngIf="headerActionsService.showSearch"><d3s-header-typeahead-search></d3s-header-typeahead-search></li>
                         <li class="header-action-li spacer" *ngIf="hasRaiseIssueButton"><d3s-raise-issue-button></d3s-raise-issue-button></li>
                         <li class="header-action-li hide-on-med-and-down" *ngIf="showShoppingCart" ><d3s-header-shopping-cart ></d3s-header-shopping-cart></li>
-                        <li class="header-action-li hide-on-med-and-down" *ngIf="headerActionsService.showFavorite && !isAdminUrl && !isSidebarUrl" ><d3s-header-favorites [uri]="uri" [favItems]="favItems" [currentObject]="currentObject" [currentObjectId]="currentObjectId"></d3s-header-favorites></li>
-                        <li class="header-action-li hide-on-med-and-down" *ngIf="headerActionsService.showFavorite && !isAdminUrl && !isSidebarUrl" ><d3s-header-homepage [uri]="uri" [favItems]="favItems" [currentObject]="currentObject" [currentObjectId]="currentObjectId"></d3s-header-homepage></li>
-                        <li class="header-action-li hide-on-med-and-down" *ngIf="headerActionsService.showFollow  && !isAdminUrl && !isSidebarUrl" ><d3s-header-follow></d3s-header-follow></li>                    
+                        <li class="header-action-li hide-on-med-and-down" *ngIf="headerActionsService.showFavorite && !isAdminUrl && !isAdminSidebarUrl" ><d3s-header-favorites [uri]="uri" [favItems]="favItems" [currentObject]="currentObject" [currentObjectId]="currentObjectId"></d3s-header-favorites></li>
+                        <li class="header-action-li hide-on-med-and-down" *ngIf="headerActionsService.showFavorite && !isAdminUrl && !isAdminSidebarUrl" ><d3s-header-homepage [uri]="uri" [favItems]="favItems" [currentObject]="currentObject" [currentObjectId]="currentObjectId"></d3s-header-homepage></li>
+                        <li class="header-action-li hide-on-med-and-down" *ngIf="headerActionsService.showFollow  && !isAdminUrl && !isAdminSidebarUrl" ><d3s-header-follow></d3s-header-follow></li>                    
                         <li class="header-action-li" *ngIf="headerActionsService.showHelp"><d3s-header-help></d3s-header-help></li>
                         <li class="header-action-li hide-on-med-and-down" *ngIf="headerActionsService.showNotifications"><a href="#" title="Go to notification settings"><i class="fa fa-bell-o"></i></a></li>
                         <li class="header-action-li hide-on-med-and-down" ><d3s-header-profile></d3s-header-profile></li>                    
@@ -38,7 +38,10 @@ export class HeaderActionsComponent {
     @ViewChild('actions') actionsUIElem : any;
 
     public isAdminUrl = false;
-    public isSidebarUrl = false;
+    public isAdminSidebarUrl = false;
+    public previousUrl: string;
+    public currentUrl: string;
+
     private uri = "";
     public notTopArtifact: boolean = true;
     public testuri: string[] = [];
@@ -67,6 +70,9 @@ export class HeaderActionsComponent {
     ngOnInit() {
         this.routerSub = this.router.events.subscribe(e => {
             if (e instanceof NavigationEnd) {
+                this.previousUrl = this.currentUrl;
+                this.currentUrl = e.url;
+                this.isAdminSidebarUrl = false;
                 this.uri = _.trimStart(e.urlAfterRedirects, '/');
                 if ((this.uri || '').toUpperCase().startsWith(SiteUrlHelpers.SITE_URL_ARTIFACT_ROOT.toUpperCase())) {
                     this.testuri = this.uri.split("/");
@@ -80,11 +86,18 @@ export class HeaderActionsComponent {
                     this.notTopArtifact = true;
                 }
 
-                //dont show raise issue button on raise issue screen or any admin screens or user profile           
+                //dont show raise issue button on raise issue screen or any admin screens or user profile    
                 this.isAdminUrl = (this.uri || '').toUpperCase().startsWith(SiteUrlHelpers.SITE_URL_ADMIN_ROOT.toUpperCase());
                 let isResourceUrl = (this.uri || '').toUpperCase().startsWith(SiteUrlHelpers.SITE_URL_RESOURCE_ROOT.toUpperCase());
-                this.isSidebarUrl = (this.uri || '').toUpperCase().startsWith('sidebar'.toUpperCase());
-                this.hasRaiseIssueButton = ((!e.urlAfterRedirects.toLowerCase().endsWith('workflow/raiseissue') && !this.isAdminUrl && !isResourceUrl && !this.isSidebarUrl && (CompanySettings.DisableIssueManagement==='false') ) == true);                
+
+                if (this.previousUrl) {
+                    console.log(this.previousUrl + " " + this.uri);
+                    this.previousUrl = _.trimStart(this.previousUrl, '/');
+                    this.isAdminSidebarUrl = (this.uri || '').toUpperCase().startsWith('sidebar'.toUpperCase()) && (this.previousUrl || '').toUpperCase().startsWith(SiteUrlHelpers.SITE_URL_ADMIN_ROOT.toUpperCase());
+                    console.log(this.isAdminSidebarUrl);
+                }
+
+                this.hasRaiseIssueButton = ((!e.urlAfterRedirects.toLowerCase().endsWith('workflow/raiseissue') && !this.isAdminUrl && !isResourceUrl && !this.isAdminSidebarUrl && (CompanySettings.DisableIssueManagement === 'false')) == true);                
                 setTimeout(() => { this.calculateControlWidth();}, 250);
             }
         });
