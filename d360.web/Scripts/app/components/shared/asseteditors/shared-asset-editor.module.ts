3ca8451ae30a240c3a28371868b89f98bac18221
Kenneth McNamee <kmcnamee@infogix.com>
2019-08-14 20:54:53 -0400

    GOV-7873 created asset delete control removed artifact delete endpoints.

---
+++ d360.web/Scripts/app/components/shared/asseteditors/shared-asset-editor.module.ts
@@ -0,0 +1,59 @@
+﻿import { NgModule } from '@angular/core';
+import { CommonModule, DeprecatedI18NPipesModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { GovernRequestInterceptor } from "../../../http-interceptors/govern-request.interceptor";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import {
+    ButtonModule,
+    ColorPickerModule,
+    DropdownModule,
+    SpinnerModule,
+    EditorModule,
+    InputTextModule,
+    SharedModule,
+} from 'primeng/primeng';
+
+import { CoreModule } from '../core.module';
+import { TilesModule } from '../tiles/tiles.module';
+import { SharedDeleteFormModule } from '../delete.form';
+
+import { AssetDeleteEditorComponent } from './asset-delete-editor.component';
+
+@NgModule({
+    imports: [CommonModule,
+        DeprecatedI18NPipesModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+        FormsModule,
+        RouterModule,
+        //d3s
+        CoreModule,
+        TilesModule,
+        SharedDeleteFormModule,
+
+        //prime        
+        ButtonModule,
+        ColorPickerModule,
+        DropdownModule,
+        SpinnerModule,
+        EditorModule,
+        InputTextModule,
+        SharedModule,
+    ],
+    declarations: [
+        AssetDeleteEditorComponent
+    ],
+    exports: [
+        AssetDeleteEditorComponent
+    ],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: GovernRequestInterceptor,
+            multi: true
+        },
+    ]
+})
+export class SharedAssetEditorsModule { }
