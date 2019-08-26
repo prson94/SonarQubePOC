681e12731879afa5a6e83ed3b734d81185172739
Kenneth McNamee <kmcnamee@infogix.com>
2019-08-26 13:23:58 -0400

    fixed issue with optional parameters

--- igx.UnitTests/Core/BaseTestClass.cs
+++ igx.UnitTests/Core/BaseTestClass.cs
@@ -130,7 +130,7 @@ namespace igx.UnitTests
             mockRepo.Setup(x => x.GetPredicateByUID(It.IsAny<Guid>()))
                 .Returns((Guid uid) => uid == Guid.Parse(DataConstants.ValidGUID) ? new Predicate() { UID = uid, Type = PredicateType.InterTypeHierarchy } : null);
 
-            mockRepo.Setup(x => x.PostAssets(It.IsAny<List<AssetInsert>>(), It.IsAny<AssetType>(), It.IsAny<ApiExecution>(), true))
+            mockRepo.Setup(x => x.PostAssets(It.IsAny<List<AssetInsert>>(), It.IsAny<AssetType>(), It.IsAny<ApiExecution>(), true,true))
                 .Returns((List<AssetInsert> assetInsertList, object o2, object o3, object o4) =>
                  {
                      if (assetInsertList.Count == 0) return null;
@@ -138,7 +138,7 @@ namespace igx.UnitTests
                  }
                 );
 
-            mockRepo.Setup(x => x.PutAssets(It.IsAny<List<AssetUpdate>>(), It.IsAny<AssetType>(), It.IsAny<ApiExecution>(), true))
+            mockRepo.Setup(x => x.PutAssets(It.IsAny<List<AssetUpdate>>(), It.IsAny<AssetType>(), It.IsAny<ApiExecution>(), true,true))
                 .Returns((List<AssetUpdate> assetUpdateList, object o2, object o3, object o4) =>
                 {
                     if (assetUpdateList.Count == 0) return null;
@@ -146,7 +146,7 @@ namespace igx.UnitTests
                 }
                 );
 
-            mockRepo.Setup(x => x.DeleteAsset(It.IsAny<AssetDeletes>(), It.IsAny<AssetType>(), It.IsAny<ApiExecution>()))
+            mockRepo.Setup(x => x.DeleteAsset(It.IsAny<AssetDeletes>(), It.IsAny<AssetType>(), It.IsAny<ApiExecution>(), true))
                 .Returns((AssetDeletes assetDeletes, object o2, object o3) =>
                 {
                     if (assetDeletes == null) return null;
@@ -160,7 +160,7 @@ namespace igx.UnitTests
             mockRepo.Setup(x => x.PutBulkAssets(It.IsAny<Guid>(), It.IsAny<List<AssetUpdate>>(), It.IsAny<ApiExecution>(), It.IsAny<bool>()))
                .Returns(Task.FromResult(new ApiExecutionInfo()));
 
-            mockRepo.Setup(x => x.BulkDeleteAssets(It.IsAny<Guid>(), It.IsAny<AssetDeletes>(), It.IsAny<ApiExecution>()))
+            mockRepo.Setup(x => x.BulkDeleteAssets(It.IsAny<Guid>(), It.IsAny<AssetDeletes>(), It.IsAny<ApiExecution>(),true))
                .Returns(Task.FromResult(new ApiExecutionInfo()));
 
             mockRepo.Setup(x => x.GetExecutionItemByUid(It.IsAny<Guid>()))
