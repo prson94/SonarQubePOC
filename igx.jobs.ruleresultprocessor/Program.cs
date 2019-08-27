2b929d272ab4adbc224bc445573682a9fc1e5214
seban200 <sabraham@infogix.com>
2019-08-27 18:02:42 -0400

    GOV-7625 ,GOV-7624 Stop using ReferenceItemType Table change to AssetType Table

--- igx.jobs.ruleresultprocessor/Program.cs
+++ igx.jobs.ruleresultprocessor/Program.cs
@@ -76,9 +76,8 @@ outer apply (
 			select	top 1
 					'ReferenceItem' as Object,
 					O.ObjectID as ObjectID
-			from	Asset O
-					inner join AssetType AT on AT.ID = O.AssetTypeID 
-					where  QT.ResolutionFieldTypeID = 0 and QT.ResolutionFieldTypeName = 'Name' and QT.ResolutionObject = 'ReferenceItemType'  and AT.[Object]='ReferenceItemType' and AT.ObjectID = QT.ResolutionObjectID and (O.DisplayValue = Q.Value OR O.Code = Q.Value)
+			from	AssetDetail O
+					where O.[Object] = 'ReferenceItem' and QT.ResolutionFieldTypeID = 0 and QT.ResolutionFieldTypeName = 'Name' and QT.ResolutionObject = 'ReferenceItemType' and O.TypeID = QT.ResolutionObjectID and O.DisplayValue = Q.Value
 			) R_R
 outer apply (
 			select	top 1
