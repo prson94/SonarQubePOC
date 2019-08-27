c08d7dc7d1d2d34cf4f5d0a69b20af7163959b8c
Brian Howard <bhoward@infogix.com>
2019-08-27 11:30:40 +0100

    GOV-8009 - passing search params along with enter event

--- d360.web/Scripts/app/components/search/home-search.component.ts
+++ d360.web/Scripts/app/components/search/home-search.component.ts
@@ -63,7 +63,7 @@ export class HomeSearchComponent extends BaseComponent {
     }
 
     private navigateSearch() {
-        this.router.navigateByUrl(SiteUrlHelpers.SITE_URL_SEARCH_ROOT);
+        this.router.navigateByUrl(`${SiteUrlHelpers.SITE_URL_SEARCH_ROOT}?query=${this.searchText ? encodeURIComponent(this.searchText) : ''}&advanced=0&types=${this.searchTypes ? this.searchTypes.join(',') : ''}`);
     }
 
     private paginate(event) {        
