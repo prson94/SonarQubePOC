fa7fbf1c8892929f6168876cc11e7f727a116a67
Michael Donato <mdonato@infogix.com>
2019-08-28 16:58:21 -0400

    -add webjob

---
+++ igx.jobs.assetsyncprocessor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace igx.jobs.assetsyncprocessor
+{
+    class Program
+    {
+        static void Main()
+        {
+            var config = new JobHostConfiguration();
+
+#if DEBUG
+            config.UseDevelopmentSettings();
+#endif
+
+            var host = new JobHost(config);
+            host.RunAndBlock();
+        }
+    }
+}
