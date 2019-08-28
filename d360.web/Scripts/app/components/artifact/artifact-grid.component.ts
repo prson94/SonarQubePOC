f7d162592c8816d786e9f89f424807ffbff9926d
Kenneth McNamee <kmcnamee@infogix.com>
2019-08-27 20:05:40 -0400

    GOV-8037 reverted as timing in prod causes these not to be loaed before the data...

--- d360.web/Scripts/app/components/artifact/artifact-grid.component.ts
+++ d360.web/Scripts/app/components/artifact/artifact-grid.component.ts
@@ -192,8 +192,7 @@ export class ArtifactGridComponent extends BaseComponent implements OnChanges {
         );
     }
 
-    getData() {
-        if (!this.columns || this.columns.length == 0) return;
+    getData() {        
         this.isLoading = true;        
         this.artifactService.getArtifacts(this.artifactType.ID, this.rowsPerPage, this.stateService.artifactTypeFilters.currentPageNumber, this.stateService.artifactTypeFilters.sortField, this.stateService.artifactTypeFilters.sortOrder, this.stateService.artifactTypeFilters.filters, this.stateService.artifactTypeFilters.relationships, this.stateService.artifactTypeFilters.attributes, this.stateService.artifactTypeFilters.simpleTextFilter, this.stateService.artifactTypeFilters.owners).pipe(debounceTime(3000))
             .subscribe(result => {
