cc8e6a59d75a95e15f9d15d150a7f998da27640a
Thomas Klok <tklok@infogix.com>
2019-09-05 13:20:29 -0400

    GOV-7681 Include asset tags in search index. Streamlined search results TypeaheadResult(c#)/SearchResult(angular) is a parent class of IndexResult(c#)/SearchFullResult(angular). Moved some formattiong logic from ElasticSearchSource to UI components

--- d360.core/queue/IndexObjectModel.cs
+++ d360.core/queue/IndexObjectModel.cs
@@ -24,6 +24,8 @@ namespace d360.core.queue
 
         public Dictionary<string, string> Fields { get; set; }
 
+        public Dictionary<string, string> Tags { get; set; }
+
         /// <summary>
         /// Returns the unique id for this search item
         /// </summary>
