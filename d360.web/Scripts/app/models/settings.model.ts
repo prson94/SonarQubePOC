3ece560f515b6f75fb1ca9f5d33e3ee3c9b42a72
Thomas Klok <tklok@infogix.com>
2019-09-06 12:03:42 -0400

    GOV-8130 Global Search : User option - settings value renamed

--- d360.web/Scripts/app/models/settings.model.ts
+++ d360.web/Scripts/app/models/settings.model.ts
@@ -97,7 +97,7 @@ export module SettingsHelper {
             { title: "Model", value: "Taxonomy", selected: false },
             { title: "Policy", value: "Policy", selected: false },
             { title: "Reference", value: "Reference", selected: false },
-            { title: "User", value: "Users", selected: false },
+            { title: "User", value: "Resource", selected: false },
             { title: "Grammatic Type", value: "Synonym", selected: false },
             { title: "Data Quality", value: "Rule", selected: false }
         ];
