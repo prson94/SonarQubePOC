de4ec95baf39df193ec19cc9328199f65fc59b02
Michael Donato <mdonato@infogix.com>
2019-09-09 13:17:38 -0400

    -initial files

---
+++ igx.jobs.assetgraphprocessor/ProcessSubscriber.cs
@@ -0,0 +1,22 @@
+﻿using d360.core.queue;
+using Microsoft.Azure.WebJobs;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace igx.jobs.assetsyncprocessor2
+{
+    public class ProcessSubscriber
+    {
+        const string functionName = "AssetGraphProcessor_ProcessSubscriber";
+        public static async Task Run([QueueTrigger("%DisplayValueQueue%"), StorageAccount("QueueStorageAccount")] string myQueueItem, TextWriter log)
+        {
+            var updateInfo = JsonConvert.DeserializeObject<GraphInfo>(myQueueItem);
+
+        }
+    }
+}
