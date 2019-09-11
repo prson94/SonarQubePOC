8a094586fe3be1cdc2dbd8dd8f4a79a0725e1446
Ken McNamee <kmcnamee@infogix.com>
2019-09-11 15:51:07 -0400

    GOV-7856 fixed unit tests

--- igx.UnitTests/Core/BaseTestClass.cs
+++ igx.UnitTests/Core/BaseTestClass.cs
@@ -131,7 +131,7 @@ namespace igx.UnitTests
                 .Returns((Guid uid) => uid == Guid.Parse(DataConstants.ValidGUID) ? new Predicate() { UID = uid, Type = PredicateType.InterTypeHierarchy } : null);
 
             mockRepo.Setup(x => x.PostAssets(It.IsAny<List<AssetInsert>>(), It.IsAny<AssetType>(), It.IsAny<ApiExecution>(), true,true,false))
-                .Returns((List<AssetInsert> assetInsertList, object o2, object o3, object o4, object o5) =>
+                .Returns((List<AssetInsert> assetInsertList, object o2, object o3, object o4, object o5, object o6) =>
                  {
                      if (assetInsertList.Count == 0) return null;
                      else return new List<DatabaseBulkAssetResult>() { };
@@ -139,7 +139,7 @@ namespace igx.UnitTests
                 );
 
             mockRepo.Setup(x => x.PutAssets(It.IsAny<List<AssetUpdate>>(), It.IsAny<AssetType>(), It.IsAny<ApiExecution>(), true,true,false))
-                .Returns((List<AssetUpdate> assetUpdateList, object o2, object o3, object o4, object o5) =>
+                .Returns((List<AssetUpdate> assetUpdateList, object o2, object o3, object o4, object o5, object o6) =>
                 {
                     if (assetUpdateList.Count == 0) return null;
                     else return new List<DatabaseBulkAssetResult>() { };
