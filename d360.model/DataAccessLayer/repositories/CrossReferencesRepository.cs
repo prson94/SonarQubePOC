c8bc57b931d41a3387f517239c4086b4c21294e9
Tomislav Đurić <tduric@infogix.com>
2019-10-23 11:38:38 +0100

    - remove unused or conflicted references

--- d360.model/DataAccessLayer/repositories/CrossReferencesRepository.cs
+++ d360.model/DataAccessLayer/repositories/CrossReferencesRepository.cs
@@ -249,9 +249,9 @@ namespace d360.model.DataAccessLayer
                 var resultsJson = StorageProvider.GetFileContentsAsString(executionInfo.StorageFolder, executionInfo.ResponseFileName);
                 bulkResult.Results = JsonConvert.DeserializeObject<List<AssetCrossReferenceResult>>(resultsJson);
             }
-            catch (Exception e)
+            catch (Exception)
             {
-               
+                bulkResult.Error = 1;
             }
            
 
