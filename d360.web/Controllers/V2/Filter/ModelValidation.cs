4fe14d530d1270b8f4276373deff3feec8dc8579
Tomislav Đurić <tduric@infogix.com>
2019-10-29 15:52:30 +0000

    - add model validation to api v2 endpoints

---
+++ d360.web/Controllers/V2/Filter/ModelValidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace d360.web.Controllers.V2
+{
+    public class ValidateModelAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            if (!actionContext.ModelState.IsValid)
+            {
+                throw new Exception("You have not provided a valid JSON structure for this request.");
+            }
+        }
+    }
+}
