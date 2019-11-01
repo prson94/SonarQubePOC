6c223820dd058ed0748ca5052417c41f427b47b1
Ken McNamee <kmcnamee@infogix.com>
2019-11-01 17:33:30 -0400

    unit test

--- igx.UnitTests/Core/BaseTestClass.cs
+++ igx.UnitTests/Core/BaseTestClass.cs
@@ -250,13 +250,13 @@ namespace igx.UnitTests
                 .Returns((Guid guid) => guid == Guid.Parse(DataConstants.InvalidGUID) ? Task.FromResult(0) : Task.FromResult(1));
 
             mock.Setup(x => x.DeleteCrossReferenceByDataSource(It.IsAny<string>(), It.IsAny<int>()))
-              .Returns((string ds) => ds == DataConstants.ValidDataSource ? Task.FromResult(1) : Task.FromResult(0));
+              .Returns((string ds,int tout) => ds == DataConstants.ValidDataSource ? Task.FromResult(1) : Task.FromResult(0));
 
             mock.Setup(x => x.DeleteCrossReferenceByDataSource(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<int>()))
-             .Returns((string ds, string type) => ds == DataConstants.ValidDataSource ? Task.FromResult(1) : Task.FromResult(0));
+             .Returns((string ds, string type, int tout) => ds == DataConstants.ValidDataSource ? Task.FromResult(1) : Task.FromResult(0));
 
             mock.Setup(x => x.DeleteCrossReferenceByType(It.IsAny<string>(), It.IsAny<int>()))
-              .Returns((string t) => t == DataConstants.ValidType ? Task.FromResult(1) : Task.FromResult(0));
+              .Returns((string t, int tout) => t == DataConstants.ValidType ? Task.FromResult(1) : Task.FromResult(0));
 
             return mock.Object;
         }
