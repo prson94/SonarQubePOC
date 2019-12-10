c0b48836b440a346c728b06ddf682581567c9aee
Tomislav Đurić <tduric@infogix.com>
2019-12-10 16:27:21 +0000

    - change from 403 to 401 on tag delete

--- d360.web/Controllers/V2/TagsController.cs
+++ d360.web/Controllers/V2/TagsController.cs
@@ -112,7 +112,7 @@ namespace d360.web.Controllers.V2
         public IHttpActionResult DeleteById(Guid tagUid, bool cascade = false)
         {
             if (!tagRepository.IsAuthorizedToEditTag(tagUid))
-                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.Forbidden, "Access Denied"));
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Access Denied"));
 
             try
             {
