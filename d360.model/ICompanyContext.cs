33f85f01072d8d696930640ae795db1803c4945b
Tomislav Đurić <tduric@infogix.com>
2020-01-13 17:00:56 +0000

    - add ProcessingStartedOn  column to api.Execution table

--- d360.model/ICompanyContext.cs
+++ d360.model/ICompanyContext.cs
@@ -327,5 +327,6 @@ namespace d360.model
         List<PredicateUpsertResult> UpdatePredicates(ApiExecution execution, PredicateUpserts import, int timeout = 3600);
         List<ResponsibilityTypeUpsertResult> UpsertResponsibilityTypes(ApiExecution execution, List<ResponsibilityTypeUpsertModel> import, int timeout = 3600);
         string GetIconText(string assetName);
+        void SetApiExecutionProcessingStartTime(Guid ExecutionId);
     }
 }
