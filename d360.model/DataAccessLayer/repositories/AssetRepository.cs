4ca53dd3a014b919908a43faaa0c90e1040be592
Michael Donato <mdonato@infogix.com>
2020-01-23 09:35:09 -0500

    GOV-8979 - fix tags and move column value logic

--- d360.model/DataAccessLayer/repositories/AssetRepository.cs
+++ d360.model/DataAccessLayer/repositories/AssetRepository.cs
@@ -491,9 +491,94 @@ namespace d360.model.DataAccessLayer
         {
             var results = await GetAssets(uid, queryParams);
             var assetType = CompanyContext.AssetTypes.FirstOrDefault(t => t.uid == uid);
-            var fieldTypes = CompanyContext.FieldTypes.Where(f => f.AssetTypeID == assetType.ID).ToList();
+            var fields = CompanyContext.FieldTypes.Where(f => f.AssetTypeID == assetType.ID).ToList();
 
-            return getExcelFromApiModel(results, fieldTypes);
+            var typesToAvoid = new List<string>() {
+                DataType.Attribute.ToString(),
+                DataType.ComplexRelationLookup.ToString(),
+                DataType.DataTableSelect.ToString(),
+                DataType.FilteredLookup.ToString(),
+                DataType.OwnershipLookup.ToString()
+            };
+
+            //add default fields
+            fields.Add(new FieldType { Type = "string", Name = "Code", FriendlyName = "Code" });
+            fields.Add(new FieldType { Type = "string", Name = "Path", FriendlyName = "Path" });
+            fields.Add(new FieldType { Type = "date", Name = "UpdatedOn", FriendlyName = "Updated On" });
+            fields.Add(new FieldType { Type = "date", Name = "CreatedOn", FriendlyName = "Created On" });
+            fields.Add(new FieldType { Type = "string", Name = "AssetUid", FriendlyName = "Asset Uid" });
+            fields.Add(new FieldType { Type = "number", Name = "AssetId", FriendlyName = "Asset Id" });
+            fields.Add(new FieldType { Type = "number", Name = "AssetTypeId", FriendlyName = "Asset Type Id" });
+            fields.Add(new FieldType { Type = "string", Name = "AssetTypeUid", FriendlyName = "Asset Type Uid" });
+
+
+            var rowData = results.items.ToList();
+
+            var document = new SLDocument();
+            const string assetSheetName = "Assets";
+            const string apiSheetName = "Api Info";
+
+
+            #region Populate Excel Document
+
+            document.RenameWorksheet(SLDocument.DefaultFirstSheetName, assetSheetName);
+
+            document.AddWorksheet(apiSheetName);
+            document.SelectWorksheet(apiSheetName);
+
+            document.SetCellValue(1, 1, "pageSize");
+            document.SetCellValue(1, 2, results.pageSize);
+            document.SetCellValue(2, 1, "pageNum");
+            document.SetCellValue(2, 2, results.pageNum);
+            document.SetCellValue(3, 1, "total");
+            document.SetCellValue(3, 2, results.total);
+
+
+            document.SelectWorksheet(assetSheetName);
+
+            int index = 1;
+
+            foreach (var field in fields)
+            {
+                if (typesToAvoid.Contains(field.Type))
+                    continue;
+                document.SetCellValue(1, index++, (string)field.FriendlyName);
+            }
+
+
+            if (rowData == null || rowData.Count == 0)
+            {
+                var s = new MemoryStream();
+                document.SaveAs(s);
+                return document;
+            }
+
+
+            int rowNumber = 1;
+            foreach (var row in rowData)
+            {
+                index = 1;
+                rowNumber++;
+                var rowValues = (row as IDictionary<string, object>);
+
+                foreach (var field in fields)
+                {
+                    if (typesToAvoid.Contains(field.Type))
+                        continue;
+
+                    if (rowValues.ContainsKey(field.Name))
+                    {
+                        var val = rowValues[field.Name];
+                        setCellValueFromField(document, rowNumber, index, field, val);
+                    }
+
+                    index++;
+                }
+            }
+
+            #endregion
+
+            return document;
         }
         public async Task<AssetsByPathApiViewModel> GetAssetsByPath(AssetsByPathApiRequestModel model)
         {
@@ -1804,155 +1889,51 @@ OFFSET(@pageNum*@pageSize) ROWS FETCH NEXT (@pageSize) ROWS ONLY
                     return "";
             }
         }
-        private SLDocument getExcelFromApiModel(AssetsApiViewModel model, List<FieldType> fields)
+        private void setCellValueFromField(SLDocument document, int rowIndex, int colIndex, FieldType field, object value)
         {
-            if (model == null)
+            var valueString = value?.ToString() ?? "";
+            switch ((field.Type ?? "").ToUpper())
             {
-                throw new ArgumentNullException("model");
-            }
-
-            var typesToAvoid = new List<string>() {
-                DataType.Attribute.ToString(),
-                DataType.ComplexRelationLookup.ToString(),
-                DataType.DataTableSelect.ToString(),
-                DataType.FilteredLookup.ToString(),
-                DataType.OwnershipLookup.ToString()
-            };
-
-            //add default fields
-            fields.Add(new FieldType { Type = "string", Name = "Code", FriendlyName = "Code" });
-            fields.Add(new FieldType { Type = "string", Name = "Path", FriendlyName = "Path" });
-            fields.Add(new FieldType { Type = "date", Name = "UpdatedOn", FriendlyName = "Updated On" });
-            fields.Add(new FieldType { Type = "date", Name = "CreatedOn", FriendlyName = "Created On" });
-            fields.Add(new FieldType { Type = "string", Name = "AssetUid", FriendlyName = "Asset Uid" });
-            fields.Add(new FieldType { Type = "Number", Name = "AssetId", FriendlyName = "Asset Id" });
-            fields.Add(new FieldType { Type = "Number", Name = "AssetTypeId", FriendlyName = "Asset Type Id" });
-            fields.Add(new FieldType { Type = "string", Name = "AssetTypeUid", FriendlyName = "Asset Type Uid" });
-
-            
-            var rowData = model.items.ToList();
-
-            //Convert tags
-            if (fields.Any(x => x.Type == "Tag"))
-            {
-                var ft = fields.FirstOrDefault(x => x.Type == "Tag");
-                rowData.ForEach(row =>
-                {
-                    var rowDic = (IDictionary<string, object>)row;
-                    string tagKey = "Field" + ft.ID;
-
-                    if (rowDic.ContainsKey(tagKey) && rowDic[tagKey] != null)
+                case "DECIMAL":
+                    double dVal = 0;
+                    if (double.TryParse(valueString, out dVal))
+                        document.SetCellValue(rowIndex, colIndex, dVal);
+                    else
+                        document.SetCellValue(rowIndex, colIndex, valueString);
+                    break;
+                case "NUMBER":
+                    int intVal = 0;
+                    if (int.TryParse(valueString, out intVal))
+                        document.SetCellValue(rowIndex, colIndex, intVal);
+                    else
+                        document.SetCellValue(rowIndex, colIndex, valueString);
+                    break;
+                case "DATE":
+                    if (DateTime.TryParse(valueString, out DateTime dateVal))
                     {
-                        string value = rowDic[tagKey].ToString();
-                        if (!string.IsNullOrEmpty(value))
+                        document.SetCellValue(rowIndex, colIndex, dateVal);
+
+                        SLStyle style = document.CreateStyle();
+                        style.FormatCode = "m/d/yyyy";
+                        document.SetCellStyle(rowIndex, colIndex, style);
+                    }
+                    break;
+                case "TAG":
+                    if (value != null)
+                    {
+                        var parsed = JsonConvert.DeserializeObject<List<TagDetailItem>>(valueString);
+                        if (parsed != null)
                         {
-                            var parsed = JsonConvert.DeserializeObject<List<TagDetailItem>>(value);
-                            if (parsed != null)
-                                rowDic[tagKey] = string.Join("|", parsed.Select(x => x.Value));
+                            document.SetCellValue(rowIndex, colIndex, string.Join("|", parsed.Select(x => x.Value)));
                         }
                     }
-                });
+                    break;
+                default:
+                    if (valueString.StartsWith("="))
+                        valueString = "'" + valueString;
+                    document.SetCellValue(rowIndex, colIndex, valueString);
+                    break;
             }
-
-            var document = new SLDocument();
-            const string assetSheetName = "Assets";
-            const string apiSheetName = "Api Info";
-
-            document.RenameWorksheet(SLDocument.DefaultFirstSheetName, assetSheetName);
-
-
-            #region Populate Excel Document
-
-            int index = 1;
-
-
-            foreach (var field in fields)
-            {
-                if (typesToAvoid.Contains(field.Type))
-                    continue;
-                document.SetCellValue(1, index++, (string)field.FriendlyName);
-            }
-
-
-            if (rowData == null || rowData.Count == 0)
-            {
-                var s = new MemoryStream();
-                document.SaveAs(s);
-                return document;
-            }
-
-
-            int rowNumber = 1;
-            foreach (var row in rowData)
-            {
-                index = 1;
-                rowNumber++;
-                var rowValues = (row as IDictionary<string, object>);
-
-                foreach (var field in fields)
-                {
-                    if (typesToAvoid.Contains(field.Type))
-                        continue;
-
-                    if (rowValues.ContainsKey(field.Name))
-                    {
-                        var val = rowValues[field.Name]?.ToString() ?? "";
-
-                        switch ((field.Type ?? "").ToUpper())
-                        {
-                            case "DECIMAL":
-                                double dVal = 0;
-                                if (double.TryParse(val, out dVal))
-                                    document.SetCellValue(rowNumber, index, dVal);
-                                else
-                                    document.SetCellValue(rowNumber, index, val);
-                                break;
-                            case "NUMBER":
-                                int intVal = 0;
-                                if (int.TryParse(val, out intVal))
-                                    document.SetCellValue(rowNumber, index, intVal);
-                                else
-                                    document.SetCellValue(rowNumber, index, val);
-                                break;
-                            case "DATE":
-                                if (DateTime.TryParse((val ?? "").ToString(), out DateTime dateVal))
-                                {
-                                    document.SetCellValue(rowNumber, index, dateVal);
-
-                                    SLStyle style = document.CreateStyle();
-                                    style.FormatCode = "m/d/yyyy";
-                                    document.SetCellStyle(rowNumber, index, style);
-                                }
-                                break;
-                            default:
-                                if (val.StartsWith("="))
-                                    val = "'" + val;
-                                document.SetCellValue(rowNumber, index, val);
-                                break;
-                        }
-                    }
-
-                    index++;
-                }
-            }
-
-
-
-            document.AddWorksheet(apiSheetName);
-            document.SelectWorksheet(apiSheetName);
-
-            document.SetCellValue(1, 1,"pageSize");
-            document.SetCellValue(1, 2, model.pageSize);
-            document.SetCellValue(2, 1, "pageNum");
-            document.SetCellValue(2, 2, model.pageNum);
-            document.SetCellValue(3, 1, "total");
-            document.SetCellValue(3, 2, model.total);
-
-
-            document.SelectWorksheet(assetSheetName);
-            #endregion
-
-            return document;
         }
         #endregion
 
