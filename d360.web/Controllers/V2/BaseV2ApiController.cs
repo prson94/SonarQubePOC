feb34e145cf9f0d0750e2061ab8934bac960e171
Brian Howard <bhoward@infogix.com>
2020-02-04 13:41:30 +0000

    GOV-9566 - catching error when spreadsheetlight tries to auto size very large column widths

--- d360.web/Controllers/V2/BaseV2ApiController.cs
+++ d360.web/Controllers/V2/BaseV2ApiController.cs
@@ -608,16 +608,24 @@ namespace d360.web.Controllers.V2
             int index = 1;
             foreach (var field in fields)
             {
-                if (field.ColumnWidth.HasValue)
+                try
                 {
-                    int width = field.ColumnWidth.Value > 0 ? field.ColumnWidth.Value / 10 : 0;
-                    document.SetColumnWidth(index, width);
+                    if (field.ColumnWidth.HasValue)
+                    {
+                        int width = field.ColumnWidth.Value > 0 ? field.ColumnWidth.Value / 10 : 0;
+                        document.SetColumnWidth(index, width);
+                    }
+                    else
+                    {
+                        document.AutoFitColumn(index);
+                    }
+                    index++;
                 }
-                else
+                catch (Exception e)
                 {
-                    document.AutoFitColumn(index);
+                    document.SetColumnWidth(index, 10);
+                    index++;
                 }
-                index++;
             }
         }
         #endregion
