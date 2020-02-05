f0378242857e00fed84aedf3011ad404464c5935
pungupta <pungupta@infogix.com>
2020-02-05 10:06:43 +0530

    GOV-9546 Correct response code for invalid UID of asset type (GET /api/v2/assets/{assetTypeUid})

--- d360.model/validators/AssetTypeValidator.cs
+++ d360.model/validators/AssetTypeValidator.cs
@@ -230,5 +230,14 @@ namespace d360.core.validators
 
             return true;
         }
+        public bool IsValidAssetTypeByUIDForGetAssets(Guid uid)
+        {
+            var assetType = CompanyContext.AssetTypes.FirstOrDefault(t => t.uid == uid);
+            if (assetType == null)
+                return false;
+            else
+                return true;
+
+        }
     }
 }
