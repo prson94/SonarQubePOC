f592659f6735a710cd6a2f2498194e75c8ce44bb
Thomas Klok <tklok@infogix.com>
2020-02-18 15:46:57 -0500

    - tweak dynamic-percentage-component so it's easier to style/change line size

--- d360.web/Scripts/app/components/shared/small-widgets/dynamic-percentage/dynamic-percentage-component.ts
+++ d360.web/Scripts/app/components/shared/small-widgets/dynamic-percentage/dynamic-percentage-component.ts
@@ -8,17 +8,20 @@ import * as _ from 'lodash';
 	selector: 'd3s-dynamic-percentage',
 	template: `
 				  <div class="d3s-chart">
-                    <svg viewBox="0 0 38 38" class="d3s-circular-chart">
-                      <path class="d3s-circle-bg"
-                        d="M18 2.0845
-                          a 15.9155 15.9155 0 0 1 0 31.831
-                          a 15.9155 15.9155 0 0 1 0 -31.831"
-                      />
+                    <svg viewBox="-16 -16 32 32" class="d3s-circular-chart">
+                      <defs>
+                        <circle id="basecircle" cx="0" cy="0" r="15.9155" />
+                        <clipPath id="clip">
+                            <use xlink:href="#basecircle"/>
+                        </clipPath>
+                      </defs>
+                      <use xlink:href="#basecircle" class="d3s-circle-bg" clip-path="url(#clip)"/>
                       <path *ngIf="percentage > 0" class="d3s-circle"
                         [attr.stroke-dasharray]="percentage + ', 100'"
-                        d="M18 2.0845
+                        d="M0 -15.9155
                           a 15.9155 15.9155 0 0 1 0 31.831
                           a 15.9155 15.9155 0 0 1 0 -31.831"
+                        clip-path="url(#clip)"
                       />
                     </svg>
                   </div>
