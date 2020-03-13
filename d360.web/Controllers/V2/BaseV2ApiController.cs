bd91b7d7a8a72dff2e39d73c577aef06e426c219
Shane Dooley <sdooley@infogix.com>
2020-03-13 11:15:45 +0000

    GOV-9910 - Adding error message for when length is greater then 10

--- d360.web/Controllers/V2/BaseV2ApiController.cs
+++ d360.web/Controllers/V2/BaseV2ApiController.cs
@@ -203,6 +203,8 @@ namespace d360.web.Controllers.V2
             if (parameters.Any(q => q.Key == "_pageSize"))
             {
                 var _pageSize = queryParams.ToList().FirstOrDefault(q => q.Key == "_pageSize").Value;
+                if(_pageSize.Length > 10)
+                    return "Invalid pageSize value provided.";
                 if (long.TryParse(_pageSize, out pageSize))
                 {
                     if (pageSize > 200000) return "Invalid pageSize value provided. Number is too large";
@@ -215,6 +217,8 @@ namespace d360.web.Controllers.V2
             if (parameters.Any(q => q.Key == "_pageNum"))
             {
                 var _pageNum = queryParams.ToList().FirstOrDefault(q => q.Key == "_pageNum").Value;
+                if(_pageNum.Length > 10)
+                    return "Invalid pageNum value provided.";
                 if (long.TryParse(_pageNum, out pageNum))
                 {
                     if (pageNum > 10000) return "Invalid pageNum value provided. Number is too large";
