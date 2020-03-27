3d4ac081b222f1338da2335fff8792439704c3ab
Tomislav Đurić <tduric@infogix.com>
2020-03-27 12:23:17 +0000

    - rollback rollbacked changes

--- d360.model/DataAccessLayer/repositories/MetricsRepository.cs
+++ d360.model/DataAccessLayer/repositories/MetricsRepository.cs
@@ -463,6 +463,13 @@ namespace d360.model.DataAccessLayer
                     from	dbo.Asset A
                     		inner join AssetType T on T.ID = A.AssetTypeID and A.[Uid] = @assetUid;
 
+                    declare @lastScoredDate date = (select top 1 RunDate from metrics.score where AssetUid = @assetUid order by RunDate desc)
+
+                    if @effectiveDate > @lastScoredDate
+                    begin
+	                    set @effectiveDate = @lastScoredDate
+                    end
+
                     select 
                         ma.[Uid], 
                         ParentUid,
