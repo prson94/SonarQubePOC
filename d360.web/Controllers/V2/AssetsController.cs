a53896489fa55ca05a321ba379ccedaf14955e59
Shane Dooley <sdooley@infogix.com>
2020-04-01 11:08:46 +0100

    GOV-10082-Adding a check to see that an asset as tag field associated wiht it before adding new tag through api

--- d360.web/Controllers/V2/AssetsController.cs
+++ d360.web/Controllers/V2/AssetsController.cs
@@ -1361,15 +1361,16 @@ namespace d360.web.Controllers.V2
                     continue;
                 }
 
+                var assetType = Company.Filter<AssetType>(x => x.ID == asset.AssetTypeID).FirstOrDefault();
+                var fieldTypes = Company.FieldTypes.Where(f => f.AssetTypeID == assetType.ID);
+                if (!fieldTypes.Any(x => x.Type.ToLower() == "tag"))
+                {
+                    return errorMessageResponse(HttpStatusCode.BadRequest, "Bad Requst", $"No Tag Fields found for AssetUID {assetTagApi.AssetUID}, Cannot add an association without a Tag field");
+                }
+
                 if (this.tagRepository.DoesAssetTagExists(currentTag.ID, asset.ID))
                 {
-                    result = new AssetTagSuccessApiModel()
-                    {
-                        Message = $"TagUID {assetTagApi.TagUID} and AssetUID {assetTagApi.AssetUID} association  already exists, it is not valid to add a second association",
-                        Success = false
-                    };
-                    resultList.Add(result);
-                    continue;
+                    return errorMessageResponse(HttpStatusCode.BadRequest, "Bad Requst", $"TagUID {assetTagApi.TagUID} and AssetUID {assetTagApi.AssetUID} association  already exists, it is not valid to add a second association");
                 }
                 if (!Company.HasAssetDefaultReadPermission(asset.Object, asset.ObjectID))
                 {
