200444143c8a26df9b948691fa8b2e1e254f02e8
Ken McNamee <kmcnamee@infogix.com>
2020-04-09 18:14:43 -0400

    GOV-10298 moved d360.utils single method into d360.core, updated version and added missing unit tests.

---
+++ d360.core/helpers/HtmlHelper.cs
@@ -0,0 +1,44 @@
+﻿using HtmlAgilityPack;
+using System.Collections.Generic;
+
+namespace d360.core.helpers
+{
+    public static class HtmlHelper
+    {
+        public static string RemoveTags(string data)
+        {
+            if (data.Length < 6) return data;
+            var document = new HtmlDocument();
+            document.LoadHtml(data);
+
+
+            var nodes = new Queue<HtmlNode>(document.DocumentNode.SelectNodes("./*|./text()"));
+
+            if (nodes == null) return data;
+
+            while (nodes.Count > 0)
+            {
+                var node = nodes.Dequeue();
+                var parentNode = node.ParentNode;
+
+                if (node.Name != "#text")
+                {
+                    var childNodes = node.SelectNodes("./*|./text()");
+
+                    if (childNodes != null)
+                    {
+                        foreach (var child in childNodes)
+                        {
+                            nodes.Enqueue(child);
+                            parentNode.InsertBefore(child, node);
+                        }
+                    }
+
+                    parentNode.RemoveChild(node);
+                }
+            }
+
+            return document.DocumentNode.InnerHtml;
+        }
+    }
+}
---
+++ igx.UnitTests/HelperTests/HtmlHelperTests.cs
@@ -0,0 +1,69 @@
+﻿using d360.model;
+using d360.model.workflow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace igx.UnitTests.HtmlHelperTests
+{
+    [Trait("Unit tests", "Helper Tests - Html Helper")]
+    public class HtmlHelperTests : BaseTest
+    {        
+        public HtmlHelperTests()
+        {
+            
+        }
+
+        [Fact]
+        public void BasicRemoveTagsHtml()
+        {
+            string html = "<div>Test</div>";
+
+            var result = d360.core.helpers.HtmlHelper.RemoveTags(html);
+
+            Assert.Equal("Test", result);
+        }
+
+        [Fact]
+        public void EmbeddedRemoveTagsHtml()
+        {
+            string html = "<div>Hi<p>Test</p></div>";
+
+            var result = d360.core.helpers.HtmlHelper.RemoveTags(html);
+
+            Assert.Equal("HiTest", result);
+        }
+
+        [Fact]
+        public void AttributeRemoveHtml()
+        {
+            string html = "<span title=\"test\">test</span>";
+
+            var result = d360.core.helpers.HtmlHelper.RemoveTags(html);
+
+            Assert.Equal("test", result);
+        }
+
+
+        [Fact]
+        public void MinLenthRemoveHtmlTest()
+        {
+            string html = "1";
+
+            var result = d360.core.helpers.HtmlHelper.RemoveTags(html);
+
+            Assert.Equal("1", result);
+        }
+
+        [Fact]
+        public void EmptyStringHtmlTest()
+        {            
+            var result = d360.core.helpers.HtmlHelper.RemoveTags(string.Empty);
+
+            Assert.Equal(string.Empty, string.Empty);
+        }
+    }
+}
