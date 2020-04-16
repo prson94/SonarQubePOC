9daa5d4f7ff6abda356818760775f7e38a10a3f3
Michael Donato <mdonato@infogix.com>
2020-04-16 12:45:28 -0400

    GOV-10070 - added preliminary sql for users post/put

--- d360.model/DataAccessLayer/repositories/MembershipRepository.cs
+++ d360.model/DataAccessLayer/repositories/MembershipRepository.cs
@@ -8,6 +8,8 @@ using System.Threading.Tasks;
 using d360.core.enums;
 using System.Net;
 using System.Data;
+using System.Text.RegularExpressions;
+using System.Data.SqlClient;
 
 namespace d360.model.DataAccessLayer
 {
@@ -126,6 +128,8 @@ namespace d360.model.DataAccessLayer
             var executionID = Guid.NewGuid();
             var results = new List<UserApiUpsertResult>();
 
+            var fieldTypes = CompanyContext.FieldTypes.Where(f => f.Object == "ResourceType").ToList();
+
             #region Data Tables
 
             var userTable = new DataTable();
@@ -142,6 +146,8 @@ namespace d360.model.DataAccessLayer
             userTable.Columns.Add("State", typeof(int));
             userTable.Columns.Add("IsAdministrator", typeof(bool));
             userTable.Columns.Add("IsNew", typeof(bool));
+            userTable.Columns.Add("Success", typeof(bool));
+            userTable.Columns.Add("Message", typeof(bool));
 
 
             fieldTable.Columns.Add("ExecutionID", typeof(Guid));
@@ -158,16 +164,9 @@ namespace d360.model.DataAccessLayer
             {
                 itemNumber++;
                 var row = userTable.NewRow();
-                var result = new UserApiUpsertResult()
-                { 
-                    ExecutionItemUid = user.ExecutionItemUid,
-                    ItemNumber = itemNumber
-                };
 
                 var success = true;
                 var messages = new List<string>();
-
-                results.Add(result);
 
 
                 if (user.IsNew)
@@ -193,42 +192,181 @@ namespace d360.model.DataAccessLayer
                     }
                 }
 
-
-                if (success)
+                if (string.IsNullOrEmpty(user.Username) || !Regex.IsMatch(user.Username + "", @"^$|\b([A-Za-z0-9'_\.-]+)@([\dA-Za-z\.-]+)\.([A-Za-z\.]{2,6})\b"))
                 {
-                    row["ExecutionID"] = executionID;
-                    if (user.Uid.HasValue) row["Uid"] = user.Uid;
-                    if (user.ExecutionItemUid.HasValue) row["ExecutionItemUId"] = user.ExecutionItemUid;
-                    row["ItemNumber"] = itemNumber;
-                    row["Username"] = user.Username;
-                    row["FirstName"] = user.FirstName;
-                    row["LastName"] = user.LastName;
-                    row["Password"] = user.Password;
-                    if (user.State.HasValue) row["State"] = (int)user.State;
-                    row["IsAdministrator"] = user.IsAdministrator;
-                    row["IsNew"] = user.IsNew;
+                    success = false;
+                    messages.Add("Username is not in a valid email format");
+                }
 
-                    userTable.Rows.Add(row);
 
-                    if (user.Fields != null)
+                row["ExecutionID"] = executionID;
+                if (user.Uid.HasValue) row["Uid"] = user.Uid;
+                if (user.ExecutionItemUid.HasValue) row["ExecutionItemUId"] = user.ExecutionItemUid;
+                row["ItemNumber"] = itemNumber;
+                row["Username"] = user.Username;
+                row["FirstName"] = user.FirstName;
+                row["LastName"] = user.LastName;
+                row["Password"] = user.Password;
+                if (user.State.HasValue) row["State"] = (int)user.State;
+                row["IsAdministrator"] = user.IsAdministrator;
+                row["IsNew"] = user.IsNew;
+
+                userTable.Rows.Add(row);
+
+                if (user.Fields != null)
+                {
+                    foreach (var field in user.Fields.Keys)
                     {
-                        foreach (var field in user.Fields.Keys)
-                        {
-                            var fieldRow = fieldTable.NewRow();
-                            fieldRow["ExecutionID"] = executionID;
-                            fieldRow["ItemNumber"] = itemNumber;
-                            fieldRow["FieldName"] = field;
-                            fieldRow["FieldValue"] = user.Fields[field];
+                        var fieldType = fieldTypes.FirstOrDefault(f => f.Name == field);
 
-                            fieldTable.Rows.Add(fieldRow);
+                        if (fieldType == null)
+                        {
+                            success = false;
+                            messages.Add($"Field type for key [{field}] not found on this asset");
                         }
+
+                        var fieldRow = fieldTable.NewRow();
+                        fieldRow["ExecutionID"] = executionID;
+                        fieldRow["ItemNumber"] = itemNumber;
+                        fieldRow["FieldName"] = field;
+                        fieldRow["FieldValue"] = user.Fields[field];
+
+                        fieldTable.Rows.Add(fieldRow);
                     }
                 }
 
-                result.Success = success;
-                result.Message = string.Join(". ", messages);
-
+                row["Success"] = success;
+                row["Message"] = string.Join(". ", messages);
             }
+
+
+            #region Bulk Copy
+
+            using (SqlTransaction trans = CompanyContext.Connection.BeginTransaction())
+            {
+                try
+                {
+                    await CompanyContext.Connection.ExecuteAsync(@"
+                        drop table if exists #UserAssets;
+                        create table #UserAssets
+                        (
+                            ExecutionID uniqueidentifier not null,
+                            ExecutionItemUid uniqueidentifier,
+                            ItemNumber int not null,
+                            Username nvarchar(500),
+                            Uid uniqueidentifier,
+                            ResourceID int,
+                            FirstName nvarchar(500),
+                            LastName nvarchar(500),
+                            Password nvarchar(500),
+                            [State] int,
+                            IsAdministrator bit not null,
+                            IsNew bit not null,
+                            Success bit not null,
+                            Message nvarchar(max)
+                        );
+
+                        drop table if exists #UserFields;
+                        create table #UserFields
+                        (
+                            ExecutionID uniqueidentifier not null,
+                            ItemNumber int not null,
+                            FieldName nvarchar(250),
+                            FieldValue nvarchar(max),
+                            FieldTypeID int,
+                            LookupValue nvarchar(max)
+                        );
+
+                        ", transaction: trans);
+
+                    SqlBulkCopy bulkCopy = new SqlBulkCopy(CompanyContext.Connection, SqlBulkCopyOptions.Default, trans);
+                    bulkCopy.DestinationTableName = "#UserAssets";
+
+                    bulkCopy.ColumnMappings.Add("ExecutionID", "ExecutionID");
+                    bulkCopy.ColumnMappings.Add("ExecutionItemUid", "ExecutionItemUid");
+                    bulkCopy.ColumnMappings.Add("ItemNumber", "ItemNumber");
+                    bulkCopy.ColumnMappings.Add("Username", "Username");
+                    bulkCopy.ColumnMappings.Add("Uid", "Uid");
+                    bulkCopy.ColumnMappings.Add("FirstName", "FirstName");
+                    bulkCopy.ColumnMappings.Add("LastName", "LastName");
+                    bulkCopy.ColumnMappings.Add("Password", "Password");
+                    bulkCopy.ColumnMappings.Add("State", "State");
+                    bulkCopy.ColumnMappings.Add("IsAdministrator", "IsAdministrator");
+                    bulkCopy.ColumnMappings.Add("IsNew", "IsNew");
+                    bulkCopy.ColumnMappings.Add("Success", "Success");
+                    bulkCopy.ColumnMappings.Add("Message", "Message");
+
+                    await bulkCopy.WriteToServerAsync(userTable);
+
+                    bulkCopy = new SqlBulkCopy(CompanyContext.Connection, SqlBulkCopyOptions.Default, trans);
+                    bulkCopy.DestinationTableName = "#UserFields";
+
+                    bulkCopy.ColumnMappings.Add("ExecutionID", "ExecutionID");
+                    bulkCopy.ColumnMappings.Add("ItemNumber", "ItemNumber");
+                    bulkCopy.ColumnMappings.Add("FieldName", "FieldName");
+                    bulkCopy.ColumnMappings.Add("FieldValue", "FieldValue");
+                    bulkCopy.ColumnMappings.Add("FieldTypeID", "FieldTypeID");
+                    bulkCopy.ColumnMappings.Add("LookupValue", "LookupValue");
+
+
+                    await bulkCopy.WriteToServerAsync(fieldTable);
+
+
+                    #region Populate table values
+
+                    await CompanyContext.Connection.ExecuteAsync(@"
+                        update  U
+                        set     U.ResourceID = G.ResourceID
+                        from    #UserAssets U
+                                inner join reporting.Global_Resources G on G.[uid] = U.[Uid] and G.[State] <> @deleted
+                        where   U.Success = 1 and U.IsNew = 0;
+
+                        update  U
+                        set     U.FieldTypeID = F.ID
+                        from    #UserFields U
+                                inner join FieldType F on F.Name = U.FieldName and F.Object = 'ResourceType' and F.ObjectID = 1
+                        where   U.ExecutionID = @executionID;
+                        ", new { executionID, deleted = (int)CompanyResourceState.Deleted }, transaction: trans);
+
+                    #endregion
+
+                    #region Validation
+
+                    await CompanyContext.Connection.ExecuteAsync(@"
+                        update  U
+                        set     U.Success = 0,
+                                U.Message = U.Message + ', Resource for this uid not found'
+                        from    #UserAssets U
+                        where   U.Success = 1 and U.IsNew = 0 and U.ResourceID is null and U.ExecutionID = @executionID;
+
+                        update  U
+                        set     U.Success = 0,
+                                U.Message = U.Message + ', One or more field values supplied is missing a field type'
+                        from    #UserAssets U
+                                cross apply (
+                                    select  count(*) as MissingCount 
+                                    from    #UserFields F 
+                                    where   F.ItemNumber = U.ItemNumber 
+                                            and F.ExecutionID = U.ExecutionID
+                                            and F.FieldTypeID is null
+                                ) C
+                        where   U.Success = 1 and U.ExecutionID = @executionID and C.MissingCount > 0
+
+                        ", new { executionID, deleted = (int)CompanyResourceState.Deleted }, transaction: trans);
+
+                    #endregion
+
+
+                    trans.Commit();
+                }
+                catch (Exception ex)
+                {
+                    trans.Rollback();
+                }
+            }
+
+
+            #endregion
 
 
             return results;
