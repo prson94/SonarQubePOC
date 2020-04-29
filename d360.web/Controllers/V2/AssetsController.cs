64b4eb4b0e0aba1dbf5bd5790830a274086c74fe
Shane Dooley <sdooley@infogix.com>
2020-04-29 20:22:35 +0100

    GOV-10315- Prevent deletion of the reference list type configured as the Governance Role reference list

--- d360.web/Controllers/V2/AssetsController.cs
+++ d360.web/Controllers/V2/AssetsController.cs
@@ -1262,6 +1262,13 @@ namespace d360.web.Controllers.V2
 
             try
             {
+                var governanceRoles = Community.CompanySettings.Where(x => x.CompanyID == Company.CurrentCompanyID && x.SettingID == 73).FirstOrDefault();
+                foreach (var asset in assetTypes)
+                {
+                    if (governanceRoles != null && governanceRoles.Value == asset.Uid.ToString())
+                        return await Task.FromResult(errorMessageResponse(HttpStatusCode.BadRequest, "Invalid request", $"UID {asset.Uid} is a reference list and is configured as the Governance Role and cannot be deleted."));
+                }
+
                 if (assetTypes == null)
                     assetTypes = readRequestJsonContent<AssetTypeDeletes>(Request).Result;
 
