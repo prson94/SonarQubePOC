c5a4e4923c92165fe056d2c6cefa869c35e237ce
Brian Howard <bhoward@infogix.com>
2020-04-30 15:25:08 +0100

    GOV-6761 - fixing allowed asset class

--- d360.web/Controllers/V2/ResponsibilitiesController.cs
+++ d360.web/Controllers/V2/ResponsibilitiesController.cs
@@ -276,7 +276,7 @@ namespace d360.web.Controllers.V2
                         AssetTypeClass.Model,
                         AssetTypeClass.Rule,
                         AssetTypeClass.Policy,
-                        AssetTypeClass.ReferenceItemType
+                        AssetTypeClass.Reference
                     };
                     if (!allowedClasses.Contains(assetType.Class)) 
                     {
