cc9cf057ca9a38ad9b075de90005eef60d72ec44
Michael Donato <mdonato@infogix.com>
2020-05-05 14:42:50 -0400

    GOV-10596 - fix password validation message

--- d360.web/App_GlobalResources/Validation.Designer.cs
+++ d360.web/App_GlobalResources/Validation.Designer.cs
@@ -19,7 +19,7 @@ namespace Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Validation {
@@ -214,7 +214,7 @@ namespace Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to be between 7 and 25 characters in length; at least 1 uppercase character; at least 1 lowercase chacter; at least 1 number.
+        ///   Looks up a localized string similar to be between 7 and 25 characters in length; at least 1 uppercase character; at least 1 lowercase character; at least 1 number.
         /// </summary>
         public static string Password_Requirements {
             get {
