26e90dbd9485bd3aa79c4eb3b4a07919eae9bc7b
Tomislav Đurić <tduric@infogix.com>
2020-05-08 16:28:27 +0100

    -move delete eecution logic to controller

--- igx.jobs.apiexecutionprocessor/Program.cs
+++ igx.jobs.apiexecutionprocessor/Program.cs
@@ -185,10 +185,7 @@ namespace igx.jobs.apiexecutionprocessor
                     if (dbExecutionItem.State == d360.core.enums.State.Deleted)
                     {
                         executeJob = false;
-                        dbExecutionItem.ErrorMessage = "Execution job was canceled by user.";
-                        dbExecutionItem.CompletedOn = DateTime.UtcNow;
                         log.WriteLine($"Execution job with UID {dbExecutionItem.ExecutionID} was canceled by user.");
-
                     }
 
                     if (executeJob)
