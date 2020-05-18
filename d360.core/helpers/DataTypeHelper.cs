7524e9da884d3ebcfc5d805b945dbc27adc03c61
Tomislav Đurić <tduric@infogix.com>
2020-05-18 14:20:46 +0100

    -remove ref list relationship from advanced filters

--- d360.core/helpers/DataTypeHelper.cs
+++ d360.core/helpers/DataTypeHelper.cs
@@ -108,7 +108,8 @@ namespace d360.core.helpers
                 DataType.DataTableSelect.ToString(),
                 DataType.File.ToString(),
                 DataType.OwnershipLookup.ToString(),
-                DataType.JSON.ToString()
+                DataType.JSON.ToString(),
+                DataType.RefListRelationship.ToString()
             };
 
             return types;
