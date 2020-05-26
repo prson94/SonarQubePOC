c536229bf690a466e71562dccf7f35d94f8c32d4
Brian Howard <bhoward@infogix.com>
2020-05-26 09:38:05 +0100

    GOV-10851 - adding check for predicate uid

--- d360.web/Scripts/app/components/shared/assettypeeditor/asset-type-editor.component.ts
+++ d360.web/Scripts/app/components/shared/assettypeeditor/asset-type-editor.component.ts
@@ -187,7 +187,7 @@ export class AssetTypeEditorComponent extends BaseComponent implements OnChanges
 
 
     ShowAutoDisplayOption() {
-        if (this.showParentPredicates) {
+        if (this.showParentPredicates && this.model.AssetType.Hierarchy.PredicateUid) {
             let selectedPredicate = this.model.Predicates.filter(x => x.value.toLowerCase() == this.model.AssetType.Hierarchy.PredicateUid.toLowerCase());
             if (selectedPredicate.length == 1) {
                 let onlyAllow = "child type of";
