660a2ab14c78e5a2c706d3d3a0262c9ee7268340
Ken McNamee <kmcnamee@infogix.com>
2020-06-19 16:56:39 -0400

    GOV-11246 wrong css being used switched to minified version.

--- d360.web/Controllers/V2/MetricsController.cs
+++ d360.web/Controllers/V2/MetricsController.cs
@@ -861,7 +861,7 @@ namespace d360.web.Controllers.V2
                 }
 
             }
-            catch (Exception ex)
+            catch
             {
                 return errorMessageResponse(HttpStatusCode.InternalServerError, "Error retrieving Data Quality Results", $"An unknown error occurred and has been logged for further investigation. Please try your request again later.");
             }
