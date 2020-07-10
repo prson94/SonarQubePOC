40630e8f556421b03554a255f6cb8a2bf173910d
Ken McNamee <kmcnamee@infogix.com>
2020-07-10 12:48:47 -0400

    GOV-11475 - added gallery for controls that is only visible in .eng. environments.  The intent here is to be able to expose our common components so they are visible outside of developers.  Initially this is just the boolean control and the existing button

---
+++ d360.web/Scripts/app/components/gallery/gallery.button.component.ts
@@ -0,0 +1,13 @@
+﻿import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+
+@Component({
+    selector: 'gallery-button',
+    templateUrl: './gallery.button.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class GalleryButtonComponent implements OnInit {
+    ngOnInit(): void {
+    }
+}
