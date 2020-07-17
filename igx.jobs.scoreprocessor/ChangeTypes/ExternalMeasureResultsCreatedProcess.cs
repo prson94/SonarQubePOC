3c5db9caa4eb4b419a568aaa1a33d242d4312072
mikejpappas <mpappas@infogix.com>
2020-07-17 09:08:18 -0400

    GOV-10891 - cleaned up models a bit and placed in a new file

--- igx.jobs.scoreprocessor/ChangeTypes/ExternalMeasureResultsCreatedProcess.cs
+++ igx.jobs.scoreprocessor/ChangeTypes/ExternalMeasureResultsCreatedProcess.cs
@@ -1,134 +1,19 @@
-﻿using d360.core;
-using d360.core.entities;
+﻿using d360.core.entities;
 using d360.core.entities.Metric;
 using d360.core.enums;
 using d360.core.queue;
-using d360.extensions.caching;
-using d360.extensions.info;
-using d360.extensions.queue;
-using d360.extensions.storage;
 using d360.model;
-using d360.utils.company;
 using Dapper;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.Entity.Core.Common.CommandTrees;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace igx.jobs.scoreprocessor.ChangeTypes
 {
-    #region Models
-
-    internal class MetConditionsModel
-    {
-        public bool ConditionMet { get; set; } = false;
-        public decimal? SelectedWeight { get; set; }
-        public float? SelectedThreshold { get; set; }
-        public Guid? SelectedConditionUid { 
-            get 
-            {
-                if (Conditions.Count > 0)
-                {
-                    return Conditions[0].ConditionUid;
-                }
-                else
-                {
-                    return null;
-                }
-            }
-        }
-        public List<MetConditionModel> Conditions { get; set; } = new List<MetConditionModel>();
-    }
-
-    internal class MetConditionModel
-    {
-        public int Position { get; set; }
-        public Guid ConditionUid { get; set; }
-        public decimal? Weight { get; set; }
-        public float? Threshold { get; set; }
-
-    }
-
-    internal class AssetAllocationPreviousResult
-    {
-        public Guid ScoreUid { get; set; }
-
-        public Guid ScoreItemUid { get; set; }
-
-        public Guid AllocationUid { get; set; }
-        public Guid AssetUid { get; set; }
-        public Guid MetricAssetUid { get; set; }
-        public Guid MetricAssetVersionUid { get; set; }
-        public Guid? ConditionUid { get; set; }
-        public DateTime EffectiveDate { get; set; }
-        public bool Value { get; set; }
-        public decimal AdjustedWeight { get; set; }
-        public decimal AdjustedMaxWeight { get; set; }
-    }
-
-    internal class MatchingScoreModel
-    {
-        public Guid ScoreUid { get; set; }
-        public Guid AllocationUid { get; set; }
-        public Guid AssetUid { get; set; }
-        public DateTime EffectiveDate { get; set; }
-    }
-    internal class MatchingScoreItemModel
-    {
-        public Guid ScoreItemUid { get; set; }
-        public Guid MetricAssetUid { get; set; }
-        public Guid AssetUid { get; set; }
-        public DateTime EffectiveDate { get; set; }
-    }
-
-    internal class AllocationDataModel
-    {
-        public Guid AllocationUid { get; set; }
-        public DateTime EffectiveDate { get; set; }
-        public Guid MetricAssetUid { get; set; }
-        public Guid? MetricParentAssetUid { get; set; }
-        public bool IsGroup { get; set; }
-        public Guid MetricAssetVersionUid { get; set; }
-        public decimal Weight { get; set; }
-        public float Threshold { get; set; }
-        public bool MatchConditionsOnly { get; set; }
-        public string Definition { get; set; }
-        public string ConditionsJson { get; set; }
-        public List<AllocationDataModelCondition> Conditions { get { return JsonConvert.DeserializeObject<List<AllocationDataModelCondition>>(ConditionsJson ?? "[]"); } }
-    }
-    
-    internal class AllocationDataModelCondition
-    {
-        public Guid ConditionUid { get; set; }
-        public MetricMatchType MatchType { get; set; }
-        public int Position { get; set; }
-        public decimal Weight { get; set; }
-        public float Threshold { get; set; }
-        public List<AllocationDataModelConditionItem> Items { get; set; }
-    }
-
-    internal class AllocationDataModelConditionItem
-    {
-        public Guid ItemUid { get; set; }
-        public MetricConditionType ConditionType { get; set; }
-        public int? ConditionFieldTypeID { get; set; }
-        public int? ConditionIntersectTypeID { get; set; }
-        public string Operator { get; set; }
-        public List<AllocationDataModelConditionItemValue> Values { get; set; }
-    }
-
-    internal class AllocationDataModelConditionItemValue
-    {
-        public string Value { get; set; }
-    }
-
-    #endregion
-
     public class ExternalMeasureResultsCreatedProcess : ProcessBase, IScoreProcess
     {
         public async Task Run()
