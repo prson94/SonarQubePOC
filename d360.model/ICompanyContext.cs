c5e75456f0bcc2ea68cc86765cb75fd563e09727
Michael Donato <mdonato7@gmail.com>
2020-07-24 12:19:37 -0400

    GOV-11597 - process graph delete of assets by execution instead of by asset to avoid performance issues

--- d360.model/ICompanyContext.cs
+++ d360.model/ICompanyContext.cs
@@ -270,7 +270,7 @@ namespace d360.model
         void RebuildDisplayValuesRequest();
         void RebuildAssetGraphRequest();
         void RebuildIndexRequest();
-        List<DatabaseBulkAssetResult> RemoveAssets(ApiExecution execution, AssetType at, AssetDeletes import, int timeout = 3600, bool sendWorkflowEvents = true);
+        List<DatabaseBulkAssetResult> RemoveAssets(ApiExecution execution, AssetType at, AssetDeletes import, int timeout = 3600, bool sendWorkflowEvents = true, bool sendGraphEvents = true);
         void RemoveResponsibilityTypeRelation(ResponsibilityTypeRelation relation);
         string RenderTooltip(string action, SystemObjects type, int id);
         void RequestObjectCertification(SystemObjects @object, int objectId, SystemObjects objectType, int objectTypeId);
