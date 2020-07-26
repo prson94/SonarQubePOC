3b2f25249c0f27f522cf77875930e8185b1677c3
Shane Dooley <sdooley@infogix.com>
2020-07-26 18:27:05 +0100

    GOV-11383-DELETE /api/v2/membership/groups/{groupUid}/{resourceUid} allows deletion of Primary Owner

--- d360.web/Controllers/V2/MembershipController.cs
+++ d360.web/Controllers/V2/MembershipController.cs
@@ -581,7 +581,28 @@ select G.* from [Group] G
 inner join Asset a on A.Object = 'Group' and A.ObjectID = G.ID 
 where a.uid = @groupUid", new { groupUid })).FirstOrDefault();
 
-                var res = await Company.Database.Connection.ExecuteAsync("delete rg from [dbo].[ResourceGroup] rg inner join[reporting].[Global_Resource] gr on gr.uid = @resource inner join[dbo].[Asset] a on a.uid = @group and a.object = 'Group' inner join[dbo].[Group] g on g.ID = a.ObjectID where rg.ResourceID = gr.ResourceID and rg.GroupID = g.ID", new { resource = resourceUid, group = groupUid });
+                var userId = _company.Assets.FirstOrDefault(x => x.Object == "Resource" && x.uid == resourceUid).ObjectID;
+
+                if (group?.PrimaryOwnerResourceID == userId)
+                    return await Task.FromResult(errorMessageResponse(HttpStatusCode.BadRequest, "Bad Request", "Cannot delete Primary Owner of group."));
+
+                if (group?.IsActiveDirectoryGroup == true)
+                    return await Task.FromResult(errorMessageResponse(HttpStatusCode.BadRequest, "Bad Request", "Group for provided UID is an active directory group and cannot be manually managed."));
+
+                var res = await Company.Database.Connection.ExecuteAsync(@"delete rg from [dbo].[ResourceGroup] rg inner join[reporting].[Global_Resource] gr on gr.uid = @resource inner join[dbo].[Asset] a on a.uid = @group and a.object = 'Group' inner join[dbo].[Group] g on g.ID = a.ObjectID where rg.ResourceID = gr.ResourceID and rg.GroupID = g.ID;  
+                    IF EXISTS    
+                    (
+                    SELECT 1
+                    FROM[Group] G
+                    inner join[dbo].[Asset] a on a.uid = @group and a.object = 'Group'
+                    WHERE A.ObjectID = G.ID and G.SecondaryOwnerResourceID = @user
+                    )
+                    BEGIN
+                        Update G set  G.SecondaryOwnerResourceID = null
+                        from[Group] AS G
+                        inner join[dbo].[Asset] a on a.uid = @group and a.object = 'Group'
+                        where G.ID = A.ObjectID and G.SecondaryOwnerResourceID = @user
+                    END", new { resource = resourceUid, group = groupUid, user = userId });
 
                 if (res > 0) return successMessageResponse(HttpStatusCode.OK, "User removed.", "User removed from group."); // deleted
                 else return await Task.FromResult(errorMessageResponse(HttpStatusCode.NotFound, "Not found", "Resource / Group doesn't exist"));
