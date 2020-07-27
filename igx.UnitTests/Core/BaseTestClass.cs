7e32605bc1cbe5700cdc99b3d2439291c1fbd095
Tomislav Đurić <tduric@infogix.com>
2020-07-27 09:30:50 +0100

    -excel export fix for non-admin users

--- igx.UnitTests/Core/BaseTestClass.cs
+++ igx.UnitTests/Core/BaseTestClass.cs
@@ -204,7 +204,7 @@ namespace igx.UnitTests
             mockRepo.Setup(x => x.GetFieldTypes(It.IsAny<Guid>()))
                 .Returns(() => JsonConvert.DeserializeObject<dynamic>(DataConstants.FieldTypesJsonFormat));
 
-            mockRepo.Setup(x => x.GetAssets(It.IsAny<Guid>(), It.IsAny<IEnumerable<KeyValuePair<string, string>>>()))
+            mockRepo.Setup(x => x.GetAssets(It.IsAny<Guid>(), It.IsAny<IEnumerable<KeyValuePair<string, string>>>(), It.IsAny<bool>()))
                 .Returns(Task.FromResult(new AssetsApiViewModel()));
 
             mockRepo.Setup(x => x.GetAssetTypeByUID(It.IsAny<Guid>()))
