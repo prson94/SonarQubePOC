dcf08bdfe1769f462ec3a43c8b3b96cc4749dd03
Tomislav Đurić <tduric@infogix.com>
2020-08-10 11:34:29 +0100

    -update go js version from  2.0.16 to 2.1.22

--- d360.web/Scripts/app/components/shared/diagram/process-diagram/process-diagram.component.ts
+++ d360.web/Scripts/app/components/shared/diagram/process-diagram/process-diagram.component.ts
@@ -354,6 +354,7 @@ export class ProcessDiagramComponent extends DiagramBaseComponent implements OnI
                     allowCopy: false,
                     allowUndo: false
                 });
+
         this.myDiagram.toolManager.mouseMoveTools.insertAt(0, new LinkLabelOnPathDraggingTool());
 
         this.myDiagram.commandHandler.editTextBlock = () => { return false; };
@@ -369,7 +370,7 @@ export class ProcessDiagramComponent extends DiagramBaseComponent implements OnI
                 return this.isEditMode;
             }
         };
-
+        
         this.myDiagram.grid.gridCellSize = new go.Size(24, 24);
         this.myDiagram.toolManager.draggingTool.isGridSnapEnabled = true;
 
