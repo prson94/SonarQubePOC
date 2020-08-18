a88d0afe5f247e65f2bc1dd5a958c33bd9d58e45
pungupta <pungupta@infogix.com>
2020-08-18 14:58:38 +0530

    GOV-11834 DELETE /api/v2/membership/groups/{groupUid}/{resourceUid} gives 500 on invalid user id.

--- d360.web/Controllers/V2/MembershipController.cs
+++ d360.web/Controllers/V2/MembershipController.cs
@@ -581,7 +581,7 @@ select G.* from [Group] G
 inner join Asset a on A.Object = 'Group' and A.ObjectID = G.ID 
 where a.uid = @groupUid", new { groupUid })).FirstOrDefault();
 
-                var userId = _company.Assets.FirstOrDefault(x => x.Object == "Resource" && x.uid == resourceUid).ObjectID;
+                var userId = _company.Assets.FirstOrDefault(x => x.Object == "Resource" && x.uid == resourceUid)?.ObjectID ?? 0;
 
                 if (group?.PrimaryOwnerResourceID == userId)
                     return await Task.FromResult(errorMessageResponse(HttpStatusCode.BadRequest, "Bad Request", "Cannot delete Primary Owner of group."));
