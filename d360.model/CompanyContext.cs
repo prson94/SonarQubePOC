80803701f78f28ab6fd32e7eda5685a0d873c205
mikejpappas <mpappas@infogix.com>
2020-09-01 10:33:52 -0400

    GOV-12032 - Score Changes break workflow functionality for getting scores

--- d360.model/CompanyContext.cs
+++ d360.model/CompanyContext.cs
@@ -3065,11 +3065,14 @@ left join Field {name}_T on {name}_T.ObjectType = '{type}' and {name}_T.ObjectID
 
         public int? GetAssetScore(long assetId, ScoreType type)
         {
-            string sql = $@"SELECT top 1
-                            cast(S.Value * 100 as int) as 'Score'                            
-                            from Asset A                            
-                            inner join metrics.Score S on S.AssetUid = A.[uid] and S.EffectiveDate <= getutcdate()
-                            WHERE S.ScoreType = @type and A.ID = @assetId order by S.EffectiveDate desc";
+            string sql = $@"
+select      top 1
+            cast(S.Value * 100 as int) as 'Score'                            
+from        Asset A                            
+            inner join metrics.Score S on S.AssetUid = A.[uid] and S.EffectiveDate <= getutcdate()
+            inner join metrics.Allocation Al on Al.Uid = S.AllocationUid and Al.ScoreType = @type and (Al.OverrideName is null or Al.OverrideName = '')
+where       A.ID = @assetId 
+order by    S.EffectiveDate desc";
             return Query<int?>(sql, new { assetId, type = (int)type }).FirstOrDefault();
         }
 
