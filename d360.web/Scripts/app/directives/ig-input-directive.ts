98c1756f4b012e5e67d59d0fcd25775ad5c2244f
Brian Howard <bhoward@infogix.com>
2020-09-03 16:49:46 +0100

    GOV-11668 - changes for the iginput directive and gallery, fix for toast color

--- d360.web/Scripts/app/directives/ig-input-directive.ts
+++ d360.web/Scripts/app/directives/ig-input-directive.ts
@@ -1,7 +1,6 @@
-﻿import { NgModule, Directive, ElementRef, AfterViewInit, OnDestroy, Input, forwardRef, Provider } from '@angular/core';
+﻿import { NgModule, Directive, ElementRef, AfterViewInit, OnDestroy, Input, forwardRef, Provider, HostListener } from '@angular/core';
 import { DomHandler } from 'primeng/dom';
 import { CommonModule } from '@angular/common';
-import { NG_VALIDATORS, AbstractControl } from '@angular/forms';
 
 @Directive({
     selector: '[igInput]'
@@ -9,55 +8,108 @@ import { NG_VALIDATORS, AbstractControl } from '@angular/forms';
 })
 export class InputDirective implements AfterViewInit, OnDestroy {
 
-    @Input() tooltip: string;
-    public _label: string;
-    public _istextarea: boolean;
-    private control: AbstractControl;
+   
+    public _size: string;
+    public _invalid: boolean;
+    public _validationMessage: string = "Please enter a valid value.";
+
+
     constructor(public el: ElementRef) { }
 
+    private wrapper: HTMLDivElement;
+
     ngAfterViewInit() {
-        DomHandler.addMultipleClasses(this.el.nativeElement, this.getStyleClass());
-        if (this.tooltip) {
-            this.el.nativeElement.setAttribute("title", this.tooltip);
-            this.el.nativeElement.setAttribute("aria-label", this.tooltip);
+        if (!this.wrapper) {
+            this.wrapper = document.createElement('div');
+            this.wrapper.className = "ig-input-wrapper";
+            this.el.nativeElement.parentNode.insertBefore(this.wrapper, this.el.nativeElement);
+            this.wrapper.appendChild(this.el.nativeElement);
         }
+
+        if (this.el.nativeElement.required) {
+            this.el.nativeElement.placeholder = "Value Required";
+            if (!this.el.nativeElement.classList.contains("ng-invalid"))
+                DomHandler.addMultipleClasses(this.el.nativeElement, "ng-invalid");
+
+        } else {
+            this.el.nativeElement.placeholder = "Optional";
+        }
+
+        if (this.el.nativeElement.disabled) {
+            DomHandler.addMultipleClasses(this.el.nativeElement, "disabled");
+        } else {
+            DomHandler.removeClass(this.el.nativeElement, "disabled");
+        }
+
+        DomHandler.addMultipleClasses(this.el.nativeElement, this.getStyleClass());
     }
 
     getStyleClass(): string {
         return 'ig-input';
     }
 
-    @Input() get label(): string {
-        return this._label;
+    @Input() get igSize(): string {
+        return this._size;
     }
-    set label(val: string) {
-        this._label = val;
+    set igSize(val: string) {
+        this._size = val;
+        if (this._size && this._size == "small") {
+            DomHandler.addMultipleClasses(this.el.nativeElement, "ig-input-small");
+        } else if (this._size && this._size == "medium") {
+            DomHandler.addMultipleClasses(this.el.nativeElement, "ig-input-medium");
+        } else if (this._size && this._size == "large") {
+            DomHandler.addMultipleClasses(this.el.nativeElement, "ig-input-large");
+        } else if (this._size && this._size == "full") {
+            DomHandler.addMultipleClasses(this.el.nativeElement, "ig-input-full");
+        }
+    }
 
-        let labelElement = DomHandler.findSingle(this.el.nativeElement, '.ig-input-label');
-        if (labelElement) {
-            this.el.nativeElement.removeChild(labelElement);
+
+    @Input() get invalid(): boolean {
+        return this._invalid;
+    } 
+
+    set invalid(val: boolean) {
+        this._invalid = val;
+
+        if (!this.wrapper) {
+            this.wrapper = document.createElement('div');
+            this.wrapper.className = "ig-input-wrapper";
+            this.el.nativeElement.parentNode.insertBefore(this.wrapper, this.el.nativeElement);
+            this.wrapper.appendChild(this.el.nativeElement);
         }
 
-        if (this._label) {
-            labelElement = document.createElement("span");
-            labelElement.className = 'ig-input-label';
-            labelElement.appendChild(document.createTextNode(this.label));
-            this.el.nativeElement.parentNode.insertBefore(labelElement, this.el.nativeElement);
-            DomHandler.removeClass(this.el.nativeElement, "ig-input-icon-only");
+        let errorMessageEl = DomHandler.findSingle(this.wrapper, '.ig-text-required');
+        if (errorMessageEl) {
+            this.wrapper.removeChild(errorMessageEl);
+        }
+
+        if (this._invalid) {
+            errorMessageEl = document.createElement("span");
+            errorMessageEl.className = "ig-text-required ig-input-" + this._size;
+            errorMessageEl.appendChild(document.createTextNode(this._validationMessage));
+            this.wrapper.appendChild(errorMessageEl);
+            DomHandler.addMultipleClasses(this.el.nativeElement, "invalid");
         } else {
-            DomHandler.addClass(this.el.nativeElement, "ig-input-icon-only");
-            throw new Error("Infogix Button Component: caption has not been set");
+            DomHandler.removeClass(this.el.nativeElement, "invalid");
         }
     }
 
-    @Input() get istextarea(): boolean {
-        return this._istextarea;
+    @Input() get validationMessage(): string {
+        return this._validationMessage;
     }
 
-    set istextarea(val: boolean) {
-        this._istextarea = val;
-        if (this._istextarea) {
-            DomHandler.addMultipleClasses(this.el.nativeElement, "text-area");
+    set validationMessage(val: string) {
+        this._validationMessage = val;
+    }
+
+
+    @HostListener('window:keyup', ['$event'])
+    keyEvent(event: KeyboardEvent) {
+        if (this.el.nativeElement.required && (this.el.nativeElement.value == undefined || this.el.nativeElement.value == null || this.el.nativeElement.value == '')) {
+            DomHandler.addMultipleClasses(this.el.nativeElement, "ng-invalid");
+        } else {
+            DomHandler.removeClass(this.el.nativeElement, "ng-invalid");
         }
     }
 
