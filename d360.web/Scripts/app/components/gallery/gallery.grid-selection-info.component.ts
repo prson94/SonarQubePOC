e82e0357b8e17c004b1e649359aff37406eedacc
Michael Donato <mdonato7@gmail.com>
2020-09-08 19:45:28 -0400

    GOV-11541 - add more shared components to gallery

---
+++ d360.web/Scripts/app/components/gallery/gallery.grid-selection-info.component.ts
@@ -0,0 +1,45 @@
+﻿import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+
+@Component({
+    selector: 'gallery-grid-selection-info',
+    templateUrl: './gallery.grid-selection-info.component.html',
+    styles: [
+        `
+        .gallery-section {
+            padding: 0 16px 32px 16px;
+        }
+
+        .gallery-section h4 {
+            padding-bottom: 8px;
+        }
+        `
+    ], changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class GalleryGridSelectionInfoComponent implements OnInit {
+    protected properties: Array<any>;
+    protected events: Array<any>;
+    protected sampleUsage: string = '<d3s-grid-selection-info></d3s-grid-selection-info>';
+
+    protected items = [
+        { name: 'Item 1' },
+        { name: 'Item 2' },
+        { name: 'Item 3' },
+        { name: 'Item 4' },
+        { name: 'Item 5' },
+    ];
+
+    protected selection = [];
+
+    ngOnInit(): void {
+        this.properties = new Array();
+        this.properties.push({ Name: "includeSelectLinks", Type: "boolean", Description: "When true the select all/none buttons will be displayed", Default: "true" });
+        this.properties.push({ Name: "model", Type: "any[]", Description: "Array of items representing the entire set of data", Default: "[]" });
+        this.properties.push({ Name: "selection", Type: "any[]", Description: "Array of items representing the seleected data", Default: "[]" });
+
+        this.events = new Array();
+        this.events.push({ Name: "onSelectAllClick", Description: "Fires when select all is clicked" });
+        this.events.push({ Name: "onSelectNoneClick", Description: "Fires when select none is clicked" });
+    }
+}
---
+++ d360.web/Scripts/app/components/gallery/gallery.loading.component.ts
@@ -0,0 +1,31 @@
+﻿import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+
+@Component({
+    selector: 'gallery-loading',
+    templateUrl: './gallery.loading.component.html',
+    styles: [
+        `
+        .gallery-section {
+            padding: 0 16px 32px 16px;
+        }
+
+        .gallery-section h4 {
+            padding-bottom: 8px;
+        }
+        `
+    ], changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class GalleryLoadingComponent implements OnInit {
+    protected properties: Array<any>;
+    protected sampleUsage: string = '<d3s-loading isLoading="true"></d3s-loading>';
+    protected isLoading1: boolean = true;
+    protected isLoading2: boolean = false;
+
+    ngOnInit(): void {
+        this.properties = new Array();
+        this.properties.push({ Name: "isLoading", Type: "boolean", Description: "Whether or not to show the loading wheel", Default: "" });
+        this.properties.push({ Name: "showTransparentLoader", Type: "boolean", Description: "Show a transparent background behind the loading wheel", Default: "false" });
+    }
+}
