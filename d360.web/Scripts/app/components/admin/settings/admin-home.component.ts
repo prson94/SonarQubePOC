1607e117c9e780ccb6a362138b6f11472d49a9d0
Ken McNamee <kmcnamee@infogix.com>
2020-09-23 15:20:10 -0400

    GOV-12286 limit homepage background image size in administration settings.  So extremely large images are not uploaded which slow govern and possibly give an error on the settings page.

--- d360.web/Scripts/app/components/admin/settings/admin-home.component.ts
+++ d360.web/Scripts/app/components/admin/settings/admin-home.component.ts
@@ -1,27 +1,19 @@
-﻿import { Component, OnInit, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+﻿import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CompanySettings, CompanyImage } from '../../../models/settings.model';
-
-import * as _ from 'lodash';
+import { MessagesObservableService } from '../../../services/messages-observable.service';
 
 @Component({
     selector: 'd3s-admin-home',
     templateUrl: './admin-home.component.html'
 })
 
-export class AdminHomeComponent implements OnInit, OnChanges {
+export class AdminHomeComponent  {
     @Input() companySettings: CompanySettings;
     @Output() companySettingsChange = new EventEmitter();
     @Input() homePageImage: CompanyImage;
     @Output() homePageImageChange = new EventEmitter();
-    constructor() {
-    }
-
-    ngOnInit() {
-    }
-
-    ngOnChanges(changes: SimpleChanges) {}
+    constructor(private messagesService: MessagesObservableService) { }
     
-
     onFileChange(event): void {
         if (this.homePageImage == null)
             this.homePageImage = new CompanyImage();
@@ -40,10 +32,17 @@ export class AdminHomeComponent implements OnInit, OnChanges {
         let target = event.target || event.srcElement;
         let files = target.files;
 
+        if (files[0] != null) {
+            if (files[0].size > (1024 * 1000)) {
+                this.messagesService.showError('File too large.', `The specified image is too large.  Please resave as a smaller image to continue.  Specified file is ${(files[0].size).toLocaleString()} bytes in size which is greater than the max allowed size of ${(1024 * 1000).toLocaleString()} bytes.`);
+                return;
+            }
+        }
+
         this.homePageImage.file = files[0];
+
         this.homePageImage.setDataUrl();
 
         this.homePageImageChange.emit(this.homePageImage);
     }
-
 }
