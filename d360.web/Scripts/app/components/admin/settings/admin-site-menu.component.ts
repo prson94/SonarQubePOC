093b9f066e4175b9c5bdf5ac34f34c3bd440c7ec
pungupta <pungupta@infogix.com>
2020-10-05 13:19:24 +0530

    GOV-12294 Admin -> Settings doesnt limit custom image sizes for company logo / site navigation

--- d360.web/Scripts/app/components/admin/settings/admin-site-menu.component.ts
+++ d360.web/Scripts/app/components/admin/settings/admin-site-menu.component.ts
@@ -135,6 +135,14 @@ export class AdminSiteMenuComponent extends AdminBaseComponent implements OnInit
         let target = event.target || event.srcElement;
         let files = target.files;
 
+        if (files[0] != null) {
+            if (files[0].size > (1024 * 1024)) {
+                this.messagesService.showError('File too large.', `Navigation icon image upload failed - the file is too large. Please choose an image file (ideally in JPG format due to smaller file size) no bigger than 1MB. `);
+                return;
+            }
+        }
+
+
         this.iconImage.file = files[0];
         this.iconImage.setDataUrl();
         if (this.formMode == FormMode.Editing) {
