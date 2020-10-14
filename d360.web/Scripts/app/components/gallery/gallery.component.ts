36f736ceff1abc4e4c87e7aa1910dac1df3bc2db
Tomislav Đurić <tduric@infogix.com>
2020-10-14 09:58:55 +0200

    - saving changes

--- d360.web/Scripts/app/components/gallery/gallery.component.ts
+++ d360.web/Scripts/app/components/gallery/gallery.component.ts
@@ -10,34 +10,44 @@
 export class GalleryComponent implements OnInit {
     activeControl: string = "radio-button";
     controls = [
-        { label: 'Switch Input', key: 'switch', type: 'form' },
-        { label: 'Button Directive', key: 'button', type: 'form' },
-        { label: 'Icon Picker', key: 'icon-picker', type: 'form' },
-        { label: 'Tag Picker', key: 'tag-picker', type: 'govern' },
-        { label: 'Text Field', key: 'input', type: 'form' },
-        { label: 'Auto Complete', key: 'auto-complete', type: 'form' },
-        { label: 'Tooltip', key: 'tooltip', type: 'overlay' },
+        { label: 'Switch Input', key: 'switch', type: 'Form' },
+        { label: 'Button Directive', key: 'button', type: 'Form' },
+        { label: 'Icon Picker', key: 'icon-picker', type: 'Form' },
+        { label: 'Tag Picker', key: 'tag-picker', type: 'Govern Components' },
+        { label: 'Text Field', key: 'input', type: 'Form' },
+        { label: 'Auto Complete', key: 'auto-complete', type: 'Form' },
+        { label: 'Tooltip', key: 'tooltip', type: 'Overlay' },
         { label: 'Auto Focus Directive', key: 'auto-focus', type: 'misc' },
-        { label: 'Color Picker', key: 'color-picker', type: 'form' },
-        { label: 'Color Variables', key: 'color-variables', type: 'govern' },
-        { label: 'Text Area', key: 'textarea', type: 'form' },
-        { label: 'Date Picker', key: 'date-picker', type: 'form' },
+        { label: 'Color Picker', key: 'color-picker', type: 'Form' },
+        { label: 'Color Variables', key: 'color-variables', type: 'Govern Components' },
+        { label: 'Text Area', key: 'textarea', type: 'Form' },
+        { label: 'Date Picker', key: 'date-picker', type: 'Form' },
         { label: 'Loading Component', key: 'loading', type: 'misc' },
         { label: 'Accordion', key: 'accordion', type: 'data' },
         { label: 'Page Info', key: 'paging-info', type: 'data' },
         { label: 'Selection Info', key: 'selection-info', type: 'data' },
-        { label: 'Number Field', key: 'number-field', type: 'form' },
+        { label: 'Number Field', key: 'number-field', type: 'Form' },
         { label: 'Message Box', key: 'message-box', type: 'data' },
         { label: 'Badge', key: 'badge', type: 'misc' },
-        { label: 'Checkbox', key: 'checkbox', type: 'form' },
-        { label: 'Popup Menu', key: 'popup-menu', type: 'overlay' },
-        { label: 'Select', key: 'select', type: 'form' },
-        { label: 'Property Group', key: 'propery-group', type: 'form' },
-        { label: 'Radio Button', key: 'radio-button', type: 'form' },
-        { label: 'Field Condition Grid', key: 'field-condition-grid', type: 'govern' },
+        { label: 'Checkbox', key: 'checkbox', type: 'Form' },
+        { label: 'Popup Menu', key: 'popup-menu', type: 'Overlay' },
+        { label: 'Select', key: 'select', type: 'Form' },
+        { label: 'Property Group', key: 'propery-group', type: 'Form' },
+        { label: 'Radio Button', key: 'radio-button', type: 'Form' },
+        { label: 'Field Condition Grid', key: 'field-condition-grid', type: 'Govern Components' },
     ];
 
+    categories: any[] = [];
+
     ngOnInit(): void {
-        this.controls = this.controls.sort((a, b) => { return a.label > b.label ? 1 : -1 });
+
+        this.controls.forEach(x => {
+            if (!this.categories.some(c => c.type == x.type)) {
+                this.categories.push({
+                    type: x.type,
+                    controls: this.controls.sort((a, b) => { return a.label > b.label ? 1 : -1 }).filter(ct => ct.type == x.type)
+                });
+            }
+        });
     }
 }
