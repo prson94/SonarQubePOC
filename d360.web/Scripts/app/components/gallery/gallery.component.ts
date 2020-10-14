72cee115291e4fb5ae3cf7e82d099dad72a91a05
Tomislav Đurić <tduric@infogix.com>
2020-10-14 12:59:46 +0200

    - save

--- d360.web/Scripts/app/components/gallery/gallery.component.ts
+++ d360.web/Scripts/app/components/gallery/gallery.component.ts
@@ -4,11 +4,24 @@
 @Component({
     selector: 'gallery',
     templateUrl: './gallery.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    styles: [`
+           .gallery-label {
+                font-size: 14px;
+                color: black;
+                padding-left: 10px;
+                margin: 10px 0px;
+                display: block;
+            }
+
+            .collection-item {
+                cursor:pointer;
+             }
+    `]
 })
 
 export class GalleryComponent implements OnInit {
-    activeControl: string = "radio-button";
+    activeControl: string = "auto-complete";
     controls = [
         { label: 'Switch Input', key: 'switch', type: 'Form' },
         { label: 'Button Directive', key: 'button', type: 'Form' },
@@ -17,18 +30,18 @@ export class GalleryComponent implements OnInit {
         { label: 'Text Field', key: 'input', type: 'Form' },
         { label: 'Auto Complete', key: 'auto-complete', type: 'Form' },
         { label: 'Tooltip', key: 'tooltip', type: 'Overlay' },
-        { label: 'Auto Focus Directive', key: 'auto-focus', type: 'misc' },
+        { label: 'Auto Focus Directive', key: 'auto-focus', type: 'Misc' },
         { label: 'Color Picker', key: 'color-picker', type: 'Form' },
         { label: 'Color Variables', key: 'color-variables', type: 'Govern Components' },
         { label: 'Text Area', key: 'textarea', type: 'Form' },
         { label: 'Date Picker', key: 'date-picker', type: 'Form' },
-        { label: 'Loading Component', key: 'loading', type: 'misc' },
-        { label: 'Accordion', key: 'accordion', type: 'data' },
-        { label: 'Page Info', key: 'paging-info', type: 'data' },
-        { label: 'Selection Info', key: 'selection-info', type: 'data' },
+        { label: 'Loading Component', key: 'loading', type: 'Misc' },
+        { label: 'Accordion', key: 'accordion', type: 'Data' },
+        { label: 'Page Info', key: 'paging-info', type: 'Data' },
+        { label: 'Selection Info', key: 'selection-info', type: 'Data' },
         { label: 'Number Field', key: 'number-field', type: 'Form' },
-        { label: 'Message Box', key: 'message-box', type: 'data' },
-        { label: 'Badge', key: 'badge', type: 'misc' },
+        { label: 'Message Box', key: 'message-box', type: 'Data' },
+        { label: 'Badge', key: 'badge', type: 'Misc' },
         { label: 'Checkbox', key: 'checkbox', type: 'Form' },
         { label: 'Popup Menu', key: 'popup-menu', type: 'Overlay' },
         { label: 'Select', key: 'select', type: 'Form' },
