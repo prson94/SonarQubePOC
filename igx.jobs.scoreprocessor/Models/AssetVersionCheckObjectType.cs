7300eed35090aa38159a66338d0e35a9826f5b01
mikejpappas <mpappas@infogix.com>
2020-10-27 18:38:11 -0400

    GOV-12067 - ownership check - include Operator property and updated query logic. GOV-12571 - object not set to instance of object when field type deleted and socring engine checks

---
+++ igx.jobs.scoreprocessor/Models/AssetVersionCheckObjectType.cs
@@ -0,0 +1,39 @@
+﻿using d360.core.enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace igx.jobs.scoreprocessor.Models
+{
+    internal class AssetVersionCheckObjectTypes : List<AssetVersionCheckObjectType>
+    {
+        public bool OkToAddToList(Guid versionUid)
+        {
+            return !this.Any(i => i.AssetVersionUid == versionUid);
+        }
+
+        public bool ShouldContinueAnalysis(Guid versionUid)
+        {
+            var shouldContinue = true;
+            var item = this.SingleOrDefault(i => i.AssetVersionUid == versionUid);
+            if (item != null)
+            {
+                shouldContinue = item.Valid;
+            }
+            else
+            {
+                shouldContinue = true;
+            }
+            return shouldContinue;
+        }
+    }
+
+    internal class AssetVersionCheckObjectType
+    {
+        public Guid AssetVersionUid { get; set; }
+        public Guid? TypeUid { get; set; }
+        public bool Valid { get; set; }
+    }
+}
