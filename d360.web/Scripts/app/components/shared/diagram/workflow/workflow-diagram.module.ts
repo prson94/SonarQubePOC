8af5db858250307aa0bd233a994b39001e231ea8
Michael Donato <mdonato7@gmail.com>
2021-01-27 17:04:47 -0500

    GOVE-12972 - fix codacy issues

--- d360.web/Scripts/app/components/shared/diagram/workflow/workflow-diagram.module.ts
+++ d360.web/Scripts/app/components/shared/diagram/workflow/workflow-diagram.module.ts
@@ -1,47 +1,47 @@
-﻿import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+﻿import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { GovernRequestInterceptor } from "../../../../http-interceptors/govern-request.interceptor";
 
-import { RouterModule } from '@angular/router';
+import { RouterModule } from "@angular/router";
 
-import { AutoCompleteModule } from 'primeng/autocomplete';
-import { CalendarModule } from 'primeng/calendar';
-import { ButtonModule } from 'primeng/button';
-import { InputMaskModule } from 'primeng/inputmask';
-import { SharedModule } from 'primeng/api';
-import { EditorModule } from 'primeng/editor';
-import { ToggleButtonModule } from 'primeng/togglebutton';
-import { MultiSelectModule } from 'primeng/multiselect';
-import { TableModule } from 'primeng/table';
+import { AutoCompleteModule } from "primeng/autocomplete";
+import { CalendarModule } from "primeng/calendar";
+import { ButtonModule } from "primeng/button";
+import { InputMaskModule } from "primeng/inputmask";
+import { SharedModule } from "primeng/api";
+import { EditorModule } from "primeng/editor";
+import { ToggleButtonModule } from "primeng/togglebutton";
+import { MultiSelectModule } from "primeng/multiselect";
+import { TableModule } from "primeng/table";
 
-import { CoreModule } from '../../core.module';
-import { TilesModule } from '../../tiles/tiles.module';
-import { D3SOverlayWindowModule } from '../../overlay-window.component';
-import { D3SEditorHeaderModule } from '../../editor-header.component';
-import { SharedGridPagingInfoModule } from '../../grid-paging-info.component';
-import { PopupMenuModule } from '../../controls/popup-menu/popup-menu.component';
+import { CoreModule } from "../../core.module";
+import { TilesModule } from "../../tiles/tiles.module";
+import { D3SOverlayWindowModule } from "../../overlay-window.component";
+import { D3SEditorHeaderModule } from "../../editor-header.component";
+import { SharedGridPagingInfoModule } from "../../grid-paging-info.component";
+import { PopupMenuModule } from "../../controls/popup-menu/popup-menu.component";
 
-import { WorkflowDiagramComponent } from './workflow-diagram.component';
-import { WorkflowStepEditorComponent } from './workflow-step-editor.component';
-import { WorkflowTransitionEditorComponent } from './workflow-transition-editor.component';
-import { WorkflowConditionEditorComponent } from './workflow-condition-editor.component';
-import { WorkflowStepFormEditorComponent } from './workflow-step-form-editor.component';
-import { WorkflowConditionListComponent } from './workflow-condition-list.component';
-import { WorkflowTemplateToolComponent } from './workflow-template-tool.component';
-import { WorkflowHistoryComponent } from './workflow-history.component';
-import { WorkflowFormHistoryComponent } from './workflow-form-history.component';
-import { WorkflowStepFieldChangeComponent } from './workflow-step-field-change.component';
-import { WorkflowStepSummaryComponent } from './workflow-step-summary.component'
-import { WorkflowTransitionSummaryComponent } from './workflow-transition-summary.component';
-import { WorkflowResponsibilitySelectorComponent } from './workflow-responsibility-selector.component';
-import { WorkflowStepHttpComponent } from './workflow-step-http.component';
-import { PipesModule } from '../../../../pipes/pipes.module';
+import { WorkflowDiagramComponent } from "./workflow-diagram.component";
+import { WorkflowStepEditorComponent } from "./workflow-step-editor.component";
+import { WorkflowTransitionEditorComponent } from "./workflow-transition-editor.component";
+import { WorkflowConditionEditorComponent } from "./workflow-condition-editor.component";
+import { WorkflowStepFormEditorComponent } from "./workflow-step-form-editor.component";
+import { WorkflowConditionListComponent } from "./workflow-condition-list.component";
+import { WorkflowTemplateToolComponent } from "./workflow-template-tool.component";
+import { WorkflowHistoryComponent } from "./workflow-history.component";
+import { WorkflowFormHistoryComponent } from "./workflow-form-history.component";
+import { WorkflowStepFieldChangeComponent } from "./workflow-step-field-change.component";
+import { WorkflowStepSummaryComponent } from "./workflow-step-summary.component"
+import { WorkflowTransitionSummaryComponent } from "./workflow-transition-summary.component";
+import { WorkflowResponsibilitySelectorComponent } from "./workflow-responsibility-selector.component";
+import { WorkflowStepHttpComponent } from "./workflow-step-http.component";
+import { PipesModule } from "../../../../pipes/pipes.module";
 
 
-import { WorkflowFieldsService } from '../../../../services/workflow-fields.service';
-import { DirectivesModule } from '../../../../directives/directives.module';
+import { WorkflowFieldsService } from "../../../../services/workflow-fields.service";
+import { DirectivesModule } from "../../../../directives/directives.module";
 
 
 @NgModule({
