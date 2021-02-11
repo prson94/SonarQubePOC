537230e95af8a688b6d54b723ca771eedd02250f
Brian Howard <bhoward@infogix.com>
2021-02-11 15:13:02 +0000

    GOV-13452 - hiding the edit and delete options on responsibilities for non admins

--- d360.web/Scripts/app/components/sidebar/permissions/permissions.component.ts
+++ d360.web/Scripts/app/components/sidebar/permissions/permissions.component.ts
@@ -13,7 +13,7 @@ import { HeaderBreadcrumbService } from '../../../services/header-breadcrumb.ser
             <div class="row" *ngIf="!isLoading">
                 <div class="col s12">
                     <div class="tile tile-detail">
-                        <d3s-responsibility-relations queryType="A" [id]="assetTypeId" [showAddButton]="false" showDeleteButton="true"></d3s-responsibility-relations>                        
+                       dfgsd <d3s-responsibility-relations queryType="A" [id]="assetTypeId" [showAddButton]="false" [showDeleteButton]="showControls" [showEditButton]="showControls"></d3s-responsibility-relations>                        
                     </div>
                 </div>
             </div>
@@ -25,6 +25,7 @@ export class PermissionsComponent extends BaseComponent implements OnInit, OnDes
     private sub: any;
     assetTypeId: number;    
     title: string;
+    showControls: boolean;
 
     constructor(private objectDetailService: ObjectDetailService,
         private route: ActivatedRoute,
@@ -44,6 +45,10 @@ export class PermissionsComponent extends BaseComponent implements OnInit, OnDes
    
         });
         this.checkSecondaryNavLocalStorage();
+        this.authenticationService.checkCurrentUserAdmin().subscribe((isAdmin) => {
+            console.log(isAdmin);
+            this.showControls = isAdmin;
+        });
     }
 
     ngOnDestroy() {
