7bda4d31b83b2469169563cd8afae5bb0b33efb4
Shane Dooley <sdooley@infogix.com>
2021-02-16 16:24:47 +0000

    GOV-13323-Removing unused code

--- d360.web/Scripts/app/components/shared/social/social-comment.component.ts
+++ d360.web/Scripts/app/components/shared/social/social-comment.component.ts
@@ -202,8 +202,4 @@ export class SocialCommentComponent extends BaseComponent implements OnInit {
     canReply(): boolean {
         return !CurrentCompanySettings.disableCommunityPosting;
     }
-
-    getResourceUid(id) {
-        
-    }
 }
