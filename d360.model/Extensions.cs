39208831b661d586c5b6e7e409a2133621dde9de
Brian Howard <bhoward@infogix.com>
2021-03-01 15:34:14 +0000

    GOV-13845 - another writeline

--- d360.model/Extensions.cs
+++ d360.model/Extensions.cs
@@ -1004,8 +1004,7 @@ namespace d360.model
                         left = GenerateStringConcat(left, right);
                         break;
                     default:
-                        Console.WriteLine("no action for {0}", op.id);
-                        break;
+                        continue;
                 }
             }
             return left;
