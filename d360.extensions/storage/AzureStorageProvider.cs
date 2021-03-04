5bfbfb5f6a1380a355f0ee222dad31d75f02bd61
Michael Donato <mdonato7@gmail.com>
2021-03-04 16:14:57 -0500

    GOV-11872 - initial rewrite of azure storage provider to use v12 SDK

--- d360.extensions/storage/AzureStorageProvider.cs
+++ d360.extensions/storage/AzureStorageProvider.cs
@@ -6,11 +6,14 @@ using System.Threading.Tasks;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Auth;
 using Microsoft.WindowsAzure.Storage.Blob;
+using Azure.Storage;
+using Azure.Storage.Blobs;
 using System.Configuration;
 using System.IO;
 using d360.core;
 using Microsoft.Azure;
 using Newtonsoft.Json;
+using Azure.Storage.Blobs.Models;
 
 namespace d360.extensions.storage
 {
@@ -18,136 +21,220 @@ namespace d360.extensions.storage
     {
         public string StorageName { get { return CloudConfigurationManager.GetSetting("AzureStorageName"); } }
         public string StorageKey { get { return CloudConfigurationManager.GetSetting("AzureStorageKey"); } }
+        public string StorageConnectionString { get { return CloudConfigurationManager.GetSetting("AzureStorageConnectionString"); } }
 
-        private StorageCredentials getCredentials()
-        {
-            return new StorageCredentials(StorageName, StorageKey);
-        }
+
+        //private StorageCredentials getCredentials()
+        //{
+        //    return new StorageCredentials(StorageName, StorageKey);
+        //}
         
-        CloudBlobContainer getContainer(string name)
+        BlobContainerClient getContainer(string name)
         {
-            var acct = new CloudStorageAccount(getCredentials(), StorageName, "core.windows.net", true);
-            return acct.CreateCloudBlobClient().GetContainerReference(name);
+            var client = new BlobServiceClient(StorageConnectionString);
+            return client.GetBlobContainerClient(name);
         }
 
-        public void CreateFolder(string name)
+        BlobClient getBlob(string folderName, string fileName)
+        {
+            var container = getContainer(folderName);
+            return container.GetBlobClient(fileName);
+        }
+
+        public async Task CreateFolder(string name)
         {
             var c = getContainer(name);
-            c.CreateIfNotExists();
+            await c.CreateIfNotExistsAsync();
         }
 
-        public void CreateFile(string folderName, string fileName, Stream file, string contentType = null, bool cache = true)
+        public async Task CreateFile(string folderName, string fileName, Stream file, string contentType = null, bool cache = true)
         {
-            var c = getContainer(folderName);
-            CloudBlockBlob blockBlob = c.GetBlockBlobReference(fileName);
-            if (!cache) blockBlob.Properties.CacheControl = "private, max-age=0, no-cache, no-store";
-            if (!string.IsNullOrEmpty(contentType)) blockBlob.Properties.ContentType = contentType;
-            blockBlob.UploadFromStream(file);
+            var blob = getBlob(folderName, fileName);
+            var headers = new BlobHttpHeaders();
+
+            if (!cache)
+            {
+                headers.CacheControl = "private, max-age=0, no-cache, no-store";
+            }
+            if (!string.IsNullOrEmpty(contentType))
+            {
+                headers.ContentType = contentType;
+            }
+
+            await blob.UploadAsync(file, headers);
+            //CloudBlockBlob blockBlob = c.GetBlo(fileName);
+            //if (!cache) blockBlob.Properties.CacheControl = "private, max-age=0, no-cache, no-store";
+            //if (!string.IsNullOrEmpty(contentType)) blockBlob.Properties.ContentType = contentType;
+            //blockBlob.UploadFromStream(file);
         }
 
-        public void CreateFile(string folderName, string fileName, string content, string contentType = null, bool cache = true)
+        public async Task CreateFile(string folderName, string fileName, string content, string contentType = null, bool cache = true)
         {
-            var c = getContainer(folderName);
-            CloudBlockBlob blockBlob = c.GetBlockBlobReference(fileName);
-            if (!cache) blockBlob.Properties.CacheControl = "private, max-age=0, no-cache, no-store";
-            if (!string.IsNullOrEmpty(contentType)) blockBlob.Properties.ContentType = contentType;
-            blockBlob.UploadText(content);
+
+            await CreateFile(folderName, fileName, new MemoryStream(Encoding.UTF8.GetBytes(content)), contentType, cache);
+
+            //var c = getContainer(folderName);
+            //CloudBlockBlob blockBlob = c.GetBlockBlobReference(fileName);
+            //if (!cache) blockBlob.Properties.CacheControl = "private, max-age=0, no-cache, no-store";
+            //if (!string.IsNullOrEmpty(contentType)) blockBlob.Properties.ContentType = contentType;
+            //blockBlob.UploadText(content);
         }
 
         
         public async Task SerializeJsonObjectToBlobAsync(string folderName, string fileName, object obj)
         {
-            var c = getContainer(folderName);
-            CloudBlockBlob blob = c.GetBlockBlobReference(fileName);
-
-            using (Stream stream = await blob.OpenWriteAsync())
-            using (StreamWriter sw = new StreamWriter(stream))
-            using (JsonTextWriter jtw = new JsonTextWriter(sw))
+            //var c = getContainer(folderName);
+            //CloudBlockBlob blob = c.GetBlockBlobReference(fileName);
+            using (MemoryStream ms = new MemoryStream())
             {
-                JsonSerializer ser = new JsonSerializer();
-                ser.Serialize(jtw, obj);
+                using (StreamWriter sw = new StreamWriter(ms,Encoding.UTF8))
+                {
+                    using (JsonTextWriter jtw = new JsonTextWriter(sw))
+                    {
+                        JsonSerializer ser = new JsonSerializer();
+                        ser.Serialize(jtw, obj);
+                        await CreateFile(folderName, fileName, ms);
+                    }
+                }
             }
+
+            //using (Stream stream = await blob.OpenWriteAsync())
+            //using (StreamWriter sw = new StreamWriter(stream))
+            //using (JsonTextWriter jtw = new JsonTextWriter(sw))
+            //{
+            //    JsonSerializer ser = new JsonSerializer();
+            //    ser.Serialize(jtw, obj);
+            //}
         }
 
         public async Task<T> DeserializeJsonObjectFromBlobAsync<T>(string folderName, string fileName)
         {
-            var c = getContainer(folderName);
-            CloudBlockBlob blob = c.GetBlockBlobReference(fileName);
+            //var c = getContainer(folderName);
+            //CloudBlockBlob blob = c.GetBlockBlobReference(fileName);
 
-            using (Stream stream = await blob.OpenReadAsync())
-            using (StreamReader sr = new StreamReader(stream))
-            using (JsonTextReader jtr = new JsonTextReader(sr))
+            var blob = getBlob(folderName, fileName);
+
+            using (MemoryStream ms = new MemoryStream())
             {
-                JsonSerializer ser = new JsonSerializer();
-                return ser.Deserialize<T>(jtr);
+                await blob.DownloadToAsync(ms);
+                using (StreamReader sr = new StreamReader(ms, Encoding.UTF8))
+                {
+                    using (JsonTextReader jtr = new JsonTextReader(sr))
+                    {
+                        JsonSerializer ser = new JsonSerializer();
+                        return ser.Deserialize<T>(jtr);
+                    }
+                }
             }
+
+
+            //using (Stream stream = await blob.OpenReadAsync())
+            //using (StreamReader sr = new StreamReader(stream))
+            //using (JsonTextReader jtr = new JsonTextReader(sr))
+            //{
+            //    JsonSerializer ser = new JsonSerializer();
+            //    return ser.Deserialize<T>(jtr);
+            //}
         }
 
-        public void DeleteFile(string folderName, string fileName)
+        public async Task DeleteFile(string folderName, string fileName)
         {
-            var c = getContainer(folderName);
-            CloudBlockBlob blockBlob = c.GetBlockBlobReference(fileName);
-            blockBlob.Delete();
+            var blob = getBlob(folderName, fileName);
+            await blob.DeleteIfExistsAsync();
+
+            //CloudBlockBlob blockBlob = c.GetBlockBlobReference(fileName);
+            //blockBlob.Delete();
         }
 
 
         public string GetFileContentsAsString(string folderName, string fileName)
         {
-            return GetFileContentsAsString(folderName, fileName, Encoding.Default);            
+            return GetFileContentsAsString(folderName, fileName, Encoding.Default).Result;          
         }
 
-        public string GetFileContentsAsString(string folderName, string fileName, Encoding encoding)
+        public async Task<string> GetFileContentsAsString(string folderName, string fileName, Encoding encoding)
         {
             string str = null;
             using (var stream = new MemoryStream())
             {
-                var c = getContainer(folderName);
-                CloudBlockBlob blockBlob = c.GetBlockBlobReference(fileName);
-                if (blockBlob.Exists())
+                var blob = getBlob(folderName, fileName);
+                if (await blob.ExistsAsync())
                 {
-                    str = blockBlob.DownloadText(encoding);
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        using (StreamReader sr = new StreamReader(ms, encoding))
+                        {
+                            await blob.DownloadToAsync(ms);
+                            str = await sr.ReadToEndAsync();
+                        }
+                    }
                 }
+
+                //var c = getContainer(folderName);
+                //CloudBlockBlob blockBlob = c.GetBlockBlobReference(fileName);
+                //if (blockBlob.Exists())
+                //{
+                //    str = blockBlob.DownloadText(encoding);
+                //}
             }
             return str;
         }
 
-        public DateTime GetFileLastModifiedDate(string folderName, string fileName)
+        public async Task<DateTime> GetFileLastModifiedDate(string folderName, string fileName)
         {            
             using (var stream = new MemoryStream())
             {
-                var c = getContainer(folderName);
-                CloudBlockBlob blockBlob = c.GetBlockBlobReference(fileName);
-                blockBlob.FetchAttributes();
-                return blockBlob.Properties.LastModified.HasValue? blockBlob.Properties.LastModified.Value.UtcDateTime : DateTime.MinValue;
+                var blob = getBlob(folderName, fileName);
+                var props = await blob.GetPropertiesAsync();
+                return props?.Value?.LastModified.UtcDateTime ?? DateTime.MinValue;
+                //var c = getContainer(folderName);
+                //CloudBlockBlob blockBlob = c.GetBlockBlobReference(fileName);
+                //blockBlob.FetchAttributes();
+                //return blockBlob.Properties.LastModified.HasValue? blockBlob.Properties.LastModified.Value.UtcDateTime : DateTime.MinValue;
             }            
         }
 
         public List<string> ListFilenamesByPrefix(string folderName, string prefix)
         {
+            var fileNames = new List<string>();
             var c = getContainer(folderName);
-            return c.ListBlobs(prefix, true, BlobListingDetails.Metadata).Select(i => i.Uri.LocalPath.Replace(folderName, "").Replace("/", "")).ToList();
+
+            foreach(BlobItem item in c.GetBlobs(BlobTraits.None, BlobStates.None, prefix))
+            {
+                fileNames.Add(item.Name);
+            }
+
+            return fileNames;
+            //return c.ListBlobs(prefix, true, BlobListingDetails.Metadata).Select(i => i.Uri.LocalPath.Replace(folderName, "").Replace("/", "")).ToList();
         }
 
         public List<StorageFileInfo> ListFiles(string folderName)
         {
             //container is the first part of the path
             var containerName = folderName.Substring(0, folderName.IndexOf('/'));
-                        
+            var files = new List<StorageFileInfo>();            
             var c = getContainer(containerName);
             
             string blobPrefix = (containerName.Length < folderName.Length ? folderName.Substring(folderName.IndexOf('/'), folderName.Length - folderName.IndexOf('/')) : null);
             blobPrefix = blobPrefix.TrimStart('/');
 
-            bool useFlatBlobListing = true;
-
-            List<StorageFileInfo> files =
-                c.ListBlobs(blobPrefix, useFlatBlobListing, BlobListingDetails.Metadata).
-                Select(b => new StorageFileInfo
+            foreach (BlobItem item in c.GetBlobs(BlobTraits.None, BlobStates.None, blobPrefix))
+            {
+                files.Add(new StorageFileInfo
                 {
-                    LastModified = (((CloudBlockBlob)b).Properties.LastModified),//put actual time
-                    Name = b.Uri.LocalPath.Remove(0, folderName.Length + 2).Replace('/', '\\')
-                }).
-                ToList();
+                    LastModified = item.Properties.LastModified,
+                    Name = item.Name
+                });
+            }
+
+            //List<StorageFileInfo> files =
+            //    c.ListBlobs(blobPrefix, useFlatBlobListing, BlobListingDetails.Metadata).
+            //    Select(b => new StorageFileInfo
+            //    {
+            //        LastModified = (((CloudBlockBlob)b).Properties.LastModified),//put actual time
+            //        Name = b.Uri.LocalPath.Remove(0, folderName.Length + 2).Replace('/', '\\')
+            //    }).
+            //    ToList();
 
             return files;
         }
