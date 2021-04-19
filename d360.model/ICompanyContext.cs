86a8d1fce69265ae98d112b785655ff308bd8711
Brian Quinlivan <bquinlivan@infogix.com>
2021-04-19 14:43:33 +0100

    GOV-14263 GOV-14268 Add POST and PUT Data Profile Endpoints

--- d360.model/ICompanyContext.cs
+++ d360.model/ICompanyContext.cs
@@ -147,6 +147,8 @@ namespace d360.model
         DbSet<WorkflowVersionStepTransition> WorkflowVersionStepTransitions { get; set; }
         DbSet<MetricAllocation> MetricAllocations { get; set; }
 
+        DbSet<AssetDataProfile> AssetDataProfile { get; set; }
+
         int ApiTimeout { get; }
         event EventHandler<AssetsPartiallyProcessedEventArgs> AssetsPartiallyProcessed;
         event EventHandler<RelationshipsPartiallyProcessedEventArgs> RelationshipsPartiallyProcessed;
@@ -321,6 +323,8 @@ namespace d360.model
         List<Guid> GetImpactedMeasureVersionsBy(MetricGovernanceCheckType check, int typeId);
 
         string GetOutputFieldValue(int stepId, long itemId, string fieldId);
+
+        List<DataProfileUpsertResponse> UpsertDataProfiles(List<DataProfileUpsertModel> request, ApiExecution execution, bool isInsert, int timeout = 3600);
 
         #region API Query Parameter Parsing
 
