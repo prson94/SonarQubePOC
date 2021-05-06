dd887fc4066bccff049bdf0eeac4420356d66e66
Shane Dooley <sdooley@infogix.com>
2021-05-06 12:08:54 +0100

    GOV-14230-Changing the default date format of date values in Govern workflow emails to DD MMM YYYY

--- d360.model/CompanyContext_Workflow.cs
+++ d360.model/CompanyContext_Workflow.cs
@@ -2823,7 +2823,23 @@ namespace d360.model
                         }
 
                         if (fieldRecord != null)
-                            fieldValue = fieldRecord.FormattedValue;
+                        {
+                            var type = FieldTypes.Where(x => x.ID == fieldRecord.FieldTypeID).FirstOrDefault().Type;
+                            if (type == "Date")
+                            {
+                                DateTime dt = DateTime.ParseExact(fieldRecord.FormattedValue, "MM/dd/yyyy HH:mm:ss", null);
+                                string formattedDate = dt.ToString("dd MMM yyyy");
+                                fieldValue = formattedDate;
+                            }
+                            else if (type == "DateTime")
+                            {
+                                DateTime dt = Convert.ToDateTime(fieldRecord.FormattedValue);
+                                string formattedDate = dt.ToString("dd MMM yyyyTHH:mm:ss");
+                                fieldValue = formattedDate;
+                            }
+                            else
+                                fieldValue = fieldRecord.FormattedValue;
+                        }
                     }
 
                     result = result.Replace(item, fieldValue);
