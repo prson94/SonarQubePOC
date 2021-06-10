28a7fafa2e6e0b53603db6f01c93314749e5128f
Michael Donato <mdonato7@gmail.com>
2021-06-09 22:15:39 -0400

    GOV-13711 - fix asset detail categories not saving state

--- d360.web/Scripts/app/components/shared/asset-detail/asset-detail.component.ts
+++ d360.web/Scripts/app/components/shared/asset-detail/asset-detail.component.ts
@@ -213,6 +213,10 @@ export class AssetDetailComponent implements OnChanges {
                 this.assetUID = f.Value;
             }
 
+            if ((f.FieldName || "").toUpperCase() === "ASSETTYPEUID") {
+                this.assetTypeUID = f.Value;
+            }
+
         });
         row.FirstColumnFields = row.FirstColumnFields.filter((f) => f.Type !== DetailFieldType.None);
 
@@ -230,7 +234,7 @@ export class AssetDetailComponent implements OnChanges {
                     });
             }
 
-            if (s.Name === 'UID') {
+            if ((s.FieldName || "").toUpperCase() === 'ASSETUID') {
                 this.assetUID = s.Value;
             }
 
