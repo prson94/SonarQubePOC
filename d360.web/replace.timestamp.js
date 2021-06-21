4753273b331f37d6e107abef706685cddd2cfd65
Ken McNamee <kmcnamee@infogix.com>
2021-06-21 14:29:59 -0400

    GOV-14979 - aot / ng build requires

---
+++ d360.web/replace.timestamp.js
@@ -0,0 +1,15 @@
+﻿var replace = require('replace-in-file');
+const options = {
+    files: './Scripts/environments/environment.prod.ts',
+    from: /{BUILD_TIMESTAMP}/g,
+    to: new Date().toLocaleString(),
+    allowEmptyPaths: false,
+};
+
+try {
+    let changedFiles = replace.sync(options);
+    console.log('Build version set: ' + new Date().toLocaleString());
+}
+catch (error) {
+    console.error('Error occurred:', error);
+}
