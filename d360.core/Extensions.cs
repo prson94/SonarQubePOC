87121bc7d5b6e8a437db271df3a3b487291331a7
mikejpappas <mpappas@infogix.com>
2021-07-09 09:39:10 -0400

    Codacy

--- d360.core/Extensions.cs
+++ d360.core/Extensions.cs
@@ -337,16 +337,25 @@ namespace d360.core
         public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source,
             Func<TSource, TKey> keySelector, IEqualityComparer<TKey> comparer)
         {
-            if (source == null) throw new ArgumentNullException(nameof(source));
-            if (keySelector == null) throw new ArgumentNullException(nameof(keySelector));
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (keySelector == null)
+            {
+                throw new ArgumentNullException(nameof(keySelector));
+            }
 
             return _(); IEnumerable<TSource> _()
             {
                 var knownKeys = new HashSet<TKey>(comparer);
                 foreach (var element in source)
                 {
-                    if (knownKeys.Add(keySelector(element)))
+                    if (knownKeys.Add(keySelector(element))) 
+                    {
                         yield return element;
+                    }
                 }
             }
         }
