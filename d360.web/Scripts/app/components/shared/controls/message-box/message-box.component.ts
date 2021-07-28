2d6cb5b6406d9a3b69dcd97b101cb67ef8b95150
pungupta <pungupta@infogix.com>
2021-07-28 09:33:30 +0530

    GOV-15283 Add a warning to the existing Branding page warning of the consequences of extreme branding

--- d360.web/Scripts/app/components/shared/controls/message-box/message-box.component.ts
+++ d360.web/Scripts/app/components/shared/controls/message-box/message-box.component.ts
@@ -5,8 +5,8 @@ import { Component, Input } from '@angular/core';
     selector: 'ig-message-box',
     template:
         `
-<div class="ig-message-box">
-    <div class="fa fa-info-circle"></div>
+<div class="ig-message-box" [ngStyle]="{backgroundColor : IsWarning ? '#fbe7bd' : '@ig-slate-t5'}">
+    <div [ngClass]="IsWarning ? 'fa fa-exclamation-triangle' : 'fa fa-info-circle'"></div>
     <div class="message-box-text">{{message}}<ng-content></ng-content></div>
 </div>
 `,
@@ -15,6 +15,7 @@ import { Component, Input } from '@angular/core';
 export class MessageBoxComponent {
 
     @Input() message: string;
+    @Input() IsWarning: boolean = false;
 
     constructor() {
     }
