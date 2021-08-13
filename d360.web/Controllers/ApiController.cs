f3c22a3b0c7d034572309166ddff486807092ad6
Tomislav Đurić <tduric@infogix.com>
2021-08-13 15:59:38 +0200

    - fix all value selection value on asset detail page

--- d360.web/Controllers/ApiController.cs
+++ d360.web/Controllers/ApiController.cs
@@ -410,8 +410,7 @@ select @fieldValue", new { fieldTypeID, obj = new DbString() { Value = obj, IsAn
                     left join fieldlookupvalue flv on flv.fieldtypeid = ft.id and flv.value = trim(Val.Value)
                     where a.uid = @uid 
                     and (ft.LookupObjectType <> '' or ft.LookupObjectType is not null)
-                    and (ft.LookupObjectID <> '' or ft.LookupObjectID is not null)
-                    and (ft.AllowAllValue <> 1 or ft.AllowAllValue is null)", new { uid = details.UID });
+                    and (ft.LookupObjectID <> '' or ft.LookupObjectID is not null)", new { uid = details.UID });
 
                 foreach (var ft in fieldTypes)
                 {
