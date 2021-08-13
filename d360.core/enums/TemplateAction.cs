0a76917d1a8e96d590d3e4239d4e7266b90e9d9b
Ken McNamee <kmcnamee@infogix.com>
2021-08-12 22:35:01 -0400

    More unit tests

--- d360.core/enums/TemplateAction.cs
+++ d360.core/enums/TemplateAction.cs
@@ -20,32 +20,4 @@ namespace d360.core
         [Description("Assigning Item Preview")]
         AssigningItemPreview
     }
-
-    public class TemplateActionInfo
-    {
-        public TemplateAction ID { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-
-    public static class TemplateActionExtensions
-    {
-        public static List<TemplateActionInfo> GetTemplateActionInfoList(this TemplateAction type)
-        {
-            var list = new List<TemplateActionInfo>();
-
-            foreach (MemberInfo tm in type.GetType().GetMembers(BindingFlags.Public | BindingFlags.Static))
-            {
-                list.Add(new TemplateActionInfo
-                {
-                    Description = ((DescriptionAttribute)tm.GetCustomAttribute(typeof(DescriptionAttribute))).Description,
-                    ID = (TemplateAction)Enum.Parse(typeof(TemplateAction), tm.Name),
-                    Name = tm.Name
-                });
-            }
-
-            return list;
-        }
-
-    }
 }
