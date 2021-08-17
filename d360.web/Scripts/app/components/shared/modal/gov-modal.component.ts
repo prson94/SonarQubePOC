ce6363882dfb0afb71b6743ca0a04165d3a96238
tduric <49392859+tduric@users.noreply.github.com>
2021-08-17 16:59:54 +0200

    Revert "GOV-15309 : update back behaviour when modal is opened"

--- d360.web/Scripts/app/components/shared/modal/gov-modal.component.ts
+++ d360.web/Scripts/app/components/shared/modal/gov-modal.component.ts
@@ -1,5 +1,4 @@
 ﻿import { Component, Input, Output, HostListener, EventEmitter, OnChanges, SimpleChanges, ViewChild, ElementRef, AfterContentInit, OnDestroy } from '@angular/core';
-import { PopupBackButtonService } from '../../../services/popup.service';
 
 
 @Component({
@@ -23,22 +22,12 @@ export class D3SModal implements OnChanges, AfterContentInit, OnDestroy {
     @ViewChild('popupBox', { static: false }) modalDiv: ElementRef;
 
     private display: boolean = false;
-    private modalUid: string = '';
-
-    constructor(private popupBackButtonService: PopupBackButtonService) {
-        this.modalUid = this.randomUid();
-
-        popupBackButtonService.backButtonClicked.subscribe((uuid) => {
-            if (uuid === this.modalUid) {
-                this.closePopUp();
-            }
-        });
-    }
 
     ngAfterContentInit() {
         if (this.appendToBody) {
             setTimeout(() => {
                 document.body.append(this.modalDiv.nativeElement);
+
             });
         }
     }
@@ -61,6 +50,17 @@ export class D3SModal implements OnChanges, AfterContentInit, OnDestroy {
         }
     }
 
+
+    checkKey(event: KeyboardEvent) {
+        if (event.keyCode) {
+            if (event.keyCode == 27) {
+                if (!event.defaultPrevented)
+                    this.closePopUp();
+            }
+        }
+    }
+
+
     @HostListener('wheel', ['$event'])
     handleWheelEvent(event) {
         let path: any[] = event.path;
@@ -76,7 +76,6 @@ export class D3SModal implements OnChanges, AfterContentInit, OnDestroy {
     }
 
     showPopUp() {
-        this.popupBackButtonService.addState(this.modalUid);
         this.display = true;
         if (this.modalDiv) {
             this.modalDiv.nativeElement.className = "modal-overlay";
@@ -94,7 +93,6 @@ export class D3SModal implements OnChanges, AfterContentInit, OnDestroy {
             }.bind(this), 250);
 
             this.display = false;
-            this.popupBackButtonService.popState(this.modalUid);
         }
 
     }
@@ -102,13 +100,6 @@ export class D3SModal implements OnChanges, AfterContentInit, OnDestroy {
     confirm() {
         this.onConfirm.emit('confirm');
         this.closePopUp();
-    }
-
-    randomUid() {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-            return v.toString(16);
-        });
     }
 }
 
