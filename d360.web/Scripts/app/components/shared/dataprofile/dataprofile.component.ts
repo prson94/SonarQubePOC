7980965cfb6336066f224b8007aa58c565da027c
Brian Quinlivan <bquinlivan@infogix.com>
2021-08-26 14:21:46 +0100

    GOV-15549 Only open target category on Match Detection dialog

--- d360.web/Scripts/app/components/shared/dataprofile/dataprofile.component.ts
+++ d360.web/Scripts/app/components/shared/dataprofile/dataprofile.component.ts
@@ -49,6 +49,7 @@ export class DataProfileComponent extends BaseComponent implements OnInit, OnCha
     private invalidCount: number;
 
     isMatchDetectionPopupVisible: boolean = false;
+    matchType = "";
 
     sampleDistributionChart: Highcharts.Chart;
     sampleChartXLabel: string = '';
