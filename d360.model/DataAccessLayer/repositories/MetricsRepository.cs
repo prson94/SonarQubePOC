87edfadddcea9c80bc4d4f0e82e11e698e729190
mikejpappas <mpappas@infogix.com>
2021-09-02 15:35:16 -0400

    GOV-10243 - removed threshold from Rule

--- d360.model/DataAccessLayer/repositories/MetricsRepository.cs
+++ d360.model/DataAccessLayer/repositories/MetricsRepository.cs
@@ -2262,7 +2262,6 @@ for json path";
                     case "FailCount":
                     case "OwningAssetUid":
                     case "PassCount":
-                    case "Passed":
                     case "PassFraction":
                     case "ResultUid":
                     case "RunDate":
@@ -2315,11 +2314,6 @@ for json path";
 			R.FailCount, 
 			R.TotalCount, 
 			R.PassFraction, 
-			case 
-				when R.PassCount = 0 and R.FailCount = 0 then null 
-				when Ru.Threshold <= R.PassFraction then cast(1 as bit) 
-				else cast(0 as bit)
-			end as Passed,
 			case 
 				when ROW_NUMBER() over (partition by O.Uid, R.EffectiveDate order by R.RunDate desc) = 1 then cast(0 as bit) 
 				else cast(1 as bit) 
@@ -2390,8 +2384,7 @@ select	R.ResultUid,
 		R.PassCount, 
 		R.FailCount, 
 		R.TotalCount, 
-		R.PassFraction, 
-		R.Passed{dupeColumnReference}
+		R.PassFraction{dupeColumnReference}
 from	R
 		left join E on E.ResultUid = R.ResultUid
 		outer apply dbo.GetAssetTypeTextPathById(E.EvaluatedAssetTypeId, ' > ') P
