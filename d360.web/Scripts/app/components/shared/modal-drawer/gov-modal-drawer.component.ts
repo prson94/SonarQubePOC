ad8e88c1a9c9d80af1aa8365d23599d2b9a78ca1
Tomislav Đurić <tduric@infogix.com>
2021-09-07 12:06:01 +0200

    - codacy

--- d360.web/Scripts/app/components/shared/modal-drawer/gov-modal-drawer.component.ts
+++ d360.web/Scripts/app/components/shared/modal-drawer/gov-modal-drawer.component.ts
@@ -89,9 +89,9 @@ export class D3SModalDrawer implements OnChanges, AfterContentInit, OnDestroy, A
         let path: any[] = event.path;
         //add scroll exceptions here
         if (this.display === true
-            && !(path.filter(x => x.tagName === 'D3S-TAG-USAGE').length > 0)
-            && !(path.filter(x => x.tagName === 'D3S-ASSET-TYPE-MODAL-EDITOR').length > 0)
-            && !(path.filter(x => x.tagName === 'P-DROPDOWNITEM').length > 0)
+            && !(path.filter((x) => x.tagName === 'D3S-TAG-USAGE').length > 0)
+            && !(path.filter((x) => x.tagName === 'D3S-ASSET-TYPE-MODAL-EDITOR').length > 0)
+            && !(path.filter((x) => x.tagName === 'P-DROPDOWNITEM').length > 0)
             && !(path.filter((x) => x.tagName === 'IG-PROPERTY-GROUP').length > 0)
         ) {
             event.preventDefault();
