4741928e03ed2c69de2bcd2f6ab0698d1006f228
Brian Quinlivan <bquinlivan@infogix.com>
2021-09-10 12:03:49 +0100

    GOV-15307 Fix Asset Path 'is' filtering and add populated/not populated options for Tags

--- d360.web/Scripts/app/components/shared/dataprofile/match-detection.component.ts
+++ d360.web/Scripts/app/components/shared/dataprofile/match-detection.component.ts
@@ -64,7 +64,7 @@ export class MatchDetectionComponent extends BaseComponent implements OnChanges 
             FriendlyName: 'Tag',
             Type: new FieldType("Tag"),
             Category: "",           
-            RemovePopulatedOperator: true
+            RemovePopulatedOperator: false
         },
         {
             Name: 'Path',
