29c14ed12e01b2039d3c76eae4bb0a30b2852d88
Michael Donato <mdonato7@gmail.com>
2021-09-20 09:11:06 -0400

    GOV-15308 - add back button to govern

---
+++ d360.web/Scripts/app/components/shared/header/header-back-button.component.ts
@@ -0,0 +1,63 @@
+﻿import {
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    OnDestroy,
+    OnInit,
+} from '@angular/core';
+import {
+    Router,
+    NavigationEnd
+} from '@angular/router';
+
+
+@Component({
+    selector: 'd3s-header-back-button',
+    template:
+        `
+            <button igButton
+                    class="header-back-button"
+                    [disabled]="disabled"
+                    type="button" 
+                    icon="fa-chevron-left" 
+                    (click)="back()" ></button>
+    `,
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class HeaderBackButtonComponent implements OnInit, OnDestroy {
+    disabled: boolean = false;
+    private sub: any = null;
+
+
+    constructor(private router: Router,
+        private ref: ChangeDetectorRef
+    ) {
+    }
+
+    ngOnInit() {
+        this.checkHistory();
+
+        this.sub = this.router.events.subscribe((val) => {
+            if (val instanceof NavigationEnd) {
+                this.checkHistory();
+            }
+        });
+    }
+
+    ngOnDestroy() {
+        if (this.sub) {
+            this.sub.unsubscribe();
+        }
+    }
+
+    back() {
+        window.history.back();
+    }
+
+    checkHistory() {
+        this.disabled = window.history.length <= 2;
+        this.ref.markForCheck();
+    }
+}
+
