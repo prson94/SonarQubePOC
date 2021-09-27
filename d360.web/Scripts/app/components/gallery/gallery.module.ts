558414f35bf51a90e2309fe281b2e3954513e120
Brian Quinlivan <bquinlivan@infogix.com>
2021-09-27 15:32:29 +0100

    GOV-15558 Codacy Fix

--- d360.web/Scripts/app/components/gallery/gallery.module.ts
+++ d360.web/Scripts/app/components/gallery/gallery.module.ts
@@ -68,7 +68,7 @@ import { GalleryInputGroupComponent } from './gallery.input-group.component';
 import { GalleryModalComponent } from "./gallery.modal.component";
 import { GalleryModalDrawerComponent } from './gallery.modal-drawer.component';
 import { ModalDrawerModule } from '../shared/modal-drawer/gov-modal-drawer.module';
-import { SidePanelModule } from '../shared/sidepanel/side-panel.module'
+import { SidePanelModule } from '../shared/sidepanel/side-panel.module';
 
 @NgModule({
     imports: [
