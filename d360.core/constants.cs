41ed64d4792b1fe17a3ddf4398d15c6ff923a4fd
mikejpappas <mpappas@infogix.com>
2021-10-21 17:22:15 -0400

    GOV-16123 - Change the build version to be generated based on the build date instead of manually updated each release

--- d360.core/constants.cs
+++ d360.core/constants.cs
@@ -30,9 +30,5 @@ namespace d360.core
         public const string TITLE_PREFIX = "Data360";
         public const string COMPANY = "Precisely.";
         public const string PRODUCT = "Data360 Govern";
-        public const string COPYRIGHT = "Copyright © Precisely. 2021";
-        public const string PRODUCT_VERSION = "2021.11.05.*";
-        public const string PRODUCT_VERSION_NOREVISION = "2021.11.05";
-
     }
 }
