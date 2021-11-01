990a8d1e433bc93361950991f74622b2e0be52da
mikejpappas <mpappas@infogix.com>
2021-11-01 09:13:07 -0400

    GOV-16123

--- d360.core/versioning.cs
+++ d360.core/versioning.cs
@@ -3,7 +3,7 @@
     public static class versioning
     {
         public const string COPYRIGHT = "Copyright © Precisely. 2021";
-        public const string PRODUCT_VERSION = "2021.10.21.*";
-        public const string PRODUCT_VERSION_NOREVISION = "2021.10.21";
+        public const string PRODUCT_VERSION = "2021.11.01.*";
+        public const string PRODUCT_VERSION_NOREVISION = "2021.11.01";
     }
 }
