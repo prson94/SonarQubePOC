0283b17df2bb6de1984534b362928e34c1060947
Viktor Lova <viktor.lova@globallogic.com>
2021-11-15 16:53:53 +0200

    Added obsolete attribute for ICompanyContext.Delete(expr)

--- d360.model/CompanyContext.cs
+++ d360.model/CompanyContext.cs
@@ -1374,6 +1374,7 @@ where	I.ID is null";
         /// <summary>
         /// Removes the item(s) from the system, as well as any dynamic fields associated with the item(s), if any.
         /// </summary>
+        [Obsolete("Please, use DeleteAsync. Delete is non-async & not transactional")]
         public override bool Delete<T>(Expression<Func<T, bool>> predicate)
         {
             var items = Filter(predicate).ToList();
