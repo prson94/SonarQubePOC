da550d30ecd601f0199a68e92cc5767ab284ee31
Brian Quinlivan <bquinlivan@infogix.com>
2021-11-23 11:33:49 +0000

    GOV-16165 Add IntersectTypeUID and AssetUID to GET /api/v2/responsibilities/types/{responsibilityTypeUid}/ownershiprules

--- d360.core/entities/Security/ResponsibilityTypeRelationRule.cs
+++ d360.core/entities/Security/ResponsibilityTypeRelationRule.cs
@@ -99,6 +99,8 @@ namespace d360.core.entities
         public int IntersectTypeID { get; set; }
         public string TargetObject { get; set; }
         public int TargetObjectID { get; set; }
+        public Guid? IntersectTypeUID { get; set; }
+        public Guid? AssetUID { get; set; }
     }
 
     [JsonConverter(typeof(StringEnumConverter), true)]
@@ -114,6 +116,7 @@ namespace d360.core.entities
     {
         public string Object { get; set; }
         public int ObjectID { get; set; }
+        public int AssetUID { get; set; }
         public List<ResponsibilityRuleDefinitionWhen> Conditions { get; set; }
         public ResponsibilityMatchType MatchType { get; set; } = ResponsibilityMatchType.And;
     }
