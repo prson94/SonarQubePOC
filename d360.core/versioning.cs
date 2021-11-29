ba24aa2b2b6aaf404a74434f32f9e9b32f4fdb2f
mikejpappas <mpappas@infogix.com>
2021-11-29 12:03:55 -0500

    GOV-16466 - manually update build version

--- d360.core/versioning.cs
+++ d360.core/versioning.cs
@@ -3,7 +3,7 @@
     public static class versioning
     {
         public const string COPYRIGHT = "Copyright © Precisely. 2021";
-        public const string PRODUCT_VERSION = "2021.11.01.*";
-        public const string PRODUCT_VERSION_NOREVISION = "2021.11.01";
+        public const string PRODUCT_VERSION = "2021.12.03.*";
+        public const string PRODUCT_VERSION_NOREVISION = "2021.12.03";
     }
 }
