d9859c9dc73b0c9e89974806248daed64124ad8f
Tomislav Đurić <tduric@infogix.com>
2021-11-30 17:35:32 +0100

    - fix asset editor multiple selection and mysterious x showing  on one->many relationship

--- d360.web/Scripts/app/components/shared/asset-editor/asset-editor-field.component.ts
+++ d360.web/Scripts/app/components/shared/asset-editor/asset-editor-field.component.ts
@@ -34,6 +34,7 @@ import { CompanySettingsService } from '../../../services/settings.service';
 import { Dropdown } from 'primeng/dropdown';
 import { OverlayPanel } from 'primeng/overlaypanel';
 import { Table } from 'primeng/table';
+import { isArray } from 'core-js/library/core/array';
 
 @Component({
     selector: 'asset-editor-field',
@@ -302,6 +303,24 @@ export class AssetEditorFieldComponent extends BaseComponent implements OnInit, 
                 });
                 this.selectSingleItem(null, { value: null });
 
+            }
+        }
+
+        if (this.field.FieldType === 'Relationship') {
+            //dropdown with showClear attribute shows clear button even if value is empty string
+            console.log(this.field.Value);
+            var hasNoValue = false;
+            if (!this.field.Value) {
+                hasNoValue = true;
+            }
+            else if (Array.isArray(this.field.Value) && (this.field.Value as []).length === 0) {
+                hasNoValue = true;
+
+            }
+            console.log(hasNoValue);
+
+            if (hasNoValue) {
+                this.form.controls[this.field.FieldName].setValue(null);
             }
         }
 
