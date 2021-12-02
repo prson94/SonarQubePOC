f81804bfb0a8d33ffcf1b5e502cca539f45e966c
Tomislav Đurić <tduric@infogix.com>
2021-12-02 17:10:37 +0100

    -get assets path tests

--- igx.UnitTests/RepositoryTests/AssetRepositoryTests.cs
+++ igx.UnitTests/RepositoryTests/AssetRepositoryTests.cs
@@ -135,7 +135,7 @@ namespace igx.UnitTests.RepositoryTests
                     if (sql.ToLowerInvariant().Contains("create table #family"))
                     {
                         var assets = param.GetType().GetProperty("assetUid").GetValue(param, null) as IEnumerable<Guid>;
-                        if (assets.FirstOrDefault() == Guid.Parse(DataConstants.ValidGUID2)) 
+                        if (assets.FirstOrDefault() == Guid.Parse(DataConstants.ValidGUID2))
                         {
                             return new List<Guid>();
                         }
@@ -269,6 +269,51 @@ namespace igx.UnitTests.RepositoryTests
             var result = assetRepository.GetAssets(assetType, pars);
 
             Assert.False(result.Status == TaskStatus.Faulted, "Invalid SQL Generated. Look at mock mockCompanyContext for QueryAsync and mustContain definition");
+        }
+
+
+        [Fact]
+        public async void GetAssetsPath()
+        {
+            var mockCompanyContext = new Mock<ICompanyContext>();
+            mockCompanyContext.Setup(x => x.CurrentResourceIsAdmin).Returns(true);
+
+            mockCompanyContext.Setup(x => x.QueryFirstOrDefaultAsync<int>(It.IsAny<string>(), It.IsAny<object>(), It.IsAny<int>()))
+                .Returns((string sql, object param, int timeout) =>
+                {
+                    return Task.FromResult(5);
+                });
+
+
+            mockCompanyContext.Setup(x => x.QueryAsync<AssetPathResult>(It.IsAny<string>(), It.IsAny<DynamicParameters>(), It.IsAny<int>()))
+                .Returns((string sql, DynamicParameters param, int timeout) =>
+                {
+                    var res = new List<AssetPathResult>();
+                    var expectedParams = new List<string> { "pagesize", "pagenum", "offset", "assetTypeId" };
+
+                    if (!expectedParams.All(x => param.ParameterNames.Select(y=> y.ToLowerInvariant()).Contains(x.ToLowerInvariant())))
+                    {
+                        return null;
+                    }
+
+                    return Task.FromResult(res as IEnumerable<AssetPathResult>);
+                });
+
+            List<string> mustContain = new List<string>();
+
+
+
+            var assetRepository = new AssetRepository(mockCompanyContext.Object, GetQueue(), GetStorage(), GetCommunity());
+
+            List<KeyValuePair<string, string>> pars = new List<KeyValuePair<string, string>>();
+            pars.Add(new KeyValuePair<string, string>("_pagesize", "30"));
+            pars.Add(new KeyValuePair<string, string>("_pagenum", "5"));
+            pars.Add(new KeyValuePair<string, string>("_includetotal", "true"));
+
+            var assetType = new AssetType { ID = 1, uid = Guid.Parse(DataConstants.ValidGUID) };
+            var result = await assetRepository.GetAssetPaths(assetType, pars);
+
+
         }
     }
 }
