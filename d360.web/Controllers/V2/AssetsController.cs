2078b8fab98fc67aa313d6c971d383f4aa782e0b
Michael Donato <mdonato7@gmail.com>
2021-12-14 14:32:39 -0500

    GOV-16583 - swagger param description missing default

--- d360.web/Controllers/V2/AssetsController.cs
+++ d360.web/Controllers/V2/AssetsController.cs
@@ -214,7 +214,7 @@ namespace d360.web.Controllers.V2
             SwaggerParameter("_exporttemplateuid", "The Uid of the template which will be used when exporting results.", DataType = "string", ParameterType = "query", Required = false),
             SwaggerParameter("_includeCreatedModifiedBy", "Include the CreatedByUid, and ModifiedByUid fields in the response. The default value is false meaning these values are not returned.", DataType = "boolean", ParameterType = "query", Required = false),
             SwaggerParameter("_includeOwnershipLookup", "Include the OwnershipLookup fields in the response. The default value is false meaning these values are not returned.  An exception to this setting is if an ownershiplookup type field is used in the sort for the asset type this setting will be ignored and the field always returned.", DataType = "boolean", ParameterType = "query", Required = false),
-            SwaggerParameter("_includeProfilingCheck", "Include a check for whether or not the asset has Data Profiling.", DataType = "boolean", ParameterType = "query", Required = false),
+            SwaggerParameter("_includeProfilingCheck", "Include a check for whether or not the asset has Data Profiling. The default value is false.", DataType = "boolean", ParameterType = "query", Required = false),
         ]
         public async Task<IHttpActionResult> GetAssetsAsync(Guid assetTypeUid, CancellationToken cancellationToken)
         {
