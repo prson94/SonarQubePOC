d174c65f8ec1c534402807a64365baca96abd421
KunetskyiGlobal <96059828+KunetskyiGlobal@users.noreply.github.com>
2021-12-22 15:56:38 +0200

    implement DisplayValueUpdateProcessor in Azure Function
    it adds one more handler for DisplayValueQueue in addition to existing WebJob
    it was requested in GOV-16644

---
+++ igx.functions/DisplayValueUpdateProcessor.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using d360.core.queue;
+using d360.utils.company;
+using Dapper;
+using Microsoft.Azure.WebJobs;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using d360.model;
+using d360.core.enums;
+
+namespace igx.functions.consumption
+{
+    public class DisplayValueUpdateProcessor
+    {
+        const string functionName = "DisplayValueUpdateProcessor";
+        private CoreFunction CoreFunction;
+
+        [FunctionName("DisplayValueUpdateProcessor")]
+        public async Task Run([QueueTrigger("%DisplayValueQueue%"), StorageAccount("QueueStorageAccount")] string myQueueItem, ExecutionContext context)
+        {
+            var config = new ConfigurationBuilder()
+                   .SetBasePath(context.FunctionAppDirectory)
+                   .AddJsonFile("appSettings.json", optional: true, reloadOnChange: true)
+                   .AddEnvironmentVariables()
+                   .Build();
+
+            CoreFunction = new CoreFunction(config);
+
+            var updateInfo = JsonConvert.DeserializeObject<DisplayUpdateInfo>(myQueueItem);
+
+            try
+            {
+                var _c = CoreFunction.GetCompaniesByCurrentSlot().FirstOrDefault(x => x.CompanyID == updateInfo.CompanyID);
+                var community = JobDbContextCreator.CreateCommunityContext(updateInfo.CompanyID, 0, _c.UrlPrefix, true);
+
+                using (var companyConnection = CompanyConnectionUtils.GetCompanyConnection(updateInfo.CompanyID))
+                {
+                    await companyConnection.OpenIfClosed();
+
+                    var assetTypeID = updateInfo.AssetTypeID;
+                    if (updateInfo.ObjectTypeID > 0)
+                    {
+                        assetTypeID = await companyConnection.QueryFirstOrDefaultAsync<int>($"select id from assettype where [object] = @obj and [objectid] = @objId", new { obj = new DbString { Value = updateInfo.ObjectType, IsFixedLength = true, Length = 20, IsAnsi = true }, objId = updateInfo.ObjectTypeID });
+                    }
+
+                    //if its an asset call the asset update proc
+                    //if its a asset type call the asset type update proc
+                    if (updateInfo.AssetID > 0)
+                    {
+                        await companyConnection.ExecuteAsync("exec GenerateAssetDisplayValue @assetID, null,-1", new { assetID = updateInfo.AssetID }, null, 2400);
+                    }
+                    else if (assetTypeID > 0)
+                    {
+                        await companyConnection.ExecuteAsync("exec GenerateAssetTypeDisplayValues @assetTypeID", new { assetTypeID }, null, 2400);
+                    }
+                    else if (updateInfo.RebuildAll)
+                    {
+                        try
+                        {
+                            await companyConnection.ExecuteAsync("exec GenerateAllAssetTypeDisplayValues", commandTimeout: 2400);
+                        }
+                        catch
+                        {
+                            throw;
+                        }
+                        finally
+                        {
+                            await community.UpdateRebuildJobStatus(CompanyRebuildJobToken.DisplayValues, CompanyRebuildJobStatusState.Inactive);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                CoreFunction.AITrackException(functionName, ex, updateInfo.CompanyID);
+            }
+        }
+    }
+}
