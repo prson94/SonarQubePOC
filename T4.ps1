8d9d9fdee2b66361991fa5662bcb990778a83a88
Ken McNamee <kmcnamee@gmail.com>
2022-01-12 13:55:52 -0500

    Create T4.ps1

---
+++ T4.ps1
@@ -0,0 +1,10 @@
+"*************************************************
+" T4 Template converter
+"*************************************************
+
+Get-ChildItem -Path .\ -Filter *.tt -Recurse -File -Name| ForEach-Object {
+    $file = [System.IO.Path]::GetFileName($_);
+    $directory = [System.IO.Path]::GetDirectoryName($_)
+    "Conversion file : " + $file
+    t4 "$directory\$file" -I="$directory"
+}
