7fe33a14aa5d869cfca54dd79debe8e2375544c7
tduric <49392859+tduric@users.noreply.github.com>
2022-01-19 11:44:14 +0100

    codacy fix

--- d360.web/Scripts/app/components/reference/reference-item-list.component.ts
+++ d360.web/Scripts/app/components/reference/reference-item-list.component.ts
@@ -69,7 +69,7 @@ export class ReferenceItemGridComponent extends BaseComponent implements OnChang
             delete this.loadParams['_filter'];
         }
 
-        if (changes.highlightUid && changes.highlightUid.currentValue != changes.highlightUid.previousValue) {
+        if (changes.highlightUid && changes.highlightUid.currentValue !== changes.highlightUid.previousValue) {
             var highlightedAsset = this.items.filter((a) => (a.AssetUid as string).toLowerCase() === this.highlightUid.toLowerCase());
             if (highlightedAsset && highlightedAsset[0]) {
                 this.selected = highlightedAsset[0];
