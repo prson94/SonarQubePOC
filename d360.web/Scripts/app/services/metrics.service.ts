f8824358a1ac679f210a718b273ec31f793f6927
mikejpappas <mpappas@infogix.com>
2022-01-26 15:39:10 -0500

    GOV-16879 - Newly created threshold based measures are changing into "Use Pass Threshold" No after save

--- d360.web/Scripts/app/services/metrics.service.ts
+++ d360.web/Scripts/app/services/metrics.service.ts
@@ -58,6 +58,7 @@ export class MetricsService extends BaseObservableService {
                         else {
                             r.HasThreshold = true;
                         }
+                        console.log(`${r.Name} (${r.Threshold}) - r.HasThreshold: ${r.HasThreshold}`);
                     });
                     //return res;
                 }),
