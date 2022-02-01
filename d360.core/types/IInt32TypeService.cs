87fccf2bb2921b4769ff71efb7eb35d8bd03aa59
Oleh Hochachko <oleh.hochachko@infogix.com>
2022-02-01 11:32:20 +0200

    [~] Added Int32 type service
    [~] Extended type service providers

---
+++ d360.core/types/IInt32TypeService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace d360.core.types
+{
+    public interface IInt32TypeService
+    {
+        bool CanParse(string input);
+
+        bool CanParse(string input, IFormatProvider provider, NumberStyles styles);
+
+        int Parse(string input, IFormatProvider provider, NumberStyles styles);
+
+        int Parse(string input);
+
+        IEnumerable<int> Parse(IEnumerable<string> input, IFormatProvider provider, NumberStyles styles);
+
+        IEnumerable<int> Parse(IEnumerable<string> input);
+
+        bool TryParse(string input, out int result, IFormatProvider provider, NumberStyles styles);
+
+        bool TryParse(string input, out int result);
+
+        bool TryParse(IEnumerable<string> input, out IList<int> result, IFormatProvider provider, NumberStyles styles);
+
+        bool TryParse(IEnumerable<string> input, out IList<int> result);
+
+        string ToString(int value);
+        string ToString(int value, string format, IFormatProvider formatProvider);
+
+        IEnumerable<string> ToString(IEnumerable<int> value);
+
+        IEnumerable<string> ToString(IEnumerable<int> value, string format, IFormatProvider formatProvider);
+    }
+}
--- d360.core/types/StaticTypeServiceProvider.cs
+++ d360.core/types/StaticTypeServiceProvider.cs
@@ -8,11 +8,14 @@ namespace d360.core.types
         private static readonly Lazy<IDateTimeService> DateTimeLazy = new Lazy<IDateTimeService>(() => new DateTimeService());
         private static readonly Lazy<IDecimalService> DecimalLazy = new Lazy<IDecimalService>(() => new DecimalService());
         private static readonly Lazy<IInt64Service> Int64Lazy = new Lazy<IInt64Service>(() => new Int64Service());
+        private static readonly Lazy<IInt32TypeService> Int32Lazy = new Lazy<IInt32TypeService>(() => new Int32TypeService());
 
         public IDateTimeService DateTimeService => DateTimeLazy.Value;
 
         public IDecimalService DecimalService => DecimalLazy.Value;
 
         public IInt64Service Int64Service => Int64Lazy.Value;
+
+        public IInt32TypeService Int32 => Int32Lazy.Value;
     }
 }
