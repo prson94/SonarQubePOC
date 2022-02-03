4e3aac956466a04439b04f9ce1b62416e8353d0e
Ruslan Behun <ruslan.behun@globallogic.com>
2022-02-03 21:44:54 +0200

    GOV-16931 - add data-cy attr to assets modal, editor and paginator elements

--- d360.web/Scripts/app/directives/ig-data-cy.directive.ts
+++ d360.web/Scripts/app/directives/ig-data-cy.directive.ts
@@ -1,33 +1,83 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Directive, ElementRef, Input, NgModule, Renderer2 } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, Input, NgModule, OnDestroy, Renderer2 } from '@angular/core';
 
 enum PrimeComponent {
     Dropdown = 'P-DROPDOWN',
+    Editor = 'P-EDITOR',
+    Table = 'P-TABLE',
 }
 
 @Directive({
     selector: '[igDataCy]'
 })
-export class DataCyDirective implements AfterViewInit {
+export class DataCyDirective implements AfterViewInit, OnDestroy {
     @Input() igDataCy = '';
     readonly attr = 'data-cy';
+    paginatorMutationObserver: MutationObserver;
 
     constructor(private el: ElementRef, private renderer: Renderer2) { }
 
     ngAfterViewInit(): void {
-        const tagName: string = this.el.nativeElement.tagName;
+        const nativeElement = this.el.nativeElement;
+        const tagName: string = nativeElement.tagName;
         switch (tagName) {
             case PrimeComponent.Dropdown:
-                const dropdown = this.el.nativeElement.querySelector('.p-dropdown');
+                const dropdown = nativeElement.querySelector('.p-dropdown');
                 this.setDataCyAttr(dropdown, this.igDataCy);
                 break;
+            case PrimeComponent.Editor:
+                const editorContent = nativeElement.querySelector('.p-editor-content');
+                this.setDataCyAttr(editorContent, this.igDataCy);
+                break;
+            case PrimeComponent.Table:
+                this.setDataCyAttrToPaginator(nativeElement);
+                break;
             default:
-                this.setDataCyAttr(this.el.nativeElement, this.igDataCy);
+                console.log('tagName: ' + tagName);
+                this.setDataCyAttr(nativeElement, this.igDataCy);
         }
     }
 
     private setDataCyAttr(el: HTMLElement, attrValue: string): void {
         this.renderer.setAttribute(el, this.attr, attrValue);
+    }
+
+    private setDataCyAttrToPaginator(nativeElement: HTMLElement): void {
+        const paginator = nativeElement.querySelector('.p-paginator');
+        if (paginator) {
+            this.setDataCyAttrToPaginatorPages(paginator);
+            this.setDataCyAttrToPaginatorDropdown(paginator);
+            this.setDataCyAttrToMutatedPaginatorPages(paginator)
+        }
+    }
+
+    private setDataCyAttrToPaginatorPages(paginator: Element): void {
+        const paginatorPages: NodeListOf<HTMLElement> = paginator.querySelectorAll('.p-paginator-page');
+        paginatorPages.forEach((paginatorPage) => {
+            this.setDataCyAttr(paginatorPage, this.igDataCy + paginatorPage.textContent);
+        });
+    }
+
+    private setDataCyAttrToMutatedPaginatorPages(paginator: Element): void {
+        this.paginatorMutationObserver = new MutationObserver(mutations => {
+            mutations.forEach((mutation) => {
+                const addedNode = mutation.addedNodes[0] as HTMLElement;
+                if (addedNode) {
+                    this.setDataCyAttr(addedNode, this.igDataCy + addedNode.textContent);
+                }
+            });
+        });
+        var config = { childList: true, subtree : true };
+        this.paginatorMutationObserver.observe(paginator, config);
+    }
+
+    private setDataCyAttrToPaginatorDropdown(paginator: Element): void {
+        const paginatorDropdown = paginator.querySelector('.p-dropdown') as HTMLElement;
+        this.setDataCyAttr(paginatorDropdown, this.igDataCy + 'Dropdown');
+    }
+
+    ngOnDestroy() {
+        this.paginatorMutationObserver.disconnect();
     }
 }
 
