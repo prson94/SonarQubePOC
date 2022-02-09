0a8afb2170fa959192df2a29373d829f03f7490b
Oleh Hochachko <oleh.hochachko@infogix.com>
2022-02-09 14:30:43 +0200

    GOV-16858: added ApplicationHealth repository and entities

---
+++ d360.core/entities/ApplicationHealthDetailsEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace d360.core.entities
+{
+    public class ApplicationHealthDetailsEntity
+    {
+        public int QueueTaskCount { get; set; }
+
+        public int ApiExecutionPendingCount { get; set; }
+
+        public int WorkflowItemPendingCount { get; set; }
+    }
+}
