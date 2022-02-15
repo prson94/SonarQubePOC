6c80065bd9b6ce1b755753d197f4b53f942d07f2
KunetskyiGlobal <96059828+KunetskyiGlobal@users.noreply.github.com>
2022-02-15 15:37:16 +0200

    Format ICreatedMetadata.cs

--- d360.core/entities/Contracts/ICreatedMetadata.cs
+++ d360.core/entities/Contracts/ICreatedMetadata.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace d360.core.entities.Contracts
 {
     /// <summary>
-    /// This interface is used in the DbContext when checking wther object have these two fields present.  
+    /// This interface is used in the DbContext when checking wether object have these two fields present.  
     /// If so, this tells the DbContext to update these values with the current date and resource ID.  
     /// These values are then used on the table triggers to add an audit record.
     /// This should be used only on customer-specific objects!
@@ -15,6 +11,7 @@ namespace d360.core.entities.Contracts
     public interface ICreatedMetadata
     {
         DateTime? CreatedOn { get; set; }
+
         int? CreatedBy { get; set; }
     }
 }
