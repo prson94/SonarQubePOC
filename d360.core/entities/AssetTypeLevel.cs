7743d4198965e31aacea4e772a8c1b6e133165b6
KunetskyiGlobal <96059828+KunetskyiGlobal@users.noreply.github.com>
2022-02-23 17:52:51 +0200

    Format AssetTypeLevel.cs

--- d360.core/entities/AssetTypeLevel.cs
+++ d360.core/entities/AssetTypeLevel.cs
@@ -1,7 +1,4 @@
-﻿using d360.core.enums;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Runtime.Serialization;
 
@@ -10,29 +7,58 @@ namespace d360.core.entities
     [DataContract(Namespace = NAMESPACE)]
     public class AssetTypeLevel : BaseObject
     {
-        [DataMember, Key, Column(Order = 1), Display(ResourceType = typeof(d360.core.resources.Fields), Name = "AssetType_Name", Description = "AssetType_Description")]
+        [
+            DataMember, 
+            Key, 
+            Column(Order = 1), 
+            Display(ResourceType = typeof(resources.Fields), 
+                Name = "AssetType_Name", 
+                Description = "AssetType_Description")
+        ]
         public int AssetTypeID { get; set; }
 
-        [DataMember, Key, Column(Order = 2), Display(ResourceType = typeof(d360.core.resources.Fields), Name = "Level_Name", Description = "Level_Description")]
+        [
+            DataMember, 
+            Key, 
+            Column(Order = 2), 
+            Display(ResourceType = typeof(resources.Fields), 
+                Name = "Level_Name", 
+                Description = "Level_Description")
+        ]
         public int Level { get; set; }
 
-        [
-        DataMember, Display(ResourceType = typeof(d360.core.resources.Fields), Name = "Name_Name", Description = "Name_Description"),
-        Required(AllowEmptyStrings = false, ErrorMessageResourceType = typeof(d360.core.resources.Fields), ErrorMessageResourceName = "Name_ErrorRequired"), StringLength(250)
+        [        
+            DataMember, 
+            Display(ResourceType = typeof(resources.Fields), 
+                Name = "Name_Name", 
+                Description = "Name_Description"),
+            Required(AllowEmptyStrings = false, 
+                ErrorMessageResourceType = typeof(resources.Fields), 
+                ErrorMessageResourceName = "Name_ErrorRequired"), 
+            StringLength(250)
         ]
         public string Name { get; set; }
 
-        [DataMember, Display(ResourceType = typeof(d360.core.resources.Fields), Name = "Description_Name", Description = "Description_Description")]
+        [
+            DataMember, 
+            Display(ResourceType = typeof(resources.Fields), 
+            Name = "Description_Name", 
+            Description = "Description_Description")
+        ]
         public string Description { get; set; }
-
 
         #region Navigation Properties
 
-        [IgnoreDataMember, ForeignKey("AssetTypeID"), Display(ResourceType = typeof(d360.core.resources.Fields), Name = "AssetType_Name", Description = "AssetType_Description")]
+        [
+            IgnoreDataMember, 
+            ForeignKey("AssetTypeID"), 
+            Display(ResourceType = typeof(resources.Fields), 
+            Name = "AssetType_Name", 
+            Description = "AssetType_Description")
+        ]
         public virtual AssetType AssetType { get; set; }
 
         #endregion
-
     }
 
     public class AssetTypeLevelApiViewModel
