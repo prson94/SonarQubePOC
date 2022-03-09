4c42aef97cd288286d9db9711860d1878a417b95
KunetskyiGlobal <96059828+KunetskyiGlobal@users.noreply.github.com>
2022-03-09 12:55:45 +0200

    Update ICreatedMetadata.cs
    fix typo issue

--- d360.core/entities/Contracts/ICreatedMetadata.cs
+++ d360.core/entities/Contracts/ICreatedMetadata.cs
@@ -3,7 +3,7 @@
 namespace d360.core.entities.Contracts
 {
     /// <summary>
-    /// This interface is used in the DbContext when checking wether object have these two fields present.  
+    /// This interface is used in the DbContext when checking whether object have these two fields present.  
     /// If so, this tells the DbContext to update these values with the current date and resource ID.  
     /// These values are then used on the table triggers to add an audit record.
     /// This should be used only on customer-specific objects!
