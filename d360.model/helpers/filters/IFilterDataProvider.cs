1e514c6d38d5fd20c82e8dc8dbf05ab36622125a
KunetskyiGlobal <96059828+KunetskyiGlobal@users.noreply.github.com>
2022-03-10 15:29:44 +0200

    Format IFilterDataProvider.cs

--- d360.model/helpers/filters/IFilterDataProvider.cs
+++ d360.model/helpers/filters/IFilterDataProvider.cs
@@ -1,14 +1,18 @@
-﻿using d360.core.entities;
-using System;
+﻿using System;
 using System.Collections.Generic;
+
+using d360.core.entities;
 
 namespace d360.model.helpers.filters
 {
     public interface IFilterDataProvider
     {
         (List<IntersectType>, List<Asset>, List<AssetType>) GetDataForRelationshipsParsing(List<Guid> IntersectUids, List<Guid> AssetUids);
+        
         int GetFieldLookupValue(string lookupObjectType, int lookupObjectId, int fieldTypeIdForLookupValue, string value);
+        
         FieldType GetFieldTypeById(int? fieldTypeId);
+        
         bool IsFieldFromRelationship(int fieldTypeId);
     }
 }
