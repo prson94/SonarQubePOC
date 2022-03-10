1ff483a142cc174497dff23e6725dd3383ea4932
KunetskyiGlobal <96059828+KunetskyiGlobal@users.noreply.github.com>
2022-03-10 16:21:57 +0200

    Format IWorkflowApiModelValidator.cs

--- d360.model/validators/IWorkflowApiModelValidator.cs
+++ d360.model/validators/IWorkflowApiModelValidator.cs
@@ -1,35 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace d360.model.validators
 {
     public interface IWorkflowApiModelValidator
     {
         bool IsValidGuidCountForWorkflowGetTypeModel(IEnumerable<KeyValuePair<string, string>> queryParams);
+        
         bool IsValidGuidForWorkflowGetTypeModel(IEnumerable<KeyValuePair<string, string>> queryParams);
+        
         bool IsValidGuidCountForWorkflowGetVersionModel(IEnumerable<KeyValuePair<string, string>> queryParams);
+        
         bool IsValidGuidForWorkflowGetVersionModel(IEnumerable<KeyValuePair<string, string>> queryParams);
+        
         bool IsValidOrderByFieldForWorkflowGetVersionModel(IEnumerable<KeyValuePair<string, string>> queryParams);
 
         bool IsValidAssetType(IEnumerable<KeyValuePair<string, string>> queryParams);
+        
         bool IsValidActionType(IEnumerable<KeyValuePair<string, string>> queryParams);
+        
         bool IsValidRelationshipType(IEnumerable<KeyValuePair<string, string>> queryParams);
+        
         bool IsValidWorkflowType(IEnumerable<KeyValuePair<string, string>> queryParams);
+        
         bool IsValidWorkflowVersion(Guid workflowVersionUID);
+        
         bool IsValidWorkflowInstance(Guid workflowVersionUID);
+        
         bool IsValidWorkflowVersion(IEnumerable<KeyValuePair<string, string>> queryParams);
 
-
         bool IsValidGuidCountForGetWorkflowModel(IEnumerable<KeyValuePair<string, string>> queryParams);
-        bool IsValidGuidForGetWorkflowModel(IEnumerable<KeyValuePair<string, string>> queryParams);
-        bool IsValidAsset(IEnumerable<KeyValuePair<string, string>> queryParams);
-        bool IsValidAction(IEnumerable<KeyValuePair<string, string>> queryParams);
-        bool IsValidRelationship(IEnumerable<KeyValuePair<string, string>> queryParams);
-        bool IsValidOrderByFieldForGetWorkflowModel(IEnumerable<KeyValuePair<string, string>> queryParams);
-        bool IsValidDirectionForWorkflowGetModel(IEnumerable<KeyValuePair<string, string>> queryParams);
         
+        bool IsValidGuidForGetWorkflowModel(IEnumerable<KeyValuePair<string, string>> queryParams);
+        
+        bool IsValidAsset(IEnumerable<KeyValuePair<string, string>> queryParams);
+        
+        bool IsValidAction(IEnumerable<KeyValuePair<string, string>> queryParams);
+        
+        bool IsValidRelationship(IEnumerable<KeyValuePair<string, string>> queryParams);
+        
+        bool IsValidOrderByFieldForGetWorkflowModel(IEnumerable<KeyValuePair<string, string>> queryParams);
+        
+        bool IsValidDirectionForWorkflowGetModel(IEnumerable<KeyValuePair<string, string>> queryParams);
     }
 }
