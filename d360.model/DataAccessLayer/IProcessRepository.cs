51fb534bdc1457e6dd4381aa7c0531b771542eff
KunetskyiGlobal <96059828+KunetskyiGlobal@users.noreply.github.com>
2022-03-10 13:09:35 +0200

    Format IProcessRepository.cs

--- d360.model/DataAccessLayer/IProcessRepository.cs
+++ d360.model/DataAccessLayer/IProcessRepository.cs
@@ -1,18 +1,22 @@
-﻿using d360.core.entities;
-using d360.core.entities.Process;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+
+using d360.core.entities;
+using d360.core.entities.Process;
 
 namespace d360.model.DataAccessLayer
 {
     public interface IProcessRepository
     {
         ProcessDiagramModel GetAssetsProcessDiagram(Guid assetUid);
+        
         Task<IEnumerable<dynamic>> GetAvailableDiagramNodesForAsset(Guid assetUid);
+        
         List<ValidationError> UpdateProcessDiagram(ApiExecution execution, ProcessDiagramModel model, List<NodeData> toAdd, List<NodeData> toUpdate, List<NodeData> toDelete, long targetAssetId, bool isDiagramReplace, List<ProcessDiagramCopyRelationshipModel> copyRelationshipModel, List<ProcessDiagramCopyMapper> pdCopyMapper);
+        
         Task<byte[]> GetDiagramExcel(Asset asset, byte[] image);
+        
         IEnumerable<ProcessDiagramBadge> GetDiagramAssetBadges(Guid assetUid);
-
     }
 }
