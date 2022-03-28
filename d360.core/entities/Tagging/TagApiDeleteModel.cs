e09b2e9c180c529610b3342568f10f973b8f6a3f
Oleh Hochachko <oleh.hochachko@infogix.com>
2022-03-28 14:42:08 +0300

    GOV-17645: Moved Tag models to separate files

---
+++ d360.core/entities/Tagging/TagApiDeleteModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace d360.core.entities
+{
+    public class TagApiDeleteModel
+    {
+        [DataMember]
+        public Guid uid { get; set; }
+
+        [DataMember]
+        public bool cascade { get; set; }
+    }
+}
