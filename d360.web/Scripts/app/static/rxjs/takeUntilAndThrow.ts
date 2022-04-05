cec507cb0c65f072df0babc50fe46f6fb23e9742
Viktor Lova <viktor.lova@globallogic.com>
2022-04-05 12:35:17 +0300

    Revert "Revert "GOV-16437 Incorrect breadcrumbs when changing pages too fast""
    This reverts commit fd07ac5c36340493fbf44f2f5fd0c2199010cea7.

---
+++ d360.web/Scripts/app/static/rxjs/takeUntilAndThrow.ts
@@ -0,0 +1,25 @@
+import { MonoTypeOperatorFunction, Observable } from "rxjs";
+
+/**
+ * It's rxjs/takeUntil that doesn't wants to be notifier to be triggered
+ * If notifier is triggered, we throw an error
+ */
+export function takeUntilAndThrow<T>(notifier: Observable<any>, getError: () => any): MonoTypeOperatorFunction<T> {
+    return (source: Observable<T>) => {
+        return new Observable<T>((subscriber) => {
+            const sourceSub = source.subscribe(subscriber);
+            const notifierSub = notifier.subscribe(() => {
+                const error = getError();
+                subscriber.error(error);
+                sourceSub.unsubscribe();
+            });
+
+            return {
+                unsubscribe: () => {
+                    sourceSub.unsubscribe();
+                    notifierSub.unsubscribe();
+                }
+            };
+        });
+    };
+}
