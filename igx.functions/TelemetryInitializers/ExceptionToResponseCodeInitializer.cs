386d0e53f6a5dfda691bfe7f44750fdc9fdd57f8
KunetskyiGlobal <96059828+KunetskyiGlobal@users.noreply.github.com>
2022-04-12 13:32:25 +0300

    Setup ResponseCode to 500 when any Exception logged
    It will help to Azure Monitor to identify Errors.
    This change will affect all the functions inside the igx.functions project.

---
+++ igx.functions/TelemetryInitializers/ExceptionToResponseCodeInitializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace igx.functions.consumption.TelemetryInitializers
+{
+    internal class ExceptionToResponseCodeInitializer : ITelemetryInitializer
+    {
+        public void Initialize(ITelemetry telemetry)
+        {
+            if (telemetry is ExceptionTelemetry)
+            {
+                //This context is a kind of Singleton for the whole Request lifecycle
+                //It means that we may use it to share some data between ITelemetry instances 
+                telemetry.Context.GlobalProperties.Add("HasError", "true");
+            }
+
+            if (telemetry is RequestTelemetry)
+            {
+                if (telemetry.Context.GlobalProperties["HasError"].Equals("true", StringComparison.OrdinalIgnoreCase))
+                {
+                    ((RequestTelemetry)telemetry).ResponseCode = "500";
+                }
+            }
+        }
+    }
+}
