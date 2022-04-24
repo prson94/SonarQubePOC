02f124feef0104e32505d94330ae9350e2c09756
Mike Donato <mdonato@infogix.com>
2022-04-24 17:46:48 -0400

    GOV-17996 - fallback to field label when friendly name isn't available (for "special" fields)

--- d360.web/Scripts/app/components/admin/responsibilities/responsibility-rule.form.ts
+++ d360.web/Scripts/app/components/admin/responsibilities/responsibility-rule.form.ts
@@ -235,7 +235,7 @@ export class ResponsibilityRuleForm extends BaseComponent implements OnInit {
             let selectedFieldType = this.whenFieldTypes.find((f) => f.value === item.FieldTypeID);
             if (selectedFieldType) {
                 selectedFieldType = _.cloneDeep(selectedFieldType);
-                item.FieldTypeName = selectedFieldType.fieldTypeName;
+                item.FieldTypeName = selectedFieldType.fieldTypeName ?? selectedFieldType.label;
                 if (selectedFieldType.isLookup) {
                     let excluded = selectedFieldType.values.findIndex(a => a.label == "Choose...");
                     if (excluded < 0) {
@@ -484,7 +484,7 @@ export class ResponsibilityRuleForm extends BaseComponent implements OnInit {
         if (clearValue !== undefined && clearValue === true) item.Value = "";
         if (selectedFieldType) {
             item.IsBool = false;
-            item.FieldTypeName = selectedFieldType.fieldTypeName;
+            item.FieldTypeName = selectedFieldType.fieldTypeName ?? selectedFieldType.label;
             if (selectedFieldType.isLookup) {
                 let excluded = selectedFieldType.values.findIndex(a => a.label == "Choose...");
                 if (excluded < 0) {
