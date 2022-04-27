1936f54c8e7bb7d7eadcc04e8ac0804b7301e963
Viktor Lova <viktor.lova@globallogic.com>
2022-04-27 19:25:13 +0300

    GOV-17993 Perfomance fix for lookup field in responsibility rules
    Simply forced SQL server to seek over FieldLookupValue.Id, not scan over FieldLookupValue.Text

--- d360.model/CompanyContext_Api.cs
+++ d360.model/CompanyContext_Api.cs
@@ -11022,12 +11022,22 @@ new { beginItemNumber, endItemNumber, execution.ExecutionID, R = CurrentResource
 				inner join fieldtype ft on pd.fieldtypeid = ft.id
 				where pd.fieldtypeid is not null and ft.type = 'Boolean'
 
-				update #parsedData
-				set Value = flv.Value
-				from #parsedData pd
-				inner join fieldtype ft on pd.fieldtypeid = ft.id
-				left join FieldLookupValue FLV on FLV.FieldTypeID = ft.ID  and TRIM(pd.value) = FLV.Text
-				where pd.fieldtypeid is not null and ft.type = 'Lookup'
+		        update #parsedData
+                set 
+	                Value = lookupValue.Value
+                from #parsedData pd 
+	                inner join fieldtype ft on pd.fieldtypeid = ft.id
+	                left join (
+		                select distinct
+			                flv.FieldTypeID,
+			                flv.Text,
+                            flv.Value
+		                from #parsedData pd
+		                left join FieldLookupValue flv on flv.FieldTypeID = pd.FieldTypeId
+	                ) lookupValue
+		                on lookupValue.FieldTypeID = pd.FieldTypeId 
+		                and trim(pd.Value) = lookupValue.Text 
+                where pd.fieldtypeid is not null and ft.type = 'Lookup'
 
 				update #parsedData
 				set ErrorMessage = 'Invalid Field name.'
