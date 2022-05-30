16c3776603c4a3c563dd291ef002bbd626af9181
Oleh Hochachko <oleh.hochachko@infogix.com>
2022-05-30 12:03:27 +0300

    GOV-18305: add check conditions

---
+++ d360.web/Utilities/Condition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace d360.web.Utilities
+{
+	public static class Condition
+	{
+		public static void Require<T>(T value, Func<Exception> exceptionFactory)
+			where T : class
+		{
+			if (value == null)
+			{
+				throw exceptionFactory();
+			}
+		}
+	}
+}
