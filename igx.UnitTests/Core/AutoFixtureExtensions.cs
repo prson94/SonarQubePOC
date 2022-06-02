f6b9e403efb3c491c7925d9ef2bc3cf94342dba2
Oleh Hochachko <oleh.hochachko@infogix.com>
2022-06-02 17:16:45 +0300

    GOV-18307: AutoFixture extended

---
+++ igx.UnitTests/Core/AutoFixtureExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using AutoFixture;
+
+namespace igx.UnitTests
+{
+	public static class AutoFixtureExtensions
+	{
+		public static IEnumerable<T> CreateEnumerable<T>(this IFixture fixture, int count = 3)
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				yield return fixture.Create<T>();
+			}
+		}
+	}
+}
