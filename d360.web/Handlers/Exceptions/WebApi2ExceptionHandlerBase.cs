2b13aca40d7657763b79c99db930e18f7b275bf9
Oleh Hochachko <oleh.hochachko@infogix.com>
2022-06-20 12:24:07 +0300

    GOV-18587: add specific exception handlers

---
+++ d360.web/Handlers/Exceptions/WebApi2ExceptionHandlerBase.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.ExceptionHandling;
+using System.Web.Http.Results;
+using d360.web.Utilities;
+using Newtonsoft.Json;
+
+namespace d360.web.Handlers.Exceptions
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public abstract class WebApi2ExceptionHandlerBase : IWebApi2ExceptionHandler
+	{
+		protected WebApi2ExceptionHandlerBase(IRuntimeInfo runtimeInfo)
+		{
+			RuntimeInfo = runtimeInfo;
+		}
+
+		protected IRuntimeInfo RuntimeInfo { get; }
+		public virtual bool IsDefault => false;
+		public abstract bool CanHandle(Exception exception);
+
+		public virtual Task HandleAsync(ExceptionHandlerContext context, CancellationToken cancellationToken)
+		{
+			var exception = context.Exception;
+			var problem = new ProblemDetailsResponse();
+			problem.Type = exception.GetType().FullName;
+			problem.Title = "Error";
+			problem.Method = context.Request.Method.ToString();
+			problem.Instance = context.Request.RequestUri.ToString();
+			problem.Status = 500;
+			var messages = GetExceptionMessageCollection(exception);
+			problem.Detail = messages.FirstOrDefault() ?? string.Empty;
+			if (RuntimeInfo.IsReleaseBuild == false || RuntimeInfo.IsDebuggerAttached)
+			{
+				problem.Extra.Add("messages", messages);
+				problem.Extra.Add("stack_trace", exception.StackTrace?.Split(new[]
+				{
+					"\r\n"
+				}, StringSplitOptions.RemoveEmptyEntries) ?? Array.Empty<string>());
+			}
+
+			ComposeErrorResponse(context, problem);
+			var json = JsonConvert.SerializeObject(problem, Formatting.Indented);
+			var response = context.Request.CreateResponse();
+			response.StatusCode = (HttpStatusCode)problem.Status;
+			response.Content = new StringContent(json, Encoding.UTF8, "application/problem+json");
+			context.Result = new ResponseMessageResult(response);
+			return Task.CompletedTask;
+		}
+
+		private static ICollection<string> GetExceptionMessageCollection(Exception exception)
+		{
+			var result = new List<string>();
+			var ex = exception;
+			do
+			{
+				if (ex is TargetInvocationException)
+				{
+					continue;
+				}
+
+				result.Add(ex.Message);
+				ex = ex.InnerException;
+			} while (ex != null);
+
+			return result;
+		}
+
+		protected abstract void ComposeErrorResponse(ExceptionHandlerContext context, ProblemDetailsResponse problemDetails);
+	}
+}
---
+++ d360.web/Handlers/Exceptions/WebApi2ExceptionHandlerMediator.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.ExceptionHandling;
+using System.Web.Http.Results;
+
+namespace d360.web.Handlers.Exceptions
+{
+	public class WebApi2ExceptionHandlerMediator
+	{
+		public WebApi2ExceptionHandlerMediator(ICollection<IWebApi2ExceptionHandler> exceptionHandlers)
+		{
+			ExceptionHandlers = exceptionHandlers;
+		}
+
+		private ICollection<IWebApi2ExceptionHandler> ExceptionHandlers { get; }
+
+		public async Task HandleAsync(ExceptionHandlerContext context, CancellationToken cancellationToken)
+		{
+			var defaultExceptionHandler = ExceptionHandlers.FirstOrDefault(x => x.IsDefault);
+			if (defaultExceptionHandler == null)
+			{
+				var response = context.Request.CreateResponse();
+				response.StatusCode = HttpStatusCode.InternalServerError;
+				response.Content = new StringContent("Exception handler configuration error", Encoding.UTF8);
+				context.Result = new ResponseMessageResult(response);
+				return;
+			}
+
+			// ReSharper disable once ReplaceWithSingleCallToFirstOrDefault
+			var exceptionHandler = ExceptionHandlers.Where(x => x.IsDefault == false && x.CanHandle(context.Exception)).FirstOrDefault() ?? defaultExceptionHandler;
+
+			await exceptionHandler.HandleAsync(context, cancellationToken);
+		}
+	}
+}
