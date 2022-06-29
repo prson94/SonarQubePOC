c3ed27b8f499f118332fb2eae774640850887007
Mike Donato <mdonato@infogix.com>
2022-06-29 00:11:19 -0400

    GOV-18607 - remove references to graph tables from workflow

--- d360.model/CompanyContext_Workflow.cs
+++ d360.model/CompanyContext_Workflow.cs
@@ -1332,8 +1332,6 @@ namespace d360.model
 						rels = new string[] { rels.First() };
 					}
 
-					List<DatabaseBulkRelationshipResult> graphEvents = new List<DatabaseBulkRelationshipResult>();
-
 					foreach (string rel in rels)
 					{
 						// split by | for type id
@@ -1374,20 +1372,8 @@ namespace d360.model
 								Intersects.Add(intersect);
 
 								SaveChanges();
-
-								graphEvents.Add(new DatabaseBulkRelationshipResult
-								{
-									Object = "Intersect",
-									uid = intersect.uid,
-									Success = true
-								});
 							}
 						}
-					}
-
-					if (graphEvents.Any())
-					{
-						SendAssetGraphEvents(graphEvents);
 					}
 				}
 			}
@@ -1396,7 +1382,6 @@ namespace d360.model
 		private void DeleteIntersects(SystemObjects @object, int objectID, int intersectTypeId, bool isSubject)
 		{
 			string sql;
-			List<DatabaseBulkRelationshipResult> graphEvents;
 
 			if (isSubject)
 			{
@@ -1405,19 +1390,6 @@ namespace d360.model
 			else
 			{
 				sql = "delete from [intersect] output deleted.uid into #deletedIntersects where [object] = @obj and objectid = @objectid and intersecttypeid = @intersectTypeId";
-			}
-
-			graphEvents = Database.Connection.Query<DatabaseBulkRelationshipResult>($@"
-				drop table if exists #deletedIntersects;
-				create table #deletedIntersects (uid uniqueidentifier);
-				{sql}
-				select uid, 'Intersect' as [Object], cast(1 as bit) as Success from #deletedIntersects"
-				, new { obj = @object.ToString(), objectid = objectID, intersectTypeId })
-				.ToList();
-
-			if (graphEvents.Any())
-			{
-				SendAssetGraphEvents(graphEvents);
 			}
 		}
 
@@ -3364,7 +3336,7 @@ namespace d360.model
 					item = GetObjectDetail(obj.ToString(), objectID);
 				}
 
-				string path = item?.UID == null ? null : Query<string>(@"select graph.GetPath(AN.Segments, ' > ', ' / ') from graph.assetNode AN where AN.Uid = @Uid", new { Uid = item.UID }).FirstOrDefault();
+				string path = item?.UID == null ? null : Query<string>(@"select dbo.GetDisplayPath(P.Segments, ' > ', ' / ') from AssetPath P inner join Asset A on A.ID = P.ID where A.Uid = @Uid", new { Uid = item.UID }).FirstOrDefault();
 
 				result = result.Replace("[ASSET_PATH]", path ?? "(unknown)");
 			}
