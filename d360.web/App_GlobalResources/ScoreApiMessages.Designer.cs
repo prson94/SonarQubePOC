db2a1f7b8855f2474a339d461f43e3228872abd9
tduric@infogix.com <tduric@infogix.com>
2022-07-13 14:41:49 +0200

    - fix post internal results validation and messaging

--- d360.web/App_GlobalResources/ScoreApiMessages.Designer.cs
+++ d360.web/App_GlobalResources/ScoreApiMessages.Designer.cs
@@ -19,7 +19,7 @@ namespace Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class ScoreApiMessages {
@@ -219,6 +219,15 @@ namespace Resources {
         public static string ScoreNotExternalCalculation {
             get {
                 return ResourceManager.GetString("ScoreNotExternalCalculation", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Score definition with {0} is not internally calculated..
+        /// </summary>
+        public static string ScoreNotInternalCalculation {
+            get {
+                return ResourceManager.GetString("ScoreNotInternalCalculation", resourceCulture);
             }
         }
         
