c6398cec7aac1c054c08749f32a75cf94a1a38c9
Artur Ladchenko <artur.ladchenko@globallogic.com>
2022-07-19 14:29:57 +0200

    GOV-18492 Made clear tooltip directive rely on DOM

--- d360.web/Scripts/app/directives/clear-tooltip.directive.ts
+++ d360.web/Scripts/app/directives/clear-tooltip.directive.ts
@@ -1,43 +1,52 @@
 ﻿import { Directive, ElementRef, OnDestroy, OnInit } from "@angular/core";
-import { Dropdown } from "primeng/dropdown";
-import { Subscription } from "rxjs";
 
 @Directive({
 	selector: "[clearTooltip]"
 })
 export class ClearTooltipDirective implements OnInit, OnDestroy {
-	dropdownChange: Subscription;
+	observer: MutationObserver;
 	clear = null;
 	tooltip = null;
 	
-	constructor(private el: ElementRef, private dropdown: Dropdown) {
-	}
+	constructor(private el: ElementRef) {}
 
 	ngOnInit(): void {
-		this.dropdownChange = this.dropdown.onChange.subscribe(() => {
-			if (this.dropdown.value != null && this.dropdown.showClear && !this.dropdown.disabled) {
-				setTimeout(() => {
-					this.clear?.removeChild(this.tooltip);
-					this.clear = this.el.nativeElement.querySelector('.p-dropdown-clear-icon');
-					this.tooltip = document.createElement('div');
-					this.tooltip.classList.add('p-tooltip-dynamic');
-					this.tooltip.append($localize`Clear`);
-					this.clear.append(this.tooltip);
-					this.tooltip.style.left = `-${(this.tooltip.offsetWidth - this.clear.offsetWidth) / 2}px`;
+		this.observer = this.createMutationObserver((mutation) => {
+			if (
+				Array.from(mutation.addedNodes).some((element) => {
+					return (<Element>element).classList.contains('p-dropdown-clear-icon');
+				})
+			) {
+				this.clear?.removeChild(this.tooltip);
+				this.clear = this.el.nativeElement.querySelector('.p-dropdown-clear-icon');
+				this.tooltip = document.createElement('div');
+				this.tooltip.classList.add('p-tooltip-dynamic');
+				this.tooltip.append($localize`Clear`);
+				this.clear.append(this.tooltip);
+				this.tooltip.style.left = `-${(this.tooltip.offsetWidth - this.clear.offsetWidth) / 2}px`;
+				this.tooltip.style.display = 'none';
+				this.clear.addEventListener('mouseover', () => {
+					this.tooltip.style.display = 'block';
+				});
+				this.clear.addEventListener('mouseout', () => {
 					this.tooltip.style.display = 'none';
-					this.clear.addEventListener('mouseover', () => {
-						this.tooltip.style.display = 'block';
-					});
-					this.clear.addEventListener('mouseout', () => {
-						this.tooltip.style.display = 'none';
-					});
-				}, 10);
+				});
 			}
+		});
+
+		this.observer.observe(this.el.nativeElement.querySelector('div.p-dropdown'), {childList: true});
+	}
+
+	createMutationObserver(callback: (mutation: MutationRecord) => void): MutationObserver {
+		return new MutationObserver((mutations) => {
+			mutations.forEach((mutation) => {
+				callback(mutation);
+			});
 		});
 	}
 
 	ngOnDestroy(): void {
-		this.dropdownChange?.unsubscribe();
+		this.observer.disconnect();
 	}
 
 }
