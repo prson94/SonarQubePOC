3dafb388108ff9c1b4a7fb3e25c40a4ad4131b3f
mikejpappas <mpappas@infogix.com>
2022-09-15 15:40:47 -0400

    GOV-19679 - When editing a child model I am presented with an error about an invalid TextPath column

--- d360.web/Controllers/V2/FieldsController.cs
+++ d360.web/Controllers/V2/FieldsController.cs
@@ -2209,9 +2209,8 @@ namespace d360.web.Controllers.V2
 				if (fieldType == null && fieldName.ToLowerInvariant() == "parentuid")
 				{
 					string sql = "";
-					bool isHierarchyGrid = atype.Object == "TaxonomyType" || atype.Object == "PolicyType";
 
-					if (!isHierarchyGrid)
+					if (!atype.Hierarchical)
 					{
 						if (!string.IsNullOrEmpty(filter))
 						{
@@ -2250,7 +2249,7 @@ namespace d360.web.Controllers.V2
 						if (!string.IsNullOrEmpty(filter))
 						{
 							filter = "%" + filter + "%";
-							whereQuery += " and P.TextPath like @filter ";
+							whereQuery += " and P.DisplayPath like @filter ";
 						}
 
 						var hierarchyItem = Company.Query<dynamic>($@"
@@ -2268,7 +2267,7 @@ namespace d360.web.Controllers.V2
 										inner join AssetPath P on P.ID = A.ID
 										cross apply dbo.GetAssetLevelById(A.ID) LV
 								where coalesce(LV.[Level], 1) <= '{hierarchyItem?.Level ?? 1}' {whereQuery}
-								order by P.TextPath 
+								order by P.DisplayPath 
 								{pagingQuery}
 								option (maxrecursion 100)
 
@@ -2290,7 +2289,7 @@ namespace d360.web.Controllers.V2
 					var resultsAssets = await Company.Connection.QueryMultipleAsync(cmd);
 					var items = resultsAssets.Read<DDLSelectItem>().ToList();
 
-					if (isHierarchyGrid && (skip == null || skip == 0))
+					if (atype.Hierarchical && (skip == null || skip == 0))
 					{
 						items = items.Prepend(new DDLSelectItem { text = "- Root -", value = Guid.Empty.ToString() }).ToList();
 					}
