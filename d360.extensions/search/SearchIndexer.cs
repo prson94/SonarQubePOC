3f3599d8c994d9142c285f9477c23806fc8e2a58
Thomas Klok <thomas.klok@precisely.com>
2022-09-29 16:30:31 -0400

    GOV-19809 Global search indexing connectivity issue

--- d360.extensions/search/SearchIndexer.cs
+++ d360.extensions/search/SearchIndexer.cs
@@ -1443,7 +1443,7 @@ namespace d360.extensions.search
         /// No need to get any records with a lower AssetID's
         /// </summary>
         /// <param name="AssetID"></param>
-        private void FetchDataPage(long AssetID)
+        protected virtual void FetchDataPage(long AssetID)
         {
             if (LastPage)
             {
@@ -1520,51 +1520,93 @@ namespace d360.extensions.search
     internal class TempTablePagedQuery<T> : BasePagedQuery<T> where T : IPagedQuerySqlModel
     {
         private readonly string _tableIdentifier;
+		private readonly string _alias = "pagedquery";
+		private readonly string _initialQuery;
+		private readonly int retryLimit = 3;
 
-        /// <summary>
-        /// Performs a paged/chunked query using a global temporary table to hold the result of the query
-        /// and then paging from that
-        /// </summary>
-        /// <param name="connection"></param>
-        /// <param name="query">Query string</param>
-        /// <param name="param"></param>
-        public TempTablePagedQuery(SqlConnection connection, string query, DynamicParameters param = null) : base(connection, param)
+		/// <summary>
+		/// Performs a paged/chunked query using a global temporary table to hold the result of the query
+		/// and then paging from that
+		/// </summary>
+		/// <param name="connection"></param>
+		/// <param name="query">Query string</param>
+		/// <param name="param"></param>
+		public TempTablePagedQuery(SqlConnection connection, string query, DynamicParameters param = null) : base(connection, param)
         {
             PageSize = 150000;
             //generate random name for global temp table
             _tableIdentifier = "pagedQuery_" + Guid.NewGuid().ToString().Replace("-", "_");
+			_initialQuery = query;
 
-            //Use <T> to specify columns to select, as SqlMapper can slow down a lot over *
-            string alias = "pagedquery";
-            string queryColumns = string.Join(", ", typeof(T).GetProperties().Select(p => $"{alias}.{p.Name}").ToArray());
-            _query = $"SELECT TOP (@PageSize) {queryColumns} FROM ##{_tableIdentifier} {alias} WHERE {alias}.AssetID >= @PagerAssetID ORDER BY {alias}.sortid";
+			CreateTempTable();
+        }
 
-            try
-            {
-                _connection.Execute($@"
+		private void CreateTempTable()
+		{
+			//Use <T> to specify columns to select, as SqlMapper can slow down a lot over *
+			string queryColumns = string.Join(", ", typeof(T).GetProperties().Select(p => $"{_alias}.{p.Name}").ToArray());
+			_query = $"SELECT TOP (@PageSize) {queryColumns} FROM ##{_tableIdentifier} {_alias} WHERE {_alias}.AssetID >= @PagerAssetID ORDER BY {_alias}.sortid";
+			var where = (_param.ParameterNames.Contains("PagerAssetID")) ? $"WHERE {_alias}.AssetID = @PagerAssetID" : "";
+
+			try
+			{
+				var createstatement = $@"
                     DROP TABLE IF EXISTS ##{_tableIdentifier};
 
                     SELECT ROW_NUMBER() OVER (ORDER BY AssetID) AS sortid, {queryColumns}
                     INTO ##{_tableIdentifier}
-                    FROM ({query}) {alias};
+                    FROM ({_initialQuery}) {_alias} {where};
 
                     CREATE UNIQUE INDEX UIX_{_tableIdentifier} ON ##{_tableIdentifier} (sortid); 
 
                     CREATE NONCLUSTERED INDEX IX_{_tableIdentifier}_AssetID ON ##{_tableIdentifier} (AssetID); 
-                ", _param, null, _defaultQueryCommandTimeout * 20); //Multiply timeout for statement creating the temp table
-            }
-            catch (Exception e)
-            {
-                throw new PagedQueryException($"TempTablePagedQuery failed to create temp table. Error: {e.Message}");
-            }
-        }
+                ";
+				_connection.Execute(createstatement, _param, null, _defaultQueryCommandTimeout * 20); //Multiply timeout for statement creating the temp table
+			}
+			catch (Exception e)
+			{
+				throw new PagedQueryException($"TempTablePagedQuery failed to create temp table. Error: {e.Message}");
+			}
+		}
 
-        ~TempTablePagedQuery()
+		~TempTablePagedQuery()
         {
             OnLastPage();
         }
 
-        protected override void OnLastPage()
+		protected override void FetchDataPage(long AssetID)
+		{
+			var currentRetry = 0;
+			for(; ; )
+			{
+				try
+				{
+					base.FetchDataPage(AssetID);
+					break;
+				}
+				catch (Exception ex)
+				{
+					currentRetry++;
+					if (currentRetry > retryLimit || !IsTransient(ex))
+					{
+						throw;
+					}
+					//re-create Temp Table
+					CreateTempTable();
+				}
+			}
+		}
+
+		private bool IsTransient(Exception ex)
+		{
+			if(ex.Message.Contains($"Error: Invalid object name '##{_tableIdentifier}'"))
+			{
+				return true;
+			}
+			return false;
+		}
+
+		protected override void OnLastPage()
         {
             base.OnLastPage();
             try
