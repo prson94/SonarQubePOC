e970fc60e6c1fb0cc0392deb910ae96f40c2f0cd
Artur Ladchenko <artur.ladchenko@globallogic.com>
2022-10-04 18:18:52 +0200

    GOV-19858 Fixed breadcrumb incorrect link to rule types

--- d360.web/Scripts/app/components/shared/header/header-breadcrumb-item.component.ts
+++ d360.web/Scripts/app/components/shared/header/header-breadcrumb-item.component.ts
@@ -1,15 +1,16 @@
-import {debounceTime} from 'rxjs/operators';
+import {catchError, debounceTime} from 'rxjs/operators';
 import { Component, Input, ElementRef, OnChanges, SimpleChange, Output, EventEmitter, OnInit,OnDestroy, ChangeDetectionStrategy, ChangeDetectorRef, ViewChild } from '@angular/core';
 import { Router }       from '@angular/router';
 import { Breadcrumb } from '../../../models/breadcrumb.model';
 import { TypeaheadSearchService } from '../../../services/typeahead-search.service';
 import { SearchResult } from '../../../models/search-result.model';
 import { TreeNode } from 'primeng/api';
-import { SubscriptionLike as ISubscription } from 'rxjs';
+import {EMPTY, SubscriptionLike as ISubscription} from 'rxjs';
+import { ObjectDetailService } from "../../../services/object-detail.service";
 
 @Component({
     selector: 'd3s-header-breadcrumb-item',
-    providers: [TypeaheadSearchService],    
+    providers: [TypeaheadSearchService, ObjectDetailService],    
     host: {
         '(window:resize)': 'setMaxHeight()'
     },  
@@ -74,8 +75,11 @@ export class HeaderBreadcrumbItemComponent implements OnChanges, OnInit, OnDestr
     private searchSub: ISubscription;
     searchingTypeahed: boolean = false;
     
-    constructor(private elementRef: ElementRef, private router: Router,
-        private typeaheadSearchService: TypeaheadSearchService, private ref: ChangeDetectorRef) { }
+    constructor(private elementRef: ElementRef,
+				private router: Router, 
+				private typeaheadSearchService: TypeaheadSearchService,
+				private ref: ChangeDetectorRef,
+				private objectDetailService: ObjectDetailService) { }
 
     ngOnChanges(changes: { [propName: string]: SimpleChange }) {
         if (this.breadcrumb)
@@ -218,13 +222,11 @@ export class HeaderBreadcrumbItemComponent implements OnChanges, OnInit, OnDestr
     }
 
     navigateToLink(url: string, res?: any) {
-        if (url && url.length > 0) 
-            {this.router.navigateByUrl(url);}
+        if (url && url.length > 0) { this.router.navigateByUrl(url); }
     }
 
     hasLink(url: string) {
-        if (url && url.length > 0 && !this.isLastItem) {return true;}
-        else {false;}
+        return url && url.length > 0 && !this.isLastItem;
     }
 
     hasClass(element, className) {
