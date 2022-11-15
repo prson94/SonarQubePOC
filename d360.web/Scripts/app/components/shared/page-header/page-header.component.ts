385dd0b13250f9357d81501855adfcba7393065f
Viktor Lova <viktor.lova@globallogic.com>
2022-11-15 20:29:06 +0200

    GOV-19714 Prevent showing our original header with incorrect data on pages which have normalized header

--- d360.web/Scripts/app/components/shared/page-header/page-header.component.ts
+++ d360.web/Scripts/app/components/shared/page-header/page-header.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input } from "@angular/core";
 import { Title } from "@angular/platform-browser";
+import { Subscription } from "rxjs";
+import { filter } from "rxjs/operators";
 import { CompanySettingEnum } from "../../../models/settings.model";
+import { SecondaryNavService } from "../../../services/right-sidebar.service";
 import { CompanySettingsService } from "../../../services/settings.service";
 
 
@@ -13,9 +16,28 @@ export class PageHeaderComponent {
     @Input() icon: string;
     @Input() header: string;
 
-    constructor(private titleService: Title, private settingsService: CompanySettingsService) { }
+    constructor(
+        private titleService: Title,
+        private settingsService: CompanySettingsService,
+        private secondaryNavService: SecondaryNavService) { }
 
     ngOnChanges() {
         this.titleService.setTitle(`${this.settingsService.getSettingById(CompanySettingEnum.BrowserTitlePrefix).StringSetting.Value} - ${this.header}`);
+    }
+
+    secondaryNavSubscription: Subscription;
+    ngOnInit() {
+        this.secondaryNavService.showHeader(false);
+
+        // Next lines fix concurrent issue when breadcrumbs are set by other page after page destroyed
+        this.secondaryNavSubscription = this.secondaryNavService.hideHeader$
+            .pipe(filter(x => x == true))
+            .subscribe(() => {
+                this.secondaryNavService.showHeader(false);
+            });
+    }
+
+    ngOnDestroy() {
+        this.secondaryNavSubscription.unsubscribe();
     }
 }
