2888f44728385c5243abc36c1442c55a3bd09895
pungupta <48975791+pungupta@users.noreply.github.com>
2022-11-24 16:26:49 +0530

    Update d360.web/Scripts/app/components/sidebar/governance-roles/governance-roles-sidebar.component.ts
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- d360.web/Scripts/app/components/sidebar/governance-roles/governance-roles-sidebar.component.ts
+++ d360.web/Scripts/app/components/sidebar/governance-roles/governance-roles-sidebar.component.ts
@@ -67,7 +67,7 @@ export class GovernanceRolesComponent extends BaseComponent implements OnInit, O
                     this.refListDDL.push({ value: x.uid, label: x.Name });
                 });
 
-                let setting = this.settingsService.getSettingById(CompanySettingEnum.GovernanceRoleReferenceListUid);
+       const setting = this.settingsService.getSettingById(CompanySettingEnum.GovernanceRoleReferenceListUid);;
                 this.originalModel = new GovernanceRole();
                 if (setting.ScalarValue && setting.ScalarValue !== "00000000-0000-0000-0000-000000000000") {
                     this.originalModel.RefListUid = setting.ScalarValue;
