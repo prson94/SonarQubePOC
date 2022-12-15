92f7dd27a3779b176754a12fd301caeb895a32cd
Viktor Lova <viktor.lova@globallogic.com>
2022-12-15 18:36:20 +0200

    GOV-20128 Fix a bug that when you switch pages, invalid header appers
    This bug was introduced in PR https://github.com/Infogix/govern/pull/9111 by commit https://github.com/Infogix/govern/pull/9111/commits/a0741079acd4d25b88a0bff9731426be695a3550 when I tried to fix layout issues.
    
    Thing is that component d3s-page-header tells to SecondaryNavService to not use old header mechanism.
    But I wrapped rendering d3s-page-header under portals, so it wasn't rendered → it can't tell to SecondaryNavService to use new header mechanism.
    
    The fix is simple: don't render d3s-page-header under portal, render content of d3s-page-header under portal

--- d360.web/Scripts/app/components/shared/page-header/page-header.module.ts
+++ d360.web/Scripts/app/components/shared/page-header/page-header.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from "@angular/common";
 import { PageHeaderComponent } from './page-header.component';
+import { PortalsModule } from '../portals/portals.module';
 
 @NgModule({
     imports: [
-        CommonModule
+        CommonModule,
+        PortalsModule
     ],
     declarations: [
         PageHeaderComponent
