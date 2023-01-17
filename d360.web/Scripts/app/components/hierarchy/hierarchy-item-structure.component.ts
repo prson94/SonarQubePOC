840dfc95b08821d83acc5d7a8c3d44873167842e
tduric <49392859+tduric@users.noreply.github.com>
2023-01-17 13:08:09 +0100

    Revert "GOV-20725 : fix scrolling issue on tree views"

--- d360.web/Scripts/app/components/hierarchy/hierarchy-item-structure.component.ts
+++ d360.web/Scripts/app/components/hierarchy/hierarchy-item-structure.component.ts
@@ -920,25 +920,4 @@ export class HierarchyItemStructureComponent extends BaseComponent implements On
 			this.secondarySidePanel = "status";
 		}
 	}
-
-	//https://jira.syncsort.com/browse/GOV-20725
-	treeTogglerClicked() {
-		const element = document.getElementsByClassName("tree-scroll-workaround");
-		if (element.length > 0) {
-			const wrapper = element[0].parentElement;
-			const currentScrollTopLocation = wrapper.scrollTop;
-			wrapper.onscroll = () => {
-				//set next onscroll event to preserve current scroll location to avoid jumping at the top of tree grid
-				try {
-					wrapper.scrollTop = currentScrollTopLocation;
-				}
-				finally {
-					//immediately reset onscroll event to work as before
-					wrapper.onscroll = () => {
-						return true;
-					};
-				}
-			};
-		}
-	}
 }
