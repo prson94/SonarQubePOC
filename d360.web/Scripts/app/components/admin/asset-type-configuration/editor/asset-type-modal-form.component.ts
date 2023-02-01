dc2abc0bf65f48cbcafea3e006bdb56e179d474b
tduric@infogix.com <tduric@infogix.com>
2023-02-01 10:05:07 +0100

    GOV-20889 : Fix various issues in the Config pages

--- d360.web/Scripts/app/components/admin/asset-type-configuration/editor/asset-type-modal-form.component.ts
+++ d360.web/Scripts/app/components/admin/asset-type-configuration/editor/asset-type-modal-form.component.ts
@@ -51,6 +51,8 @@ export class ConfigurationAssetTypeModalForm implements OnChanges, OnInit, After
 	gatewayTooltip = $localize`A gateway is represented by the diamond shape and is used to control the divergence and convergence of connections. It will determine traditional decisions, as well as the forking, merging, and joining of paths.`;
 	activityTooltip = $localize`An activity is represented by a rounded-corner rectangle and is a generic term for work that the company performs. The types of activities are Task and Sub-Process.`;
 
+	private isEditFormUpdated: boolean = false;
+
 	constructor(private fb: FormBuilder,
 		private assetService: AssetService,
 		private assetTypeService: AssetTypeService,
@@ -113,20 +115,20 @@ export class ConfigurationAssetTypeModalForm implements OnChanges, OnInit, After
 	}
 	setForm() {
 		this.assetTypeForm = this.fb.group({
-			name: [null, { validators: [Validators.required], updateOn: "blur" }],
-			displayFormat: [null, { validators: [Validators.required], updateOn: "blur" }],
-			description: [null, { updateOn: "blur" }],
-			isDescriptionEnabled: [false, { updateOn: "blur" }],
-			descriptionButtonName: [null, { updateOn: "blur" }],
-			isDescriptionVisibleByDefault: [false, { updateOn: "blur" }],
-			backgroundColor: [null, { updateOn: "blur" }],
-			icon: [null, { updateOn: "blur" }],
-			useAsTransformation: [null, { updateOn: "blur" }],
-			predicateUid: [null, { updateOn: "blur" }],
-			autoDisplayParent: [null, { updateOn: "blur" }],
-			canEditParent: [null, { updateOn: "blur" }],
-			flowObjectType: [null, { updateOn: "blur" }],
-			maxDepth: [null, { updateOn: "blur" }]
+			name: [null, { validators: [Validators.required] }],
+			displayFormat: [null, { validators: [Validators.required] }],
+			description: [null],
+			isDescriptionEnabled: [false],
+			descriptionButtonName: [null],
+			isDescriptionVisibleByDefault: [false],
+			backgroundColor: [null],
+			icon: [null],
+			useAsTransformation: [null],
+			predicateUid: [null],
+			autoDisplayParent: [null],
+			canEditParent: [null],
+			flowObjectType: [null],
+			maxDepth: [null]
 		});
 
 		this.setDefaultFormValues();
@@ -212,7 +214,16 @@ export class ConfigurationAssetTypeModalForm implements OnChanges, OnInit, After
 					this.assetTypeForm.controls["flowObjectType"].setValue(assetType.FlowObjectType);
 				}
 
+				this.isEditFormUpdated = false;
+				setTimeout(() => {
+					this.assetTypeForm.valueChanges.subscribe((change) => {
+						this.isEditFormUpdated = true;
+					});
+				},200)
+
 				this.isLoading = false;
+
+				
 			});
 		}
 		else {
@@ -339,11 +350,15 @@ export class ConfigurationAssetTypeModalForm implements OnChanges, OnInit, After
 	}
 
 	get isFormDisabled(): boolean {
-		return this.savingInProgress || this.assetTypeForm.invalid;
+		return this.savingInProgress || this.assetTypeForm.invalid || (this.uid && !this.isEditFormUpdated);
 	}
 
 	get saveButtonLabel(): string {
 		return this.uid ? $localize`Save Changes` : $localize`Add Asset Type`;
+	}
+
+	get closeButtonLabel(): string {
+		return this.uid ? $localize`Discard Changes` : $localize`Cancel`;
 	}
 
 	@HostListener('window:resize', ['$event'])
