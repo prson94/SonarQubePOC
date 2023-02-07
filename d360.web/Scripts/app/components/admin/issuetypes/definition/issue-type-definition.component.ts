c679c36605b25b2793cef42793807f506ead0e3b
tduric@infogix.com <tduric@infogix.com>
2023-02-07 13:35:37 +0100

    GOV-20667 : fix codacy

--- d360.web/Scripts/app/components/admin/issuetypes/definition/issue-type-definition.component.ts
+++ d360.web/Scripts/app/components/admin/issuetypes/definition/issue-type-definition.component.ts
@@ -26,7 +26,6 @@ export class IssueTypeDefinitionComponent {
 	open(newTab: boolean = false) {
 		const url = `/admin/configuration/WorkflowActions/${this.workflowIssueType.Uid}/fields`;
 		if (newTab) {
-			// eslint-disable-next-line detect-non-literal-fs-filename
 			window.open(url, "_blank");
 		}
 		else {
