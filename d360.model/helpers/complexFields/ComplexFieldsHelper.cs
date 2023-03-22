b687dbee2f5e26d736745825f59f477c9886f896
Puneet Gupta <puneet.gupta@Precisely.com>
2023-03-22 09:14:54 +0530

    GOV-21175 Relationship Lookup field shown when  = FALSE

--- d360.model/helpers/complexFields/ComplexFieldsHelper.cs
+++ d360.model/helpers/complexFields/ComplexFieldsHelper.cs
@@ -73,7 +73,7 @@ namespace d360.model.helpers
 
 					if (definition.Relations.IndexOf(rel) == 0)
 					{
-						if (IsBothSideSame)
+						if (IsBothSideSame && rel.Direction == FieldTypeComplexLookupRelationDirection.Both)
 						{
 							joins.Add($@"cross apply (select Ia{idx}.SubjectAssetId 
 												  from [Intersect] Ia{idx} 
@@ -101,7 +101,7 @@ namespace d360.model.helpers
 					}
 					else
 					{
-						if (IsBothSideSame)
+						if (IsBothSideSame && rel.Direction == FieldTypeComplexLookupRelationDirection.Both)
 						{
 							joins.Add($@"cross apply (select Ia{idx}.id, Ia{idx}.ObjectAssetId 
 												  from [Intersect] Ia{idx} 
