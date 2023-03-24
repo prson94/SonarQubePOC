1988c6fa62387697f0f1d5ff37bc2a69793a60f7
tduric@infogix.com <tduric@infogix.com>
2023-03-24 14:56:23 +0100

    GOV-21238 : fix routing for audit and comments

--- d360.web/Scripts/app/components/sidebar/comments/comments.routes.ts
+++ d360.web/Scripts/app/components/sidebar/comments/comments.routes.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CommentsComponent } from './comments.component';
 
 const routes: Routes = [
-	{ path: ':assetUid/comments', component: CommentsComponent }
+	{ path: '', component: CommentsComponent }
 ];
 
 @NgModule({
