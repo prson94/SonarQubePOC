6f098552af7b195c5c6b3735930d38b52fa8b142
tduric@infogix.com <tduric@infogix.com>
2023-04-13 16:50:41 +0200

    GOV-21359 : fix routing for relationship type field defintion

--- d360.web/Scripts/app/app.routes.ts
+++ d360.web/Scripts/app/app.routes.ts
@@ -55,7 +55,7 @@ const routes: Routes = [
 	{ path: "semantics/:uid/log", loadChildren: () => import("./components/sidebar/audit/audit.module").then((m) => m.AuditModule) },
 	{ path: "semantics", loadChildren: () => import("./components/semantic/semantics.module").then((m) => m.SemanticsModule) },
 	{ path: "tag", loadChildren: () => import("./components/sidebar/audit/audit.module").then((m) => m.AuditModule) },
-	{ path: "admin/relationships", data: { type: 'relationship' }, loadChildren: () => import("./components/sidebar/fields/fields.module").then((m) => m.FieldsModule) },
+	{ path: "admin/relationships/:assetTypeUid/fields", data: { type: 'relationship' }, loadChildren: () => import("./components/sidebar/fields/fields.module").then((m) => m.FieldsModule) },
 	{ path: "dataCatalog", loadChildren: () => import("./components/data-catalog/data-catalog.module").then((m) => m.DataCatalogModule) },
 
 	{
