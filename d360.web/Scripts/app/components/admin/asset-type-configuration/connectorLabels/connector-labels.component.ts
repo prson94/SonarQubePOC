682fc1c7f3a69c2ea7323a9167c10fc5e62e276f
tduric@infogix.com <tduric@infogix.com>
2023-04-20 15:23:04 +0200

    GOV-20755 : fix context menu not appearing when clicking on type name

--- d360.web/Scripts/app/components/admin/asset-type-configuration/connectorLabels/connector-labels.component.ts
+++ d360.web/Scripts/app/components/admin/asset-type-configuration/connectorLabels/connector-labels.component.ts
@@ -377,19 +377,11 @@ export class ConnectorLabelsComponent extends AdminBaseComponent {
 	positionContextMenu(
 		$event: MouseEvent, container: HTMLElement, floatMenu: PopupMenu, assetGridTools: HTMLElement
 	): void {
-		if (!assetGridTools.contains(<Node>$event.target) && !this.isElementLink(<HTMLElement>$event.target)) {
+		if (!assetGridTools.contains(<Node>$event.target)) {
 			container.style.top = `${$event['layerY']}px`;
 			container.style.left = `${$event['layerX']}px`;
 			floatMenu.toggle($event);
 			$event.preventDefault();
 		}
-	}
-
-	private isElementLink(element: HTMLElement): boolean {
-		while (element.parentElement) {
-			if (element.tagName === 'A') { return true; }
-			element = element.parentElement;
-		}
-		return false;
 	}
 }
