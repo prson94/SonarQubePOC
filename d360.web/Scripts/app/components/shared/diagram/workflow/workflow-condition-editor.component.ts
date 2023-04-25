fb380fef3c46f212d5f73a61cac8f9f0be5c2455
tduric@infogix.com <tduric@infogix.com>
2023-04-25 09:36:25 +0200

    GOV-21453 : fix workflow value input not appearing on form inputs

--- d360.web/Scripts/app/components/shared/diagram/workflow/workflow-condition-editor.component.ts
+++ d360.web/Scripts/app/components/shared/diagram/workflow/workflow-condition-editor.component.ts
@@ -218,7 +218,8 @@ export class WorkflowConditionEditorComponent extends BaseComponent implements O
                     });
             }
         } else if (this.selectedField.split('|')[0] === 'FormInput') {
-            const input = this.formFields.find((f) => f['@id'] === this.selectedField.split('|')[2] && f['@stepId'] === this.selectedField.split('|')[1]);
+debugger;
+            const input = this.formFields.find((f) => f['@id'] === this.selectedField.split('|')[2] && +f['@stepId'] === +this.selectedField.split('|')[1]);
 
             this.selectedType = input['@type'].toLowerCase();
 
