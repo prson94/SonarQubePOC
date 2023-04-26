73e6187e9efd1a47dd00e1cf146532292126e69f
tduric@infogix.com <tduric@infogix.com>
2023-04-26 11:32:36 +0200

    GOV-21168 : fix font size on take action button

--- d360.web/Scripts/app/components/admin/actions/raise-action/raise-issue.component.ts
+++ d360.web/Scripts/app/components/admin/actions/raise-action/raise-issue.component.ts
@@ -1,4 +1,4 @@
-﻿import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+﻿import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnChanges, SimpleChanges, ViewEncapsulation } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Breadcrumb } from '../../../../models/breadcrumb.model';
 import { ActionEditorModel, WorkflowIssueType } from '../../../../models/workflow.model';
@@ -14,6 +14,7 @@ import { BaseComponent } from '../../../shared/base.component';
 	templateUrl: 'raise-issue.component.html',
 	styleUrls: ['raise-issue.component.less'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
+	encapsulation: ViewEncapsulation.None
 })
 
 export class RaiseIssueComponent extends BaseComponent implements OnChanges {
