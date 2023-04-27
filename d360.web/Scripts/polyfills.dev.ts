ebdf26571684461c97e0661afa955c1ce2b7b125
tduric@infogix.com <tduric@infogix.com>
2023-04-27 13:01:17 +0200

    GOV-21506 : fix codacy

--- d360.web/Scripts/polyfills.dev.ts
+++ d360.web/Scripts/polyfills.dev.ts
@@ -2,4 +2,5 @@
 //in development mode, we will not include localization to improve build performance
 //so we need to manually define $localize function
 
+// eslint-disable-next-line
 (window as any).$localize = (x: any) => x;
