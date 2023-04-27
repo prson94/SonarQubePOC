bdd9de8235378ceaedf1e8944f6affe5fe056e89
tduric@infogix.com <tduric@infogix.com>
2023-04-27 12:37:54 +0200

    GOV-21506 : add development build configuration

---
+++ d360.web/Scripts/polyfills.dev.ts
@@ -0,0 +1,5 @@
+﻿//global var for angular i18n translations
+//in development mode, we will not include localization to improve build performance
+//so we need to manually define $localize function
+
+(window as any).$localize = (x: any) => x;
