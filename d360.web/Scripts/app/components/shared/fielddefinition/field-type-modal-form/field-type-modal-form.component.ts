3c777fdb706aa8ad5d51d5992b2f83d8928419c7
tduric@infogix.com <tduric@infogix.com>
2023-05-24 12:35:44 +0200

    GOV-20705 : product review fixes

--- d360.web/Scripts/app/components/shared/fielddefinition/field-type-modal-form/field-type-modal-form.component.ts
+++ d360.web/Scripts/app/components/shared/fielddefinition/field-type-modal-form/field-type-modal-form.component.ts
@@ -6,8 +6,10 @@ import { forkJoin, of, Subscription } from "rxjs";
 import { AssetTypeClass, } from "../../../../models/asset.model";
 import { AssetTypeAncestry } from "../../../../models/fields.model";
 import { FieldType, FieldTypeAPIModel, FieldTypeAPIModelField } from "../../../../models/fieldtype-api.model";
+import { RelationshipType } from "../../../../models/relationship.model";
 import { AssetService } from "../../../../services/asset.service";
 import { FieldsObservableService } from "../../../../services/fieldsObservable.service";
+import { RelationshipsService } from "../../../../services/relationships.service";
 import { FormHelpers } from "../../../../static/form-helpers";
 import { PropertyGroupComponent } from "../../../shared/controls/property-group/property-group.component";
 import { D3SModal } from "../../../shared/modal/gov-modal.component";
@@ -36,9 +38,7 @@ export class ConfigurationFieldTypeModalFormComponent implements OnChanges, OnIn
 	@Input() assetTypeUid: string;
 	@Input() relationshipTypeUid: string;
 
-	@Input() showShowInDetailTile: boolean = true;
 	@Input() showDescription: boolean = true;
-	@Input() hasDisplayInColumn: boolean = false;
 
 	@Output() onClose = new EventEmitter();
 	@Output() onUpdated = new EventEmitter();
@@ -46,6 +46,8 @@ export class ConfigurationFieldTypeModalFormComponent implements OnChanges, OnIn
 	fieldType: FieldTypeAPIModel;
 	typeFieldTypes: FieldTypeAPIModelField[] = [];
 	editedFieldType: FieldTypeAPIModelField;
+
+	relationshipTypes: RelationshipType[];
 
 	title = 'unset';
 	subTitle = 'unset';
@@ -91,6 +93,8 @@ export class ConfigurationFieldTypeModalFormComponent implements OnChanges, OnIn
 	isInitialDataLoaded: boolean = false;
 	numberOfAssetsForType: number = 0;
 
+	displayFormatLabel = $localize`Display Format`;
+
 	private fieldTypeNameToApiNameMap = {
 		'FieldFromRelationship': 'ComputedRelationshipField',
 		'OwnershipLookup': 'ComputedOwnershipLookup',
@@ -110,6 +114,7 @@ export class ConfigurationFieldTypeModalFormComponent implements OnChanges, OnIn
 
 	constructor(private fb: FormBuilder,
 		private fieldsService: FieldsObservableService,
+		private relationshipService: RelationshipsService,
 		private assetService: AssetService,
 		private cdRef: ChangeDetectorRef
 	) {
@@ -142,7 +147,8 @@ export class ConfigurationFieldTypeModalFormComponent implements OnChanges, OnIn
 			this.fieldsService.getFieldsV2(this.assetTypeUid, this.actionTypeUid, this.relationshipTypeUid),
 			this.fieldsService.getLookups(this.assetTypeUid, this.actionTypeUid, this.relationshipTypeUid),
 			this.assetTypeUid ? this.assetService.getAssetCountsByAssetTypeUid(this.assetTypeUid) : of([]),
-			this.assetTypeUid ? this.fieldsService.getAvailableScoreTypes(this.assetTypeUid) : of([])
+			this.assetTypeUid ? this.fieldsService.getAvailableScoreTypes(this.assetTypeUid) : of([]),
+			this.assetTypeUid ? this.relationshipService.getRelationshipTypes(this.assetTypeUid, false, false, true) : of([])
 		).subscribe((results) => {
 			//fields
 			if (results[0]) {
@@ -201,6 +207,11 @@ export class ConfigurationFieldTypeModalFormComponent implements OnChanges, OnIn
 			//score types
 			if (results[3].length > 0) {
 				this.scoreTypeOptions = results[3];
+			}
+
+			//relationship types
+			if (results[4].length > 0) {
+				this.relationshipTypes = results[4];
 			}
 
 			this.setForm();
@@ -462,10 +473,28 @@ export class ConfigurationFieldTypeModalFormComponent implements OnChanges, OnIn
 			if (this.selectedFieldType === 'ComputedRelationshipField') {
 				this.loadFieldsFromRelationships(value);
 			}
+			else if (this.selectedFieldType === 'Relationship' && (value !== null && value !== '')) {
+				const uid = (value as string).split('|')[0].toLowerCase();
+				const type = this.relationshipTypes.find(r => r.Uid.toLowerCase() === uid.toLowerCase());
+
+				if (type.Subject.Uid === this.assetTypeUid) {
+					this.relationshipDisplayFormatValueOptions[0].label = `${this.displayFormatLabel} : ${type.Object.DisplayFormat}`
+				} else {
+					this.relationshipDisplayFormatValueOptions[0].label = `${this.displayFormatLabel} : ${type.Subject.DisplayFormat}`
+				}
+			}
 		});
 
 		this.fieldTypeForm.controls["LookupUid"].valueChanges.subscribe((value) => {
 			this.loadLookupDefaultValue(value);
+		});
+
+		this.fieldTypeForm.controls["ValidationPattern"].valueChanges.subscribe((value) => {
+			this.isValidPattern = null;
+		});
+
+		this.fieldTypeForm.controls["RegexTestString"].valueChanges.subscribe((value) => {
+			this.isValidPattern = null;
 		});
 
 		this.fieldTypeForm.controls["DisplayAsList"].valueChanges.subscribe((value) => {
@@ -1024,39 +1053,82 @@ export class ConfigurationFieldTypeModalFormComponent implements OnChanges, OnIn
 	}
 
 	get showIsPartOfKey(): boolean {
+		if (this.isGroupType) {
+			return false;
+		}
 		const allowedTypes = ['Counter', 'Date', 'DateTime', 'Decimal', 'Html', 'Number', 'Text', 'Boolean'];
-		return this.assetTypeUid && allowedTypes.indexOf(this.selectedFieldType) > -1;
+		return (this.assetTypeUid || this.relationshipTypeUid) && allowedTypes.indexOf(this.selectedFieldType) > -1;
 	}
 
 	get showIsListable(): boolean {
+		if (this.assetTypeClass === AssetTypeClass.DiagramAsset) {
+			return false;
+		}
+
 		const allowedTypes = ['Counter', 'Date', 'DateTime', 'Decimal', 'Html', 'Link', 'Lookup', 'Number', 'ComputedOwnershipLookup', 'Score', 'Text', 'Tag', 'Boolean', 'Path', 'ComputedRelationshipField', 'Relationship'];
 		return this.assetTypeUid && allowedTypes.indexOf(this.selectedFieldType) > -1;
 	}
 
 	get showPersistInFilters(): boolean {
+		if (this.assetTypeClass === AssetTypeClass.DiagramAsset || this.assetTypeClass === AssetTypeClass.ReferenceItemType) {
+			return false;
+		}
+
+		if (this.isGroupType || this.isUserType) {
+			return false;
+		}
+
 		const allowedTypes = ['Counter', 'Date', 'DateTime', 'Decimal', 'Html', 'Link', 'Lookup', 'Number', 'Relationship', 'Score', 'Text', 'Tag', 'Boolean'];
 		return this.assetTypeUid && allowedTypes.indexOf(this.selectedFieldType) > -1;
 	}
 
 	get showIsEditable(): boolean {
 		const allowedTypes = ['Date', 'DateTime', 'Decimal', 'Html', 'Link', 'Lookup', 'Number', 'Relationship', 'Text', 'Boolean'];
-		return this.assetTypeUid && allowedTypes.indexOf(this.selectedFieldType) > -1;
+		return (this.assetTypeUid || this.relationshipTypeUid) && allowedTypes.indexOf(this.selectedFieldType) > -1;
 	}
 
 	get showIsRequired(): boolean {
 		const allowedTypes = ['Date', 'DateTime', 'Decimal', 'Html', 'Link', 'Lookup', 'Number', 'Text', 'Boolean'];
-		return this.assetTypeUid && allowedTypes.indexOf(this.selectedFieldType) > -1;
+		return (this.assetTypeUid || this.relationshipTypeUid) && allowedTypes.indexOf(this.selectedFieldType) > -1;
 	}
 
 	get enableAllowMultipleValues(): boolean {
 		const allowedTypes = ['Lookup'];
-		return this.assetTypeUid && allowedTypes.indexOf(this.selectedFieldType) > -1;
+		return (this.assetTypeUid || this.relationshipTypeUid) && allowedTypes.indexOf(this.selectedFieldType) > -1;
 	}
 
 	get hasFormDescription(): boolean {
 		const allowedTypes = ['Date', 'DateTime', 'Decimal', 'Html', 'Link', 'Lookup', 'Number', 'Relationship', 'Text', 'Boolean'];
 		return allowedTypes.indexOf(this.selectedFieldType) > -1;
 	}
+
+	get hasDisplayInColumn(): boolean {
+		if (this.relationshipTypeUid) {
+			return false;
+		}
+
+		if (this.isGroupType || this.isUserType) {
+			return false;
+		}
+		return this.fieldTypeForm.get('IsDisplayable').value && this.selectedFieldType !== 'ComputedRelationshipReferenceList' && this.selectedFieldType !== 'JSON';
+	}
+
+	get isGroupType(): boolean {
+		return (this.assetTypeUid ?? '').toLocaleLowerCase() === '00000001-0000-0000-0000-b00000000012'.toLocaleLowerCase();
+	}
+
+	get isUserType(): boolean {
+		return (this.assetTypeUid ?? '').toLocaleLowerCase() === '00000001-0000-0000-0000-a00000000011'.toLocaleLowerCase();
+	}
+
+	get showShowInDetailTile(): boolean {
+		if (this.isGroupType || this.isUserType) {
+			return false;
+		}
+
+		return true;
+	}
+
 
 	public getLocaleDateString(): string {
 		return FormHelpers.getLocaleDateString();
