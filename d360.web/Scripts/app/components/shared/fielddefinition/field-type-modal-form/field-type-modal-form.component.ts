ac5bacc8a95c03675ab9cb71030239f284a335ad
tduric@infogix.com <tduric@infogix.com>
2023-05-31 10:54:09 +0200

    GOV-28159 : fix default category names for action types

--- d360.web/Scripts/app/components/shared/fielddefinition/field-type-modal-form/field-type-modal-form.component.ts
+++ d360.web/Scripts/app/components/shared/fielddefinition/field-type-modal-form/field-type-modal-form.component.ts
@@ -136,9 +136,13 @@ export class ConfigurationFieldTypeModalFormComponent implements OnChanges, OnIn
 		this.cdRef.markForCheck();
 	}
 
+	get defaultCategoryName(): string {
+		return this.actionTypeUid ? $localize`Form Fields` : $localize`General`;
+	}
+
 	categoryTokens = [
 		{
-			"title": $localize`General`
+			"title": this.defaultCategoryName
 		}
 	]
 
@@ -168,8 +172,8 @@ export class ConfigurationFieldTypeModalFormComponent implements OnChanges, OnIn
 						title: x
 					});
 				});
-				if (!this.categoryTokens.some((x) => x.title === $localize`General`)) {
-					this.categoryTokens.push({ title: $localize`General` });
+				if (!this.categoryTokens.some((x) => x.title === this.defaultCategoryName)) {
+					this.categoryTokens.push({ title: this.defaultCategoryName });
 				}
 				this.categoryTokens = this.categoryTokens.sort((a, b) => a.title > b.title ? 1 : -1);
 			}
@@ -426,7 +430,7 @@ export class ConfigurationFieldTypeModalFormComponent implements OnChanges, OnIn
 		this.fieldTypeForm = this.fb.group({
 			FriendlyName: [null, { validators: [Validators.required, Validators.maxLength(250)] }],
 			Name: [null, { validators: Validators.compose([Validators.required, this.apiNameValidator(), Validators.maxLength(250)]) }],
-			Category: [$localize`General`, { validators: [Validators.maxLength(100), Validators.required] }],
+			Category: [this.defaultCategoryName, { validators: [Validators.maxLength(100), Validators.required] }],
 			AssetPathListSegment: [null],
 			DisplayDescription: [null],
 			FormDescription: [null],
@@ -529,7 +533,7 @@ export class ConfigurationFieldTypeModalFormComponent implements OnChanges, OnIn
 		this.fieldTypeForm.get('DisplayAsList').setValue('false');
 		this.fieldTypeForm.get('AllowAllValue').setValue(false);
 		this.fieldTypeForm.get('SortByAscending').setValue('true');
-		this.fieldTypeForm.get('Category').setValue($localize`General`);
+		this.fieldTypeForm.get('Category').setValue(this.defaultCategoryName);
 
 		if (this.selectedFieldType === 'Decimal' || this.selectedFieldType === 'Number') {
 			this.fieldTypeForm.controls["DefaultValue"].addValidators(this.numberDefaultValueValidator());
@@ -614,7 +618,7 @@ export class ConfigurationFieldTypeModalFormComponent implements OnChanges, OnIn
 
 				this.fieldTypeForm.controls["Name"].setValue(this.editedFieldType.Name);
 				this.fieldTypeForm.controls["FriendlyName"].setValue(this.editedFieldType.FriendlyName);
-				this.fieldTypeForm.controls["Category"].setValue(this.editedFieldType.Category ?? $localize`General`);
+				this.fieldTypeForm.controls["Category"].setValue(this.editedFieldType.Category ?? this.defaultCategoryName);
 
 				const type = this.editedFieldType.Type[this.selectedFieldType];
 
