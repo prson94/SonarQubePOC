3fb9ce8dc5b57df322658776954622fdbd23ea83
tduric@infogix.com <tduric@infogix.com>
2023-06-02 14:44:32 +0200

    GOV-21762,28187,28159,28167 : bugfixes for field editor

--- d360.web/Scripts/app/components/shared/fielddefinition/field-type-modal-form/field-type-modal-form.component.ts
+++ d360.web/Scripts/app/components/shared/fielddefinition/field-type-modal-form/field-type-modal-form.component.ts
@@ -1121,6 +1121,9 @@ export class ConfigurationFieldTypeModalFormComponent implements OnChanges, OnIn
 	}
 
 	get showAddToSearch(): boolean {
+		if (this.assetTypeClass === AssetTypeClass.Reference) {
+			return false;
+		}
 		const allowedTypes = ['Counter', 'Date', 'DateTime', 'Decimal', 'ComputedRelationshipField', 'Link', 'Lookup', 'Number', 'Relationship', 'Text', 'Boolean'];
 		return this.assetTypeUid && allowedTypes.indexOf(this.selectedFieldType) > -1;
 	}
@@ -1128,6 +1131,7 @@ export class ConfigurationFieldTypeModalFormComponent implements OnChanges, OnIn
 	get showIsPartOfKey(): boolean {
 		if (this.isGroupType || this.assetTypeClass === AssetTypeClass.ReferenceItemType
 			|| this.assetTypeClass === AssetTypeClass.Reference
+			|| this.assetTypeClass === AssetTypeClass.DiagramAsset
 			|| this.relationshipTypeUid) {
 			return false;
 		}
@@ -1331,6 +1335,7 @@ export class ConfigurationFieldTypeModalFormComponent implements OnChanges, OnIn
 	back() {
 		this.storeControlsValues();
 		this.formState = FormState.FieldTypeSelection;
+		this.setDefaultTitle();
 	}
 
 	controlsStoredValue = [];
