e98721c3890761cf7542a824ae431ef9a8f9ffff
tduric@infogix.com <tduric@infogix.com>
2023-06-05 16:30:52 +0200

    GOV-28197 : fix issue when adding new relationship field

--- d360.model/DataAccessLayer/repositories/FieldsRepository.cs
+++ d360.model/DataAccessLayer/repositories/FieldsRepository.cs
@@ -1812,7 +1812,7 @@ namespace d360.model.DataAccessLayer
 					newFieldType.DisplayInColumn = f.Type.Relationship.DisplayInColumn;
 					newFieldType.UseDisplayFormat = f.Type.Relationship.UseDisplayFormat;
 					newFieldType.IsSubject = f.Type.Relationship.IsSubject;
-					newFieldType.IsRequired = f.Type.Relationship.Validation.IsRequired;
+					newFieldType.IsRequired = f.Type.Relationship.Validation?.IsRequired ?? false;
 
 					if (f.Type.Relationship.Search != null)
 					{
