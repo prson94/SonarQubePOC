8f56d87f49ca18eb4c11f890fd12f79ec6fa9d0e
tduric@infogix.com <tduric@infogix.com>
2023-06-06 11:29:40 +0200

    GOV-28200 : add logic to handle required fields per rel type on lookup editor

---
+++ d360.web/Scripts/app/pipes/dropdown-category-filter.pipe.ts
@@ -0,0 +1,16 @@
+﻿import { Pipe, PipeTransform } from '@angular/core';
+import { SelectItemGroup } from 'primeng/api';
+
+@Pipe({ name: 'dropdownCategoryFilter' })
+export class DropdownCategoryGroupPipe implements PipeTransform {
+	transform(items: SelectItemGroup[], relIdx: number): any {
+		let selectlist: SelectItemGroup[] = [];
+		if (!relIdx) {
+			return items;
+		}
+		else {
+			selectlist = items.filter((x) => x.label.endsWith(relIdx.toLocaleString()));
+		}
+        return selectlist;
+    }
+}
