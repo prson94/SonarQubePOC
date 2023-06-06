2dd9719b576593e2bd9516fdd52a0ed0379373f8
tduric@infogix.com <tduric@infogix.com>
2023-06-06 15:16:27 +0200

    GOV-21136 : fix flickering dropdown

--- d360.web/Scripts/app/components/shared/fielddefinition/field-type-modal-form/relation-lookup-field-type-editor/relation-lookup-field-type-editor.component.ts
+++ d360.web/Scripts/app/components/shared/fielddefinition/field-type-modal-form/relation-lookup-field-type-editor/relation-lookup-field-type-editor.component.ts
@@ -144,7 +144,7 @@ export class RelationLookupFieldTypeEditorComponent implements OnChanges {
 
 		this.intervalHandle = setInterval(() => {
 			this.setDropdowns();
-		}, 250);
+		}, 1000);
 	}
 
 	setDropdowns() {
