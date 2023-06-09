269727d8e9125c6a0fc6849330db7b2609106550
tduric@infogix.com <tduric@infogix.com>
2023-06-09 13:37:23 +0200

    GOV-28242 : make json attribute path required and fix styles on autocomplete component

--- d360.web/Scripts/app/directives/ig-autocomplete-directive.ts
+++ d360.web/Scripts/app/directives/ig-autocomplete-directive.ts
@@ -1,64 +1,80 @@
-﻿import { AfterViewInit, Directive, ElementRef, Input, NgModule, OnDestroy } from '@angular/core';
+﻿import { AfterContentInit, AfterViewInit, Directive, ElementRef, Input, NgModule, OnDestroy } from '@angular/core';
 import { DomHandler } from 'primeng/dom';
 import { CommonModule } from '@angular/common';
+import { AutoComplete } from 'primeng/autocomplete';
 
 @Directive({
-    selector: '[igAutocomplete]'
+	selector: '[igAutocomplete]'
 
 })
-export class AutocompleteDirective implements AfterViewInit, OnDestroy {
-    public _size: string;
+export class AutocompleteDirective implements AfterViewInit, AfterContentInit, OnDestroy {
+	public _size: string;
 
-    constructor(public el: ElementRef) { }
+	constructor(public el: ElementRef, public autoCompleteRef: AutoComplete) { }
 
-    ngAfterViewInit() {
-        DomHandler.addMultipleClasses(this.el.nativeElement, this.getStyleClass());
-        var htmlEl = this.el.nativeElement as HTMLElement;
-        var input = htmlEl.getElementsByTagName('INPUT')[0];
-        DomHandler.addClass(input, 'ig-input');
+	ngAfterViewInit() {
+		DomHandler.addMultipleClasses(this.el.nativeElement, this.getStyleClass());
+		var htmlEl = this.el.nativeElement as HTMLElement;
+		var input = htmlEl.getElementsByTagName('INPUT')[0];
+		DomHandler.addClass(input, 'ig-input');
 
-        DomHandler.removeClass(input, 'p-inputtext');
-        DomHandler.removeClass(input, 'p-component');
-        DomHandler.removeClass(input, 'p-state-default');
-        DomHandler.removeClass(input, 'p-corner-all');
-        DomHandler.removeClass(input, 'p-autocomplete-input');
+		DomHandler.removeClass(input, 'p-inputtext');
+		DomHandler.removeClass(input, 'p-component');
+		DomHandler.removeClass(input, 'p-state-default');
+		DomHandler.removeClass(input, 'p-corner-all');
+		DomHandler.removeClass(input, 'p-autocomplete-input');
 
-        //remove z index from autocomplete so input within takes advantage
-        htmlEl.tabIndex = -1;
+		//remove z index from autocomplete so input within takes advantage
+		htmlEl.tabIndex = -1;
 
-        //set igSize
-        if (this._size && this._size === "small") {
-            DomHandler.addMultipleClasses(input, "ig-input-small");
-        } else if (this._size && this._size === "medium") {
-            DomHandler.addMultipleClasses(input, "ig-input-medium");
-        } else if (this._size && this._size === "large") {
-            DomHandler.addMultipleClasses(input, "ig-input-large");
-        } else if (this._size && this._size === "full") {
-            DomHandler.addMultipleClasses(input, "ig-input-full");
-        }
-    }
+		//set igSize
+		if (this._size && this._size === "small") {
+			DomHandler.addMultipleClasses(input, "ig-input-small");
+		} else if (this._size && this._size === "medium") {
+			DomHandler.addMultipleClasses(input, "ig-input-medium");
+		} else if (this._size && this._size === "large") {
+			DomHandler.addMultipleClasses(input, "ig-input-large");
+		} else if (this._size && this._size === "full") {
+			DomHandler.addMultipleClasses(input, "ig-input-full");
+		}
+	}
 
-    getStyleClass(): string {
-        return 'ig-autocomplete';
-    }
+	ngAfterContentInit() {
+		const required = this.el.nativeElement.getAttribute("required");
+		const placeholder = this.el.nativeElement.getAttribute("placeholder");
+		const isPlaceholderSet = !(placeholder == null || placeholder === "");
+		if (!isPlaceholderSet) {
+			if (required == null) {
+				this.autoCompleteRef.placeholder = $localize`Optional`;
+			} else {
+				this.autoCompleteRef.placeholder = $localize`Value required`;
+			}
+		} else {
+			this.autoCompleteRef.placeholder = placeholder;
+		}
+	}
 
-    @Input() get igSize(): string {
-        return this._size;
-    }
-    set igSize(val: string) {
-        this._size = val;
-    }
+	getStyleClass(): string {
+		return 'ig-autocomplete';
+	}
 
-    ngOnDestroy() {
-        while (this.el.nativeElement.hasChildNodes()) {
-            this.el.nativeElement.removeChild(this.el.nativeElement.lastChild);
-        }
-    }
+	@Input() get igSize(): string {
+		return this._size;
+	}
+	set igSize(val: string) {
+		this._size = val;
+	}
+
+	ngOnDestroy() {
+		while (this.el.nativeElement.hasChildNodes()) {
+			this.el.nativeElement.removeChild(this.el.nativeElement.lastChild);
+		}
+	}
 }
 
 @NgModule({
-    imports: [CommonModule],
-    exports: [AutocompleteDirective],
-    declarations: [AutocompleteDirective]
+	imports: [CommonModule],
+	exports: [AutocompleteDirective],
+	declarations: [AutocompleteDirective]
 })
 export class AutocompleteModule { }
