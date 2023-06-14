a85be96e0c72414837ab59e7e788496ce68a38b7
SYNCSORTINC\Saurabh.Bhutani <saurabh.bhutani@precisely.com>
2023-06-14 18:37:53 +0530

    GOV-21396 - get workflow version details from object detail service

--- d360.web/Scripts/app/components/assignments/workflow-version-list/workflow-version-information/workflow-version-information.component.ts
+++ d360.web/Scripts/app/components/assignments/workflow-version-list/workflow-version-information/workflow-version-information.component.ts
@@ -1,57 +1,20 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {WorkflowDiagramModel} from "../../../../models/workflow.model";
-import {WorkflowService} from "../../../../services/workflow.service";
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'd3s-workflow-version-information',
-  templateUrl: './workflow-version-information.component.html',
-  styleUrls: ['./workflow-version-information.component.less']
+	selector: 'd3s-workflow-version-information',
+	templateUrl: './workflow-version-information.component.html',
+	styleUrls: ['./workflow-version-information.component.less']
 })
 export class WorkflowVersionInformationComponent {
-	@Input() shouldBePadded: boolean = true;
-	@Input() showHeaderLine: boolean = true;
-	@Input() isSidePanel: boolean = false;
-	@Input() interceptLinkClick: boolean = false;
 	@Output() linkClick = new EventEmitter();
 	@Output() close: EventEmitter<void> = new EventEmitter<void>();
 
-	workflowDiagramModel: WorkflowDiagramModel;
-	isLoading: boolean = false;
-	version: number;
+	@Input() workflowTypeVersionId: number;
 
-	private id: number = 0;
-	private uid: string = '00000000-0000-0000-0000-000000000000';
+	@Input() workflowTypeId: number;
 
-	@Input() set workflowTypeId(value: number) {
-		this.id = value;
-		this.getWorkflowTypeDetails();
-	}
+	@Input() workflowTypeUid: string;
 
-	@Input() set workflowTypeUid(value: string) {
-		this.uid = value;
-		this.getWorkflowTypeDetails();
-	}
+	@Input() workflowTypeVersion: number;
 
-	get workflowTypeUid(): string {
-		return this.uid;
-	}
-
-	@Input() set workflowTypeVersion(value: number){
-		this.version = value;
-		this.getWorkflowTypeDetails();
-	}
-
-	constructor(private workflowService: WorkflowService) {
-	}
-
-	ngOnInit(): void {
-	}
-
-	private getWorkflowTypeDetails() {
-		this.isLoading = true;
-		this.workflowService.getWorkflowDiagram(this.id, this.uid, this.version).subscribe(response => {
-			this.isLoading = false;
-			this.workflowDiagramModel = response;
-		});
-	}
 }
