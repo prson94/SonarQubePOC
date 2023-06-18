6aa1a161fd96a3aef9026b24345e5ec03fff1d5d
mrigank.singh <mrigank.singh@winshuttle.com>
2023-06-18 12:07:32 +0530

    GOV-21396 --initial structure commit 68 ( assignment response form )

---
+++ d360.web/Scripts/app/components/assignments/assignment-progress-step-details/assignment-form-response/assignment-form-response.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'd3s-assignment-form-response',
+  templateUrl: './assignment-form-response.component.html',
+  styleUrls: ['./assignment-form-response.component.less']
+})
+export class AssignmentFormResponseComponent {
+
+}
--- d360.web/Scripts/app/components/assignments/assignments.module.ts
+++ d360.web/Scripts/app/components/assignments/assignments.module.ts
@@ -1,49 +1,49 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {AssignmentsContainerComponent} from './assignments-container.component';
-import {AssignmentListComponent} from './assignment-list/assignment-list.component';
-import {RouterModule} from '@angular/router';
-import {AssignmentsRoutingModule} from './assignments.routes';
-import {AdvancedFiltersModule} from '../assets-grid/advanced-filtering/advanced-filtering.module';
-import {AngularSplitModule} from 'angular-split';
-import {AssetDetailModule} from '../shared/asset-detail/asset-detail.module';
-import {ButtonModule} from '../../directives/ig-button-directive';
-import {CheckboxModule} from 'primeng/checkbox';
-import {D3SSortIconModule} from '../shared/turbotable-sorticon.component';
-import {DirectivesModule} from '../../directives/directives.module';
-import {IgBadgeModule} from '../shared/controls/badge/badge.module';
-import {PopupMenuModule} from '../shared/controls/popup-menu/popup-menu.component';
-import {SearchFieldModule} from '../shared/controls/search-field/search-field.component';
-import {SharedGridPagingInfoModule} from '../shared/grid-paging-info.component';
-import {SharedModule} from 'primeng/api';
-import {SidePanelModule} from '../shared/sidepanel/side-panel.module';
-import {TableModule} from 'primeng/table';
-import {TooltipModule} from 'primeng/tooltip';
-import {FormsModule} from '@angular/forms';
-import {AssignmentGridComponent} from './assignment-grid/assignment-grid.component';
-import {D3SColumnFilterModule} from '../shared/turbotable-column-filter.component';
-import {AssignmentInformationComponent} from './assignment-information/assignment-information.component';
-import {AssignmentProgressComponent} from './assignment-progress/assignment-progress.component';
-import {PropertyGroupModule} from '../shared/controls/property-group/property-group.component';
-import {SharedDeleteFormModule} from '../shared/delete.form';
-import {AssignmentMultiDeleteComponent} from './assignment-multi-delete/assignment-multi-delete.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AssignmentsContainerComponent } from './assignments-container.component';
+import { AssignmentListComponent } from './assignment-list/assignment-list.component';
+import { RouterModule } from '@angular/router';
+import { AssignmentsRoutingModule } from './assignments.routes';
+import { AdvancedFiltersModule } from '../assets-grid/advanced-filtering/advanced-filtering.module';
+import { AngularSplitModule } from 'angular-split';
+import { AssetDetailModule } from '../shared/asset-detail/asset-detail.module';
+import { ButtonModule } from '../../directives/ig-button-directive';
+import { CheckboxModule } from 'primeng/checkbox';
+import { D3SSortIconModule } from '../shared/turbotable-sorticon.component';
+import { DirectivesModule } from '../../directives/directives.module';
+import { IgBadgeModule } from '../shared/controls/badge/badge.module';
+import { PopupMenuModule } from '../shared/controls/popup-menu/popup-menu.component';
+import { SearchFieldModule } from '../shared/controls/search-field/search-field.component';
+import { SharedGridPagingInfoModule } from '../shared/grid-paging-info.component';
+import { SharedModule } from 'primeng/api';
+import { SidePanelModule } from '../shared/sidepanel/side-panel.module';
+import { TableModule } from 'primeng/table';
+import { TooltipModule } from 'primeng/tooltip';
+import { FormsModule } from '@angular/forms';
+import { AssignmentGridComponent } from './assignment-grid/assignment-grid.component';
+import { D3SColumnFilterModule } from '../shared/turbotable-column-filter.component';
+import { AssignmentInformationComponent } from './assignment-information/assignment-information.component';
+import { AssignmentProgressComponent } from './assignment-progress/assignment-progress.component';
+import { PropertyGroupModule } from '../shared/controls/property-group/property-group.component';
+import { SharedDeleteFormModule } from '../shared/delete.form';
+import { AssignmentMultiDeleteComponent } from './assignment-multi-delete/assignment-multi-delete.component';
 import {
 	AssignmentInformationGeneralComponent
 } from './assignment-information/assignment-information-general/assignment-information-general.component';
 import {
 	AssignmentInformationRequestComponent
 } from './assignment-information/assignment-information-request/assignment-information-request.component';
-import {CoreModule} from '../shared/core.module';
+import { CoreModule } from '../shared/core.module';
 import {
 	AssignmentProgressStepComponent
 } from './assignment-progress/assignment-progress-step/assignment-progress-step.component';
-import {CompleteAssignmentComponent} from './complete-assignment/complete-assignment.component';
-import {SiteModalModule} from '../shared/modal/gov-modal.module';
+import { CompleteAssignmentComponent } from './complete-assignment/complete-assignment.component';
+import { SiteModalModule } from '../shared/modal/gov-modal.module';
 import {
 	AssignmentProgressStepDetailsComponent
 } from './assignment-progress-step-details/assignment-progress-step-details.component';
-import {WorkflowDiagramModule} from '../shared/diagram/workflow/workflow-diagram.module';
-import {WorkflowInformationComponent} from './workflow-information/workflow-information.component';
+import { WorkflowDiagramModule } from '../shared/diagram/workflow/workflow-diagram.module';
+import { WorkflowInformationComponent } from './workflow-information/workflow-information.component';
 import {
 	WorkflowInformationGeneralComponent
 } from './workflow-information/workflow-information-general/workflow-information-general.component';
@@ -56,7 +56,7 @@ import {
 import {
 	AssignmentStepEmailDetailsComponent
 } from './assignment-progress-step-details/assignment-step-email-details/assignment-step-email-details.component';
-import {PipesModule} from '../../pipes/pipes.module';
+import { PipesModule } from '../../pipes/pipes.module';
 import {
 	AssignmentStepHttpDetailsComponent
 } from './assignment-progress-step-details/assignment-step-http-details/assignment-step-http-details.component';
@@ -72,7 +72,7 @@ import {
 import {
 	AssignmentStepFormDetailsComponent
 } from './assignment-progress-step-details/assignment-step-form-details/assignment-step-form-details.component';
-import {WorkflowVersionListComponent} from './workflow-version-list/workflow-version-list.component';
+import { WorkflowVersionListComponent } from './workflow-version-list/workflow-version-list.component';
 import {
 	WorkflowVersionGridComponent
 } from './workflow-version-list/workflow-version-grid/workflow-version-grid.component';
@@ -85,6 +85,9 @@ import {
 import {
 	WorkflowPendingAssignmentsComponent
 } from './workflow-version-list/workflow-version-information/workflow-pending-assignments/workflow-pending-assignments.component';
+import {
+	AssignmentFormResponseComponent
+} from './assignment-progress-step-details/assignment-form-response/assignment-form-response.component';
 
 
 @NgModule({
@@ -114,7 +117,8 @@ import {
 		WorkflowVersionDetailsComponent,
 		WorkflowVersionGridComponent,
 		WorkflowVersionInformationComponent,
-		WorkflowVersionListComponent
+		WorkflowVersionListComponent,
+		AssignmentFormResponseComponent
 	],
 	imports: [
 		AdvancedFiltersModule,
