8a822da70094a810759d46cb93ef78ffe4188cd0
Puneet Gupta <puneet.gupta@Precisely.com>
2023-06-20 17:58:08 +0530

    GOV-28260 UI Issue: Information in the side panel for the Relation Lookup field in incorrect.

--- d360.web/Scripts/app/components/shared/fielddefinition/field-type-details/field-type-details.component.ts
+++ d360.web/Scripts/app/components/shared/fielddefinition/field-type-details/field-type-details.component.ts
@@ -63,7 +63,7 @@ export class FieldTypeDetailsComponent implements OnChanges {
 	}
 
 	hasIsListable() {
-		const excludeTypes: string[] = [, 'Relationship'];
+		const excludeTypes: string[] = ['Relationship'];
 		if (excludeTypes.indexOf(this.fieldType.FieldTypeValue) > -1) {
 			return false;
 		}
