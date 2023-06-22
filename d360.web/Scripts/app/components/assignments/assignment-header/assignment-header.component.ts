379c262e96f99bd54bb6eb3d5b75853f246afee2
mrigank.singh <mrigank.singh@winshuttle.com>
2023-06-22 16:44:44 +0530

    GOV-21396 --initial structure commit 77 ( recreate tabs assignment listing )

--- d360.web/Scripts/app/components/assignments/assignment-header/assignment-header.component.ts
+++ d360.web/Scripts/app/components/assignments/assignment-header/assignment-header.component.ts
@@ -2,12 +2,13 @@ import { Component, Input } from '@angular/core';
 import { Tab } from '../../shared/tabs/tabs.models';
 
 @Component({
-  selector: 'd3s-assignment-header',
-  templateUrl: './assignment-header.component.html'
+	selector: 'd3s-assignment-header',
+	templateUrl: './assignment-header.component.html'
 })
 export class AssignmentHeaderComponent {
-	@Input() icon: string = 'fa-list-ul'
-	@Input() header: string = 'Assignments'
+	@Input() icon: string = 'fa-list-ul';
+	@Input() header: string = 'Assignments';
+	@Input() showTabs: boolean = true;
 	tabs: Tab[] = [
 		{
 			url: `/assignments`,
