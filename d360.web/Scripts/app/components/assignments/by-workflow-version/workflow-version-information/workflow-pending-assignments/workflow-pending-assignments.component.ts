0e195ba1d7b0bf24dce36845c3ff172768958cae
SYNCSORTINC\Saurabh.Bhutani <saurabh.bhutani@precisely.com>
2023-06-28 20:47:06 +0530

    GOV-21396 - update pending assignment version component to pass UID and version to underlying component and not make the service call.

--- d360.web/Scripts/app/components/assignments/by-workflow-version/workflow-version-information/workflow-pending-assignments/workflow-pending-assignments.component.ts
+++ d360.web/Scripts/app/components/assignments/by-workflow-version/workflow-version-information/workflow-pending-assignments/workflow-pending-assignments.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { LinkModel, NodeModel, WorkflowDiagramModel } from '../../../../../models/workflow.model';
-import { WorkflowService } from '../../../../../services/workflow.service';
+import { NodeModel } from '../../../../../models/workflow.model';
 
 @Component({
 	selector: 'd3s-workflow-pending-assignments',
@@ -8,48 +7,14 @@ import { WorkflowService } from '../../../../../services/workflow.service';
 	styleUrls: ['./workflow-pending-assignments.component.less']
 })
 export class WorkflowPendingAssignmentsComponent {
-	isLoading: boolean = false;
-	version: number;
-	private id: number = 0;
-	private uid: string = '00000000-0000-0000-0000-000000000000';
-	@Output() nodeSelection: EventEmitter<NodeModel> = new EventEmitter<NodeModel>()
+	@Input() workflowTypeUid: string = '00000000-0000-0000-0000-000000000000';
 
-	@Input() set workflowTypeId(value: number) {
-		if (value) {
-			this.id = value;
-			this.getWorkflowTypeDetails();
-		}
-	}
+	@Input() workflowTypeVersion: number;
 
-	@Input() set workflowTypeUid(value: string) {
-		if (value) {
-			this.uid = value;
-			this.getWorkflowTypeDetails();
-		}
-	}
-
-	@Input() set workflowTypeVersion(value: number) {
-		if (value) {
-			this.version = value;
-			this.getWorkflowTypeDetails();
-		}
-	}
-
-	workflowDiagramModel: WorkflowDiagramModel;
-
-	constructor(private workflowService: WorkflowService) {
-	}
-
-	private getWorkflowTypeDetails() {
-		this.isLoading = true;
-		this.workflowService.getWorkflowDiagram(this.id, this.uid, this.version).subscribe(response => {
-			this.isLoading = false;
-			this.workflowDiagramModel = response;
-		});
-	}
+	@Output() nodeSelection: EventEmitter<NodeModel> = new EventEmitter<NodeModel>();
 
 	onNodeClick(event: NodeModel) {
-		this.nodeSelection.emit(event)
+		this.nodeSelection.emit(event);
 	}
 
 }
