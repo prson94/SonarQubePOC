afbccea06a9f698bf6457f775f5b541e67faa7f0
Thomas Klok <thomas.klok@precisely.com>
2023-07-26 10:50:47 -0400

    GOV-28651 Add FeatureFlag component to Gallery

---
+++ d360.web/Scripts/app/components/gallery/gallery.featureflag.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { LaunchDarklyService } from '@precisely/prism-ng/launch-darkly';
+
+interface FeatureFlag {
+	flag: string,
+	value: boolean
+}
+
+@Component({
+	selector: 'gallery-featureflag',
+	templateUrl: './gallery.featureflag.component.html',
+	styles: [
+		`
+        .gallery-section {
+            padding: 0 16px 32px 16px;
+        }
+
+        .gallery-section h4 {
+            padding-bottom: 8px;
+        }
+        `
+	], changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class GalleryFeatureflagComponent implements OnInit {
+	properties: Array<any>;
+	public featureFlags: FeatureFlag[] = [];
+
+	constructor(
+		private featureFlagService: LaunchDarklyService
+	) {	}
+
+	ngOnInit(): void {
+		this.properties = [];
+
+		const allFlags = this.featureFlagService.client.allFlags()
+		this.featureFlags = Object.keys(allFlags)
+			.filter((key) => key.startsWith("Govern"))
+			.reduce((obj, key) => {
+				obj.push({
+					flag: key,
+					value: allFlags[key]
+				});
+				return obj;
+			}, []);
+	}
+}
