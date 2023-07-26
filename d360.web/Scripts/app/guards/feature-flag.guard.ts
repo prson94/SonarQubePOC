41690ea5d7ddbde281b281ea32078a8a8039ff22
Brian Quinlivan <bquinlivan@infogix.com>
2023-07-26 17:05:01 +0100

    GOV-28566 Add Assignment and Request menu links

---
+++ d360.web/Scripts/app/guards/feature-flag.guard.ts
@@ -0,0 +1,33 @@
+﻿import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+//import { CompanySettingEnum } from '../models/settings.model';
+//import { CompanySettingsService } from '../services/settings.service';
+import { LaunchDarklyService } from '@precisely/prism-ng/launch-darkly';
+import { FeatureFlags } from '../services/feature-flags.enum';
+import { SiteUrlHelpers } from '../static/site-url-helpers';
+
+@Injectable({ providedIn: 'root' })
+export class FeatureFlagGuard implements CanActivate {
+	constructor(
+		protected router: Router,
+		protected featureFlagService: LaunchDarklyService
+	) { }
+
+	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
+
+		if (state.url.startsWith('/monitor')) {
+			if (this.featureFlagService.variation<boolean>(FeatureFlags.AssignmentsFlag)) {
+				this.router.navigate([SiteUrlHelpers.SITE_URL_HOME_ROOT]);
+			}
+		}
+
+		if (state.url.startsWith('/assignments') || state.url.startsWith('/requests')) {
+			if (!this.featureFlagService.variation<boolean>(FeatureFlags.AssignmentsFlag)) {
+				this.router.navigate([SiteUrlHelpers.SITE_URL_HOME_ROOT]);
+			}
+		}
+
+		return true;
+    }
+}
