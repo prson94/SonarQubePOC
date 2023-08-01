b9a66f5e8d96fade6caf658b943d8eafe5515ec9
tduric@infogix.com <tduric@infogix.com>
2023-08-01 09:37:05 +0200

    GOV-21399 : add module and components

---
+++ d360.web/Scripts/app/components/assignments/user-assignments/user-assignments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'd3s-user-assignments',
+  templateUrl: './user-assignments.component.html',
+  styleUrls: ['./user-assignments.component.less']
+})
+export class UserAssignmentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ d360.web/Scripts/app/components/assignments/user-assignments/user-assignments.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserAssignmentsComponent } from './user-assignments.component';
+
+
+
+@NgModule({
+  declarations: [
+    UserAssignmentsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UserAssignmentsModule { }
