5a4bde26ce4968751ce32110eb19b3ac05c54ad0
mrigank.singh <mrigank.singh@winshuttle.com>
2023-08-02 10:38:03 +0530

    GOV-21397
    code formatting

--- d360.web/Scripts/app/components/assignments/complete-assignment/complete-assignment-form-fields/complete-assignment-form-fields.component.ts
+++ d360.web/Scripts/app/components/assignments/complete-assignment/complete-assignment-form-fields/complete-assignment-form-fields.component.ts
@@ -1,78 +1,60 @@
-import {
-
-	Component,
-	
-	ElementRef,
-	
-	EventEmitter,
-	
-	Input,
-	OnDestroy,
-	OnInit,
-	Output,
-} from "@angular/core";
-import { BaseComponent } from "../../../shared/base.component";
-import {
-	WorkflowFormField,
-	WorkflowFormFieldType,
-} from "../../../../models/workflow.model";
-import { ControlContainer, NgForm, Validators } from "@angular/forms";
-import { AssignmentService } from "../../assignment.service";
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { WorkflowFormField, WorkflowFormFieldType } from '../../../../models/workflow.model';
+import { ControlContainer, NgForm } from '@angular/forms';
+import { AssignmentService } from '../../assignment.service';
 
 @Component({
-	selector: "d3s-complete-assignment-form-fields",
-	templateUrl: "./complete-assignment-form-fields.component.html",
-	styleUrls: ['./complete-assignment-form-fields.component.less'],
-	viewProviders: [{ provide: ControlContainer, useExisting: NgForm }],
+	selector: 'd3s-complete-assignment-form-fields',
+	templateUrl: './complete-assignment-form-fields.component.html',
+	viewProviders: [{ provide: ControlContainer, useExisting: NgForm }]
 })
 export class CompleteAssignmentFormFieldsComponent implements OnInit {
 	@Input() fields: WorkflowFormField[] = [];
 	@Input() form: NgForm;
 	@Input() formElement;
-	// @Output() discardForm:boolean=false
 	@Output() discardForm: EventEmitter<boolean> = new EventEmitter<boolean>();
 
 	fieldType = WorkflowFormFieldType;
 
 	private isSetValidatior: boolean = false;
 
-	constructor(private assignmentService:AssignmentService) {}
-
-	ngOnInit(): void {
-		this.assignmentService.setFormValidators.subscribe(()=>{
-			this.setValidators()
-		})
-		this.handleFormInput()
+	constructor(private assignmentService: AssignmentService) {
 	}
 
-	handleFormInput(){
-		this.form.valueChanges
-		.pipe()
-		.subscribe(value => {
-		  for (var propName in value) {
-			  if (value[propName] === null || value[propName] === undefined || value[propName]==="")  {
-				delete value[propName];
-			  }
-			}
-			if(Object.keys(value).length){
-			  this.discardForm.emit(true);
-			}
-			else{
-			  this.discardForm.emit(false);
-			}
+	ngOnInit(): void {
+		this.assignmentService.setFormValidators.subscribe(() => {
+			this.setValidators();
 		});
+		this.handleFormInput();
+	}
+
+	handleFormInput() {
+		this.form.valueChanges
+			.pipe()
+			.subscribe(value => {
+				for (var propName in value) {
+					if (value[propName] === null || value[propName] === undefined || value[propName] === '') {
+						delete value[propName];
+					}
+				}
+				if (Object.keys(value).length) {
+					this.discardForm.emit(true);
+				} else {
+					this.discardForm.emit(false);
+				}
+			});
 	}
 
 	public setValidators() {
 		// debugger
 		// setTimeout(() => {
-			this.fields.forEach((x, i) => {
-				if (x.Required) {
-					this.form.form.controls[`input_${i}`].setErrors({
-						required: true,
-					});
-				}
-			});
+		this.fields.forEach((x, i) => {
+			if (x.Required) {
+				this.form.form.controls[`input_${i}`].setErrors({
+					required: true
+				});
+			}
+		});
 	}
 
 	public prepareValuesForSubmit() {
@@ -81,7 +63,7 @@ export class CompleteAssignmentFormFieldsComponent implements OnInit {
 				const name = this.form.form.controls[`inputName_${i}`].value;
 				const url = this.form.form.controls[`inputUrl_${i}`].value;
 				x.Value =
-					name.length + url.length === 0 ? "" : name + "|" + url;
+					name.length + url.length === 0 ? '' : name + '|' + url;
 			} else if (Array.isArray(x.Value)) {
 				x.Value = x.Value.join();
 			}
