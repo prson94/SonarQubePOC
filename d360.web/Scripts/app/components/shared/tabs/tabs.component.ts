a8fa2abf1af85176db5476f82ecb7aeb0e0e62b7
tduric@infogix.com <tduric@infogix.com>
2023-09-11 11:01:58 +0200

    GOV-29071 : fix tab selection on assignments

--- d360.web/Scripts/app/components/shared/tabs/tabs.component.ts
+++ d360.web/Scripts/app/components/shared/tabs/tabs.component.ts
@@ -23,78 +23,78 @@ import { SiteUrlHelpers } from '../../../static/site-url-helpers';
 /*global $localize*/
 
 @Component({
-    selector: 'd3s-tabs',
-    templateUrl: 'tabs.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    providers: [ArtifactService],
-    styleUrls: ['./tabs.component.less'],
-    host: { '(window:resize)': 'checkSize()' }
+	selector: 'd3s-tabs',
+	templateUrl: 'tabs.component.html',
+	changeDetection: ChangeDetectionStrategy.OnPush,
+	providers: [ArtifactService],
+	styleUrls: ['./tabs.component.less'],
+	host: { '(window:resize)': 'checkSize()' }
 })
 
 export class TabsComponent implements OnDestroy {
-    @Input() items: Tab[];
-    @Input() showOnlyMainTab = false;
-    @Input() hideMainTab = true;
-    @Input() area = { icon: 'fa-folder', title: '', tabTitle: '' };
-    @Input() emitSecondaryNav = false;
-    @Input() statistics: ObjectStatistics;
+	@Input() items: Tab[];
+	@Input() showOnlyMainTab = false;
+	@Input() hideMainTab = true;
+	@Input() area = { icon: 'fa-folder', title: '', tabTitle: '' };
+	@Input() emitSecondaryNav = false;
+	@Input() statistics: ObjectStatistics;
 	@Input() assignmentCount: number;
-    @Input() searchDetails: SearchDetail;
-    @Input() isScoringScreen = false;
+	@Input() searchDetails: SearchDetail;
+	@Input() isScoringScreen = false;
 
-    homeUrlChangeSub: Subscription;
-    homeUrl: string;
+	homeUrlChangeSub: Subscription;
+	homeUrl: string;
 
-    routerUrlChangeSub: Subscription;
-    private routerUrl = '';
+	routerUrlChangeSub: Subscription;
+	private routerUrl = '';
 
-    @ViewChildren('tabScroller') tabScroller: QueryList<ElementRef>;
-    showScrollButtons = false;
-    disableScrollLeft = false;
-    disableScrollRight = false;
+	@ViewChildren('tabScroller') tabScroller: QueryList<ElementRef>;
+	showScrollButtons = false;
+	disableScrollLeft = false;
+	disableScrollRight = false;
 
-    constructor(
-        private secondaryNavService: SecondaryNavService,
-        private ref: ChangeDetectorRef,
+	constructor(
+		private secondaryNavService: SecondaryNavService,
+		private ref: ChangeDetectorRef,
 		private router: Router,
 		private settingsService: CompanySettingsService,
 		private authenticationService: AuthenticationService) {
-    }
+	}
 
-    get visibleItems() {
-        if (!this.items) {
-            return [];
-        }
+	get visibleItems() {
+		if (!this.items) {
+			return [];
+		}
 
-        return this.items.filter((item) => {
-            if (item.isVisible) {
-                return item.isVisible();
-            }
+		return this.items.filter((item) => {
+			if (item.isVisible) {
+				return item.isVisible();
+			}
 
-            return true;
-        });
-    }
+			return true;
+		});
+	}
 
-    ngOnInit() {
-        this.routerUrl = this.router.url;
-        this.routerUrlChangeSub = this.router.events.subscribe(() => {
-            this.routerUrl = this.router.url;
-            this.ref.markForCheck();
-        });
+	ngOnInit() {
+		this.routerUrl = this.router.url;
+		this.routerUrlChangeSub = this.router.events.subscribe(() => {
+			this.routerUrl = this.router.url;
+			this.ref.markForCheck();
+		});
 
-        this.homeUrlChangeSub = this.secondaryNavService.homeUrlChange$.subscribe(
-            (item) => {
+		this.homeUrlChangeSub = this.secondaryNavService.homeUrlChange$.subscribe(
+			(item) => {
 				this.homeUrl = item;
-            }
-        );
-    }
+			}
+		);
+	}
 
-    ngAfterViewInit() {
-        this.checkSize();
-    }
+	ngAfterViewInit() {
+		this.checkSize();
+	}
 
 	isTabVisible = (tab: Tab) => {
-        let visible = (tab.isVisible != null ? tab.isVisible() : true)
+		let visible = (tab.isVisible != null ? tab.isVisible() : true)
 			&& this.filterScoringTabHasNoValue(tab);
 
 		if (tab.title === $localize`Comments`) {
@@ -102,143 +102,148 @@ export class TabsComponent implements OnDestroy {
 		}
 
 		return visible;
-    }
+	}
 
-    filterScoringTabHasNoValue = (tab: Tab) => {
+	filterScoringTabHasNoValue = (tab: Tab) => {
 		if (tab.title === $localize`Scoring`) {
-            return Boolean(this.searchDetails?.Scores.length);
-        }
-        return true;
-    };
+			return Boolean(this.searchDetails?.Scores.length);
+		}
+		return true;
+	};
 
-    checkSize() {
-        if (this.tabScroller && this.tabScroller.length > 0) {
-            const maxWidth = this.getElementRightPosition(this.tabScroller.first.nativeElement.parentElement);
-            const lastTab = this.getElementRightPosition(this.tabScroller.first.nativeElement.lastElementChild);
-            this.showScrollButtons = lastTab > maxWidth;
-        }
-        this.checkScrollPos();
-    }
+	checkSize() {
+		if (this.tabScroller && this.tabScroller.length > 0) {
+			const maxWidth = this.getElementRightPosition(this.tabScroller.first.nativeElement.parentElement);
+			const lastTab = this.getElementRightPosition(this.tabScroller.first.nativeElement.lastElementChild);
+			this.showScrollButtons = lastTab > maxWidth;
+		}
+		this.checkScrollPos();
+	}
 
-    checkScrollPos() {
-        if (this.tabScroller && this.tabScroller.length > 0) {
-            const currentPosition = this.tabScroller.first.nativeElement.scrollLeft;
-            this.disableScrollLeft = currentPosition === 0;
+	checkScrollPos() {
+		if (this.tabScroller && this.tabScroller.length > 0) {
+			const currentPosition = this.tabScroller.first.nativeElement.scrollLeft;
+			this.disableScrollLeft = currentPosition === 0;
 
-            const maxWidth = this.getElementRightPosition(this.tabScroller.first.nativeElement.parentElement);
-            const lastTab = this.getElementRightPosition(this.tabScroller.first.nativeElement.lastElementChild);
-            this.disableScrollRight = lastTab <= maxWidth;
+			const maxWidth = this.getElementRightPosition(this.tabScroller.first.nativeElement.parentElement);
+			const lastTab = this.getElementRightPosition(this.tabScroller.first.nativeElement.lastElementChild);
+			this.disableScrollRight = lastTab <= maxWidth;
 
-            this.ref.markForCheck();
-        }
-    }
+			this.ref.markForCheck();
+		}
+	}
 
-    getElementRightPosition(element) {
-        if (element && element.getBoundingClientRect) {
-            return element.getBoundingClientRect().right;
-        }
-        return NaN;
-    }
+	getElementRightPosition(element) {
+		if (element && element.getBoundingClientRect) {
+			return element.getBoundingClientRect().right;
+		}
+		return NaN;
+	}
 
-    scroll(direction: string) {
-        let scrollAmount = 0;
-        const scrollDistance = 300;
+	scroll(direction: string) {
+		let scrollAmount = 0;
+		const scrollDistance = 300;
 
-        const move = () => {
-            if (direction === 'L') {
-                this.tabScroller.first.nativeElement.scrollLeft -= 10;
-            } else {
-                this.tabScroller.first.nativeElement.scrollLeft += 10;
-            }
-            scrollAmount += 10;
-            if (scrollAmount >= scrollDistance) {
-                this.checkScrollPos();
-                window.clearInterval(id);
-            }
-            this.checkScrollPos();
-        };
+		const move = () => {
+			if (direction === 'L') {
+				this.tabScroller.first.nativeElement.scrollLeft -= 10;
+			} else {
+				this.tabScroller.first.nativeElement.scrollLeft += 10;
+			}
+			scrollAmount += 10;
+			if (scrollAmount >= scrollDistance) {
+				this.checkScrollPos();
+				window.clearInterval(id);
+			}
+			this.checkScrollPos();
+		};
 
-        const id = window.setInterval(move, 5);
-    }
+		const id = window.setInterval(move, 5);
+	}
 
-    getTitle(item: Tab) {
-        if (this.statistics && this.statistics.IssueCount > 0 && item.title === 'Actions') {
-            const plurality = this.statistics.IssueCount === 1 ? ' is' : 's are';
-            return this.statistics.IssueCount + " outstanding action" + plurality + " assigned to you";
-        } else {
-            return "";
-        }
-    }
+	getTitle(item: Tab) {
+		if (this.statistics && this.statistics.IssueCount > 0 && item.title === 'Actions') {
+			const plurality = this.statistics.IssueCount === 1 ? ' is' : 's are';
+			return this.statistics.IssueCount + " outstanding action" + plurality + " assigned to you";
+		} else {
+			return "";
+		}
+	}
 
-    isTabActive(tab: Tab) {
-        if (this.secondaryNavService.activeTabTitle === tab.title) {
-            return true;
-        }
+	//removes query parameters from url
+	getPathFromUrl(url) {
+		return url.split(/[?#]/)[0];
+	}
 
-        return this.routerUrl === tab.url
-            || (tab.subTabsUrl ?? []).some((subTabUrl) => this.routerUrl.startsWith(subTabUrl));
-    }
+	isTabActive(tab: Tab) {
+		if (this.secondaryNavService.activeTabTitle === tab.title) {
+			return true;
+		}
 
-    trackById(index, item) {
-        return item.tag;
-    }
+		return this.getPathFromUrl(this.routerUrl) === tab.url
+			|| (tab.subTabsUrl ?? []).some((subTabUrl) => this.routerUrl.startsWith(subTabUrl));
+	}
 
-    itemClicked(item: Tab) {
-        if (this.isTabActive(item)) {
-            return;
-        }
+	trackById(index, item) {
+		return item.tag;
+	}
 
-        if (this.AllClosed()) {
-            if (this.emitSecondaryNav) {
-                this.secondaryNavService.setLocalHomeUrl(this.router.url);
-            }
+	itemClicked(item: Tab) {
+		if (this.isTabActive(item)) {
+			return;
+		}
 
-            this.homeUrl = this.router.url;
-        }
-        this.closeAll();
-        if (item.title === "homeClick") {
-            this.secondaryNavService.clearLocalActiveItem();
+		if (this.AllClosed()) {
+			if (this.emitSecondaryNav) {
+				this.secondaryNavService.setLocalHomeUrl(this.router.url);
+			}
 
-            let home = this.homeUrl ? this.homeUrl : this.secondaryNavService.getLocalHomeUrl();
-            if (!home) {
-                home = `/artifact/${this.secondaryNavService.artifactTypeId}`;
-                this.secondaryNavService.activeTabTitle = null;
-                this.secondaryNavService.artifactTypeId = null;
+			this.homeUrl = this.router.url;
+		}
+		this.closeAll();
+		if (item.title === "homeClick") {
+			this.secondaryNavService.clearLocalActiveItem();
+
+			let home = this.homeUrl ? this.homeUrl : this.secondaryNavService.getLocalHomeUrl();
+			if (!home) {
+				home = `/artifact/${this.secondaryNavService.artifactTypeId}`;
+				this.secondaryNavService.activeTabTitle = null;
+				this.secondaryNavService.artifactTypeId = null;
 			}
 			this.router.navigateByUrl(SiteUrlHelpers.federateUrl(home));
-            return;
-        }
+			return;
+		}
 
 		if (item.url) {
 			this.router.navigateByUrl(SiteUrlHelpers.federateUrl(item.url));
 		}
 
-        if (this.emitSecondaryNav) {
-            this.secondaryNavService.itemClicked(item as SecondaryNavItem);
-        }
+		if (this.emitSecondaryNav) {
+			this.secondaryNavService.itemClicked(item as SecondaryNavItem);
+		}
 
-        this.AllClosed();
-    }
+		this.AllClosed();
+	}
 
-    AllClosed() {
-        const count = this.items.filter((x) => this.isTabActive(x)).length;
-        if (count === 0) { this.secondaryNavService.setLocalActiveItem(undefined); }
-        return count === 0;
-    }
+	AllClosed() {
+		const count = this.items.filter((x) => this.isTabActive(x)).length;
+		if (count === 0) { this.secondaryNavService.setLocalActiveItem(undefined); }
+		return count === 0;
+	}
 
-    closeAll() {
-        for (const ritem of this.items) {
-            if (this.isTabActive(ritem)) {
-                if (this.emitSecondaryNav) {
-                    this.secondaryNavService.itemClicked(ritem as SecondaryNavItem);
-                }
-            }
-        }
-    }
+	closeAll() {
+		for (const ritem of this.items) {
+			if (this.isTabActive(ritem)) {
+				if (this.emitSecondaryNav) {
+					this.secondaryNavService.itemClicked(ritem as SecondaryNavItem);
+				}
+			}
+		}
+	}
 
-    ngOnDestroy() {
-        this.routerUrlChangeSub?.unsubscribe();
-        this.homeUrlChangeSub?.unsubscribe();
+	ngOnDestroy() {
+		this.routerUrlChangeSub?.unsubscribe();
+		this.homeUrlChangeSub?.unsubscribe();
 	}
 
 	get isMainTabVisible() {
