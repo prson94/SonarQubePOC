6ad468ee3c5d2aaa125106d2c8ba9a3ec0a156b7
vikasprashar99 <--global>
2023-09-12 03:16:12 -0400

    added feature flag

--- d360.web/Scripts/app/components/reference-v2/tabs/shared/reference-tabs.component.ts
+++ d360.web/Scripts/app/components/reference-v2/tabs/shared/reference-tabs.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit, OnDestroy } from "@angular/core";
+import { LaunchDarklyService } from "@precisely/prism-ng/launch-darkly";
 import { Subscription } from "rxjs";
 import { AssetService } from '../../../../services/asset.service';
 import { AuthenticationService } from '../../../../services/authentication.service';
@@ -26,7 +27,8 @@ export class ReferenceItemTypeTabsComponent implements OnInit, OnDestroy {
 		private cdRef: ChangeDetectorRef,
 		private assetService: AssetService,
 		private authenticationService: AuthenticationService,
-		private permissionsService: PermissionsService
+		private permissionsService: PermissionsService,
+		private launchDarklyService:LaunchDarklyService
 	) { }
 
 	get tabs(): Tab[] {
@@ -61,7 +63,7 @@ export class ReferenceItemTypeTabsComponent implements OnInit, OnDestroy {
 				tag: "relationship",
 				isVisible: () => this.isAdmin || this.typePermission.ReadRelationships,
 			},
-			FeatureFlags.AssignmentsFlag?this.assignmentTab(baseUrl):this.workflowTab(baseUrl),
+			this.launchDarklyService?.variation<boolean>(FeatureFlags.AssignmentsFlag) ?this.assignmentTab(baseUrl):this.workflowTab(baseUrl),
 			{
 				url: `${baseUrl}/log`,
 				title: $localize`Change Log`,
