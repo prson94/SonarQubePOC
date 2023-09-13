625852d05864206e3758ab3239b610a7725fd94a
vikasprashar99 <--global>
2023-09-13 04:47:25 -0400

    workflow feature guard

--- d360.web/Scripts/app/guards/feature-flag.guard.ts
+++ d360.web/Scripts/app/guards/feature-flag.guard.ts
@@ -28,6 +28,12 @@ export class FeatureFlagGuard implements CanActivate {
 			}
 		}
 
+		if (state.url.endsWith('/workflow')) {
+			if (this.featureFlagService.variation<boolean>(FeatureFlags.AssignmentsFlag)) {
+				this.router.navigate([SiteUrlHelpers.SITE_URL_HOME_ROOT]);
+			}
+		}
+
 		if (state.url.startsWith('/reference')) {
 			if (!this.featureFlagService.variation<boolean>(FeatureFlags.ReferenceListV2Flag)) {
 				this.router.navigate([SiteUrlHelpers.SITE_URL_HOME_ROOT]);
