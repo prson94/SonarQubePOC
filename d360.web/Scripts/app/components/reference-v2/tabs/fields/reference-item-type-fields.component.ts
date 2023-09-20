7f78fa1992bf24e4311c918e5321a5ce1c4780c6
Thomas Klok <thomas.klok@precisely.com>
2023-09-20 10:21:38 -0400

    GOV-29103 Reference Item add field not showing Reference type name

--- d360.web/Scripts/app/components/reference-v2/tabs/fields/reference-item-type-fields.component.ts
+++ d360.web/Scripts/app/components/reference-v2/tabs/fields/reference-item-type-fields.component.ts
@@ -1,24 +1,38 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { ArtifactType } from "../../../../models/artifact-type.model";
+import { Subscription } from "rxjs";
+import { AssetTypeApiModel } from "../../../../models/asset.model";
+import { AssetTypeService } from "../../../../services/asset-type.service";
 
 @Component({
 	selector: "d3s-reference-item-type-fields",
 	templateUrl: './reference-item-type-fields.component.html'
 })
-export class ReferenceItemTypeFieldsComponent {
+export class ReferenceItemTypeFieldsComponent implements OnInit, OnDestroy {
 	uid: string;
-	assetType: ArtifactType;
+	assetType: AssetTypeApiModel;
 	objectName: string = null;
+	subscription: Subscription;
 
 	constructor(
+		protected assetTypeService: AssetTypeService,
 		private route: ActivatedRoute) {
 	}
 
 	ngOnInit() {
 		this.route.params.subscribe((params) => {
 			this.uid = params["uid"];
+			this.load();
 		});
 	}
 
+	private load() {
+		this.subscription = this.assetTypeService.GetAssetTypeByUid(this.uid).subscribe((res) => {
+			this.assetType = res;
+		});
+	}
+
+	ngOnDestroy() {
+		this.subscription?.unsubscribe();
+	}
 }
