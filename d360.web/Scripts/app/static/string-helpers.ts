39d22a255893b648bdddc3d56e869bb26e602e76
Saurabh <130632998+sbgix@users.noreply.github.com>
2023-10-26 11:42:49 +0530

    GOV-29629 - fixed codacy issue

--- d360.web/Scripts/app/static/string-helpers.ts
+++ d360.web/Scripts/app/static/string-helpers.ts
@@ -80,9 +80,9 @@ export class StringHelpers {
 	}
 
 	static newGuid(): string {
-		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-			var r = Math.random() * 16 | 0,
-				v = c === 'x' ? r : (r & 0x3 | 0x8);
+		return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c: string) {
+			const r: number = Math.random() * 16 | 0,
+				v: number = c === 'x' ? r : (r & 0x3 | 0x8);
 			return v.toString(16);
 		});
 	}
