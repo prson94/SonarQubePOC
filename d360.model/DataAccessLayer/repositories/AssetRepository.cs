bef65f4c5ea23775e933ab95c2cf5c5826165d01
tduric@infogix.com <tduric@infogix.com>
2024-01-16 10:28:53 +0100

    GOV-30863 : fix performance issue on GET Assets

--- d360.model/DataAccessLayer/repositories/AssetRepository.cs
+++ d360.model/DataAccessLayer/repositories/AssetRepository.cs
@@ -1834,8 +1834,6 @@ WHERE NR.Object = A.Object and NR.ObjectId = A.ObjectId) as SynonymAllocationStr
 			var sql = $@"
 				{(useTempTableForResults ? "drop table if exists #results;" : "")}
 
-				{fieldJoins.JoinFieldTempTable}
-
 				select 
 					{(useTempTableForResults ? $"row_number() over (order by TempA.ID) as _rowid," : "")}
 					A.ID as AssetId,
@@ -1865,7 +1863,7 @@ WHERE NR.Object = A.Object and NR.ObjectId = A.ObjectId) as SynonymAllocationStr
 				left join AssetPath Node on Node.ID = a.ID
 				left join Asset CA on CA.ObjectID  = A.CreatedBy and CA.Object = 'Resource'
 				left join Asset UA on UA.ObjectID  = A.UpdatedBy and UA.Object = 'Resource'
-				{fieldJoins.SQLJoinStatementWithTempTable}
+				{fieldJoins.SQLJoinStatement}
 				{(isForTreeGrid ? "outer apply dbo.GetAssetLevelById(A.Id)LVL" : "")}
 				{(includeColor ? "outer apply dbo.GetAssetColorJsonByColor(A.Color) ACJ" : "")}
 				{(includePermissionDetails ? permissionDetailSQL : "")}
