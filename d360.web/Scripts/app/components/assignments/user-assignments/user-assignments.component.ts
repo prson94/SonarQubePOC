7e9bb649575eefd088ddc5f9c288a0bc4f01727f
Deepanki Jain <--global>
2024-02-08 12:28:49 +0530

    modal opening fix

--- d360.web/Scripts/app/components/assignments/user-assignments/user-assignments.component.ts
+++ d360.web/Scripts/app/components/assignments/user-assignments/user-assignments.component.ts
@@ -136,15 +136,11 @@ export class UserAssignmentsComponent extends BaseComponent implements OnInit, O
 					if (this.urlWorkflowStepUid) {
 						this.workflowService.getWorkflowStateForUser(this.urlWorkflowStepUid)
 							.subscribe((res) => {
-								this.handleWorkflowItemLoad(res, forcedRefresh);
-								this.isLoading = false;
-								this.changeDetectorRef.markForCheck();
+								this.handleWorkflowItemLoad(res, forcedRefresh);			
 							});
 					}
 					else if (this.urlWorkflowTypeUid) {
-						this.handleWorkflowItemLoad({ exists: false, hasAccess: true, isCompleted: true, workflowItemUid: null, workflowName: null, assignmentCount: 0, isAssignee: true }, forcedRefresh); //parameter isAssignee to be updated after backend implementation
-						this.isLoading = false;
-						this.changeDetectorRef.markForCheck();
+						this.handleWorkflowItemLoad({ exists: false, hasAccess: true, isCompleted: true, workflowItemUid: null, workflowName: null, assignmentCount: 0, isAssignee: true }, forcedRefresh);
 					}
 					else {
 						this.isLoading = false;
@@ -155,15 +151,15 @@ export class UserAssignmentsComponent extends BaseComponent implements OnInit, O
 
 	private handleWorkflowItemLoad(res: WorkflowStateForUser, forcedRefresh: boolean) {
 		// check if there is exiting step in assignments
-		// if (this.urlWorkflowItemUid) {
-		// 	this.workflowService.getAssignmentItemSteps(this.urlWorkflowItemUid)
-		// 	.subscribe((response: AssignmentItemStep[]): void => {
-		// 		const step = response.find(step=> step.Uid === this.urlWorkflowStepUid);
-		// 		if(step?.ActivityType !== 'Form'){
-		// 			this.redirectToDetails = true;
-		// 		} else {
-		// 			this.redirectToDetails = false;
-		// 		}
+		if (this.urlWorkflowItemUid) {
+			this.workflowService.getAssignmentItemSteps(this.urlWorkflowItemUid)
+			.subscribe((response: AssignmentItemStep[]): void => {
+				const step = response.find(step=> step.Uid === this.urlWorkflowStepUid);
+				if(step?.ActivityType !== 'Form'){
+					this.redirectToDetails = true;
+				} else {
+					this.redirectToDetails = false;
+				}
 				let assignmentItem = this.assignments.find((x) => x.Version === this.urlWorkflowVersion && x.AssociatedItems.some((ai) => ai.ItemStepUid.toLowerCase() === this.urlWorkflowStepUid.toLowerCase()));
 				if (assignmentItem && !this.redirectToDetails) {
 					this.onItemClick(null, assignmentItem);
@@ -178,8 +174,10 @@ export class UserAssignmentsComponent extends BaseComponent implements OnInit, O
 						this.handleNoAssignments(res);
 					}
 				}
-		// 	});
-		// }	
+				this.isLoading = false;
+				this.changeDetectorRef.markForCheck();
+			});
+		}	
 		
 	}
 
@@ -207,7 +205,7 @@ export class UserAssignmentsComponent extends BaseComponent implements OnInit, O
 			this.modalVisible = true;
 			this.showAssignmentDetailsLink = false;
 		}
-		else if (!res.isAssignee) { //parameter to be updated after backend implementation
+		else if (!res.isAssignee && !this.redirectToDetails) {
 			this.errorModalTitle = $localize`Not Assigned to You`;
 			this.errorModalMessage = $localize`You are not an assignee for this form, but you can view the Assignment's details.`;
 			this.modalVisible = true; 
