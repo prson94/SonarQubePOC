4a7101ced3cdc1bf919b1db2df41745664a2fd7d
Deepanki Jain <--global>
2024-03-22 10:50:46 +0530

    GOV-21043 UI: In Workflow Creation Dropdown Field Values are not updated correctly during Field Change when the user selects one field in edit option and cancels it without updating and again tries to edit the same

--- d360.web/Scripts/app/components/shared/diagram/workflow/workflow-step-field-change.component.ts
+++ d360.web/Scripts/app/components/shared/diagram/workflow/workflow-step-field-change.component.ts
@@ -422,7 +422,6 @@ export class WorkflowStepFieldChangeComponent extends BaseComponent implements O
             const fieldTypeIndex = this.fields.findIndex((f) => f.ID.toString() === field['@FieldId'].toString());
             if (fieldTypeIndex > -1) {
                 this.usedFields.push(this.fields[fieldTypeIndex]);
-                this.fields.splice(fieldTypeIndex, 1);
             }
         }
         this.selectedField = null;
