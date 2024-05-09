436047a6332ae263d2e44d1bdcab121bd2eccbf0
Thomas Klok <thomas.klok@precisely.com>
2024-05-09 09:28:14 -0400

    GOV-32849 Comments notification email broken links

--- igx.jobs.apiexecutionprocessor/jobs/NotificationHandler.cs
+++ igx.jobs.apiexecutionprocessor/jobs/NotificationHandler.cs
@@ -57,7 +57,7 @@ namespace igx.jobs.apiexecutionprocessor
 						"select * from Comment where ID = @CommentId; " +
 						resourceSql +
 						"select GR.FirstName + ' ' + GR.LastName as ResourceName from reporting.Global_Resource gr inner join Comment c on c.CreatedBy = gr.ResourceID and c.ID = @CommentId;" +
-						"select * from AssetDetail d inner join Comment c on c.ID = @CommentId and c.AssetID = d.ID;",
+						"select d.* from AssetDetail d inner join Comment c on c.ID = @CommentId and c.AssetID = d.ID;",
 						new { CommentId = info.Payload }
 					);
 					var comment = await query.ReadFirstAsync<Comment>();
