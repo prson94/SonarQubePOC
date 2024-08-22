ff676d54a231ab3753a19cc637cad81a35e47477
Thomas Klok <tklok@infogix.com>
2024-08-22 13:24:54 -0400

    GOV-34200 Add ItemStepTransitionState table

---
+++ d360.core/entities/Workflow/WorkflowItemStepTransitionState.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using d360.core.enums.Workflow;
+
+namespace d360.core.entities.Workflow
+{
+	[DataContract, Table("ItemStepTransitionState", Schema = "workflow")]
+	public class WorkflowItemStepTransitionState : BaseObject
+	{
+		[DataMember, Key, Column(Order = 1)]
+		public long FromItemStepID { get; set; }
+
+		[DataMember, Key, Column(Order = 2)]
+		public long VersionStepTransitionID { get; set; }
+
+		[DataMember]
+		public bool Passed { get; set; }
+
+		[DataMember]
+		public StepState? State { get; set; }
+
+
+		[IgnoreDataMember, ForeignKey("FromItemStepID")]
+		public virtual WorkflowItemStep FromItemStep { get; set; }
+
+		[IgnoreDataMember, ForeignKey("VersionStepTransitionID")]
+		public virtual WorkflowVersionStepTransition VersionStepTransition { get; set; }
+	}
+}
