828722231a316954b12c5cec87348d1f7ce20159
Thomas Klok <tklok@infogix.com>
2024-11-21 17:06:37 -0500

    Increase max_old_space_size for NPM build

--- d360.web/replace.timestamp.js
+++ d360.web/replace.timestamp.js
@@ -1,4 +1,6 @@
 ﻿var replace = require('replace-in-file');
+const os = require('os');
+const v8 = require('node:v8');
 const options = {
     files: './Scripts/environments/environment.prod.ts',
     from: /{BUILD_TIMESTAMP}/g,
@@ -6,8 +8,21 @@ const options = {
     allowEmptyPaths: false,
 };
 
+function formatMem(size) {
+	const gbNow = size / 1024 / 1024 / 1024;
+	const gbRounded = Math.round(gbNow * 100) / 100;
+	return gbRounded;
+}
+
 /* eslint-disable no-console */
+function logMemoryInfo() {
+	console.log('Total memory: ' + formatMem(os.totalmem()) + ' GB');
+	console.log('Free memory: ' + formatMem(os.freemem()) + ' GB');
+	console.log('Heap Statistics: ' + v8.getHeapStatistics().heap_size_limit / (1024 * 1024));
+}
+
 try {
+	logMemoryInfo();
     let changedFiles = replace.sync(options);
     console.log('Build version set: ' + new Date().toLocaleString());
 }
