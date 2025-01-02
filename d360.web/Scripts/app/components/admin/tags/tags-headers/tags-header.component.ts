faffbd452f1f9117da62172994abdb8e9b9e2029
Chetan <chetan.gupta@precisely.com>
2025-01-02 19:55:59 +0530

    GOV-28953 | Show error modal in case any error, close TagTypes dropdown when clicked outside

--- d360.web/Scripts/app/components/admin/tags/tags-headers/tags-header.component.ts
+++ d360.web/Scripts/app/components/admin/tags/tags-headers/tags-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, OnInit, Output, Renderer2, ViewChild } from '@angular/core';
 import { Tab } from '../../../shared/tabs/tabs.models';
 import { TagTypesViewModel } from '../tag-types/tag-types.model';
 import { LaunchDarklyService } from '@precisely/prism-ng/launch-darkly';
@@ -11,23 +11,30 @@ import { FeatureFlags } from '../../../../services/feature-flags.enum';
 	templateUrl: './tags-header.component.html',
 	styleUrls: ['./tags-header.component.less'],
 })
-export class TagsHeaderComponent implements OnInit {
+export class TagsHeaderComponent implements OnInit, AfterViewInit {
 
 	@Input() flowContext: string = 'Tags';
 	@Output() onTagTypeSelected = new EventEmitter<string>();
+	@ViewChild('tabsContainer', { static: false }) tabsContainer: ElementRef;
 
 	icon: string;
 	iconPath: string;
 	header: string;
 	showTagTypes = false;
 	isTagTypesOpen = false;
+	preventBodyClick = false;
 	tabs: Tab[] = [];
 	btnTagsText = 'Tag Types';
 	get isTagTypesFeatureEnabled(): boolean {
         return this.featureFlagService.variation<boolean>(FeatureFlags.TagTypesEnabled);
     }
 
-	constructor(private featureFlagService: LaunchDarklyService) {}
+	constructor(
+		private featureFlagService: LaunchDarklyService,
+		private changeDetectorRef: ChangeDetectorRef,
+		private renderer: Renderer2
+
+	) {	}
 
 	ngOnInit(): void {
 		this.header = $localize`Tags`;
@@ -39,6 +46,17 @@ export class TagsHeaderComponent implements OnInit {
 			},
 		]
 
+	}
+
+	ngAfterViewInit(): void{
+		this.renderer.listen('window', 'click',(e:Event)=>{
+			e.stopPropagation();
+			if(e.target != this.tabsContainer.nativeElement && !this.tabsContainer.nativeElement.contains(e.target)){
+			  this.showTagTypes = false;
+			  this.isTagTypesOpen = false;
+			  this.changeDetectorRef.detectChanges();
+			}
+		});
 	}
 
 	toggleTagTypesPanel() {
