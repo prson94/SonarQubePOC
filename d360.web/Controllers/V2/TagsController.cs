e713a0e3041be8ba2eda97651e10929c2f22dd9c
Chetan <chetan.gupta@precisely.com>
2025-02-24 18:58:21 +0530

    GOV-36934 | Exclude 'General' tag type from the list

--- d360.web/Controllers/V2/TagsController.cs
+++ d360.web/Controllers/V2/TagsController.cs
@@ -687,7 +687,9 @@ namespace d360.web.Controllers.V2
 		public async Task<IHttpActionResult> GetTagTypes()
 		{
 				var models = await Catalog.ReadTagTypesAsync();
-				return Ok(models);
+				//Exclude 'General' TagType
+				var result = models.Where((m) => !string.Equals(m.Value, "General", StringComparison.OrdinalIgnoreCase));
+				return Ok(result);
 		}
 
 		/// <summary>
