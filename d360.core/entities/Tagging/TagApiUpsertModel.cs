950afac6ad9e8230d900eae736f87492b186b719
tklok <tklok@infogix.com>
2025-03-12 14:13:56 -0400

    Revert "GOV-37192 | Added validation to Tag & TagType value"

--- d360.core/entities/Tagging/TagApiUpsertModel.cs
+++ d360.core/entities/Tagging/TagApiUpsertModel.cs
@@ -2,27 +2,9 @@
 
 namespace d360.core.entities
 {
-	public class TagApiUpsertModel
-	{
-		private string _value;
-
-		public string Value
-		{
-			get => _value;
-			set
-			{
-				string trimmedValue = value?.Trim();
-				if (trimmedValue != null && trimmedValue.IsValidForTag())
-				{
-					_value = trimmedValue;
-				}
-				else
-				{
-					throw new ArgumentException($"Provided Tag value '{value}' is not valid");
-				}
-			}
-		}
-
+    public class TagApiUpsertModel
+    {
+        public string Value { get; set; }
 		public Guid? TagTypeUid { get; set; }
-	}
+    }
 }
