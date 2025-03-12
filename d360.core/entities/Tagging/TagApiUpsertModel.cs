8ee98af0ddfcc074ff17fbbc0177fe69ef96f13a
Chetan <chetan.gupta@precisely.com>
2025-03-12 21:20:15 +0530

    GOV-37192 | Added validation to Tag & TagType value

--- d360.core/entities/Tagging/TagApiUpsertModel.cs
+++ d360.core/entities/Tagging/TagApiUpsertModel.cs
@@ -2,9 +2,27 @@
 
 namespace d360.core.entities
 {
-    public class TagApiUpsertModel
-    {
-        public string Value { get; set; }
+	public class TagApiUpsertModel
+	{
+		private string _value;
+
+		public string Value
+		{
+			get => _value;
+			set
+			{
+				string trimmedValue = value?.Trim();
+				if (trimmedValue != null && trimmedValue.IsValidForTag())
+				{
+					_value = trimmedValue;
+				}
+				else
+				{
+					throw new ArgumentException($"Provided Tag value '{value}' is not valid");
+				}
+			}
+		}
+
 		public Guid? TagTypeUid { get; set; }
-    }
+	}
 }
--- d360.core/entities/Tagging/TagTypeApiUpsertModel.cs
+++ d360.core/entities/Tagging/TagTypeApiUpsertModel.cs
@@ -4,6 +4,23 @@ namespace d360.core.entities
 {
 	public class TagTypeApiUpsertModel
 	{
-		public string Value { get; set; }
+		private string _value;
+
+		public string Value
+		{
+			get => _value;
+			set
+			{
+				string trimmedValue = value?.Trim();
+				if (trimmedValue != null && trimmedValue.IsValidForTag())
+				{
+					_value = trimmedValue;
+				}
+				else
+				{
+					throw new ArgumentException($"Provided TagType value '{value}' is not valid");
+				}
+			}
+		}
 	}
 }
