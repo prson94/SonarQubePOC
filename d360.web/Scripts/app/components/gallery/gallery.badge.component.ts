b1b15b15060bfe0f2473c61ba7a0459f83e88e02
Thomas Klok <tklok@infogix.com>
2025-03-13 10:29:52 -0400

    GOV-37260 Score Rounding in edge cases

--- d360.web/Scripts/app/components/gallery/gallery.badge.component.ts
+++ d360.web/Scripts/app/components/gallery/gallery.badge.component.ts
@@ -1,4 +1,5 @@
 ﻿import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { AssetScore } from '../../models/search-result.model';
 
 
 @Component({
@@ -21,7 +22,38 @@ export class GalleryBadgeComponent implements OnInit {
     properties: Array<any>;
     sampleUsage: string = '<ig-badge [text]="\'Im a badge!\'"></ig-badge>';
     
-    protected clicks: string[] = [];
+	protected clicks: string[] = [];
+
+	scoreType: boolean = true;
+	useMiniBadge: boolean = false;
+	_precision: number = 1;
+	get precision(): number {
+		return this._precision;
+	}
+	set precision(val: number|string) {
+		this._precision = (typeof val == 'string') ? parseInt(val) : val;
+	}
+	_scoreValue: number = 0.123456789;
+	get scoreValue(): number {
+		return this._scoreValue;
+	}
+	set scoreValue(val: number | string) {
+		this._scoreValue = (typeof val == 'string') ? parseFloat(val) : val;
+	}
+
+	public get score(): AssetScore {
+		return {
+			AssetUid: "0000",
+			EffectiveDate: "2025-01-01T00:00:00.000Z",
+			EndDate: null,
+			Value: this.scoreValue,
+			ScoreType: this.scoreType ? "Data Quality" : "Governance",
+			ShortName: this.scoreType ? "DQ": "GV",
+			RunDate: "2025-01-01T00:00:00.000Z",
+			LowerThreshold: 50,
+			UpperThreshold: 90,
+		};
+	}
 
     ngOnInit(): void {
         this.properties = [];
