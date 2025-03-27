62c7c86f53ebbad3e8a9722d726583f0b01d6c7d
Puneet Gupta <puneet.gupta@Precisely.com>
2025-03-27 15:04:48 +0530

    GOV-37500-When recreate delete user then password not updated. Not consider new password and using old password.(2025-Sprint-0325)

--- repositories.azure/Community.cs
+++ repositories.azure/Community.cs
@@ -335,6 +335,20 @@ from    #Users U
 		inner join [Resource] R on R.Username = U.Username and U.ResourceID is null
 where  coalesce(U.Success,1) = 1;
 
+if exists(select 1 
+		 from [Resource] r 
+		 inner join CompanyResource cr on r.ID = cr.ResourceID and cr.CompanyID = @companyId 
+		 inner join #Users S on s.ResourceID = r.ID
+		 where coalesce(cr.state,1) = 3 and S.State = 1 and coalesce(s.Password,'') != '')
+begin
+		update r
+		set r.Password = s.Password
+		from [Resource] r 
+		inner join CompanyResource cr on r.ID = cr.ResourceID and cr.CompanyID = @companyId 
+		inner join #Users S on s.ResourceID = r.ID
+		where coalesce(cr.state,1) = 3 and S.State = 1 and coalesce(s.Password,'') != '';
+end
+
 if exists(select 1 from #Users T group by T.ResourceID having count(1)>1)
 begin
 	update	T
