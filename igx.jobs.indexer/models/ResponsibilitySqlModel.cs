80c591d3732ccf7ca011132eebf7abb9756a81a3
michael.pappas <michael.pappas@precisely.com>
2025-03-28 15:49:38 -0400

    leaving stub for indexer job

--- igx.jobs.indexer/models/ResponsibilitySqlModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace igx.jobs.indexer.models
-{
-	internal class ResponsibilitySqlModel : IPagedQuerySqlModel
-	{
-		public long AssetID { get; set; }
-		public string SecurityAsset { get; set; }
-		public int SecurityAssetID { get; set; }
-	}
-}
