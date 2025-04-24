a872045139ea97919834ec9fe2f7bc9467713c44
Thomas Klok <tklok@infogix.com>
2025-04-24 16:54:48 -0400

    GOV-37897 Add "Listable" option for ReferenceList

--- d360.web/Scripts/app/components/shared/fielddefinition/field-type-modal-form/field-type-modal-form.component.ts
+++ d360.web/Scripts/app/components/shared/fielddefinition/field-type-modal-form/field-type-modal-form.component.ts
@@ -1306,7 +1306,7 @@ export class ConfigurationFieldTypeModalFormComponent implements OnChanges, OnIn
 			return true;
 		}
 
-		const allowedTypes = ['Counter', 'Date', 'DateTime', 'Decimal', 'Html', 'Link', 'Lookup', 'Number', 'ComputedOwnershipLookup', 'Score', 'Text', 'Tag', 'Boolean', 'Path', 'ComputedRelationshipField', 'Relationship', 'JsonElement'];
+		const allowedTypes = ['Counter', 'Date', 'DateTime', 'Decimal', 'Html', 'Link', 'Lookup', 'Number', 'ComputedOwnershipLookup', 'Score', 'Text', 'Tag', 'Boolean', 'Path', 'ComputedRelationshipField', 'Relationship', 'ReferenceList', 'JsonElement'];
 		return (this.assetTypeUid || this.relationshipTypeUid) && allowedTypes.indexOf(this.selectedFieldType) > -1;
 	}
 
