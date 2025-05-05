6a23b963b8dcd098ef7eba8a188177fe54a45652
Puneet Gupta <puneet.gupta@Precisely.com>
2025-05-05 15:27:15 +0530

    GOV-38015-GET semantic types provides 500 for few filtering conditions.(2025-sprint-0422)

--- repositories.azure/Catalog.Semantics.cs
+++ repositories.azure/Catalog.Semantics.cs
@@ -149,7 +149,12 @@ namespace repositories.azure
 				statusSql = statusBuilder.ToString() + " " + sortDirection;
 			}
 
-			string whereSql = "where " + string.Join(" and ", wheres);
+			string whereSql = "";
+
+			if (wheres.Count > 0)
+			{
+				whereSql = "where " + string.Join(" and ", wheres);
+			}
 			string orderBySql = $"{sortColumn} {sortDirection}";
 
 			var tableQuery = $"select ROW_NUMBER() OVER(PARTITION BY Qualifier ORDER BY EffectiveDate desc ) AS RowNum, * {statusSql} from Semantic where EffectiveDate <= @asOfEffectiveDate";
