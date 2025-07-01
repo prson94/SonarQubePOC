15a77f71d51d577ea72c3fdc50a8d1f9e2386cd6
michael.pappas <michael.pappas@precisely.com>
2025-07-01 17:27:31 -0400

    Add Azurite to use local queue and blob storage. See "compose-up.bat"

---
+++ compose-up.bat
@@ -0,0 +1,6 @@
+npm install -g azurite
+azurite --silent --location c:\azurite --debug c:\azurite\debug.log
+
+rem You should now run setup-local project to create the storage folders and queues that are needed.
+
+rem docker compose up
---
+++ setup-local/Program.cs
@@ -0,0 +1,21 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Queues;
+
+string connectionString = "UseDevelopmentStorage=true";
+
+List<string> containers = ["api-execution", "bulk-loads", "resoruces", "themes"];
+var containerClient = new BlobServiceClient(connectionString);
+containers.ForEach(containerName => 
+{
+	containerClient.GetBlobContainerClient(containerName).CreateIfNotExists();
+});
+
+List<string> queues = [
+	"asset-type-change", "bulk-load", "display-value", "execution", "notification", "parse-owner-rule", "post-execution", 
+	"score", "search", "security-policy", "workflow"
+];
+var queueClient = new QueueServiceClient(connectionString);
+queues.ForEach(queueName => 
+{
+	queueClient.GetQueueClient(queueName).CreateIfNotExists();
+});
